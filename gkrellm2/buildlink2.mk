44be9b4a6938e9c160c024a582980f254949a9ba
cubidou <cubidou>
2003-04-07 17:54:28 +0000

    o Clean-up of Makefiles for better pkglinting. o Update to 2.1.9.

--- gkrellm2/buildlink2.mk
+++ gkrellm2/buildlink2.mk
@@ -4,13 +4,15 @@
 GKRELLM_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gkrellm
-BUILDLINK_DEPENDS.gkrellm?=		gkrellm>=2.1.8a
+BUILDLINK_DEPENDS.gkrellm?=		gkrellm>=2.1.9
 BUILDLINK_PKGSRCDIR.gkrellm?=		../../wip/gkrellm2
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gkrellm=gkrellm
 BUILDLINK_PREFIX.gkrellm_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gkrellm+=	include/gkrellm2/gkrellm.h
 BUILDLINK_FILES.gkrellm+=	include/gkrellm2/gkrellm-public-proto.h
+
+PTHREAD_OPTS+=		require
 
 .include "../../x11/gtk2/buildlink2.mk"
 
