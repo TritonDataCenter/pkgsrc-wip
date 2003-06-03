c4eecd9ffd0feab4677f5f042f5a4076165e1628
marc <marc>
2003-06-03 21:02:31 +0000

    Make it compile.

--- py-gnome2/buildlink2.mk
+++ py-gnome2/buildlink2.mk
@@ -13,12 +13,13 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.pygnome2=${PYPKGPREFIX}-gnome2
 BUILDLINK_PREFIX.pygnome2_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.pygnome2+=	lib/pkgconfig/gnome-python-2.0.pc
 
+.include "../../www/libgtkhtml/buildlink2.mk"
+.include "../../net/py-ORBit/buildlink2.mk"
 .include "../../print/libgnomeprintui/buildlink2.mk"
+.include "../../sysutils/nautilus/buildlink2.mk"
 .include "../../x11/libzvt/buildlink2.mk"
 .include "../../x11/gnome-panel/buildlink2.mk"
-.include "../../www/libgtkhtml/buildlink2.mk"
-.include "../../wip/py-ORBit/buildlink2.mk"
-.include "../../wip/py-gtk2/buildlink2.mk"
+.include "../../x11/py-gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	pygnome2-buildlink
 
