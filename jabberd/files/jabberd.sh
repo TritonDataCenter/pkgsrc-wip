890f65128f0a8b4719fb0763dff4294f193ad43f
xtraeme <xtraeme>
2003-06-25 15:37:21 +0000

    Don't run the daemon as root, use the jabberd user.

--- jabberd/files/jabberd.sh
+++ jabberd/files/jabberd.sh
@@ -15,6 +15,7 @@ command="@PREFIX@/sbin/${name}"
 command_args="-B -H /var/spool/jabberd -c \$required_files"
 pidfile="/var/run/${help_name}.pid"
 required_files="@PKG_SYSCONFDIR@/jabber.xml"
+jabberd_user="jabberd"
 
 load_rc_config $name
 run_rc_command "$1"
