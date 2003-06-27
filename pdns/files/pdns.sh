4f1e36fed89255caa90abad895598e933e94ae43
poppnk <poppnk>
2003-06-27 02:47:12 +0000

    Initial revision

---
+++ pdns/files/pdns.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# $NetBSD$
+#
+
+# PROVIDE: pdns
+# REQUIRE: DAEMON
+
+. /etc/rc.subr
+
+name="pdns"
+rcvar=$name
+command="@PREFIX@/sbin/pdns_server"
+pidfile=/var/run/${name}.pid
+required_files="@PKG_SYSCONFDIR@/pdns.conf"
+
+load_rc_config $name
+run_rc_command "$1"
