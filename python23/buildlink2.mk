5c7c49f937bbcb9ee4ea54ed93c3d276588816c2
marc <marc>
2003-07-31 15:15:35 +0000

    only buildlink the shared libpython

--- python23/buildlink2.mk
+++ python23/buildlink2.mk
@@ -18,15 +18,11 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.python23=python23
 BUILDLINK_PREFIX.python23_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES_CMD.python23= \
 	${BUILDLINK_PLIST_CMD.python23} |				\
-		${GREP} '^\(include\|lib/libpython${PKGVERSION}.*\|lib.*/lib[^/]*$$\)'
+		${GREP} '^\(include\|lib/libpython2\.3.*\)'
 BUILDLINK_TRANSFORM+=		l:python:python23
 
 BUILDLINK_CPPFLAGS.python23+= \
 	-I${BUILDLINK_PREFIX.python23}/include/python23
-BUILDLINK_LDFLAGS.python23+= \
-	-L${BUILDLINK_PREFIX.python23}/lib/python23/config		\
-	-Wl,${RPATH_FLAG}${BUILDLINK_PREFIX.python23}/lib/python23/config
-FIX_RPATH+=	BUILDLINK_LDFLAGS.python23
 
 BUILDLINK_TARGETS+=	python23-buildlink
 
