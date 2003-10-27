a27138ed73e12eedb950ed253b775c0b7c15151a
bubuchka <bubuchka>
2003-10-27 20:26:59 +0000

    Use the drweb_smf so as it more readable and stylized.

--- drweb-clients-shared/files/drweb-smf.sh
+++ drweb-clients-shared/files/drweb-smf.sh
@@ -2,20 +2,20 @@
 #
 # $NetBSD$
 
-# PROVIDE: drwebsmf
+# PROVIDE: drweb_smf
 # REQUIRE: DAEMON
 # BEFORE: LOGIN
 
 . @RCD_SUBR@
 
-name="drwebsmf"
+name="drweb_smf"
 rcvar=$name
 command="@PREFIX@/sbin/drweb-smf"
 procname="${command}"
-start_precmd="drwebsmf_clean"
+start_precmd="drweb_smf_clean"
 required_files="@PKG_SYSCONFDIR@/drweb-smf.conf"
 
-drwebsmf_clean()
+drweb_smf_clean()
 {
 	if [ -e @DRW_SMF_SOCKFILE@ ]; then
 		rm -f @DRW_SMF_SOCKFILE@
