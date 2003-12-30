187a611a2cb575fc193c5be5beeb1a98b78da360
grantbeattie <grantbeattie>
2003-12-30 22:55:28 +0000

    moved to pkgsrc/www/MozillaFirebird-gtk2-bin

--- MozillaFirebird-bin-xft2/files/MozillaFirebird.sh
+++
@@ -1,7 +0,0 @@
-#!@SH@
-# $NetBSD$
-
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/MozillaFirebird-xft2-@PLATFORM@:@PREFIX@/lib"
-export LD_LIBRARY_PATH
-
-exec @PREFIX@/lib/MozillaFirebird-xft2-@PLATFORM@/MozillaFirebird "$@"
