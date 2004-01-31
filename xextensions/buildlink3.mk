f5d977a6fd900f199478e915b24984a204773195
minskim <minskim>
2004-01-31 18:12:18 +0000

    bl3ify

---
+++ xextensions/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+#
+# This Makefile fragment is included by packages that use xextensions.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+XEXTENSIONS_BUILDLINK3_MK:=	${XEXTENSIONS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	xextensions
+.endif
+
+.if !empty(XEXTENSIONS_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			xextensions
+BUILDLINK_DEPENDS.xextensions?=		xextensions>=1.0
+BUILDLINK_PKGSRCDIR.xextensions?=		../../wip/xextensions
+
+.endif # XEXTENSIONS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
