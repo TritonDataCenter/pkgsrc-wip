1633aa796d317859fd19afb3e33c3d9d6a58d523
jeremy-c-reed <jeremy-c-reed>
2004-02-10 18:21:43 +0000

    Add pthread include. And add to LIBS too.
    Also, change BUILDLINK_DEPENDS.libX11 to append instead.

--- libX11/buildlink3.mk
+++ libX11/buildlink3.mk
@@ -14,8 +14,11 @@ BUILDLINK_DEPENDS+=	libX11
 
 .if !empty(LIBX11_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			libX11
-BUILDLINK_DEPENDS.libX11?=		libX11>=6.2.1
+BUILDLINK_DEPENDS.libX11+=		libX11>=6.2.1
 BUILDLINK_PKGSRCDIR.libX11?=		../../wip/libX11
+
+.include "../../mk/pthread.buildlink3.mk"
+LIBS+=		${BUILDLINK_LDADD.pthread}
 
 .include "../../wip/xextensions/buildlink3.mk"
 .include "../../wip/xproto/buildlink3.mk"
