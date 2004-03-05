f7a3733f86d29ec9714d9eecd92f7e81d004fdc5
bencollver <bencollver>
2004-03-05 22:16:27 +0000

    Convert to buildlink3.

---
+++ libmodplug/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+LIBMODPLUG_BUILDLINK3_MK:=		${LIBMODPLUG_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=			libmodplug
+.endif
+
+.if !empty(LIBMODPLUG_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libmodplug
+BUILDLINK_DEPENDS.libmodplug+=		libmodplug>=0.7
+BUILDLINK_PKGSRCDIR.libmodplug?=	../../wip/libmodplug
+
+.endif # LIBMODPLUG_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=    			 ${BUILDLINK_DEPTH:S/+$//}
