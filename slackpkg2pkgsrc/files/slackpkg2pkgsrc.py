9840efa1547470f98d3b915abb6e3914e5469f6c
mpasternak <mpasternak>
2004-03-15 13:26:11 +0000

    Change note about license (by cube@)

--- slackpkg2pkgsrc/files/slackpkg2pkgsrc.py
+++ slackpkg2pkgsrc/files/slackpkg2pkgsrc.py
@@ -4,9 +4,9 @@
 #
 # (C) 2004 Michal Pasternak <dotz@irc.pl>
 #
-# This script is distributed under BSD license
+# This script is distributed as public domain.
 #
-# $Id: slackpkg2pkgsrc.py,v 1.1 2004/03/15 12:38:51 mpasternak Exp $
+# $Id: slackpkg2pkgsrc.py,v 1.2 2004/03/15 13:26:11 mpasternak Exp $
 #
 
 import sys, os, socket, shutil
@@ -67,7 +67,7 @@ f = open("Makefile", "w")
 f.write(
 """
 # $NetBSD$
-# $Id: slackpkg2pkgsrc.py,v 1.1 2004/03/15 12:38:51 mpasternak Exp $
+# $Id: slackpkg2pkgsrc.py,v 1.2 2004/03/15 13:26:11 mpasternak Exp $
 #
 
 DISTNAME=		%s
