1f33bd2aca187a4bd5f2fdfefc9f4b916e924d2d
xtraeme <xtraeme>
2004-04-05 04:25:05 +0000

    Update allegro to 4.1.13 (latest development version) and bl3ify, remove TODO because it works fine now.
    Changes:
    
    ====================================================================
    ============ Changes from 4.1.12 to 4.1.13 (March 2004) ============
    ====================================================================
    
       Minor changes and clarifications in the docs by Sven Sandberg, Andy Goth,
       Jon Rafkind, Nathan Smith and Daniel Schlyder.
    
       Elias Pschernig extensively modified the ALSA 0.9/1.0 sound driver to make
       it work more nicely.
    
       Julien Cugniere and Eric Botcazou fixed the crash on exit with the aRts
       sound driver.
    
       Nathan Smith implemented the recursive handling of directories for the
       dat utility.
    
       Eric Botcazou renamed the new '-r' option of the dat utility into '-f'.
    
       Omar Cornut improved the handling of KEY_COLON2 on Japanese keyboards.
    
       Omar Cornut fixed the Windows joystick driver to handle a weird peculiarity
       of the DirectInput API.
    
       Elias Pschernig corrected the value of the alpha-channel depth field in the
       header when saving TGA image files.
    
       Eric Botcazou fixed again the formula giving the stride value in the Linux
       framebuffer console.
    
       Eric Botcazou restored the pixel-correct version of line() and added
       fastline() after Doug Eleveld had reported the problem.
    
       Andy Goth and Eric Botcazou fixed the constructor support in dat2s on Unix.
    
       Angelo Mottola revamped the MacOS X package creation process to make it
       work under 10.3 and create an enduser package that holds just the shared
       framework.
    
       Angelo Mottola changed the MacOS X shared library building process to have
       a prebound shared library separated from a small static libray holding just
       the magic main stuff.
    
       Angelo Mottola removed window shadow flickering when moving the program
       window around on MacOS X.

---
+++ allegro/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+ALLEGRO_BUILDLINK3_MK:=	${ALLEGRO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	allegro
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nallegro}
+BUILDLINK_PACKAGES+=	allegro
+
+.if !empty(ALLEGRO_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.allegro+=	allegro>=4.1.13
+BUILDLINK_PKGSRCDIR.allegro?=	../../wip/allegro
+.endif	# ALLEGRO_BUILDLINK3_MK
+
+.include "../../audio/esound/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
