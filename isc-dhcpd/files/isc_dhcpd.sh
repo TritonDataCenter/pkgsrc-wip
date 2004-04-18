206752e98d7682c14381e6744ede585b777dab66
mpasternak <mpasternak>
2004-04-18 17:22:12 +0000

    Add rc.d script, stolen from NetBSD rc.d; still more stuff to do with it (see TODO), but the basic functionality is here

---
+++ isc-dhcpd/files/isc_dhcpd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# $NetBSD: dhcpd,v 1.5 2002/03/22 04:33:58 thorpej Exp $
+# $Id: isc_dhcpd.sh,v 1.1 2004/04/18 17:22:13 mpasternak Exp $
+
+# PROVIDE: dhcpd
+# REQUIRE: DAEMON
+# BEFORE:  LOGIN
+
+. /etc/rc.subr
+
+name="dhcpd"
+rcvar="isc_dhcpd"
+command="@PREFIX@/sbin/${name}"
+pidfile="/var/run/${name}.pid"
+required_files="@PREFIX@/etc/${name}.conf /var/db/${name}.leases"
+
+load_rc_config $name
+run_rc_command "$1"
