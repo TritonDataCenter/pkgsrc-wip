7f3eae22e909cac9c3ec6ee511cd6b5f60306da5
thomasklausner <thomasklausner>
2004-05-14 14:38:31 +0000

    Remove gcc3-ada-devel, gcc3-c-devel, and gcc3-c++-devel: johnrshannon says they've been superseded by the gcc34 packages.

--- gcc3-ada-devel/files/gcc3.mk
+++
@@ -1,7 +0,0 @@
-# $NetBSD$
-#
-# make configuration file for @PKGNAME@
-
-CC=		@GCC_PREFIX@/bin/cc
-CPP=		@GCC_PREFIX@/bin/cpp
-ADAC=		@GCC_PREFIX@/bin/gcc
--- gcc3-c-devel/files/gcc3.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: gcc3.mk,v 1.5 2003/09/16 15:47:21 drochner Exp $
-#
-# make configuration file for @PKGNAME@
-
-CC=		@GCC_PREFIX@/bin/cc
-CPP=		@GCC_PREFIX@/bin/cpp
