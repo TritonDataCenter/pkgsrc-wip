a02fd71c5d72217c259cf5ddd839440516bbda16
ppostma <ppostma>
2004-06-18 14:38:05 +0000

    Add reload target, for reloading the pf configuration.

--- pflkm/files/pf.sh
+++ pflkm/files/pf.sh
@@ -15,6 +15,8 @@ config="@PKG_SYSCONFDIR@/pf.conf"
 start_cmd="${command} -e -f ${config}"
 stop_cmd="${command} -d"
 status_cmd="${command} -s info"
+reload_cmd="${command} -f ${config}"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
