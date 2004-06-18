703353c8cd22d20c92b5c2aa5d63236a7d75b907
ppostma <ppostma>
2004-06-18 14:14:36 +0000

    Use PKG_SYSCONFDIR.

--- pflkm/files/pf.sh
+++ pflkm/files/pf.sh
@@ -11,7 +11,7 @@
 name="pf"
 rcvar=$name
 command="@PREFIX@/sbin/pfctl"
-config="@PREFIX@/etc/pf.conf"
+config="@PKG_SYSCONFDIR@/etc/pf.conf"
 start_cmd="${command} -e -f ${config}"
 stop_cmd="${command} -d"
 status_cmd="${command} -s info"
