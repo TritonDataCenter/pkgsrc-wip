f2adcfd7b91dfba9b0aa76328c43da6ae1b3b248
shattered <shattered>
2004-06-28 15:45:05 +0000

    Nuke extra whitespace.  Add 'extra_commands="reload"'.

--- smartmontools/files/smartd.sh
+++ smartmontools/files/smartd.sh
@@ -7,11 +7,12 @@
 
 . /etc/rc.subr
 
-name="smartd"                      
-rcvar=$name         
+name="smartd"
+rcvar=$name
 command="/usr/pkg/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 smartd_flags="-p $pidfile"
-              
+extra_commands="reload"
+
 load_rc_config $name
 run_rc_command "$1"
