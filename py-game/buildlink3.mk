2058a0cf2cc7bafc502936c97ee40a2df042316c
thomasklausner <thomasklausner>
2004-07-27 22:09:16 +0000

    smpeg moved to multimedia/

--- py-game/buildlink3.mk
+++ py-game/buildlink3.mk
@@ -18,8 +18,8 @@ BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-game?=	../../wip/py-game
 
 .include "../../audio/SDL_mixer/buildlink3.mk"
 .include "../../devel/SDL_ttf/buildlink3.mk"
-.include "../../devel/smpeg/buildlink3.mk"
 .include "../../graphics/SDL_image/buildlink3.mk"
+.include "../../multimedia/smpeg/buildlink3.mk"
 .include "../../math/py-Numeric/buildlink3.mk"
 
 .endif	# PY_GAME_BUILDLINK3_MK
