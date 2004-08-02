2e148b9d45e8475ea278dbbdd1258c3c99bab3b6
bencollver <bencollver>
2004-08-02 04:43:47 +0000

    Initial revision

---
+++ fluidsynth/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+FLUIDSYNTH_BUILDLINK3_MK:=	${FLUIDSYNTH_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	fluidsynth
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfluidsynth}
+BUILDLINK_PACKAGES+=	fluidsynth
+
+.if !empty(FLUIDSYNTH_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.fluidsynth+=	fluidsynth>=1.0.4
+BUILDLINK_PKGSRCDIR.fluidsynth?=	../../wip/fluidsynth
+.endif	# FLUIDSYNTH_BUILDLINK3_MK
+
+.include "../../devel/readline/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
