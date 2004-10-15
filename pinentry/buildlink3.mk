711dbf5228adfde7ce2c10c8ea072f924b597f40
johnrshannon <johnrshannon>
2004-10-15 22:02:48 +0000

    Removed unnecessary include from buildlink3.mk

--- pinentry/buildlink3.mk
+++ pinentry/buildlink3.mk
@@ -15,8 +15,4 @@ BUILDLINK_DEPENDS.pinentry+=	pinentry>=0.7.1
 BUILDLINK_PKGSRCDIR.pinentry?=	../../wip/pinentry
 .endif	# PINENTRY_BUILDLINK3_MK
 
-.include "../../security/libksba/buildlink3.mk"
-.include "../../x11/qt3-libs/buildlink3.mk"
-.include "../../x11/gtk/buildlink3.mk"
-
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
