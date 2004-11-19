663d78c377da5342c6bbdb531f05ef1d1310476a
daprice <daprice>
2004-11-19 19:23:31 +0000

    Initial revision

---
+++ jalview/buildlink2.mk
@@ -0,0 +1,25 @@
+# $NetBSD$
+#
+# This Makefile fragment is included by packages that use jalview_binary.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(JALVIEW_BINARY_BUILDLINK2_MK)
+JALVIEW_BINARY_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			jalview_binary
+BUILDLINK_DEPENDS.jalview_binary?=		jalview_binary>=1.8
+BUILDLINK_PKGSRCDIR.jalview_binary?=		../../wip/jalview
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.jalview_binary=jalview_binary
+BUILDLINK_PREFIX.jalview_binary_DEFAULT=	${LOCALBASE}
+
+BUILDLINK_FILES.jalview_binary=		lib/java/jalview.jar
+BUILDLINK_FILES.jalview_binary+=	lib/java/jalview_1.8.jar
+
+BUILDLINK_TARGETS+=	jalview_binary-buildlink
+
+jalview_binary-buildlink: _BUILDLINK_USE
+
+.endif	# JALVIEW_BINARY_BUILDLINK2_MK
