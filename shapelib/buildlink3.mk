57ab2248113568543d41da57ba3f2a2262c46734
thomasklausner <thomasklausner>
2005-03-27 21:16:35 +0000

    Change category to geography, and remove XXX comments from buildlink3.mk.

--- shapelib/buildlink3.mk
+++ shapelib/buildlink3.mk
@@ -1,14 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.4.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.shapelib?=	build
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SHAPELIB_BUILDLINK3_MK:=	${SHAPELIB_BUILDLINK3_MK}+
@@ -22,7 +12,7 @@ BUILDLINK_PACKAGES+=	shapelib
 
 .if !empty(SHAPELIB_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.shapelib+=	shapelib>=1.2.10
-BUILDLINK_PKGSRCDIR.shapelib?=	../../wip/shapelib
+BUILDLINK_PKGSRCDIR.shapelib?=	../../geography/shapelib
 .endif	# SHAPELIB_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
