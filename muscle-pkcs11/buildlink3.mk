87f3861407c2a73f5c887691b9e82926807ccf80
udontknow <udontknow>
2005-07-25 21:28:57 +0000

    Initial revision

---
+++ muscle-pkcs11/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+MUSCLE_PKCS11_BUILDLINK3_MK:=	${MUSCLE_PKCS11_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	muscle-pkcs11
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmuscle-pkcs11}
+BUILDLINK_PACKAGES+=	muscle-pkcs11
+
+.if !empty(MUSCLE_PKCS11_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.muscle-pkcs11+=	muscle-pkcs11>=1.1.5
+BUILDLINK_PKGSRCDIR.muscle-pkcs11?=	../../wip/muscle-pkcs11
+.endif	# MUSCLE_PKCS11_BUILDLINK3_MK
+
+.include "../../wip/libmusclecard/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
