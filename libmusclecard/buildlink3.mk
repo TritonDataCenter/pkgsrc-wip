d9cce53148815affab1afd38e92ac713e403100b
udontknow <udontknow>
2005-07-25 17:41:58 +0000

    added buildlink3.mk, which was missing on the import

---
+++ libmusclecard/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBMUSCLECARD_BUILDLINK3_MK:=	${LIBMUSCLECARD_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libmusclecard
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibmusclecard}
+BUILDLINK_PACKAGES+=	libmusclecard
+
+.if !empty(LIBMUSCLECARD_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libmusclecard+=	libmusclecard>=1.2.9
+BUILDLINK_PKGSRCDIR.libmusclecard?=	../../wip/libmusclecard
+.endif	# LIBMUSCLECARD_BUILDLINK3_MK
+
+.include "../../wip/pcsc-lite/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
