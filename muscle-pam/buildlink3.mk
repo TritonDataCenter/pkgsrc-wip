e0f45b6dabe18d9079330822b8744ad755d3b569
udontknow <udontknow>
2005-07-26 20:06:49 +0000

    Initial revision

---
+++ muscle-pam/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+MUSCLE_PAM_BUILDLINK3_MK:=	${MUSCLE_PAM_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	muscle-pam
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmuscle-pam}
+BUILDLINK_PACKAGES+=	muscle-pam
+
+.if !empty(MUSCLE_PAM_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.muscle-pam+=	muscle-pam>=1.1.5
+BUILDLINK_PKGSRCDIR.muscle-pam?=	../../wip/muscle-pam
+.endif	# MUSCLE_PAM_BUILDLINK3_MK
+
+.include "../../wip/libmusclecard/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
