fd41d16ca57dcd22e4c15ad4f5ad57d723f5ebab
adrian_p <adrian_p>
2005-08-02 21:39:07 +0000

    - Update to 0.0.Alpha9.0 - Startup script now 1/2 works as in it will start the daemon but not stop it :) - Update smx.conf for 0.0.Alpha9.0 - Add MESSAGE - Add local hostname to mt via INSTALL

--- sendmailX/files/sendmailx.sh
+++ sendmailX/files/sendmailx.sh
@@ -15,16 +15,18 @@ name="sendmailx"
 rcvar=$name
 ctl_command="@PREFIX@/sbin/mcp"
 conf_file="@PKG_SYSCONFDIR@/smx.conf"
-required_files=$conf_file
+required_files=${conf_file}
 extra_commands="restart"
 start_cmd="sendmailx_start"
 pidfile="@VARBASE@/run/${name}.pid"
 logdir="@VARBASE@/log/smx/"
+spooldir="@VARBASE@/spool/smx"
 # restart_cmd="sendmailx_restart"
 
 sendmailx_start()
 {
-	${ctl_command} -l -p ${pidfile} -L ${logdir} ${conf_file} &
+	cd ${spooldir} && \
+		${ctl_command} -l -p ${pidfile} -L ${logdir} ${conf_file} &
 }
 
 load_rc_config $name
