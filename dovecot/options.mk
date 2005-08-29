9cedbdc2a82084d5fadee37db48e297d8cd125b8
tvierling <tvierling>
2005-08-29 13:04:33 +0000

    The SSL option is now --with, not --enable. Default to OpenSSL (and prefer it, if both ssl and gnutls are set). Allow gnutls as fallback if ssl is not set.

--- dovecot/options.mk
+++ dovecot/options.mk
@@ -1,18 +1,22 @@
 # $NetBSD: options.mk,v 1.6 2005/01/08 19:58:16 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dovecot
-PKG_SUPPORTED_OPTIONS=	gnutls inet6 ldap mysql pam pgsql
+PKG_SUPPORTED_OPTIONS=	gnutls inet6 ldap mysql pam pgsql ssl
+PKG_SUGGESTED_OPTIONS=	ssl
+
 .include "../../mk/bsd.options.mk"
 
 ###
 ### Build with GNU TLS or OpenSSL as the underlying crypto library.
 ###
-.if !empty(PKG_OPTIONS:Mgnutls)
-CONFIGURE_ARGS+=	--enable-ssl=gnutls
+.if !empty(PKG_OPTIONS:Mssl)
+CONFIGURE_ARGS+=        --with-ssl=openssl
+.  include "../../security/openssl/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mgnutls)
+CONFIGURE_ARGS+=	--with-ssl=gnutls
 .  include "../../security/gnutls/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=        --enable-ssl=openssl
-.  include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=	--without-ssl
 .endif
 
 ###
