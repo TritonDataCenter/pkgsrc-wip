bffe33a743cb06c036fa42fc4bd5d8aad69d2b15
poppnk <poppnk>
2005-09-02 17:06:42 +0000

    PKGREVISION=1 fix PLIST for removal warnings fix start/stop commands for the rc script

--- gnump3d/files/gnump3d.sh
+++ gnump3d/files/gnump3d.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: proftpd.sh,v 1.1 2003/03/22 10:15:27 jmmv Exp $
+# $NetBSD: gnump3d.sh$
 #
 
 # PROVIDE: gnump3d
@@ -11,9 +11,37 @@
 
 name="gnump3d"
 rcvar=$name
-command="@PREFIX@/bin/${name} --background"
+start_cmd="gnump3d_start"
+stop_cmd="gnump3d_stop"
+command="@PREFIX@/bin/${name}"
+command_args="--quiet"
 pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/gnump3d/gnump3d.conf"
+current_pid=`cat $pidfile`
+
+gnump3d_start() {
+	${command} ${command_args} 2>&1 > $pidfile &
+	PID=$!
+	sleep 1
+	kill -s CONT $PID 2>/dev/null
+	if [ $? = 0 ]; then
+		echo "Starting ${name}."
+		echo $PID > ${pidfile}
+	else
+		cat $pidfile
+		echo $current_pid > ${pidfile}
+	fi
+}
+
+gnump3d_stop() {
+	kill -s CONT $current_pid 2>/dev/null
+	if [ $? = 0 ]; then
+		echo Stopping ${name}
+		kill $current_pid
+	else
+		echo "${name} not running? (check ${pidfile})."
+	fi
+}
 
 load_rc_config $name
 run_rc_command "$1"
