67eeaeb334ffd7f1b023c9ae94184985622d958a
udontknow <udontknow>
2005-09-11 05:26:43 +0000

    Initial revision

---
+++ libobby/options.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=        PKG_OPTIONS.obby
+PKG_SUPPORTED_OPTIONS=  zeroconf
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mzeroconf)
+CONFIGURE_ARGS+=	--with-howl
+.include "../../net/howl/buildlink3.mk"
+PLIST_SRC=		${PKGDIR}/PLIST.zeroconf
+.endif
