d7771a1cd16b4b5a17d9549593da0f1fb33e54cf
jeremy-c-reed <jeremy-c-reed>
2005-10-17 19:21:55 +0000

    Update to use modular X.org version from release candidate 0. xlibs was non-official and experimental.
    Also include wip/libXau/buildlink3.mk and wip/xextproto/buildlink3.mk.
    
    Adds man pages.
    
    Remove includes for xproto and xextensions from buildlink3.mk.
    (Don't depend on X if using this modular X component.)

--- libXext/buildlink3.mk
+++ libXext/buildlink3.mk
@@ -12,12 +12,10 @@ BUILDLINK_DEPENDS+=	libXext
 
 .if !empty(LIBXEXT_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			libXext
-BUILDLINK_DEPENDS.libXext+=		libXext>=6.4.1
+BUILDLINK_DEPENDS.libXext+=		libXext>=0.99.0
 BUILDLINK_PKGSRCDIR.libXext?=		../../wip/libXext
 
 .include "../../wip/libX11/buildlink3.mk"
-.include "../../wip/xproto/buildlink3.mk"
-.include "../../x11/xextensions/buildlink3.mk"
 
 .endif # LIBXEXT_BUILDLINK3_MK
 
