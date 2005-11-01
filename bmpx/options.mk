38a5e1aed683b42d4db858994421bf58f5008894
leonardschmidt <leonardschmidt>
2005-11-01 12:54:25 +0000

    The libxml2 dependency belongs into Makefile.common.

--- bmpx/options.mk
+++ bmpx/options.mk
@@ -12,7 +12,6 @@ BUILDLINK_DEPENDS.libglade2+=	libglade2>=2.5.1
 .  include "../../devel/libglade2/buildlink3.mk"
 .  include "../../devel/pango/buildlink3.mk"
 .  include "../../graphics/cairo/buildlink3.mk"
-.  include "../../textproc/libxml2/buildlink3.mk"
 .  include "../../x11/gtk2/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-gui
 .else
