345cf9553902c257b49be7c8d84554c067353f9a
jeremy-c-reed <jeremy-c-reed>
2005-11-10 01:49:53 +0000

    Initial revision

---
+++ xf86vidmodeproto/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+BUILDLINK_DEPMETHOD.xf86vidmodeproto?=	build
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+XF86VIDMODEPROTO_BUILDLINK3_MK:=	${XF86VIDMODEPROTO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	xf86vidmodeproto
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxf86vidmodeproto}
+BUILDLINK_PACKAGES+=	xf86vidmodeproto
+
+.if !empty(XF86VIDMODEPROTO_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.xf86vidmodeproto+=	xf86vidmodeproto>=2.2.1
+BUILDLINK_PKGSRCDIR.xf86vidmodeproto?=	../../new/xf86vidmodeproto
+.endif	# XF86VIDMODEPROTO_BUILDLINK3_MK
+
+.include "../../wip/xproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
