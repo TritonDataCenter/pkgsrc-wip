84aef73156fe821327579b8016370107771dc39a
jeremy-c-reed <jeremy-c-reed>
2005-11-10 01:33:40 +0000

    Initial revision

---
+++ damageproto/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+BUILDLINK_DEPMETHOD.damageproto?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+DAMAGEPROTO_BUILDLINK3_MK:=	${DAMAGEPROTO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	damageproto
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ndamageproto}
+BUILDLINK_PACKAGES+=	damageproto
+
+.if !empty(DAMAGEPROTO_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.damageproto+=	damageproto>=1.0.1
+BUILDLINK_PKGSRCDIR.damageproto?=	../../wip/damageproto
+.endif	# DAMAGEPROTO_BUILDLINK3_MK
+
+.include "../../wip/xproto/buildlink3.mk"
+.include "../../wip/fixesproto/buildlink3.mk" 
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
