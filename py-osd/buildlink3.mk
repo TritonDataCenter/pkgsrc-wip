4d682666f2191228eb0ec5e6694ff8bba4d48036
dhowland <dhowland>
2005-11-13 05:56:49 +0000

    Initial revision

---
+++ py-osd/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+PY_OSD_BUILDLINK3_MK:=	${PY_OSD_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	pyosd
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyosd}
+BUILDLINK_PACKAGES+=	pyosd
+
+.if !empty(PY_OSD_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.pyosd+=	${PYPKGPREFIX}-pyosd>=0.2.14
+BUILDLINK_PKGSRCDIR.pyosd?=	../../wip/py-osd
+.endif	# PY_OSD_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
