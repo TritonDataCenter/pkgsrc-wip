24b997d30fbfbf387b78356698c193ff01cd2e14
adrian_p <adrian_p>
2005-11-16 23:07:30 +0000

    Update default config file Tidy up INSTALL so it makes a little more sence Fix ./configure options in options.mk

--- sendmailX/options.mk
+++ sendmailX/options.mk
@@ -1,7 +1,7 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sendmailX
-PKG_SUPPORTED_OPTIONS=	tls sasl
+PKG_SUPPORTED_OPTIONS=	tls sasl pmilter
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,9 +10,10 @@ PKG_SUPPORTED_OPTIONS=	tls sasl
 ###
 .if !empty(PKG_OPTIONS:Mtls)
 .	include "../../security/openssl/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-TLS
+CONFIGURE_ARGS+=	-enable-TLS
 CFLAGS+=		-DSM_USE_TLS
 .else
+CONFIGURE_ARGS+=        -disable-TLS
 CONFIGURE_ENV+=		ac_cv_header_openssl_ssl_h=no
 .endif
 
@@ -21,6 +22,15 @@ CONFIGURE_ENV+=		ac_cv_header_openssl_ssl_h=no
 ###
 .if !empty(PKG_OPTIONS:Msasl)
 .	include "../../security/cyrus-sasl2/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-SASL
+CONFIGURE_ARGS+=	-enable-SASL
 CFLAGS+=		-DSM_USE_SASL
+.else
+CONFIGURE_ARGS+=	-disable-SASL
+.endif
+
+###
+### Use policy milter protocol
+###
+.if !empty(PKG_OPTIONS:Mpmilter)
+CONFIGURE_ARGS+=	-enable-pmilter
 .endif
