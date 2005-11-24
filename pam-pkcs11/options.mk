31d5eef58a227f4be87012652ad36b002f5102dc
udontknow <udontknow>
2005-11-24 13:42:36 +0000

    Initial revision

---
+++ pam-pkcs11/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=        PKG_OPTIONS.pam-pkcs11
+PKG_SUPPORTED_OPTIONS=	pcsc-lite ldap
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpcsc-lite)
+.include "../../wip/pcsc-lite/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pcsclite
+PLIST_SRC+=		PLIST.pcsc-lite
+.endif
+
+.if !empty(PKG_OPTIONS:Mldap)
+.include "../../databases/openldap/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-ldap
+PLIST_SRC+=		PLIST.ldap
+.endif
