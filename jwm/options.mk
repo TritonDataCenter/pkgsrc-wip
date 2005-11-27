a208a5c523d9d7b7a2d634afd4adcb229cf759f6
leonardschmidt <leonardschmidt>
2005-11-27 10:08:43 +0000

    Update to 1.1 and add a debug option.  Also, the old distfile was not available anymore.
    Changes:
    
    20051120:
    	- Added "nextstacked" key binding.
    	- Released v1.1.
    
    20051119:
    	- Added ClockStyle, TrayButtonStyle, and TrayStyle options.
    	- Now icon aspect ratios are preserved when resized.
    
    20051116:
    	- Added button border to TrayButtons.
    	- Added Clock.
    
    20051114:
    	- Fixed task list overflow.
    	- Fixed minimization on restart.
    
    20051113:
    	- Released v1.0.
    
    20051112:
    	- Make maximization work in a more sane manner.
    
    20051111:
    	- Focus next client in the stacking order when the active client is closed.
    	- Added "Desktops" root menu item.
    	- Fixed key actions with click-to-focus.
    	- Improved window placement.
    
    20051110:
    	- Now desktops can be named, changes to the configuration for this.
    
    20051109:
    	- Send ClientMessage instead of PropertyNotify for _JWM commands.
    	- More EWMH support.
    	- Fix label attribute for RootMenu.
    	- Added menu includes (rarsa).
    	- Fix a minor menu bug.
    
    20051106:
    	- Restore maximization status on restart.
    
    20051027:
    	- Fix byte-order issue with PNG images.
    
    20051026:
    	- Fix lockup issue when restoring transient windows.
    	- Added a separator to the window menu before kill/close.
    
    20051024:
    	- Fix 64-bit X server issues.
    
    20051016:
    	- Tray button can now execute external programs (or show the root menu).
    
    20051013:
    	- Support for vertical trays, pagers, and task lists.
    
    20051012:
    	- Added the ability to swallow applications in the tray.
    
    20051010:
    	- Overhaul of the tray. Multiple trays now supported.
    	  [The configuration file changed]
    
    20051007:
    	- Large windows are now handled in a more sane manner.
    
    20051003:
    	- Flush the X connection before closing it.
    	- Make status windows show on the screen with the mouse.
    	- Fixed an off-by-one error drawing the load.
    
    20051001:
    	- Can now use XRender for rendering icons.
    	- Added support for PNG icons (optional).
    	- Icons are now scaled independently for title bars and the task bar.
    	- Added "height" attribute to RootMenu and Menu.
    
    20050925:
    	- Now uses Xft for antialiasing, which can be disabled at compile time.
    	- Made drawing of border double-buffered.
    
    20050924:
    	- Added "enabled" option to Pager.
    	- Fixed a bug with loading icons.
    	- Attempted to fix color issues on 64-bit X-Servers.

---
+++ jwm/options.mk
@@ -0,0 +1,10 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.jwm
+PKG_SUPPORTED_OPTIONS=	debug
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.endif
