da73b8c2d82ce44117ecb5b34cdca7c64212f5e3
jsonn <jsonn>
2005-12-08 16:49:23 +0000

    Initial revision

---
+++ libinstaller/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBINSTALLER_BUILDLINK3_MK:=	${LIBINSTALLER_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libinstaller
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibinstaller}
+BUILDLINK_PACKAGES+=	libinstaller
+
+.if !empty(LIBINSTALLER_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libinstaller+=	libinstaller>=5.1
+BUILDLINK_PKGSRCDIR.libinstaller?=	../../wip/libinstaller
+.endif	# LIBINSTALLER_BUILDLINK3_MK
+
+.include "../../wip/libaura/buildlink3.mk"
+.include "../../wip/libdfui/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
