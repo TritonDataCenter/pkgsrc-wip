5cf8e5e3838040741c5dbaac8c18c0bada920bfd
rillig <rillig>
2005-12-19 07:56:14 +0000

    Fixed pkglint warnings about "PKG_FAIL_REASON+=" and the use of semicolons.

--- mk/cvs-package.mk
+++ mk/cvs-package.mk
@@ -48,14 +48,14 @@ _CVS_FLAGS=		-Q
 _CVS_CHECKOUT_FLAGS=	-P -d ${DISTNAME:Q} -r ${CVS_TAG:Q}
 _CVS_PASSFILE=		${WRKDIR}/.cvs_passwords
 .if !defined(CVS_REPOSITORY)
-PKG_FAIL_REASON=	"[cvs-package.mk] No CVS repository specified."
+PKG_FAIL_REASON+=	"[cvs-package.mk] No CVS repository specified."
 .elif !defined(CVS_MODULE)
-PKG_FAIL_REASON=	"[cvs-package.mk] No CVS module specified."
+PKG_FAIL_REASON+=	"[cvs-package.mk] No CVS module specified."
 .else
 .endif
 
 do-extract:
-	${_PKG_DEBUG}${_PKG_SILENT}set -e;				\
+	${_PKG_SILENT}${_PKG_DEBUG}set -e;				\
 	cd ${WRKDIR};							\
 	case ${CVS_REPOSITORY:Q} in					\
 	  :pserver:*)							\
