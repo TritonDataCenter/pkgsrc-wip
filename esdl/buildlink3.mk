c9de62bffc5db495996402daec1d67537da6788f
ghen <ghen>
2005-12-30 14:00:05 +0000

    Add a buildlink3.mk file for this package now that lang/erlang has one.

---
+++ esdl/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+ESDL_BUILDLINK3_MK:=		${ESDL_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=		esdl
+.endif
+
+BUILDLINK_PACKAGES:=		${BUILDLINK_PACKAGES:Nesdl}
+BUILDLINK_PACKAGES+=		esdl
+
+.if !empty(ESDL_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.esdl+=	esdl>=0.95
+BUILDLINK_PKGSRCDIR.esdl?=	../../wip/esdl
+.endif	# ESDL_BUILDLINK3_MK
+
+.include "../../lang/erlang/buildlink3.mk"
+.include "../../devel/SDL/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
