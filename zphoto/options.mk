dbfa0f565a03008ff8a3efdb28f4461ecfaaaab4
obache <obache>
2006-01-04 02:52:04 +0000

    use PKG_OPTIONS_REQUIRED_GROUPS

--- zphoto/options.mk
+++ zphoto/options.mk
@@ -1,7 +1,9 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.zphoto
-PKG_SUPPORTED_OPTIONS=	imlib2 magick zip avifile wx
+PKG_SUPPORTED_OPTIONS=	zip avifile wx
+PKG_OPTIONS_REQUIRED_GROUPS=	graphics
+PKG_OPTIONS_GROUP.graphics=	imlib2 magick
 PKG_SUGGESTED_OPTIONS=	imlib2
 
 .include "../../mk/bsd.options.mk"
