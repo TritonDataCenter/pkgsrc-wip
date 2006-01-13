de61d87812deed1239d3ea2f047bc389fe3aadba
rillig <rillig>
2006-01-13 01:00:56 +0000

    Fixed pkglint warnings.

--- gdal-lib/buildlink3.mk
+++ gdal-lib/buildlink3.mk
@@ -15,4 +15,4 @@ BUILDLINK_DEPENDS.gdal-lib+=	gdal-lib>=1.3.1
 BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip/gdal-lib
 .endif	# GDAL_LIB_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
