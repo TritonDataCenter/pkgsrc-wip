2b5c54810e5da3ed4d09b69fce7e5d5787651e27
ghen <ghen>
2006-02-01 13:24:45 +0000

    Initial revision

---
+++ seamonkey/options.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR		= PKG_OPTIONS.gecko
+PKG_SUPPORTED_OPTIONS	= debug
+
+.include "../../mk/bsd.options.mk"
+
+# this .if test looks backward, but the missing options disables debug,
+# so it is correct
+.if empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+= --disable-debug
+.endif
