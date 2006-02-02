35ed6e943505b4798871e1ae8d14d81a949dcf35
absd <absd>
2006-02-02 22:11:30 +0000

    adjust buildlink values for wip

--- gaim/buildlink3.mk
+++ gaim/buildlink3.mk
@@ -11,9 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngaim}
 BUILDLINK_PACKAGES+=	gaim
 
 .if !empty(GAIM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gaim+=	gaim>=1.1.2
-BUILDLINK_RECOMMENDED.gaim+=	gaim>=1.5.0nb3
-BUILDLINK_PKGSRCDIR.gaim?=	../../chat/gaim
+BUILDLINK_DEPENDS.gaim+=	gaim>=1.9
+BUILDLINK_RECOMMENDED.gaim+=	gaim>=1.9
+BUILDLINK_PKGSRCDIR.gaim?=	../../wip/gaim
 
 PRINT_PLIST_AWK+=	/^@dirrm lib\/gaim$$/ \
 				{ print "@comment in gaim: " $$0; next }
