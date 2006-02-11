93173a0762dd269bd9526a02f1c0b86b05bbae09
obache <obache>
2006-02-11 16:20:06 +0000

    fix pkglint -Wall

--- libupnp12/buildlink3.mk
+++ libupnp12/buildlink3.mk
@@ -7,14 +7,15 @@ LIBUPNP_BUILDLINK3_MK:=	${LIBUPNP_BUILDLINK3_MK}+
 BUILDLINK_DEPENDS+=	libupnp
 .endif
 
+.if !empty(LIBUPNP_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibupnp}
 BUILDLINK_PACKAGES+=	libupnp
-BUILDLINK_DEPENDS.libupnp?=		libupnp>=1.2.1
+BUILDLINK_DEPENDS.libupnp+=		libupnp>=1.2.1
 BUILDLINK_RECOMMENDED.libupnp?=		libupnp>=1.2.1
 BUILDLINK_PKGSRCDIR.libupnp?=		../../wip/libupnp12
-
 BUILDLINK_FILES.libupnp+=	include/libupnp/*.h
 BUILDLINK_FILES.libupnp+=	lib/libupnp.* lib/libthreadutil.* lib/libixml.*
+.endif	# LIBUPNP_BUILDLINK3_MK
 
 .include "../../wip/libuuid/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
