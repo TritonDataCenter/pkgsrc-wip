5e55fac13087461c1d25cd0fa6e69b0391a79ee8
obache <obache>
2006-02-18 11:41:34 +0000

    no need libuuid.

--- libupnp12/buildlink3.mk
+++ libupnp12/buildlink3.mk
@@ -17,7 +17,6 @@ BUILDLINK_FILES.libupnp+=	include/libupnp/*.h
 BUILDLINK_FILES.libupnp+=	lib/libupnp.* lib/libthreadutil.* lib/libixml.*
 .endif	# LIBUPNP_BUILDLINK3_MK
 
-.include "../../wip/libuuid/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
