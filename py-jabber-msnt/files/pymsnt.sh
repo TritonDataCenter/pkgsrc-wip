bc8a947d4aa86e5ea5c14ccd19b9a76e63d8ba92
schnoebe <schnoebe>
2006-02-21 20:30:05 +0000

    Completed updates for the move to PyMSNt 0.11

--- py-jabber-msnt/files/pymsnt.sh
+++ py-jabber-msnt/files/pymsnt.sh
@@ -12,6 +12,7 @@ name="pymsnt"
 rcvar=$name
 command=@PREFIX@/bin/PyMSNt
 command_interpreter="@PYTHONBIN@"
+command_args="-b"	# background/daemonize
 pymsnt_user="@JABBERD_USER@"
 pidfile="@JABBERD_PIDDIR@/${name}.pid"
 logfile="@JABBERD_LOGDIR@/${name}.log"
