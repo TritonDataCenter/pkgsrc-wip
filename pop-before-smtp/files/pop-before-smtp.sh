8afc39fe86e399b400c5b7aca5a81819837ae22d
obache <obache>
2006-03-09 14:44:48 +0000

    change rc script to work 'stop' well.

--- pop-before-smtp/files/pop-before-smtp.sh
+++ pop-before-smtp/files/pop-before-smtp.sh
@@ -10,12 +10,13 @@
 
 name="pop_before_smtp"
 rcvar=${name}
-command="@PREFIX@/sbin/${name}"
+command="@PREFIX@/sbin/pop-before-smtp"
 pidfile="@VARBASE@/run/${name}.pid"
 conffile="@PKG_SYSCONFDIR@/pop-before-smtp-conf.pl"
 required_files=${conffile}
 
 command_args="--config=${conffile} --daemon=${pidfile}"
+command_interpreter=@PERL5@
 
 load_rc_config ${name}
 run_rc_command "$1"
