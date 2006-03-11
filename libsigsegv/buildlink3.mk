ad1db6689a6d24f5ffdb68e3037b9613373955f8
thomasklausner <thomasklausner>
2006-03-11 21:57:59 +0000

    Fix.

--- libsigsegv/buildlink3.mk
+++ libsigsegv/buildlink3.mk
@@ -1,5 +1,5 @@
 # $NetBSD$
-BUILDLINK_DEPMETHOD/libsigsegv?=	build
+
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSIGSEGV_BUILDLINK3_MK:=	${LIBSIGSEGV_BUILDLINK3_MK}+
 
@@ -13,6 +13,8 @@ BUILDLINK_PACKAGES+=	libsigsegv
 .if !empty(LIBSIGSEGV_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libsigsegv+=	libsigsegv>=2.2
 BUILDLINK_PKGSRCDIR.libsigsegv?=	../../wip/libsigsegv
+# only installs a static library
+BUILDLINK_DEPMETHOD.libsigsegv?=	build
 .endif	# LIBSIGSEGV_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
