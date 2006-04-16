2899660818d5216dd9f1770a1188584ddfe54f33
jeremy-c-reed <jeremy-c-reed>
2006-04-16 04:12:14 +0000

    Use lower number for BUILDLINK_API_DEPENDS.libXinerama (it didn't even exist here!)
    And include xextproto and xineramaproto buildlink3.mk
    files instead of xproto and panoramixext.

--- libXinerama/buildlink3.mk
+++ libXinerama/buildlink3.mk
@@ -11,13 +11,13 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXinerama}
 BUILDLINK_PACKAGES+=	libXinerama
 
 .if !empty(LIBXINERAMA_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libXinerama+=	libXinerama>=1.0.2
+BUILDLINK_API_DEPENDS.libXinerama+=	libXinerama>=1.0.1
 BUILDLINK_PKGSRCDIR.libXinerama?=	../../wip/libXinerama
 .endif	# LIBXINERAMA_BUILDLINK3_MK
 
 .include "../../wip/libX11/buildlink3.mk"
 .include "../../wip/libXext/buildlink3.mk"
-.include "../../wip/panoramixext/buildlink3.mk"
-.include "../../wip/xproto/buildlink3.mk"
+.include "../../wip/xextproto/buildlink3.mk"
+.include "../../wip/xineramaproto/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
