2eca353185b6a5c73ed9a14a5eaa4deaca4176e3
anielski <anielski>
2006-05-04 20:31:53 +0000

    New PowerDNS version - current and modular (first plugin, gmysql backend already made, adding rest will be simple).

---
+++ pdns2/files/pdns.sh
@@ -0,0 +1,18 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD$
+#
+
+# PROVIDE: pdns
+# REQUIRE: DAEMON
+
+. /etc/rc.subr
+
+name="pdns"
+rcvar=$name
+command="@PREFIX@/sbin/pdns_server"
+pidfile=/var/run/${name}.pid
+required_files="@PKG_SYSCONFDIR@/pdns.conf"
+
+load_rc_config $name
+run_rc_command "$1"
