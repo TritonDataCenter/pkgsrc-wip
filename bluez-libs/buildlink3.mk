c11dca20797010927bfafeaaa789368ad08284cb
poppnk <poppnk>
2006-06-17 15:12:48 +0000

    * Up to 3.0 library API * Up hcidump to 1.31 to match 3.0 api

--- bluez-libs/buildlink3.mk
+++ bluez-libs/buildlink3.mk
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nbluez-libs}
 BUILDLINK_PACKAGES+=	bluez-libs
 
 .if !empty(BLUEZ_LIBS_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.bluez-libs+=	bluez-libs>=2.25
+BUILDLINK_API_DEPENDS.bluez-libs+=	bluez-libs>=3.0
 BUILDLINK_PKGSRCDIR.bluez-libs?=	../../wip/bluez-libs
 .endif	# BLUEZ_LIBS_BUILDLINK3_MK
 
