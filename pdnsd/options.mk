ce4df88de922ab0c88ffcdb74f553219001e0a82
gschwarz <gschwarz>
2006-06-17 09:19:02 +0000

    Initial revision

---
+++ pdnsd/options.mk
@@ -0,0 +1,11 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.pdnsd
+PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.endif
