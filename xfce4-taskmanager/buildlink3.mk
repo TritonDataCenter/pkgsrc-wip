5b1c3b9630e0e53dbb0c71cd7c03a9c408e78217
thomasklausner <thomasklausner>
2006-07-14 11:01:36 +0000

    regen, so pkglint is happy, and the DEPENDS line has the correct PKGNAME :)

--- xfce4-taskmanager/buildlink3.mk
+++ xfce4-taskmanager/buildlink3.mk
@@ -1,21 +1,22 @@
 # $NetBSD$
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_TASKMANAGER_BUILDLINK3_MK:=	${XFCE4_TASKMANAGER_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	xfce4-taskmanager
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-taskmanager}
 BUILDLINK_PACKAGES+=	xfce4-taskmanager
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-taskmanager
 
-.if !empty(XFCE4_TASKMANAGER_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.xfce4-taskmanager+=	xfce4-appfinder>=0.3.1
+.if ${XFCE4_TASKMANAGER_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.xfce4-taskmanager+=	xfce4-taskmanager>=0.3.1nb1
 BUILDLINK_PKGSRCDIR.xfce4-taskmanager?=	../../wip/xfce4-taskmanager
 .endif	# XFCE4_TASKMANAGER_BUILDLINK3_MK
 
 .include "../../x11/libxfce4gui/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
