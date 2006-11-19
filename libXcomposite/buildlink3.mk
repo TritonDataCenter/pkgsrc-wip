0fba42d4bad460929179b7c62427f16639a626c9
thomasklausner <thomasklausner>
2006-11-19 11:16:10 +0000

    Fix path to other xorg libraries that were imported into pkgsrc.

--- libXcomposite/buildlink3.mk
+++ libXcomposite/buildlink3.mk
@@ -18,8 +18,8 @@ BUILDLINK_PKGSRCDIR.libXcomposite?=	../../wip/libXcomposite
 
 .include "../../x11/compositeproto/buildlink3.mk"
 .include "../../x11/fixesproto/buildlink3.mk"
-.include "../../wip/libX11/buildlink3.mk"
-.include "../../wip/libXext/buildlink3.mk"
-.include "../../wip/libXfixes/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXfixes/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
