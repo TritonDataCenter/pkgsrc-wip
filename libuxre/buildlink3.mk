a85503142795e1d07c248de52cb8f77cddfa958d
cheusov <cheusov>
2006-12-16 18:41:46 +0000

    libuxre is now built with shared object files on platforms     that support this. libtool is used for this.

--- libuxre/buildlink3.mk
+++ libuxre/buildlink3.mk
@@ -13,7 +13,6 @@ BUILDLINK_PACKAGES+=	libuxre
 .if ${LIBUXRE_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libuxre+=	libuxre>=060122
 BUILDLINK_PKGSRCDIR.libuxre?=	../../wip/libuxre
-BUILDLINK_DEPMETHOD.pkg=	build
 BUILDLINK_INCDIRS.libuxre+=	include/uxre
 .endif	# LIBUXRE_BUILDLINK3_MK
 
