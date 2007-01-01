e05a4c250937d4b6c78822fa9d07d9cb1f281820
gschwarz <gschwarz>
2007-01-01 18:40:20 +0000

    Initial revision

---
+++ tftp-hpa/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.tftp-hpa
+PKG_SUPPORTED_OPTIONS=	tcpwrappers remap
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mtcpwrappers)
+.include "../../security/tcp_wrappers/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-tcpwrappers
+.endif
+
+.if empty(PKG_OPTIONS:Mremap)
+CONFIGURE_ARGS+=	--without-remap
+.endif
