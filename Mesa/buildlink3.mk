457461f33142fa408b69f3753eb7f87ffd4222fd
bsadewitz <bsadewitz>
2007-01-23 17:48:28 +0000

    This is Mesa 6.5.2, primarily for recent modular X.org servers.
    This package is from complete or polished and were unabashedly haphazardly put
    together.  You have been warned, and please let me know about any problems,
    etc.  Many thanks to Jeremy Reed for doing the bulk of the work (patches).

---
+++ Mesa/buildlink3.mk
@@ -0,0 +1,7 @@
+# $NetBSD$
+
+BUILDLINK_PREFIX.Mesa=	${BUILDLINK_PREFIX.MesaLib}
+
+.include "../../wip/MesaLib/buildlink3.mk"
+.include "../../wip/glu/buildlink3.mk"
+.include "../../wip/glut/buildlink3.mk"
