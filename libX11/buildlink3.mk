7dc2cf3b3994c1dd0a07cc0ba604f18a78fd46cf
bsadewitz <bsadewitz>
2007-01-23 14:55:03 +0000

    Lower the buildlink API for seamless building of packages which buildlink to this if the pkgsrc libX11 1.0.3 is installed.  I am not certain of the API differences (if any), so do this at your own risk.
    Add kludge to buildlink3.mk to pull in libxcb if it is installed.  Is there
    a better way to do this?
    
    Bump revision.

--- libX11/buildlink3.mk
+++ libX11/buildlink3.mk
@@ -18,9 +18,13 @@ BUILDLINK_PACKAGES+=	libX11
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libX11
 
 .if ${LIBX11_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libX11+=	libX11>=1.1.1nb1
+BUILDLINK_API_DEPENDS.libX11+=	libX11>=1.0.3
 BUILDLINK_PKGSRCDIR.libX11?=	../../wip/libX11
 .endif	# LIBX11_BUILDLINK3_MK
+
+.if exists(${LOCALBASE}/lib/pkgconfig/xcb-xlib.pc)
+.include "../../wip/libxcb/buildlink3.mk"
+.endif
 
 .include "../../x11/inputproto/buildlink3.mk"
 .include "../../x11/kbproto/buildlink3.mk"
