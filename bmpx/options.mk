58cbc112743ec9b12606b88d0eb98f4028997c56
thomasklausner <thomasklausner>
2007-01-28 09:37:52 +0000

    Update to 0.36.1: Lots of changes since previous packaged version. gstreamer is the only supported backend, support for winamp skins was dropped, lots of new features.

--- bmpx/options.mk
+++ bmpx/options.mk
@@ -1,22 +1,10 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bmpx
-PKG_SUPPORTED_OPTIONS=	debug gui
-PKG_SUGGESTED_OPTIONS=	gui
+PKG_SUPPORTED_OPTIONS=	debug
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
-.endif
-
-.if !empty(PKG_OPTIONS:Mgui)
-BUILDLINK_API_DEPENDS.libglade2+=	libglade2>=2.5.1
-
-.  include "../../devel/libglade2/buildlink3.mk"
-.  include "../../devel/pango/buildlink3.mk"
-.  include "../../graphics/cairo/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gui
-.else
-CONFIGURE_ARGS+=	--disable-gui
 .endif
