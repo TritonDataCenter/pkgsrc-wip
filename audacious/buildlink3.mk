c7045590097edf07c9383419f899edee37fb78e2
netcap <netcap>
2007-03-14 01:53:13 +0000

    Update to 1.3.1, most notable changes: - Lots of fixes - Some new plugins - Getting rid of legacy bmp/xmms code - Dump gnome-vfs dependency in favor of the much slinker libmcs to abstract configuration storage. - flac plugin is now distributed with audacious-plugins.

--- audacious/buildlink3.mk
+++ audacious/buildlink3.mk
@@ -12,11 +12,12 @@ BUILDLINK_PACKAGES+=	audacious
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}audacious
 
 .if ${AUDACIOUS_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.audacious+=	audacious>=1.2.2
-BUILDLINK_PKGSRCDIR.audacious?=		../../wip/audacious
-BUILDLINK_DEPMETHOD.audacious?=		build
+BUILDLINK_API_DEPENDS.audacious+=	audacious>=1.3.1
+BUILDLINK_PKGSRCDIR.audacious?=	../../wip/audacious
+BUILDLINK_DEPMETHOD.audacious?=	build
 .endif	# AUDACIOUS_BUILDLINK3_MK
 
+.include "../../wip/libmcs/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libglade2/buildlink3.mk"
