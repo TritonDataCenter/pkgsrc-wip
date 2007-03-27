cb8a4632f9788a15893ea6117ac8dd79011cec48
thomasklausner <thomasklausner>
2007-03-27 23:51:53 +0000

    Depend on libogg, since it's needed by the pkg-config file.

--- ffmpeg-snapshot/buildlink3.mk
+++ ffmpeg-snapshot/buildlink3.mk
@@ -16,4 +16,6 @@ BUILDLINK_API_DEPENDS.ffmpeg-snapshot+=	ffmpeg-snapshot>=20061213
 BUILDLINK_PKGSRCDIR.ffmpeg-snapshot?=	../../wip/ffmpeg-snapshot
 .endif	# FFMPEG_SNAPSHOT_BUILDLINK3_MK
 
+.include "../../multimedia/libogg/buildlink3.mk"
+
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
