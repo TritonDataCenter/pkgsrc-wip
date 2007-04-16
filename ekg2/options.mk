58eae76b6b0938a923d786e0e42125268daf6858
shindenmorr <shindenmorr>
2007-04-16 19:00:24 +0000

    Initial revision

---
+++ ekg2/options.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ekg2
+PKG_SUPPORTED_OPTIONS=	libgadu jabber
+PKG_SUGGESTED_OPTIONS=	libgadu jabber
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mjabber)
+.include "../../textproc/expat/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+PLIST_SUBST+=	JABBER=""
+.else
+PLIST_SUBST+=	JABBER="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibgadu)
+.include "../../wip/libgadu/buildlink3.mk"
+PLIST_SUBST+=	GG=""
+.else
+PLIST_SUBST+=	GG="@comment "
+.endif
