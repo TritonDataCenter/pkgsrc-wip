7c24d85e6015ea42a63e644eb88f96818e0e9b9c
tnn2 <tnn2>
2007-04-22 13:38:52 +0000

    Initial revision

---
+++ posix_headers/files/stdint.h
@@ -0,0 +1,12 @@
+/* $NetBSD$ */
+#ifndef __hpux
+#error This header file doesn't apply to this platform.
+#endif
+
+#ifndef _PKGSRC_STDINT_H_
+#define _PKGSRC_STDINT_H_
+#ifdef __hpux
+#include <inttypes.h>
+/* more to be added ... */
+#endif /* __hpux */
+#endif /* _PKGSRC_STDINT_H_ */
---
+++ posix_headers/files/sys/select.h
@@ -0,0 +1,6 @@
+/* $NetBSD$ */
+#ifdef __hpux
+#include <sys/time.h>
+#else
+#error This header file only applies to HP-UX.
+#endif
