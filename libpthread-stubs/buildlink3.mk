5108bc25e7e2c1f40f3b5b816a7f343d8a0f958e
bsadewitz <bsadewitz>
2007-04-29 20:22:14 +0000

    Add BUILDLINK_{LDFLAGS,LIBS} to buildlink3.mk.  Bump rev.

--- libpthread-stubs/buildlink3.mk
+++ libpthread-stubs/buildlink3.mk
@@ -14,6 +14,8 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libpthread-stubs
 .if ${PTHREAD_STUBS_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libpthread-stubs+=	libpthread-stubs>=0.1
 BUILDLINK_PKGSRCDIR.libpthread-stubs?=	../../wip/libpthread-stubs
+BUILDLINK_LDADD.libpthread-stubs=	-lpthread-stubs
+BUILDLINK_LIBS.libpthread-stubs=	${BUILDLINK_LDADD.libpthread-stubs}
 .endif	# PTHREAD_STUBS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
