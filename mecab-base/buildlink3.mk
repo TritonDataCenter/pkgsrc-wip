d8538d392807d5ffccc9c7f60ee3c0f1ec700412
obache <obache>
2007-05-09 13:21:09 +0000

    Clean up, no functionally change.

--- mecab-base/buildlink3.mk
+++ mecab-base/buildlink3.mk
@@ -1,17 +1,18 @@
 # $NetBSD$
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MECAB_BASE_BUILDLINK3_MK:=	${MECAB_BASE_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	mecab-base
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmecab-base}
 BUILDLINK_PACKAGES+=	mecab-base
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mecab-base
 
-.if !empty(MECAB_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.mecab-base+=		mecab-base>=0.90
+.if ${MECAB_BASE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.mecab-base+=	mecab-base>=0.90
 BUILDLINK_PKGSRCDIR.mecab-base?=	../../wip/mecab-base
 BUILDLINK_FILES.mecab-base+=		include/mecab.h
 BUILDLINK_FILES.mecab-base+=		lib/libmecab.*
@@ -19,4 +20,4 @@ BUILDLINK_FILES.mecab-base+=		lib/libmecab.*
 
 .include "../../mk/pthread.buildlink3.mk"
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
