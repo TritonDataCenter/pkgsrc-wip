139ccec28007609b277e40edf27e0a35d06b1384
jukka <jukka>
2007-08-06 11:16:22 +0000

    - Rework configure stage. Previously this worked (and in pkgsrc it   still does work) by chance if it did... - Add option `dkim-stats' to build and install dkim-stats(8) and its   manpage.

--- dkim-milter/options.mk
+++ dkim-milter/options.mk
@@ -1,7 +1,7 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dkim-milter
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 dkim-stats
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,4 +12,19 @@ PKG_SUPPORTED_OPTIONS=	inet6
 SUBST_SED.libs+=	-e 's|@INET6@||'
 .else
 SUBST_SED.libs+=	-e 's|@INET6@|dnl|'
+.endif
+
+###
+### Install dkim-stats(8).
+###
+.if !empty(PKG_OPTIONS:Mdkim-stats)
+SUBST_SED.libs+=	-e 's|@STATS@||'
+PLIST_SRC+=		PLIST.stats
+stats-install:
+	cd ${WRKSRC}/obj.`uname -s`.`uname -r`.`uname -m`; \
+	${INSTALL_PROGRAM} dkim-filter/dkim-stats ${PREFIX}/sbin;
+	${INSTALL_MAN} ${WRKSRC}/dkim-filter/dkim-stats.8 ${PREFIX}/${PKGMANDIR}/man8
+.else
+SUBST_SED.libs+=	-e 's|@STATS@|dnl|'
+stats-install:
 .endif
