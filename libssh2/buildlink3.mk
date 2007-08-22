38dbf7ea2ac160060fc1c933cee0d667031d6d00
bsadewitz <bsadewitz>
2007-08-22 06:49:10 +0000

    Oops, pkg-build-options.mk stuff belongs inside .if ... .endif ${LIBSSH2_BUILDLINK3_MK} == "+".

--- libssh2/buildlink3.mk
+++ libssh2/buildlink3.mk
@@ -14,11 +14,11 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libssh2
 .if ${LIBSSH2_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libssh2+=	libssh2>=0.17
 BUILDLINK_PKGSRCDIR.libssh2?=	../../local/libssh2
-.endif	# LIBSSH2_BUILDLINK3_MK
 
 pkgbase:=	libssh2
-
 .include "../../mk/pkg-build-options.mk"
+
+.endif	# LIBSSH2_BUILDLINK3_MK
 
 .if !empty(PKG_BUILD_OPTIONS:Mssl)
 .	include "../../security/openssl/buildlink3.mk"
