3231d8dcbbe952e92733a71f05a0bf2951725cf2
hoka_adam <hoka_adam>
2007-11-14 23:24:01 +0000

    Add inet6 option. (forgot from the previous commit)

---
+++ linux-kernel/options.mk
@@ -0,0 +1,7 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.linux-kernel
+PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUGGESTED_OPTIONS=	inet6
+
+.include "../../mk/bsd.options.mk"                                                
