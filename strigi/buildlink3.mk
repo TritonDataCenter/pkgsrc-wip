1fbaaae93d7700f7e841e1dd2e7ff910a3d694e1
mwdavies <mwdavies>
2007-12-12 02:36:14 +0000

    Initial revision

---
+++ strigi/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+STRIGI_BUILDLINK3_MK:=	${STRIGI_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	strigi
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nstrigi}
+BUILDLINK_PACKAGES+=	strigi
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}strigi
+
+.if ${STRIGI_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.strigi+=	strigi>=0.5.7
+BUILDLINK_PKGSRCDIR.strigi?=	../../wip/strigi
+.endif	# STRIGI_BUILDLINK3_MK
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../wip/libclucene/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
