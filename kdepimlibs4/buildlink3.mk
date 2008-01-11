bcfb4c25c92e2bc0224e5222e79c7a7411fdd02c
mwdavies <mwdavies>
2008-01-11 20:47:01 +0000

    kde4.0.0

--- kdepimlibs4/buildlink3.mk
+++ kdepimlibs4/buildlink3.mk
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	kdepimlibs
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}kdepimlibs
 
 .if ${KDEPIMLIBS_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.kdepimlibs+=	kdepimlibs>=3.96.1
+BUILDLINK_API_DEPENDS.kdepimlibs+=	kdepimlibs4>=4.0.0
 BUILDLINK_PKGSRCDIR.kdepimlibs?=	../../wip/kdepimlibs4
 .endif	# KDEPIMLIBS_BUILDLINK3_MK
 
