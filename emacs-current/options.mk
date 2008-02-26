5504e58efefb23ed1f9bb841256f7f4fa3bff10d
obache <obache>
2008-02-26 12:04:44 +0000

    libotf is come.

--- emacs-current/options.mk
+++ emacs-current/options.mk
@@ -63,7 +63,7 @@ CONFIGURE_ARGS+=	--without-carbon
 ### Enable font backend
 ###
 .  if !empty(PKG_OPTIONS:Mfont-backend)
-#.include "../../graphics/libotf/buildlink3.mk"
+.include "../../graphics/libotf/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../fonts/Xft2/buildlink3.mk"
 #.include "../../devel/m17n-lib/buildlink3.mk"
