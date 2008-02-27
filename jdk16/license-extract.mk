b7a29b2a26fb931e629de54fc44f5f264c3f3645
tnn2 <tnn2>
2008-02-27 20:41:24 +0000

    Assume yes to the interactive extract prompt iff both BATCH and PKG_DEVELOPER are defined, to facilitate bulk builds.

--- jdk16/license-extract.mk
+++ jdk16/license-extract.mk
@@ -1,5 +1,9 @@
 # $NetBSD$
 
+.if !defined(BATCH) || !defined(PKG_DEVELOPER)
+INTERACTIVE_STAGE+=	extract
+.endif
+
 post-extract:
 	@${CHMOD} -R u+w ${WRKSRC}
 	@cp ${PKGSRCDIR}/wip/jdk16/files/x_x2zip.c ${WRKSRC}
@@ -11,9 +15,13 @@ post-extract:
 	@echo "=> to abort."
 	@sh -c 'read ANS'
 	cd ${WRKSRC} && unzip -p ${JRL_SRCFILE} LICENSE | more
+.if !defined(BATCH) || !defined(PKG_DEVELOPER)
 	@sh -c 'echo -n "=> Do you accept the terms of the license agreement? "; read ANS; if [ "$$ANS" != "yes" ]; then exit 1; fi'
+.endif
 	cd ${WRKSRC} && unzip -p ${JRL_BINFILE} LICENSE | more
+.if !defined(BATCH) || !defined(PKG_DEVELOPER)
 	@sh -c 'echo -n "=> Do you accept the terms of the license agreement? "; read ANS; if [ "$$ANS" != "yes" ]; then exit 1; fi'
+.endif
 	cd ${WRKSRC} && unzip -q ${JRL_BINFILE} X_X && ./x_x2zip 'YES I ACCEPT THE CLICK THROUGH LICENSE.  '  X_X && unzip -q X_X.zip
 	rm -f ${WRKSRC}/X_X
 	cd ${WRKSRC} && unzip -q ${JRL_SRCFILE} X_X && ./x_x2zip 'YES I ACCEPT THE CLICK THROUGH LICENSE.  '  X_X && unzip -q X_X.zip
