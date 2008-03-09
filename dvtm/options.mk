9253692f754d63e4eae7f3b8fdced126fd2f8b4f
tnn2 <tnn2>
2008-03-09 16:49:24 +0000

    Make it ready for import: - Use wide curses by default - Use BUILD_TARGET instead of MAKE_FLAGS

--- dvtm/options.mk
+++ dvtm/options.mk
@@ -2,7 +2,7 @@
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.dvtm
 PKG_SUPPORTED_OPTIONS=		wide-curses
-PKG_SUGGESTED_OPTIONS=		# empty
+PKG_SUGGESTED_OPTIONS=		wide-curses
 PKG_OPTIONS_LEGACY_OPTS+=	ncursesw:wide-curses
 
 .include "../../mk/bsd.options.mk"
@@ -11,7 +11,7 @@ USE_NCURSES=	yes	# mouse extensions (not X/Open Curses compliant)
 
 .if !empty(PKG_OPTIONS:Mwide-curses)
 .include "../../devel/ncursesw/buildlink3.mk"
-MAKE_FLAGS+=	unicode
+BUILD_TARGET=	unicode
 .else
 .include "../../devel/ncurses/buildlink3.mk"
 .endif
