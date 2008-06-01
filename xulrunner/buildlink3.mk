c760c971e833960a1e6bb1d3705d488d8c79ed28
tnn2 <tnn2>
2008-06-01 15:22:33 +0000

    - add bl3 - clean up configure args - depend on lcms - install unstable headers

---
+++ xulrunner/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+XULRUNNER_BUILDLINK3_MK:=	${XULRUNNER_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	xulrunner
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxulrunner}
+BUILDLINK_PACKAGES+=	xulrunner
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xulrunner
+
+.if ${XULRUNNER_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9rc1
+BUILDLINK_PKGSRCDIR.xulrunner?=	../../wip/xulrunner
+.endif	# XULRUNNER_BUILDLINK3_MK
+
+#.include "../../net/libIDL/buildlink3.mk"
+#.include "../../x11/gtk2/buildlink3.mk"
+#.include "../../x11/libXt/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
