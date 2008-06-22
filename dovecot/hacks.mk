513af452290f7498b541deac2f81d371fe674293
ghen <ghen>
2008-06-22 17:45:06 +0000

    Remove dovecot and dovecot-sieve packages from pkgsrc-wip, dovecot 1.1.x has been merged into pkgsrc now.
    These packages can be revived later for dovecot 1.2 beta's.

--- dovecot/hacks.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD$
-
-.include "../../mk/compiler.mk"
-
-# Avoid an ICE in gcc2 on sparc64
-.if !empty(CC_VERSION:Mgcc-*)
-PKG_HACKS+=		sparc64-gcc2-ice
-CONFIGURE_ENV+=		F77=${FALSE:Q}
-.endif
