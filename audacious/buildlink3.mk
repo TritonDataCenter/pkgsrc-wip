78ca3d610580a51a9c214bf2a4d2db54f5ba6921
thomasklausner <thomasklausner>
2008-06-24 15:04:58 +0000

    Fix path to mowgli. Sort lines.

--- audacious/buildlink3.mk
+++ audacious/buildlink3.mk
@@ -17,12 +17,12 @@ BUILDLINK_PKGSRCDIR.audacious?=	../../wip/audacious
 BUILDLINK_DEPMETHOD.audacious?=	build
 .endif	# AUDACIOUS_BUILDLINK3_MK
 
-.include "../../wip/mowgli/buildlink3.mk"
-.include "../../devel/libmcs/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/atk/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libmcs/buildlink3.mk"
+.include "../../devel/mowgli/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
