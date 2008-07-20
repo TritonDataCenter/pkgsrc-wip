c6529490f0f393f297f516878d1106aea5c6718f
phonohawk <phonohawk>
2008-07-20 07:01:06 +0000

    Initial revision

---
+++ hxt/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+HXT_BUILDLINK3_MK:=	${HXT_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	hxt
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nhxt}
+BUILDLINK_PACKAGES+=	hxt
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hxt
+
+.if ${HXT_BUILDLINK3_MK} == "+"
+BUILDLINK_DEPMETHOD.hxt?=	build
+BUILDLINK_API_DEPENDS.hxt+=	hxt>=8.1.0
+BUILDLINK_PKGSRCDIR.hxt?=	../../wip/hxt
+.endif	# HXT_BUILDLINK3_MK
+
+.include "../../wip/hs-curl/buildlink3.mk"
+.include "../../wip/tagsoup/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
