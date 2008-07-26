1493fb7e4c3aac0ccae01707d18bceaf972b5db3
obache <obache>
2008-07-26 13:46:39 +0000

    Fixes some pkglint warinings.

--- mingw-runtime-bin/buildlink3.mk
+++ mingw-runtime-bin/buildlink3.mk
@@ -3,19 +3,20 @@
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 MINGW_RUNTIME_BIN_BUILDLINK3_MK:=	${MINGW_RUNTIME_BIN_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	mingw-runtime-bin
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmingw-runtime-bin}
 BUILDLINK_PACKAGES+=	mingw-runtime-bin
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mingw-runtime-bin
 
 .include "../../wip/mingw/Makefile.common"
 
-.if !empty(MINGW_RUNTIME_BIN_BUILDLINK3_MK:M+)
+.if ${MINGW_RUNTIME_BIN_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.mingw-runtime-bin+=	mingw-runtime-bin>=3.5
 BUILDLINK_PKGSRCDIR.mingw-runtime-bin?=	../../wip/mingw-runtime-bin
 BUILDLINK_FNAME_TRANSFORM.mingw-runtime-bin+=	-e "s,${MINGW_TARGET}/lib,lib/,g"
 .endif	# MINGW_RUNTIME_BIN_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
