cdcfa95f7efbe11cf367e634059ee0bf7024ec85
yazzy <yazzy>
2008-08-07 14:16:43 +0000

    removed comments

--- e17/buildlink3.mk
+++ e17/buildlink3.mk
@@ -1,14 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.13.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.e17?=	build
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 E17_BUILDLINK3_MK:=	${E17_BUILDLINK3_MK}+
