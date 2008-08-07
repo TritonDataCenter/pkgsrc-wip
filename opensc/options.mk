ff4f234cacb9f05c6549ac594dadef5090a7a653
htepper <htepper>
2008-08-07 19:03:31 +0000

    * Update to the latest version - 0.11.5:
      * Apply security fix for cardos driver and extend pkcs15-tool to test
        cards for the security vulnerability and update them (CVE-2008-2235).
      * Build system rewritten (NOTICE: configure options was modified).
        The build system can produce outputs for *NIX, cygwin and native
        windows (using mingw).
      * ruToken now supported.
      * Allow specifying application name for data objects.
      * Basic reader hotplug support.
      * PC/SC library is dynamic linked no longer compile time dependency.
      * PKCS#11 provider is now installed at LIBDIR/pkcs11
      * PKCS#11 - Number of virtual slots moved into configuration.
      * PKCS#11 - Fix fork() compliance.
      * make sign_with_decrypt hack configureable for siemens cards.
    
    * Single PLIST file.
    * Use CONF_FILES for configuration file.
    * DESTDIR ready.

--- opensc/options.mk
+++ opensc/options.mk
@@ -3,6 +3,7 @@
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opensc
 PKG_SUPPORTED_OPTIONS=	nsplugin openct pcsc-lite
 PKG_SUGGESTED_OPTIONS=	nsplugin pcsc-lite
+PLIST_VARS+=		nsplugin 
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,17 +12,23 @@ PKG_SUGGESTED_OPTIONS=	nsplugin pcsc-lite
 .include "../../security/pinentry/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-nsplugin
-CONFIGURE_ARGS+=	--with-plugin-dir=/usr/pkg/lib/firefox/plugins
+CONFIGURE_ARGS+=	--with-plugindir=/usr/pkg/lib/firefox/plugins
 CONFIGURE_ARGS+=	--with-pinentry=/usr/pkg/bin/pinentry
-PLIST_SRC+=		${PKGDIR}/PLIST.nsplugin
+PLIST.nsplugin=		yes
 .else
 CONFIGURE_ARGS+=	--disable-nsplugin
 .endif
 
 .if !empty(PKG_OPTIONS:Mpcsc-lite)
 .include "../../security/pcsc-lite/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-pcsc
+.else
+CONFIGURE_ARGS+=	--disable-pcsc
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenct)
 .include "../../wip/openct/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-openct
+.else
+CONFIGURE_ARGS+=	--disable-openct
 .endif
