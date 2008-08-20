fa73626be5ec12d6e1889c3f64783bd9e5cb3c35
bsadewitz <bsadewitz>
2008-08-20 04:41:14 +0000

    If we're not building with dri, don't apply patch-mb.

--- modular-xorg-server/options.mk
+++ modular-xorg-server/options.mk
@@ -12,6 +12,7 @@ PLIST.dri=		yes
 .include "mesaconfig.mk"
 DISTFILES=		${DEFAULT_DISTFILES}
 DISTFILES+=		MesaLib-7.0.4.tar.bz2
+_PKGSRC_PATCHES+=	${FILESDIR}/patch-mesa # XXX is there a better way?
 SITES.MesaLib-7.0.4.tar.bz2=\
 			${MASTER_SITE_SOURCEFORGE:=mesa3d/}
 MESA_SRC=		${WRKDIR}/Mesa-7.0.4
