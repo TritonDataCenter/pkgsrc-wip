ffaa0c5a10b69c0fa1bb05fd4230df549436234a
thomasklausner <thomasklausner>
2008-09-08 14:56:32 +0000

    Enable all options by default, add one for lensfun.

--- digikam/options.mk
+++ digikam/options.mk
@@ -1,11 +1,18 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.digikam
-PKG_SUPPORTED_OPTIONS=	pim # libmarblewidget liblensfun
-
-# XXX: package the two other optional dependencies
+PKG_SUPPORTED_OPTIONS=	lensfun marble pim
+PKG_SUGGESTED_OPTIONS=	lensfun marble pim
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlensfun)
+.include "../../wip/lensfun/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mmarble)
+.include "../../wip/kdeedu4/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mpim)
 .include "../../wip/kdepimlibs4/buildlink3.mk"
