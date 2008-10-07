198bf2b0107abb9d2cb73d418790ddfd0b3e6522
rsmarples <rsmarples>
2008-10-07 21:12:14 +0000

    Import parpd, a Proxy ARP Daemon.

---
+++ parpd/files/parpd.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# $NetBSD: $
+#
+
+# PROVIDE: parpd
+# REQUIRE: DAEMON
+
+$_rc_subr_loaded . /etc/rc.subr
+
+name="parpd"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+required_files="/etc/${name}.conf"
+
+load_rc_config $name
+run_rc_command "$1"
