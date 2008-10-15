d3ff598ebd8597e2193e8491c616c4d47a036482
hoka_adam <hoka_adam>
2008-10-15 20:27:18 +0000

    Update to 4.6 beta 1.

--- libxfce4util/buildlink3.mk
+++ libxfce4util/buildlink3.mk
@@ -12,11 +12,13 @@ BUILDLINK_PACKAGES+=	libxfce4util
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libxfce4util
 
 .if ${LIBXFCE4UTIL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.4.1
+BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.5.91
 BUILDLINK_PKGSRCDIR.libxfce4util?=	../../wip/libxfce4util
 .endif	# LIBXFCE4UTIL_BUILDLINK3_MK
 
 .include "../../wip/xfce4-dev-tools/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
