1f24e46686e38fb05c3258888f9aa374fd207a95
makoto <makoto>
2008-11-18 02:11:41 +0000

    Initial revision

---
+++ wl-current/options.mk
@@ -0,0 +1,13 @@
+# $NetBSD: options.mk,v 1.2 2008/04/12 22:43:04 jlam Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.wl
+PKG_SUPPORTED_OPTIONS.wl=	emacs-w3m emacs-w3m-current
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		emacs-w3m-current
+
+.if !empty(PKG_OPTIONS:Memacs-w3m-current)
+.include "../../www/emacs-w3m/buildlink3.mk"
+PLIST.emacs-w3m-current=	yes
+.endif
