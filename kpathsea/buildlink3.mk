a6f585e7d1897b84ce3419dcc3ef7c282433fc74
minskim <minskim>
2008-11-20 01:22:31 +0000

    Add templates to rebuild ls-R databases in texmf trees.

--- kpathsea/buildlink3.mk
+++ kpathsea/buildlink3.mk
@@ -16,4 +16,20 @@ BUILDLINK_API_DEPENDS.kpathsea+=	kpathsea>=3.5.7
 BUILDLINK_PKGSRCDIR.kpathsea?=	../../wip/kpathsea
 .endif	# KPATHSEA_BUILDLINK3_MK
 
+.if !empty(TEXMFDIRS)
+_TEXMFDIRS=	# empty
+.for _texmfdir_ in ${TEXMFDIRS}
+_TEXMFDIRS+=	${PREFIX}/${_texmfdir_}
+.endfor
+FILES_SUBST+=		TEXMFDIRS=${_TEXMFDIRS:Q}
+FILES_SUBST+=		MKTEXLSR="${BUILDLINK_PREFIX.kpathsea}/bin/mktexlsr"
+INSTALL_TEMPLATES+=	../../wip/kpathsea/files/texmf-install.tmpl
+DEINSTALL_TEMPLATES+=	../../wip/kpathsea/files/texmf-deinstall.tmpl
+
+.for _texmfdir_ in ${TEXMFDIRS}
+PRINT_PLIST_AWK+=       /^${_texmfdir_:S|/|\\/|g}\/ls-R$$/ { next; }
+PRINT_PLIST_AWK+=	/^@dirrm ${_texmfdir_:S|/|\\/|g}$$/ { next; }
+.endfor
+.endif
+
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
