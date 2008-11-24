76c0f2ee7aca67ba348473e6b00d7b89f2693899
makoto <makoto>
2008-11-24 02:11:24 +0000

    convert to PLIST_VARS (emacs-w3m-current)
    ------
    By the way, currently,  following changes are necessary on
    CVS Repository to enable this package, wl-current:
    
    (a) update editors/emacs/modules.mk
        (add emacs23)
         ... adding wip/emacs-current/modules.mk doesn't work,
         for apel/flim/semi reference editors/emacs version.
    (b) update misc/bbdb
        (emacs23, emacs/modules.mk) .
    (c) add    wip/emacs-current/versions.mk
    (d) add    wip/emacs-current/buildlink.mk
    ------

--- wl-current/options.mk
+++ wl-current/options.mk
@@ -1,13 +1,13 @@
 # $NetBSD: options.mk,v 1.2 2008/04/12 22:43:04 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wl
-PKG_SUPPORTED_OPTIONS=	emacs-w3m emacs-w3m-current
+PKG_SUPPORTED_OPTIONS=	emacs-w3m-current
+# note: emacs-w3m is not supported by wip/emacs-current
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		emacs-w3m-current
-
 .if !empty(PKG_OPTIONS:Memacs-w3m-current)
-.include "../../www/emacs-w3m/buildlink3.mk"
+.include "../../wip/emacs-w3m-current/buildlink3.mk"
+PLIST_VARS+=			emacs-w3m-current
 PLIST.emacs-w3m-current=	yes
 .endif
