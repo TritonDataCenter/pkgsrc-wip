11c9d1a97bd89e9ea85c90d6dc77bbceb3f4b5dd
minskim <minskim>
2008-11-24 08:10:14 +0000

    Initial revision

---
+++ silgraphite/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+SILGRAPHITE_BUILDLINK3_MK:=	${SILGRAPHITE_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	silgraphite
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsilgraphite}
+BUILDLINK_PACKAGES+=	silgraphite
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}silgraphite
+
+.if ${SILGRAPHITE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.silgraphite+=	silgraphite>=2.3
+BUILDLINK_PKGSRCDIR.silgraphite?=	../../wip/silgraphite
+.endif	# SILGRAPHITE_BUILDLINK3_MK
+
+.include "../../graphics/freetype2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
