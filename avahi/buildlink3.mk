37068ba27fde45cab9d7637d5ec4eca558958378
thomasklausner <thomasklausner>
2008-12-04 13:16:48 +0000

    Don't link everything against libintl just because avahi-common.la needs it.
    Reduce number of options, it was too much to handle all of them.
    Make gtk a non-option; reduce remainder to python, gdbm, and avahi-howl.
    
    Probably they should be put into separate packages, so if someone
    feels like it, please go ahead.
    
    Fix various PLIST problems.

---
+++ avahi/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+AVAHI_BUILDLINK3_MK:=	${AVAHI_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	avahi
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Navahi}
+BUILDLINK_PACKAGES+=	avahi
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}avahi
+
+.if ${AVAHI_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.avahi+=	avahi>=0.6.23
+BUILDLINK_PKGSRCDIR.avahi?=	../../wip/avahi
+.endif	# AVAHI_BUILDLINK3_MK
+
+#.include "../../textproc/expat/buildlink3.mk"
+#.include "../../wip/libdaemon/buildlink3.mk"
+#.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
