6bdf287f1ba87413035e9dde42f93333358fcbaf
thomasklausner <thomasklausner>
2008-12-07 15:17:30 +0000

    Try handling gdbm.

--- avahi/buildlink3.mk
+++ avahi/buildlink3.mk
@@ -16,6 +16,12 @@ BUILDLINK_API_DEPENDS.avahi+=	avahi>=0.6.23
 BUILDLINK_PKGSRCDIR.avahi?=	../../wip/avahi
 .endif	# AVAHI_BUILDLINK3_MK
 
+pkgbase := avahi
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.avahi:Mgdbm)
+.  include "../../databases/gdbm/buildlink3.mk"
+.endif
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
