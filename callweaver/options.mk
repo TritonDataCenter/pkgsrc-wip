baef3fd395b1c26cc12ddbb475b30f5c4efe02f6
x400 <x400>
2008-12-18 09:11:19 +0000

    *** empty log message ***

--- callweaver/options.mk
+++ callweaver/options.mk
@@ -5,20 +5,16 @@ PKG_SUPPORTED_OPTIONS=	mysql pgsql
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=		mysql pgsql
+
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../databases/mysql5-client/buildlink3.mk"
-PLIST_SRC+=		PLIST.mysql
-CONF_FILES+=		${PKG_SYSCONFDIR}/cdr_mysql.conf \
-			${PKG_SYSCONFDIR}/res_mysql.conf
-
+PLIST.mysql=		yes
 CONFIGURE_ARGS+=	--enable-mysql --with-app_sql_mysql --with-cdr_mysql --with-res_config_mysql
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .include "../../databases/postgresql83-client/buildlink3.mk"
-PLIST_SRC+=		PLIST.pgsql
-CONF_FILES+=		${PKG_SYSCONFDIR}/cdr_pgsql.conf \
-			${PKG_SYSCONFDIR}/res_pgsql.conf
-
+PLIST.pgsql=		yes
 CONFIGURE_ARGS+=	--enable-postgresql --with-app_sql_postgres --with-cdr_pgsql --with-res_config_pgsql
 .endif
