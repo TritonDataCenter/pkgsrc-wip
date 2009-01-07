45d3d6bc8eb4888474e1c1e849f9697ac45fb632
tnn2 <tnn2>
2009-01-07 12:33:56 +0000

    Initial revision

---
+++ openjdk/plugs.mk
@@ -0,0 +1,14 @@
+# $NetBSD: $
+
+#PLUGS=			jdk-7-ea-plug-b42-linux-i586-19_dec_2008.jar
+#SITES.${PLUGS}=	http://www.java.net/download/openjdk/jdk7/promoted/b42/
+#DISTFILES+=		${PLUGS}
+#EXTRACT_ONLY+=		${PLUGS}
+#MAKE_ENV+=		ALT_BINARY_PLUGS_PATH=${WRKDIR}/openjdk-binary-plugs
+
+# XXX from OpenBSD ports
+PLUGS=			jdk-7-icedtea-plugs-1.6a.tar.gz
+SITES.${PLUGS}=		http://www.intricatesoftware.com/distfiles/
+DISTFILES+=		${PLUGS}
+EXTRACT_ONLY+=		${PLUGS}
+MAKE_ENV+=		ALT_BINARY_PLUGS_PATH=${WRKDIR}/jdk-7-icedtea-plugs
