08b143e426bb338eeb0c9b0a1340fd2c1f419977
tnn2 <tnn2>
2009-01-11 11:54:25 +0000

    Fix most X11 related breakage. Apps now launch, but fonts don't look right.

--- openjdk/bootstrap.mk
+++ openjdk/bootstrap.mk
@@ -2,11 +2,11 @@
 
 ONLY_FOR_PLATFORM=	NetBSD-5.*-i386 NetBSD-5.*-x86_64
 
-BOOTKIT.i386=		bootstrap-jdk-7-bin-netbsd-5-i386-20090107-2.tar.bz2
+BOOTKIT.i386=		bootstrap-jdk-7-bin-netbsd-5-i386-20090111.tar.bz2
 SITES.${BOOTKIT.i386}=	http://mx1.nygren.pp.se/distfiles/
 DISTFILES+=		${BOOTKIT.i386}
 
-BOOTKIT.amd64=		bootstrap-jdk-7-bin-netbsd-5-amd64-20090106.tar.bz2
+BOOTKIT.amd64=		bootstrap-jdk-7-bin-netbsd-5-amd64-20090111.tar.bz2
 SITES.${BOOTKIT.amd64}=	http://mx1.nygren.pp.se/distfiles/
 DISTFILES+=		${BOOTKIT.amd64}
 
