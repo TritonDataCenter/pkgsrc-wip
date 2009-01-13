54146c58fbd71dabc54bf0adc64c46eb79f78375
phonohawk <phonohawk>
2009-01-13 22:36:38 +0000

    Updated to hslogger 1.0.7

--- hslogger/buildlink3.mk
+++ hslogger/buildlink3.mk
@@ -13,8 +13,12 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hslogger
 
 .if ${HSLOGGER_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.hslogger?=	build
-BUILDLINK_API_DEPENDS.hslogger+=	hslogger>=1.0.5
+BUILDLINK_API_DEPENDS.hslogger+=	hslogger>=1.0.7
 BUILDLINK_PKGSRCDIR.hslogger?=	../../wip/hslogger
 .endif	# HSLOGGER_BUILDLINK3_MK
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
