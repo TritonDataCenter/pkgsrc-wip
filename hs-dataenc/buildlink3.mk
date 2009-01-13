6af83387b693f054f3c01dbe79f56eaf4b6695c1
phonohawk <phonohawk>
2009-01-13 22:41:35 +0000

    Updated to dataenc 0.11.1

--- hs-dataenc/buildlink3.mk
+++ hs-dataenc/buildlink3.mk
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hs-dataenc
 
 .if ${HS_DATAENC_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.hs-dataenc?=	build
-BUILDLINK_API_DEPENDS.hs-dataenc+=	hs-dataenc>=0.11
+BUILDLINK_API_DEPENDS.hs-dataenc+=	hs-dataenc>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-dataenc?=	../../wip/hs-dataenc
 .endif	# HS_DATAENC_BUILDLINK3_MK
 
