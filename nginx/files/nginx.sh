21d368ad908807e055e9e14933afa84f692fe6ad
anielski <anielski>
2009-01-24 10:38:07 +0000

    Configuration reload (SIGHUP) support

--- nginx/files/nginx.sh
+++ nginx/files/nginx.sh
@@ -13,6 +13,7 @@ command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/${name}.conf"
 pidfile="@VARBASE@/run/${name}.pid"
 start_precmd="ulimit -n 2048"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
