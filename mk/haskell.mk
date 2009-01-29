00357ce35c9cd70a6aff69f4bcb39b804008711e
phonohawk <phonohawk>
2009-01-29 10:22:31 +0000

    Fixed a bug in DESTDIR support.

--- mk/haskell.mk
+++ mk/haskell.mk
@@ -150,12 +150,22 @@ do-build:
 # Define install target. We need installed-pkg-config to be installed
 # for package registration (if any).
 _HASKELL_PKG_DESCR_FILE=	${PREFIX}/lib/${DISTNAME}/${_HASKELL_VERSION}/package-description
+
+.if ${DESTDIR} == ""
+do-install:
+	cd ${WRKSRC} && \
+		${_RUNHASKELL_BIN} ${_CABAL_SETUP_SCRIPT} copy && \
+		if [ -f dist/installed-pkg-config ]; then \
+			${INSTALL_DATA} dist/installed-pkg-config ${_HASKELL_PKG_DESCR_FILE}; \
+		fi
+.else
 do-install:
 	cd ${WRKSRC} && \
 		${_RUNHASKELL_BIN} ${_CABAL_SETUP_SCRIPT} copy --destdir=${DESTDIR} && \
 		if [ -f dist/installed-pkg-config ]; then \
 			${INSTALL_DATA} dist/installed-pkg-config ${DESTDIR}${_HASKELL_PKG_DESCR_FILE}; \
 		fi
+.endif
 
 # Substitutions for INSTALL and DEINSTALL.
 FILES_SUBST+=	DISTNAME=${DISTNAME}
