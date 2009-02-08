b9175da48b5691256b7c5999e6606f33a74e3188
pbraun9 <pbraun9>
2009-02-08 12:29:53 +0000

    - path fix for --confdir - rc script fix

--- postoffice/files/postoffice.sh
+++ postoffice/files/postoffice.sh
@@ -2,6 +2,7 @@
 #
 # $NetBSD$
 #
+
 # PROVIDE: postoffice
 # REQUIRE: DAEMON
 
@@ -9,16 +10,9 @@ $_rc_subr_loaded . /etc/rc.subr
 
 name="postoffice"
 rcvar=$name
-command="@PREFIX@/libexec/${name}"
-pidfile="@VARBASE@/run/${name}.pid"
-required_files="@PREFIX@/etc/${name}.cf"
-start_precmd="postoffice_precmd"
-
-postoffice_precmd()
-{
-	rc_flags="-C@PREFIX@/etc/${name}.cf $rc_flags"
-	return 0
-}
+command="/usr/pkg/lib/${name}"
+pidfile="/var/run/${name}.pid"
+required_files="/usr/pkg/etc/${name}.cf"
 
 load_rc_config $name
 run_rc_command "$1"
