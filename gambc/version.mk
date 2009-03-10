5a5627a8b430ee526ad0435c478962574c6d3131
fulviociriaco <fulviociriaco>
2009-03-10 15:01:25 +0000

    first commit, gambac 4.4 line

---
+++ gambc/version.mk
@@ -0,0 +1,10 @@
+# $NetBSD: version.mk,v 1.2 2008/07/16 07:14:36 bjs Exp $
+###
+.if !defined(GAMBC_VERSION_MK)
+GAMBC_VERSION_MK=	# empty
+###
+### The package version sets the distribution version.
+###
+GAMBC_VERSION=		4.4.1
+GAMBC_DIST_VERSION=	${GAMBC_VERSION:C|^.*|v&|:S|.|_|g}
+.endif
