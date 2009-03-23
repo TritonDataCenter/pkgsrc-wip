595b64f1c51cc863bd64d5eb6cfb490437b15fa7
mstapelberg <mstapelberg>
2009-03-23 11:55:49 +0000

    Version-bump xcb-util from 0.2pl0 to 0.3.3

--- xcb-util/buildlink3.mk
+++ xcb-util/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	xcb-util
 .if !defined(XCB_UTIL_BUILDLINK3_MK)
 XCB_UTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xcb-util+=	xcb-util>=0.2pl0
+BUILDLINK_API_DEPENDS.xcb-util+=	xcb-util>=0.3.3
 BUILDLINK_PKGSRCDIR.xcb-util?=	../../wip/xcb-util
 
 .include "../../wip/libxcb/buildlink3.mk"
