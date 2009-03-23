63865b50405758ed8509fd422628096ca3637f54
mstapelberg <mstapelberg>
2009-03-23 12:55:55 +0000

    Update buildlink3.mk to use the new format

--- xcb-util/buildlink3.mk
+++ xcb-util/buildlink3.mk
@@ -5,8 +5,9 @@ BUILDLINK_TREE+=	xcb-util
 .if !defined(XCB_UTIL_BUILDLINK3_MK)
 XCB_UTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xcb-util+=	xcb-util>=0.3.3
-BUILDLINK_PKGSRCDIR.xcb-util?=	../../wip/xcb-util
+BUILDLINK_API_DEPENDS.xcb-util?=	xcb-util>=0.3.3
+BUILDLINK_PKGSRCDIR.xcb-util?=		../../wip/xcb-util
+BUILDLINK_DEPMETHOD.xcb-utl?=		build
 
 .include "../../wip/libxcb/buildlink3.mk"
 .endif # XCB_UTIL_BUILDLINK3_MK
