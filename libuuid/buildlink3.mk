786a81806360919d9885aeb5c1f6bb4cf1d33794
thomasklausner <thomasklausner>
2009-03-27 13:22:05 +0000

    Fix broken bl3.mk file.

--- libuuid/buildlink3.mk
+++ libuuid/buildlink3.mk
@@ -2,6 +2,9 @@
 
 BUILDLINK_TREE+=	libuuid
 
+.if !defined(LIBUUID_BUILDLINK3_MK)
+LIBUUID_BUILDLINK3_MK:=
+
 BUILDLINK_API_DEPENDS.libuuid+=	libuuid>=1.36
 BUILDLINK_PKGSRCDIR.libuuid?=	../../wip/libuuid
 
