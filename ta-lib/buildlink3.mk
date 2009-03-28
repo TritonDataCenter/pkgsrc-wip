6b6e29e19326881b71171806871b4fb593447fd5
jihbed <jihbed>
2009-03-28 10:49:28 +0000

    Initial revision

---
+++ ta-lib/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	ta-lib
+
+.if !defined(TA_LIB_BUILDLINK3_MK)
+TA_LIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ta-lib+=	ta-lib>=0.4.0
+BUILDLINK_PKGSRCDIR.ta-lib?=	../../wip/ta-lib
+.endif	# TA_LIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ta-lib
