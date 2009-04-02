698a80168dc84cd58ef0c23da29ee1a670d36846
x400 <x400>
2009-04-02 09:08:49 +0000

    *** empty log message ***

--- spandsp-current/buildlink3.mk
+++ spandsp-current/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	spandsp
 .if !defined(SPANDSP_BUILDLINK3_MK)
 SPANDSP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.spandsp+=	spandsp>=0.0.6pre2
+BUILDLINK_API_DEPENDS.spandsp+=	spandsp>=0.0.6pre7
 BUILDLINK_PKGSRCDIR.spandsp?=	../../wip/spandsp-current
 
 .include "../../graphics/tiff/buildlink3.mk"
