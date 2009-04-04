320b153b35bbf8c767f64cabf958170bd0597cee
jihbed <jihbed>
2009-04-04 14:48:17 +0000

    Initial revision

---
+++ py-webkit/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+#
+
+BUILDLINK_TREE+=	py25-webkitgtk
+
+.if !defined(PY25_WEBKITGTK_BUILDLINK3_MK)
+PY25_WEBKITGTK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py25-webkitgtk+=	py25-webkitgtk>=1.1.4
+BUILDLINK_PKGSRCDIR.py25-webkitgtk?=	../../category/py-webkit
+
+.include "../../x11/py-gtk2/buildlink3.mk"
+.include "../../www/webkit-gtk/buildlink3.mk"
+.endif	# PY25_WEBKITGTK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py25-webkitgtk
