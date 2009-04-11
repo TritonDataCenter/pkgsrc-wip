e26a952ac4fc16dcbe69ff27e68926d511993868
jihbed <jihbed>
2009-04-11 15:24:47 +0000

    Initial revision

---
+++ libmrss/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libmrss
+
+.if !defined(LIBMRSS_BUILDLINK3_MK)
+LIBMRSS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libmrss+=	libmrss>=0.19.2
+BUILDLINK_PKGSRCDIR.libmrss?=	../../wip/librmss
+
+.include "../../wip/libnxml/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.endif	# LIBMRSS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libmrss
