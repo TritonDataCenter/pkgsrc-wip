cb1352ce4ebf9ffc907aa69da24eff38a5d801eb
jihbed <jihbed>
2009-04-18 00:19:24 +0000

    Initial revision

---
+++ taglib-sharp/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	taglib-sharp
+
+.if !defined(TAGLIB_SHARP_BUILDLINK3_MK)
+TAGLIB_SHARP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.taglib-sharp+=	taglib-sharp>=2.0.3.2
+BUILDLINK_PKGSRCDIR.taglib-sharp?=	../../wip/taglib-sharp
+
+
+.include "../../lang/mono/buildlink3.mk"
+.include "../../x11/gnome-sharp/buildlink3.mk"
+.endif	# TAGLIB_SHARP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-taglib-sharp
