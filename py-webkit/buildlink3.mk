9ffaf43a91d5152a442b8d2221da11f64c5c61d4
jihbed <jihbed>
2009-05-05 21:43:03 +0000

    fixed: change word pywebkitgtk to py-webkitgtk

--- py-webkit/buildlink3.mk
+++ py-webkit/buildlink3.mk
@@ -1,15 +1,14 @@
 # $NetBSD$
-#
 
-BUILDLINK_TREE+=	pywebkitgtk
+BUILDLINK_TREE+=	py-webkitgtk
 
 .if !defined(PY_WEBKITGTK_BUILDLINK3_MK)
 PY_WEBKITGTK_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.pywebkitgtk+=	${PYPKGPREFIX}-webkitgtk>=1.1.4
-BUILDLINK_PKGSRCDIR.pywebkitgtk?=	../../wip/py-webkit
+BUILDLINK_API_DEPENDS.py-webkitgtk+=	${PYPKGPREFIX}-webkitgtk>=1.1.4
+BUILDLINK_PKGSRCDIR.py-webkitgtk?=	../../wip/py-webkit
 
 .include "../../x11/py-gtk2/buildlink3.mk"
 .include "../../www/webkit-gtk/buildlink3.mk"
