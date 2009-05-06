ecb11eea7c0f20922cf8e1f185548fddec515156
makoto <makoto>
2009-05-06 04:22:14 +0000

    new file as version.mk to be referenced from devel/flim etc via editor/emacs/modules.mk = need some change also

---
+++ emacs-current/version.mk
@@ -0,0 +1,11 @@
+# $NetBSD: version.mk,v 1.2 2008/10/11 09:56:38 uebayasi Exp $
+
+_EMACS_FLAVOR=	emacs
+_EMACS_REQD=	emacs>=23
+_EMACS_PKGDEP.base=
+_EMACS_PKGDEP.leim=
+
+_EMACS_VERSION_MAJOR=	23
+_EMACS_VERSION_MINOR=	0
+_EMACS_VERSION_MICRO=	93
+
