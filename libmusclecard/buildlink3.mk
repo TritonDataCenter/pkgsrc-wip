dd5ccd1fc995836d819427aa86fafdb136e8b181
cheusov <cheusov>
2009-05-06 10:09:30 +0000

    fixed: if-less endif

--- libmusclecard/buildlink3.mk
+++ libmusclecard/buildlink3.mk
@@ -2,6 +2,9 @@
 
 BUILDLINK_TREE+=	libmusclecard
 
+.if !defined(LIBMUSCLECARD_BUILDLINK3_MK)
+LIBMUSCLECARD_BUILDLINK3_MK:=
+
 BUILDLINK_API_DEPENDS.libmusclecard+=	libmusclecard>=1.2.9
 BUILDLINK_PKGSRCDIR.libmusclecard?=	../../wip/libmusclecard
 
