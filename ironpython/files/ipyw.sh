2be91432aeb86096fe3a3b585aa2c295fd81d5ba
jihbed <jihbed>
2009-05-12 15:17:03 +0000

    Initial revision

---
+++ ironpython/files/ipyw.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# $NetBSD$
+#
+
+# Fix up LD_LIBRARY_PATH so that /lib is always searched for shared
+# libraries.
+#
+prefix=/usr/pkg
+
+LD_LIBRARY_PATH=/usr/pkg/lib/ironpython; export LD_LIBRARY_PATH
+
+exec $prefix/bin/mono $prefix/lib/ironpython/ipyw.exe "$@"
