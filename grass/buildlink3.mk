4b868315893fcf2dd5e182f946d2094dd1d7a289
brook1 <brook1>
2009-05-18 16:08:47 +0000

    Update grass to v6.3.0.

--- grass/buildlink3.mk
+++ grass/buildlink3.mk
@@ -5,11 +5,9 @@ BUILDLINK_TREE+=	grass
 .if !defined(GRASS_BUILDLINK3_MK)
 GRASS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.grass+=	grass>=6.0.0
-BUILDLINK_ABI_DEPENDS.grass?=	grass>=6.0.2nb2
+BUILDLINK_API_DEPENDS.grass+=	grass>=6.3.0
 BUILDLINK_PKGSRCDIR.grass?=	../../wip/grass
 
-.include "../../geography/gdal-lib/buildlink3.mk"
-.endif # GRASS_BUILDLINK3_MK
+.endif	# GRASS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-grass
