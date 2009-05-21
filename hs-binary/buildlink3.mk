fc9f90be4496a6515445f6d97cd377145012cb22
phonohawk <phonohawk>
2009-05-21 04:50:37 +0000

    Upstream update to binary-0.5.0.1

--- hs-binary/buildlink3.mk
+++ hs-binary/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	hs-binary
 .if !defined(HS_BINARY_BUILDLINK3_MK)
 HS_BINARY_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-binary?=	build
-BUILDLINK_API_DEPENDS.hs-binary+=	hs-binary>=0.4.4nb1
+BUILDLINK_API_DEPENDS.hs-binary+=	hs-binary>=0.5.0.1
 BUILDLINK_PKGSRCDIR.hs-binary?=	../../wip/hs-binary
 .endif # HS_BINARY_BUILDLINK3_MK
 
