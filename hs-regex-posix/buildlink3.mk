984d0d817d3e80628a0230104b409cb25c0a5bc9
phonohawk <phonohawk>
2009-05-21 04:54:13 +0000

    Upstream update to regex-posix-0.94.1

--- hs-regex-posix/buildlink3.mk
+++ hs-regex-posix/buildlink3.mk
@@ -5,11 +5,10 @@ BUILDLINK_TREE+=	hs-regex-posix
 .if !defined(HS_REGEX_POSIX_BUILDLINK3_MK)
 HS_REGEX_POSIX_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-regex-posix?=	build
-BUILDLINK_API_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.93.2
+BUILDLINK_API_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.94.1
 BUILDLINK_PKGSRCDIR.hs-regex-posix?=	../../wip/hs-regex-posix
 
 .include "../../wip/hs-regex-base/buildlink3.mk"
-.endif # HS_REGEX_POSIX_BUILDLINK3_MK
+.endif	# HS_REGEX_POSIX_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-regex-posix
