437304e9e8739585c883782e357f7b7732f211ef
phonohawk <phonohawk>
2009-05-29 05:24:07 +0000

    Initial revision

---
+++ hs-convertible/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-convertible
+
+.if !defined(HS_CONVERTIBLE_BUILDLINK3_MK)
+HS_CONVERTIBLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-convertible+=	hs-convertible>=1.0.1
+BUILDLINK_PKGSRCDIR.hs-convertible?=	../../wip/hs-convertible
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
+.endif	# HS_CONVERTIBLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-convertible
