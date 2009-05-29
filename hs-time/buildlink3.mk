abb0227ff708c499acc0d3af48308a937e94047b
phonohawk <phonohawk>
2009-05-29 04:44:52 +0000

    Upstream update to time-1.1.2.4

--- hs-time/buildlink3.mk
+++ hs-time/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	hs-time
 .if !defined(HS_TIME_BUILDLINK3_MK)
 HS_TIME_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-time?=	build
-BUILDLINK_API_DEPENDS.hs-time+=	hs-time>=1.1.2.2
+BUILDLINK_API_DEPENDS.hs-time+=	hs-time>=1.1.2.4
 BUILDLINK_PKGSRCDIR.hs-time?=	../../wip/hs-time
-.endif # HS_TIME_BUILDLINK3_MK
+.endif	# HS_TIME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-time
