f5c5b8797cab8e00992d8c08848fc1f9dec339d7
noud4 <noud4>
2009-06-02 14:36:42 +0000

    adjusted X11_TYPE

--- ncbi-lib/buildlink3.mk
+++ ncbi-lib/buildlink3.mk
@@ -8,8 +8,12 @@ NCBI_LIB_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ncbi-lib+=	ncbi-lib>=20060301
 BUILDLINK_PKGSRCDIR.ncbi-lib?=		../../wip/ncbi-lib
 
+.include "../../mk/bsd.fast.prefs.mk"
+
 .if ${X11_TYPE} == "modular"
 .include "../../x11/libX11/buildlink3.mk"
+.else
+.include "../../mk/x11.buildlink3.mk"
 .endif # X11_TYPE
 .endif # NCBI_LIB_BUILDLINK3_MK
 
