5dd4ccd3fbc9c3e53ef3e990f4a719952782334a
phonohawk <phonohawk>
2009-06-02 06:57:32 +0000

    Initial revision

---
+++ hs-HUnit/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-HUnit
+
+.if !defined(HS_HUNIT_BUILDLINK3_MK)
+HS_HUNIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-HUnit+=	hs-HUnit>=1.2.0.3
+BUILDLINK_PKGSRCDIR.hs-HUnit?=	../../wip/hs-HUnit
+.endif	# HS_HUNIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-HUnit
