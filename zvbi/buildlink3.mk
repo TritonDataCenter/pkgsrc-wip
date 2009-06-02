a1b328b6600b122a0ecf35405c2bd5a902ad3f12
noud4 <noud4>
2009-06-02 12:47:26 +0000

    modular X11_TYPE added

--- zvbi/buildlink3.mk
+++ zvbi/buildlink3.mk
@@ -9,6 +9,9 @@ BUILDLINK_API_DEPENDS.zvbi+=	zvbi>=0.2.16
 BUILDLINK_PKGSRCDIR.zvbi?=	../../wip/zvbi
 
 .include "../../textproc/libunicode/buildlink3.mk"
+.if ${X11_TYPE} == "modular"
+.include "../../x11/libX11/buildlink3.mk"
+.endif # X11_TYPE
 .endif # ZVBI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-zvbi
