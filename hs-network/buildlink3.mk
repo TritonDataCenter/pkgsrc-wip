c43dd1a34197e73684014913fa6850430750e6bd
phonohawk <phonohawk>
2009-06-02 07:09:41 +0000

    Follow the renaming of wip/parsec to wip/hs-parsec

--- hs-network/buildlink3.mk
+++ hs-network/buildlink3.mk
@@ -8,7 +8,7 @@ HS_NETWORK_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.2.1.2
 BUILDLINK_PKGSRCDIR.hs-network?=	../../wip/hs-network
 
-.include "../../wip/parsec/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
 .endif	# HS_NETWORK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-network
