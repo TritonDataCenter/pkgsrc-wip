18de37407ac1e78d71ff5cfe9773cc8aa87d1f2d
phonohawk <phonohawk>
2009-06-02 07:51:55 +0000

    Upstream update to hs-curl-1.3.5

--- hs-curl/buildlink3.mk
+++ hs-curl/buildlink3.mk
@@ -5,11 +5,10 @@ BUILDLINK_TREE+=	hs-curl
 .if !defined(HS_CURL_BUILDLINK3_MK)
 HS_CURL_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-curl?=	build
-BUILDLINK_API_DEPENDS.hs-curl+=	hs-curl>=1.3.4
+BUILDLINK_API_DEPENDS.hs-curl+=	hs-curl>=1.3.5
 BUILDLINK_PKGSRCDIR.hs-curl?=	../../wip/hs-curl
 
 .include "../../www/curl/buildlink3.mk"
-.endif # HS_CURL_BUILDLINK3_MK
+.endif	# HS_CURL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-curl
