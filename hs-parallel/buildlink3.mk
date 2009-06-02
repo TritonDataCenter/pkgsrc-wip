f797001a5c2d7e9149ffef941d56f628cf1771b9
phonohawk <phonohawk>
2009-06-02 07:54:25 +0000

    Upstream update to parallel-1.1.0.1

--- hs-parallel/buildlink3.mk
+++ hs-parallel/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	hs-parallel
 .if !defined(HS_PARALLEL_BUILDLINK3_MK)
 HS_PARALLEL_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-parallel?=	build
-BUILDLINK_API_DEPENDS.hs-parallel+=	hs-parallel>=1.1.0.0
+BUILDLINK_API_DEPENDS.hs-parallel+=	hs-parallel>=1.1.0.1
 BUILDLINK_PKGSRCDIR.hs-parallel?=	../../wip/hs-parallel
-.endif # HS_PARALLEL_BUILDLINK3_MK
+.endif	# HS_PARALLEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-parallel
