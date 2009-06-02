b15d6320642ea72a995ac0948973b0e9c26c9987
noud4 <noud4>
2009-06-02 13:14:30 +0000

    modular X11_TYPE added

--- ncbi-lib/buildlink3.mk
+++ ncbi-lib/buildlink3.mk
@@ -8,6 +8,9 @@ NCBI_LIB_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ncbi-lib+=	ncbi-lib>=20060301
 BUILDLINK_PKGSRCDIR.ncbi-lib?=		../../wip/ncbi-lib
 
+.if ${X11_TYPE} == "modular"
+.include "../../x11/libX11/buildlink3.mk"
+.endif # X11_TYPE
 .endif # NCBI_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ncbi-lib
