2e8ad332008d6c279cb05c4b661e05ffd8bec9b1
phonohawk <phonohawk>
2009-06-03 04:10:48 +0000

    Initial revision

---
+++ hs-HsOpenSSL/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-HsOpenSSL
+
+.if !defined(HS_HSOPENSSL_BUILDLINK3_MK)
+HS_HSOPENSSL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-HsOpenSSL+=	hs-HsOpenSSL>=0.6.1
+BUILDLINK_PKGSRCDIR.hs-HsOpenSSL?=	../../wip/hs-HsOpenSSL
+
+.include "../../security/openssl/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
+.endif	# HS_HSOPENSSL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-HsOpenSSL
