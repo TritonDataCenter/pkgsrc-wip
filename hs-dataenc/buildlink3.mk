d94793fd2b71a473b7ec4c2b7d6232e18ada6108
phonohawk <phonohawk>
2009-06-04 05:09:16 +0000

    Upstream update to dataenc-0.13.0.0

--- hs-dataenc/buildlink3.mk
+++ hs-dataenc/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	hs-dataenc
 .if !defined(HS_DATAENC_BUILDLINK3_MK)
 HS_DATAENC_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-dataenc?=	build
-BUILDLINK_API_DEPENDS.hs-dataenc+=	hs-dataenc>=0.11.1
+BUILDLINK_API_DEPENDS.hs-dataenc+=	hs-dataenc>=0.13.0.0
 BUILDLINK_PKGSRCDIR.hs-dataenc?=	../../wip/hs-dataenc
-.endif # HS_DATAENC_BUILDLINK3_MK
+.endif	# HS_DATAENC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-dataenc
