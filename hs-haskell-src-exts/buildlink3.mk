09b8b56009afb1eb4ac7f8e7a95448bf39be9da9
phonohawk <phonohawk>
2009-06-04 03:34:10 +0000

    Initial revision

---
+++ hs-haskell-src-exts/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-haskell-src-exts
+
+.if !defined(HS_HASKELL_SRC_EXTS_BUILDLINK3_MK)
+HS_HASKELL_SRC_EXTS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=0.4.8
+BUILDLINK_PKGSRCDIR.hs-haskell-src-exts?=	../../wip/hs-haskell-src-exts
+
+.include "../../wip/hs-cpphs/buildlink3.mk"
+.endif	# HS_HASKELL_SRC_EXTS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-haskell-src-exts
