6b29230ebb0927a395b5f2473adba1aecf11bbc4
jsonn <jsonn>
2009-06-07 21:53:06 +0000

    Update to 2009/04/22 snapshot.

--- edbus/buildlink3.mk
+++ edbus/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	edbus
 .if !defined(EDBUS_BUILDLINK3_MK)
 EDBUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.01
+BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.05
 BUILDLINK_PKGSRCDIR.edbus?=	../../wip/edbus
 .endif # EDBUS_BUILDLINK3_MK
 
