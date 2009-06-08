8a8c3369a55fc0f625b99f766b73611c5764fb77
jsonn <jsonn>
2009-06-08 15:31:56 +0000

    Bump required version.

--- efreet/buildlink3.mk
+++ efreet/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	efreet
 .if !defined(EFREET_BUILDLINK3_MK)
 EFREET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.efreet+=	efreet>=0.5.0.043
-BUILDLINK_ABI_DEPENDS.efreet?=	efreet>=0.5.0.043nb1
+BUILDLINK_API_DEPENDS.efreet+=	efreet>=0.5.0.060
 BUILDLINK_PKGSRCDIR.efreet?=	../../wip/efreet
 
 .include        "../../wip/ecore/buildlink3.mk"
