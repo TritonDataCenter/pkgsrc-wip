81a50a25fcd3c931f5a3ad6d0037bcef1c0bf5bf
jsonn <jsonn>
2009-06-08 15:31:06 +0000

    Needs dbus recursively

--- edbus/buildlink3.mk
+++ edbus/buildlink3.mk
@@ -7,6 +7,8 @@ EDBUS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.05
 BUILDLINK_PKGSRCDIR.edbus?=	../../wip/edbus
+
+.include "../../sysutils/dbus/buildlink3.mk"
 .endif # EDBUS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-edbus
