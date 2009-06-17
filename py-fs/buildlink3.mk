21b6163452c9379d54951d67cbf0955f31139e05
jihbed <jihbed>
2009-06-17 23:30:54 +0000

    Initial revision

---
+++ py-fs/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	py-fs
+
+.if !defined(PY_FS_BUILDLINK3_MK)
+PY_FS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py-fs+=	py-fs>=0.1.0
+BUILDLINK_PKGSRCDIR.py-fs?=	../../wip/py-fs
+.endif	# PY_FS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py-fs
