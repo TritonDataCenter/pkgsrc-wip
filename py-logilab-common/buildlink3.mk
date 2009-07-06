954876f60305f6af47bef54aa4331f0fe6c54170
jihbed <jihbed>
2009-07-06 22:33:21 +0000

    Update package: 	-------------- 	o 0.41.0 to 0.42.0 	o Added dependency: py-readline, py-django,Py-Pyro 	o Improve Test : done
    	Observation:
    	-----------
    	Please review options.mk
    	Please global review

--- py-logilab-common/buildlink3.mk
+++ py-logilab-common/buildlink3.mk
@@ -5,7 +5,8 @@ BUILDLINK_TREE+=	py-logilab-common
 .if !defined(PY_LOGILAB_COMMON_BUILDLINK3_MK)
 PY_LOGILAB_COMMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py-logilab-common+=	py-logilab-common>=0.41.0
+BUILDLINK_API_DEPENDS.py-logilab-common+=	${PYPKGPREFIX}-logilab-common>=0.42.0
+BUILDLINK_ABI_DEPENDS.py-logilab-common+=	${PYPKGPREFIX}-logilab-common>=2.41.0
 BUILDLINK_PKGSRCDIR.py-logilab-common?=	../../wip/py-logilab-common
 
 .endif	# PY_LOGILAB_COMMON_BUILDLINK3_MK
