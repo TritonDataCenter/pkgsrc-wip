1833f55768485fd968aa5c165d986dcfda6eeee3
koifren <koifren>
2009-07-21 07:30:43 +0000

    Update to 0.1.4. The former version was no longer available for download. Generate buildlink3.mk file with this ocassion

---
+++ taglib-extras/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	taglib-extras
+
+.if !defined(TAGLIB_EXTRAS_BUILDLINK3_MK)
+TAGLIB_EXTRAS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.taglib-extras+=	taglib-extras>=0.1.4
+BUILDLINK_PKGSRCDIR.taglib-extras?=	../../wip/taglib-extras
+
+.include "../../audio/taglib/buildlink3.mk"
+.endif	# TAGLIB_EXTRAS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-taglib-extras
