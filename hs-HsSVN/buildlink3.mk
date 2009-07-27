c031300eda250366bd4804d057ae77a88b7f4a90
phonohawk <phonohawk>
2009-07-27 04:15:49 +0000

    Initial revision

---
+++ hs-HsSVN/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-HsSVN
+
+.if !defined(HS_HSSVN_BUILDLINK3_MK)
+HS_HSSVN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-HsSVN+=	hs-HsSVN>=0.4.1
+BUILDLINK_PKGSRCDIR.hs-HsSVN?=	../../wip/hs-HsSVN
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-stm/buildlink3.mk"
+.include "../../devel/subversion-base/buildlink3.mk"
+.endif	# HS_HSSVN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-HsSVN
