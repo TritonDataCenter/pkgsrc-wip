89f1c18cc20c45389a419516e88a08b849b2320f
phonohawk <phonohawk>
2009-07-27 03:37:08 +0000

    Upstream update to HTTP-4000.0.7

--- hs-HTTP/buildlink3.mk
+++ hs-HTTP/buildlink3.mk
@@ -5,12 +5,11 @@ BUILDLINK_TREE+=	hs-HTTP
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-HTTP?=	build
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.0.1
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.0.7
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../wip/hs-HTTP
 
 .include "../../wip/hs-network/buildlink3.mk"
-.include "../../wip/parsec/buildlink3.mk"
-.endif # HS_HTTP_BUILDLINK3_MK
+.include "../../wip/hs-parsec/buildlink3.mk"
+.endif	# HS_HTTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-HTTP
