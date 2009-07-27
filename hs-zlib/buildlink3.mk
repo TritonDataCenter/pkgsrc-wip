29a2d3b7f9d42c9d63d929e32b25cfe80b82ce55
phonohawk <phonohawk>
2009-07-27 03:08:33 +0000

    Upstream update to zlib-0.5.2.0

--- hs-zlib/buildlink3.mk
+++ hs-zlib/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-zlib
 .if !defined(HS_ZLIB_BUILDLINK3_MK)
 HS_ZLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.5.0.0nb1
+BUILDLINK_API_DEPENDS.hs-zlib+=	hs-zlib>=0.5.2.0
 BUILDLINK_PKGSRCDIR.hs-zlib?=	../../wip/hs-zlib
 
 .include "../../devel/zlib/buildlink3.mk"
