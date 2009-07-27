0d71496901e3e160341616d347d6a06c3cbb7cd7
phonohawk <phonohawk>
2009-07-27 04:30:38 +0000

    Added LICENSE line

--- hs-magic/buildlink3.mk
+++ hs-magic/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	hs-magic
 HS_MAGIC_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.hs-magic?=	build
-BUILDLINK_API_DEPENDS.hs-magic+=	hs-magic>=1.0.7
+BUILDLINK_API_DEPENDS.hs-magic+=	hs-magic>=1.0.7nb1
 BUILDLINK_PKGSRCDIR.hs-magic?=	../../wip/hs-magic
 
 .include "../../sysutils/file/buildlink3.mk"
