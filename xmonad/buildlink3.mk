88ea8516a514f6777d429ac562aadcb7861fd332
zul_ <zul_>
2009-08-09 18:18:43 +0000

    Update xmonad from 0.7 to 0.8.1

--- xmonad/buildlink3.mk
+++ xmonad/buildlink3.mk
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	xmonad
 XMONAD_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.xmonad?=	build
-BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.7
-BUILDLINK_ABI_DEPENDS.xmonad?=	xmonad>=0.7nb1
+BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.8.1
+BUILDLINK_ABI_DEPENDS.xmonad?=	xmonad>=0.8.1
 BUILDLINK_PKGSRCDIR.xmonad?=	../../wip/xmonad
 
 .include "../../lang/ghc/buildlink3.mk"
