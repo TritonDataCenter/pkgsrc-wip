faddaaf0c35f438ee39bf0a6e7ac6d21e766616f
zul_ <zul_>
2009-08-09 18:16:17 +0000

    Update from 1.4.1 to 1.4.5

--- hs-x11/buildlink3.mk
+++ hs-x11/buildlink3.mk
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	hs-x11
 HS_X11_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.hs-x11?=	build
-BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.4.1
-BUILDLINK_ABI_DEPENDS.hs-x11?=	hs-x11>=1.4.1nb1
+BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.4.5
+BUILDLINK_ABI_DEPENDS.hs-x11?=	hs-x11>=1.4.5
 BUILDLINK_PKGSRCDIR.hs-x11?=	../../wip/hs-x11
 
 .include "../../lang/ghc/buildlink3.mk"
