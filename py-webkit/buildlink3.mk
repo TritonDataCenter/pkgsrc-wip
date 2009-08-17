622c22a9803529d99c5247b222b3dbfb37bb6733
jihbed <jihbed>
2009-08-17 20:50:34 +0000

    pkgsrc-changes: 	-------------- 	o update version

--- py-webkit/buildlink3.mk
+++ py-webkit/buildlink3.mk
@@ -5,11 +5,9 @@ BUILDLINK_TREE+=	py-webkitgtk
 .if !defined(PY_WEBKITGTK_BUILDLINK3_MK)
 PY_WEBKITGTK_BUILDLINK3_MK:=
 
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_API_DEPENDS.py-webkitgtk+=	${PYPKGPREFIX}-webkitgtk>=1.1.4
-BUILDLINK_ABI_DEPENDS.py-webkitgtk?=	py25-webkitgtk>=1.1.5nb1
+BUILDLINK_API_DEPENDS.py-webkitgtk+=	${PYPKGPREFIX}-webkitgtk>=1.1.6
 BUILDLINK_PKGSRCDIR.py-webkitgtk?=	../../wip/py-webkit
+
 
 .include "../../x11/py-gtk2/buildlink3.mk"
 .include "../../www/webkit-gtk/buildlink3.mk"
