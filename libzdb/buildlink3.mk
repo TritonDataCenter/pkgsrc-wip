b9532575c439dd775d107140a9ace4fab41679ff
udontknow <udontknow>
2009-08-19 18:09:29 +0000

    Initial revision

---
+++ libzdb/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libzdb
+
+.if !defined(LIBZDB_BUILDLINK3_MK)
+LIBZDB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libzdb+=	libzdb>=2.6
+BUILDLINK_PKGSRCDIR.libzdb?=	../../wip/libzdb
+.endif	# LIBZDB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libzdb
