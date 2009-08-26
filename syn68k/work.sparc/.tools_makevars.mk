8e11ead5a6103d1801a5625f0407cb8727f23758
christtrekker <christtrekker>
2009-08-26 19:26:01 +0000

    Initial revision

---
+++ syn68k/work.sparc/.tools_makevars.mk
@@ -0,0 +1,15 @@
+.if !defined(_MAKEVARS_MK)
+_MAKEVARS_MK=	defined
+
+TOOLS_PREFIX.digest=	/usr/pkg
+TOOLS_PREFIX.ftp=	/usr/pkg
+TOOLS_PREFIX.gzcat=	/usr/pkg
+_BLNK_PHYSICAL_PATH.LOCALBASE=	/usr/pkg
+_BLNK_PHYSICAL_PATH.WRKDIR=	/usr/pkgsrc/wip/syn68k/work.sparc
+_IGNORE_INFO_PATH=	
+_MANCOMPRESSED=	no
+_MANZ=	no
+_USE_TOOLS=	[ awk basename cat chgrp chmod chown cmp cp cut date diff digest dirname echo egrep env expr false file find ftp grep gzcat head hostname id install ln ls mkdir mv printf pwd rm rmdir sed sh sort tail tar test touch tr true uname wc xargs
+_WRAP_PATH=	/usr/pkgsrc/wip/syn68k/work.sparc/.buildlink/bin:/usr/pkgsrc/wip/syn68k/work.sparc/.gcc/bin:/usr/pkgsrc/wip/syn68k/work.sparc/.tools/bin:/usr/pkg/bin:/usr/sbin:/usr/bin:/usr/ucb:/usr/ccs/bin:/usr/xpg4/bin:/usr/dt/bin:/usr/X11/bin:/opt/gcc/bin:/usr/pkg/sbin:/usr/pkg/bin:/usr/openwin/bin:/usr/sfw/sbin:/usr/sfw/bin:/usr/local/sbin:/usr/local/bin
+
+.endif # _MAKEVARS_MK
