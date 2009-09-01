71fafa5f7fa43b1cca593a6b4af5bfee0f9c2266
thomasklausner <thomasklausner>
2009-09-01 12:33:10 +0000

    Add buildlink3.mk file.

--- openal-soft/buildlink3.mk
+++ openal-soft/buildlink3.mk
@@ -5,11 +5,8 @@ BUILDLINK_TREE+=	openal-soft
 .if !defined(OPENAL_SOFT_BUILDLINK3_MK)
 OPENAL_SOFT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openal-soft+=	openal-soft>=1.2.218
-#BUILDLINK_PKGSRCDIR.openal-soft?=	../../audio/openal-soft
+BUILDLINK_API_DEPENDS.openal-soft+=	openal-soft>=1.5.304
 BUILDLINK_PKGSRCDIR.openal-soft?=	../../wip/openal-soft
-
-.include "../../mk/dlopen.buildlink3.mk"
-.endif # OPENAL_SOFT_BUILDLINK3_MK
+.endif	# OPENAL_SOFT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-openal-soft
