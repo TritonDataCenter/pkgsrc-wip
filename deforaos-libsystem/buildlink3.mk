571aac0ee8b394e2f8dba7c5a495a8bf84eaa326
khorben <khorben>
2009-09-06 23:25:31 +0000

    Fixed some warnings as reported by pkglint

--- deforaos-libsystem/buildlink3.mk
+++ deforaos-libsystem/buildlink3.mk
@@ -8,7 +8,6 @@ DEFORAOS_LIBSYSTEM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.deforaos-libsystem+=	deforaos-libsystem>=0.1.2
 BUILDLINK_PKGSRCDIR.deforaos-libsystem?=	../../wip/deforaos-libsystem
 
-#.include "../../security/openssl/buildlink3.mk"
 .endif	# DEFORAOS_LIBSYSTEM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-libsystem
