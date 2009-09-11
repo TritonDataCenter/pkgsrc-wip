b0827c16865dce7c070adb9f49fd62d5515bbd2f
jsonn <jsonn>
2009-09-11 23:05:15 +0000

    Initial revision

---
+++ elementary/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	edbus
+
+.if !defined(EDBUS_BUILDLINK3_MK)
+EDBUS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.5.0.062
+BUILDLINK_PKGSRCDIR.edbus?=	../../wip/edbus
+
+.include "../../sysutils/dbus/buildlink3.mk"
+.endif # EDBUS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-edbus
