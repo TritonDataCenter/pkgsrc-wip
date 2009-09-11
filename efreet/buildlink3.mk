e6726955cf54c548a65c5cef8160880559b7c49f
jsonn <jsonn>
2009-09-11 22:27:48 +0000

    Request latest version, no ABI compatibility.

--- efreet/buildlink3.mk
+++ efreet/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	efreet
 .if !defined(EFREET_BUILDLINK3_MK)
 EFREET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.efreet+=	efreet>=0.5.0.060
+BUILDLINK_API_DEPENDS.efreet+=	efreet>=0.5.0.062
 BUILDLINK_PKGSRCDIR.efreet?=	../../wip/efreet
 
 .include        "../../wip/ecore/buildlink3.mk"
