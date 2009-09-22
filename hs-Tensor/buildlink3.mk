5268bc000b1012a9adacf9d8f4f4f5eb41949b0c
phonohawk <phonohawk>
2009-09-22 14:01:20 +0000

    Initial revision

---
+++ hs-Tensor/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-Tensor
+
+.if !defined(HS_TENSOR_BUILDLINK3_MK)
+HS_TENSOR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-Tensor+=	hs-Tensor>=1.0.0.1
+BUILDLINK_PKGSRCDIR.hs-Tensor?=	../../wip/hs-Tensor
+.endif	# HS_TENSOR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-Tensor
