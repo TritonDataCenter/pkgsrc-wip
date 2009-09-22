fd8cdc6bd7b3e7d8584391ae9ef99d0ed3f5b0a7
phonohawk <phonohawk>
2009-09-22 13:47:43 +0000

    Initial revision

---
+++ hs-ObjectName/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-ObjectName
+
+.if !defined(HS_OBJECTNAME_BUILDLINK3_MK)
+HS_OBJECTNAME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-ObjectName+=	hs-ObjectName>=1.0.0.0
+BUILDLINK_PKGSRCDIR.hs-ObjectName?=	../../wip/hs-ObjectName
+.endif	# HS_OBJECTNAME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-ObjectName
