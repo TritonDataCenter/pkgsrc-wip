ac76b3437e1931a35dcc36edcacb78c388d3e50a
phonohawk <phonohawk>
2009-09-23 05:56:35 +0000

    added missing dependency to mtl

--- hs-QuickCheck/buildlink3.mk
+++ hs-QuickCheck/buildlink3.mk
@@ -5,8 +5,10 @@ BUILDLINK_TREE+=	hs-QuickCheck
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.1.0.2
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.1.0.2nb1
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../wip/hs-QuickCheck
+
+.include "../../wip/hs-mtl/buildlink3.mk"
 .endif	# HS_QUICKCHECK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-QuickCheck
