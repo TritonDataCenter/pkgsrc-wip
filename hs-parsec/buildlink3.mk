b822f84aacf95d455b68b5fe4dfe1da724b3aa8d
phonohawk <phonohawk>
2009-09-23 08:59:55 +0000

    Upstream update to parsec-3.0.1

--- hs-parsec/buildlink3.mk
+++ hs-parsec/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-parsec
 .if !defined(HS_PARSEC_BUILDLINK3_MK)
 HS_PARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.0.0
+BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.0.1
 BUILDLINK_PKGSRCDIR.hs-parsec?=	../../wip/hs-parsec
 
 .include "../../wip/hs-mtl/buildlink3.mk"
