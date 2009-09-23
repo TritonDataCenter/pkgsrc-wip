d2fcab5fd163b6513c5d521e22ae9fa83dd0df2d
phonohawk <phonohawk>
2009-09-23 13:00:58 +0000

    Initial revision

---
+++ hs-gnuplot/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-gnuplot
+
+.if !defined(HS_GNUPLOT_BUILDLINK3_MK)
+HS_GNUPLOT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-gnuplot+=	hs-gnuplot>=0.3.3
+BUILDLINK_PKGSRCDIR.hs-gnuplot?=	../../wip/hs-gnuplot
+
+.include "../../wip/hs-monoid-transformer/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
+.include "../../wip/hs-utility-ht/buildlink3.mk"
+.endif	# HS_GNUPLOT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-gnuplot
