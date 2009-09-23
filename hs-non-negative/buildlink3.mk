49294cdefd3755d34c54772440684ac63c256d38
phonohawk <phonohawk>
2009-09-23 06:08:11 +0000

    Initial revision

---
+++ hs-non-negative/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-non-negative
+
+.if !defined(HS_NON_NEGATIVE_BUILDLINK3_MK)
+HS_NON_NEGATIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-non-negative+=	hs-non-negative>=0.0.5
+BUILDLINK_PKGSRCDIR.hs-non-negative?=	../../wip/hs-non-negative
+
+.include "../../wip/hs-QuickCheck1/buildlink3.mk"
+.endif	# HS_NON_NEGATIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-non-negative
