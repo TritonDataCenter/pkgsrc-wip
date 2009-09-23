2beccad9a2357017ea1dbb1bca243f6f77da7471
phonohawk <phonohawk>
2009-09-23 05:19:11 +0000

    Initial revision

---
+++ hs-data-accessor/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-data-accessor
+
+.if !defined(HS_DATA_ACCESSOR_BUILDLINK3_MK)
+HS_DATA_ACCESSOR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-data-accessor+=	hs-data-accessor>=0.2.1
+BUILDLINK_PKGSRCDIR.hs-data-accessor?=	../../wip/hs-data-accessor
+
+.include "../../wip/hs-transformers/buildlink3.mk"
+.endif	# HS_DATA_ACCESSOR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-data-accessor
