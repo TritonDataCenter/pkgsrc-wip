e7cc1fa787daa4224cd6baff589c74b7054950ed
phonohawk <phonohawk>
2009-09-23 05:41:29 +0000

    Initial revision

---
+++ hs-QuickCheck/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-QuickCheck
+
+.if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
+HS_QUICKCHECK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.1.0.2
+BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../wip/hs-QuickCheck
+.endif	# HS_QUICKCHECK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-QuickCheck
