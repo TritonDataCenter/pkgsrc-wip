a969158077f5c2d7e15d94fe27fc15de03b78b32
thomasklausner <thomasklausner>
2009-09-24 10:26:25 +0000

    Bump depends to 1.0.1, amarok needs >=1.0 and is only user of this package anyway.

--- taglib-extras/buildlink3.mk
+++ taglib-extras/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	taglib-extras
 .if !defined(TAGLIB_EXTRAS_BUILDLINK3_MK)
 TAGLIB_EXTRAS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.taglib-extras+=	taglib-extras>=0.1.4
+BUILDLINK_API_DEPENDS.taglib-extras+=	taglib-extras>=1.0.1
 BUILDLINK_PKGSRCDIR.taglib-extras?=	../../wip/taglib-extras
 
 .include "../../audio/taglib/buildlink3.mk"
