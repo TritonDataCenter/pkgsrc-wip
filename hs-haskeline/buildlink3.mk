57a375b1684eaf038e74c9d0c958ed307e14407e
phonohawk <phonohawk>
2009-09-24 06:22:54 +0000

    Upstream update to haskeline-0.6.2.1

--- hs-haskeline/buildlink3.mk
+++ hs-haskeline/buildlink3.mk
@@ -5,12 +5,12 @@ BUILDLINK_TREE+=	hs-haskeline
 .if !defined(HS_HASKELINE_BUILDLINK3_MK)
 HS_HASKELINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.6.1.6
+BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.6.2.1
 BUILDLINK_PKGSRCDIR.hs-haskeline?=	../../wip/hs-haskeline
 
-.include "../../converters/libiconv/buildlink3.mk"
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-terminfo/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
 .endif	# HS_HASKELINE_BUILDLINK3_MK
 
