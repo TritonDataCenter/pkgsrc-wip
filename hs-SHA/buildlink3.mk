3183ce37fc7e78ceae2384b6a17f5d8c96c00ffc
phonohawk <phonohawk>
2009-09-24 02:05:29 +0000

    Upstream update to SHA-1.4.0

--- hs-SHA/buildlink3.mk
+++ hs-SHA/buildlink3.mk
@@ -5,11 +5,10 @@ BUILDLINK_TREE+=	hs-SHA
 .if !defined(HS_SHA_BUILDLINK3_MK)
 HS_SHA_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-SHA?=	build
-BUILDLINK_API_DEPENDS.hs-SHA+=	hs-SHA>=1.0.3
+BUILDLINK_API_DEPENDS.hs-SHA+=	hs-SHA>=1.4.0
 BUILDLINK_PKGSRCDIR.hs-SHA?=	../../wip/hs-SHA
 
-.include "../../wip/QuickCheck/buildlink3.mk"
-.endif # HS_SHA_BUILDLINK3_MK
+.include "../../wip/hs-binary/buildlink3.mk"
+.endif	# HS_SHA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-SHA
