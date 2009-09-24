bc05288bed013058b38e6b0e473fc176dc6f1a4d
phonohawk <phonohawk>
2009-09-24 09:32:19 +0000

    add missing dependency to devel/ncurses

--- hs-terminfo/buildlink3.mk
+++ hs-terminfo/buildlink3.mk
@@ -8,6 +8,7 @@ HS_TERMINFO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-terminfo+=	hs-terminfo>=0.3.1.1
 BUILDLINK_PKGSRCDIR.hs-terminfo?=	../../wip/hs-terminfo
 
+.include "../../devel/ncurses/buildlink3.mk"
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .endif	# HS_TERMINFO_BUILDLINK3_MK
 
