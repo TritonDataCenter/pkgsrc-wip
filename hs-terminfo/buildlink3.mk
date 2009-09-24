1452d7986e66ef60f991712e0d8d7dd3ff7435ac
phonohawk <phonohawk>
2009-09-24 06:11:22 +0000

    Initial revision

---
+++ hs-terminfo/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-terminfo
+
+.if !defined(HS_TERMINFO_BUILDLINK3_MK)
+HS_TERMINFO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-terminfo+=	hs-terminfo>=0.3.1.1
+BUILDLINK_PKGSRCDIR.hs-terminfo?=	../../wip/hs-terminfo
+
+.include "../../wip/hs-extensible-exceptions/buildlink3.mk"
+.endif	# HS_TERMINFO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-terminfo
