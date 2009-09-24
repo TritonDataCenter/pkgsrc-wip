970aa86cfefab1919ffa4da72e80c66210f56ea0
phonohawk <phonohawk>
2009-09-24 06:41:03 +0000

    Bump revision to nb2

--- hs-html/buildlink3.mk
+++ hs-html/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	hs-html
 .if !defined(HS_HTML_BUILDLINK3_MK)
 HS_HTML_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-html?=	build
-BUILDLINK_API_DEPENDS.hs-html+=	hs-html>=1.0.1.2
+BUILDLINK_API_DEPENDS.hs-html+=	hs-html>=1.0.1.2nb2
 BUILDLINK_PKGSRCDIR.hs-html?=	../../wip/hs-html
-.endif # HS_HTML_BUILDLINK3_MK
+.endif	# HS_HTML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-html
