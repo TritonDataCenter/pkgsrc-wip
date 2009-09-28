dbe8925a2263d962116d9fc2550aa643cfb8db30
phonohawk <phonohawk>
2009-09-28 05:29:31 +0000

    the ETL package has moved from wip to devel

--- synfigstudio/buildlink3.mk
+++ synfigstudio/buildlink3.mk
@@ -8,10 +8,10 @@ SYNFIGSTUDIO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.synfigstudio+=	synfigstudio>=0.61.08
 BUILDLINK_PKGSRCDIR.synfigstudio?=	../../wip/synfigstudio
 
+.include "../../devel/ETL/buildlink3.mk"
 .include "../../devel/libsigc++/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glibmm/buildlink3.mk"
-.include "../../wip/ETL/buildlink3.mk"
 .include "../../wip/synfig/buildlink3.mk"
 .include "../../x11/gtkmm/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
