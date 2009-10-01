b19f2824bda6768fa8609c23cef48b5190099b97
phonohawk <phonohawk>
2009-10-01 05:49:36 +0000

    Initial revision

---
+++ hs-uniplate/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-uniplate
+
+.if !defined(HS_UNIPLATE_BUILDLINK3_MK)
+HS_UNIPLATE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.2.0.3
+BUILDLINK_PKGSRCDIR.hs-uniplate?=	../../wip/hs-uniplate
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.endif	# HS_UNIPLATE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-uniplate
