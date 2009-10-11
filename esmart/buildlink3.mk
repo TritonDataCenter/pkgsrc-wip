7a0631658782034651c7b96633ab1c454b02506d
cheusov <cheusov>
2009-10-11 12:50:32 +0000

    fix: wip/eina was imported to devel/

--- esmart/buildlink3.mk
+++ esmart/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.esmart+=	esmart>=0.9.0.42nb2
 BUILDLINK_PKGSRCDIR.esmart?=	../../wip/esmart
 
 .include "../../wip/ecore/buildlink3.mk"
-.include "../../wip/eina/buildlink3.mk"
+.include "../../devel/eina/buildlink3.mk"
 .include "../../wip/edje/buildlink3.mk"
 .include "../../wip/epsilon/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
