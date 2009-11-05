afc5867cf43ce5eb8b4bfb7fc47ab7adcf860766
schnoebe <schnoebe>
2009-11-05 05:08:41 +0000

    Correct the BUILDLINK_{API_DEPENDS,PKGSRCDIR} variables.

--- poco-data-sqlite/buildlink3.mk
+++ poco-data-sqlite/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	poco-data-sqlite
 .if !defined(POCO_DATA_SQLITE_BUILDLINK3_MK)
 POCO_DATA_SQLITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.poco-data-sqlite+=	poco-data>=1.3.5
-BUILDLINK_PKGSRCDIR.poco-data-sqlite?=	../../wip/poco-data
+BUILDLINK_API_DEPENDS.poco-data-sqlite+=	poco-data-sqlite>=1.3.5
+BUILDLINK_PKGSRCDIR.poco-data-sqlite?=	../../wip/poco-data-sqlite
 
 .include "../../wip/poco-data/buildlink3.mk"
 .endif # POCO_DATA_SQLITE_BUILDLINK3_MK
