614e781f2d0a81d1ca20bb8b82abf6bb97960662
phonohawk <phonohawk>
2009-11-10 03:44:43 +0000

    Initial revision

---
+++ hs-hscolour/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-hscolour
+
+.if !defined(HS_HSCOLOUR_BUILDLINK3_MK)
+HS_HSCOLOUR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.15
+BUILDLINK_PKGSRCDIR.hs-hscolour?=	../../wip/hs-hscolour
+.endif	# HS_HSCOLOUR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hscolour
