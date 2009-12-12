46dcc108581bec0b17d9c4c3db1b87c5396ff817
phonohawk <phonohawk>
2009-12-12 15:06:31 +0000

    Initial revision

---
+++ hs-mmap4/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-mmap4
+
+.if !defined(HS_MMAP4_BUILDLINK3_MK)
+HS_MMAP4_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-mmap4+=	hs-mmap4>=0.4.1
+BUILDLINK_PKGSRCDIR.hs-mmap4?=	../../wip/hs-mmap4
+.endif	# HS_MMAP4_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-mmap4
