178ca55a9e328008cd983a4f61dbe34a12efbbe5
gregoire <gregoire>
2009-12-16 14:37:31 +0000

    Rewrite of grub2 package. - based on official distribution version 1.97.1. - patches to fix configuration, compilation and installation   (see README.patches). - options:   . new option `grub-mkfont', which induces a dependency on freetype2.   . use of PKG_OPTIONS_VAR for handling of options in PLIST. - makefile:   . explicit support of opendisk on NetBSD (this really belongs to the     configure script -- bug report sent upstream).   . build dependency on help2man for man page generation. - moved potentially non-obsolete stuff into README.attic.

--- grub2/cvs.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD$
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-CVS_REPOSITORIES=	grub2
-CVS_ROOT.grub2=		:pserver:anonymous:@cvs.savannah.gnu.org:/sources/grub
-CVS_MODULE.grub2=	grub2
-
-GRUB_VERSION?=	${:!${DATE} '+%Y%m%d'!}
