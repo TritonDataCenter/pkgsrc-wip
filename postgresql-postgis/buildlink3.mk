8db940aab88d468596581ae63808735dbd0e457d
moubctez <moubctez>
2009-12-17 10:17:47 +0000

    buildlink3.mk again

--- postgresql-postgis/buildlink3.mk
+++ postgresql-postgis/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	postgresql84-postgis
 .if !defined(POSTGRESQL84_POSTGIS_BUILDLINK3_MK)
 POSTGRESQL84_POSTGIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.postgresql84-postgis+=	postgresql84-postgis>=1.4.1
+BUILDLINK_API_DEPENDS.postgresql84-postgis+=	postgresql84-postgis>=1.4
 BUILDLINK_PKGSRCDIR.postgresql84-postgis?=	../../wip/postgresql-postgis
 
 .include "../../geography/geos/buildlink3.mk"
