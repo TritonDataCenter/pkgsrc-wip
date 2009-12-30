75e5c65d9fc69536862ffdd3b64cb9ec12f8d6c6
khorben <khorben>
2009-12-30 22:25:54 +0000

    Packaging DeforaOS Desktop Audio Mixer (doesn't link yet)

---
+++ deforaos-libdesktop/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	deforaos-libdesktop
+
+.if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
+DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.1
+BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
+
+.endif	# DEFORAOS_LIBDESKTOP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-libdesktop
