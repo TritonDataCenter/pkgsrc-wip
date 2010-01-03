d03d9a4e6132b2243c85460ae67f02faed37c429
pallegra <pallegra>
2010-01-03 15:11:39 +0000

    Initial revision

---
+++ glx-utils/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	glu
+
+.if !defined(GLU_BUILDLINK3_MK)
+GLU_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.glu+=	glu>=7.7
+BUILDLINK_PKGSRCDIR.glu?=	../../wip/glu
+
+.include "../../wip/gl/buildlink3.mk"
+.endif	# GLU_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-glu
