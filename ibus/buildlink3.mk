7bda07eef7daa13510a5e4bd30504f5d4dd88e7e
obache <obache>
2010-01-10 13:10:21 +0000

    Drop ${PYPKGPREFIX} from ${PKGNAME}.

--- ibus/buildlink3.mk
+++ ibus/buildlink3.mk
@@ -6,9 +6,7 @@ BUILDLINK_TREE+=	ibus
 .if !defined(IBUS_BUILDLINK3_MK)
 IBUS_BUILDLINK3_MK:=
 
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_API_DEPENDS.ibus+=	${PYPKGPREFIX}-ibus>=1.2.0.20091024
+BUILDLINK_API_DEPENDS.ibus+=	ibus>=1.2.0.20091024
 BUILDLINK_PKGSRCDIR.ibus?=	../../wip/ibus
 
 .include "../../devel/gettext-lib/buildlink3.mk"
