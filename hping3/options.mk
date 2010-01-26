ecb84a3ff9907b1e89369af7ca29c903bfbd6741
pettai <pettai>
2010-01-26 23:00:05 +0000

    - Added DESTDIR support - Made amd64 installation work - Added option for TCL support

---
+++ hping3/options.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.hping3
+PKG_SUPPORTED_OPTIONS=		tcl
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
+
+###
+### TCL scripting support
+###
+.if !empty(PKG_OPTIONS:Mtcl)
+.include "../../lang/tcl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --no-tcl
+.endif
