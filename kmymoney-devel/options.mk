59dd37711fe50324d9d73463b789e8918f9fb43a
thomasklausner <thomasklausner>
2010-01-31 21:30:36 +0000

    Remove trailing whitespace.

--- kmymoney-devel/options.mk
+++ kmymoney-devel/options.mk
@@ -1,7 +1,7 @@
 # $NetBSD: options.mk,v 1.2 2008/04/12 22:43:00 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kmymoney2
-PKG_SUPPORTED_OPTIONS=	libofx 
+PKG_SUPPORTED_OPTIONS=	libofx
 PKG_SUGGESTED_OPTIONS=	libofx
 
 .include "../../mk/bsd.options.mk"
