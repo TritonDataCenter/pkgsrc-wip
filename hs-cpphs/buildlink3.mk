ffa90aab4d7f66ecc9c7905171e78f946b30f2b9
phonohawk <phonohawk>
2010-02-02 05:56:04 +0000

    Upstream update to cpphs-1.10

--- hs-cpphs/buildlink3.mk
+++ hs-cpphs/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-cpphs
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.9
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.10
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../wip/hs-cpphs
 .endif	# HS_CPPHS_BUILDLINK3_MK
 
