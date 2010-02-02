fb9021d9ce5a024134fa65875258a98059c61305
phonohawk <phonohawk>
2010-02-02 06:24:13 +0000

    Upstream update to uniplate-1.5.1

--- hs-uniplate/buildlink3.mk
+++ hs-uniplate/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-uniplate
 .if !defined(HS_UNIPLATE_BUILDLINK3_MK)
 HS_UNIPLATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.2.0.3
+BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.5.1
 BUILDLINK_PKGSRCDIR.hs-uniplate?=	../../wip/hs-uniplate
 
 .include "../../wip/hs-mtl/buildlink3.mk"
