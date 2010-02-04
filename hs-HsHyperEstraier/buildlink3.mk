0c2f5d1df4108d54954da335022970056fd3c3db
phonohawk <phonohawk>
2010-02-04 06:58:22 +0000

    Upstream update to HsHyperEstraier-0.3.2

--- hs-HsHyperEstraier/buildlink3.mk
+++ hs-HsHyperEstraier/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-HsHyperEstraier
 .if !defined(HS_HSHYPERESTRAIER_BUILDLINK3_MK)
 HS_HSHYPERESTRAIER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HsHyperEstraier+=	hs-HsHyperEstraier>=0.3.1
+BUILDLINK_API_DEPENDS.hs-HsHyperEstraier+=	hs-HsHyperEstraier>=0.3.2
 BUILDLINK_PKGSRCDIR.hs-HsHyperEstraier?=	../../wip/hs-HsHyperEstraier
 
 .include "../../textproc/hyperestraier/buildlink3.mk"
