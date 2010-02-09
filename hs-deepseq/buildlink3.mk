0e7ffd4eaa3b185c8c9c71efe485d035f63471b2
phonohawk <phonohawk>
2010-02-09 03:25:07 +0000

    Initial revision

---
+++ hs-deepseq/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-deepseq
+
+.if !defined(HS_DEEPSEQ_BUILDLINK3_MK)
+HS_DEEPSEQ_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-deepseq+=	hs-deepseq>=1.1.0.0
+BUILDLINK_PKGSRCDIR.hs-deepseq?=	../../wip/hs-deepseq
+.endif	# HS_DEEPSEQ_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-deepseq
