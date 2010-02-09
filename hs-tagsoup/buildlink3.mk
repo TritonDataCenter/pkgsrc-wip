ab8b04ce4d0d169cc9108bc97857d84f58030748
phonohawk <phonohawk>
2010-02-09 04:12:57 +0000

    Upstream update to tagsoup-0.8

--- hs-tagsoup/buildlink3.mk
+++ hs-tagsoup/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-tagsoup
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.6
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.8
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../wip/hs-tagsoup
 
 .include "../../wip/hs-mtl/buildlink3.mk"
