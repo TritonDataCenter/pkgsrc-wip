ce63f36cf82508c2a7717ef10c2f04e94bf24c3a
tnn2 <tnn2>
2010-02-16 14:46:58 +0000

    Update to firefox-3.6.2 (prerelease snaphot)

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -4,9 +4,9 @@
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.6	# .${MOZ_BRANCH_MINOR}
+FIREFOX_VER=	3.6${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	1.9.2
-MOZ_BRANCH_MINOR=	# empty
+MOZ_BRANCH_MINOR=	.2pre
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
@@ -14,3 +14,8 @@ DISTINFO_FILE=	${.CURDIR}/../../wip/xulrunner/distinfo
 PATCHDIR=	${.CURDIR}/../../wip/xulrunner/patches
 
 WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
+
+# snapshot overrides
+DISTNAME=	14fd499129bf
+MASTER_SITES=	http://hg.mozilla.org/releases/mozilla-1.9.2/archive/
+WRKSRC=		${WRKDIR}/mozilla-1-9-2-14fd499129bf
