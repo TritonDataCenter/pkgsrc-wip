c6d3d92a8ae0d185e8d565eeef73e91a7d9e80b2
phonohawk <phonohawk>
2010-02-17 14:55:48 +0000

    Upstream update to HsOpenSSL-0.8

--- hs-HsOpenSSL/buildlink3.mk
+++ hs-HsOpenSSL/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	hs-HsOpenSSL
 .if !defined(HS_HSOPENSSL_BUILDLINK3_MK)
 HS_HSOPENSSL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HsOpenSSL+=	hs-HsOpenSSL>=0.6.5
-BUILDLINK_ABI_DEPENDS.hs-HsOpenSSL?=	hs-HsOpenSSL>=0.6.5nb1
+BUILDLINK_API_DEPENDS.hs-HsOpenSSL+=	hs-HsOpenSSL>=0.8
 BUILDLINK_PKGSRCDIR.hs-HsOpenSSL?=	../../wip/hs-HsOpenSSL
 
 .include "../../security/openssl/buildlink3.mk"
