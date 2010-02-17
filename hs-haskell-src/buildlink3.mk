d73af6dee516813f638a0c067b200a5c675cbe54
phonohawk <phonohawk>
2010-02-17 15:02:38 +0000

    Added missing dependency to happy

--- hs-haskell-src/buildlink3.mk
+++ hs-haskell-src/buildlink3.mk
@@ -7,6 +7,7 @@ HS_HASKELL_SRC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-haskell-src+=	hs-haskell-src>=1.0.1.3
 BUILDLINK_PKGSRCDIR.hs-haskell-src?=	../../wip/hs-haskell-src
+
 .endif	# HS_HASKELL_SRC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-haskell-src
