eaf762c064a8e4d926f24abed38b4b1077814da4
phonohawk <phonohawk>
2010-02-19 06:44:40 +0000

    Upstream update to binary-strict-0.4.8

--- hs-binary-strict/buildlink3.mk
+++ hs-binary-strict/buildlink3.mk
@@ -5,8 +5,10 @@ BUILDLINK_TREE+=	hs-binary-strict
 .if !defined(HS_BINARY_STRICT_BUILDLINK3_MK)
 HS_BINARY_STRICT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-binary-strict+=	hs-binary-strict>=0.4.5
+BUILDLINK_API_DEPENDS.hs-binary-strict+=	hs-binary-strict>=0.4.8
 BUILDLINK_PKGSRCDIR.hs-binary-strict?=	../../wip/hs-binary-strict
+
+.include "../../wip/hs-mtl/buildlink3.mk"
 .endif	# HS_BINARY_STRICT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-binary-strict
