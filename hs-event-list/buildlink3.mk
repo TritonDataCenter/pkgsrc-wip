1e8966a8be1df9e5432c7630fa3d6da50fe87bcd
phonohawk <phonohawk>
2010-02-19 08:20:55 +0000

    Upstream update to event-list-0.0.10.1

--- hs-event-list/buildlink3.mk
+++ hs-event-list/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-event-list
 .if !defined(HS_EVENT_LIST_BUILDLINK3_MK)
 HS_EVENT_LIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-event-list+=	hs-event-list>=0.0.10
+BUILDLINK_API_DEPENDS.hs-event-list+=	hs-event-list>=0.0.10.1
 BUILDLINK_PKGSRCDIR.hs-event-list?=	../../wip/hs-event-list
 
 .include "../../wip/hs-non-negative/buildlink3.mk"
