64658586af24b5f7b85abf1d950c14308c925192
jihbed <jihbed>
2010-02-24 10:05:59 +0000

    Initial revision

---
+++ cddlib/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	cddlib
+
+.if !defined(CDDLIB_BUILDLINK3_MK)
+CDDLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.cddlib+=	cddlib>=0.94
+BUILDLINK_PKGSRCDIR.cddlib?=	../../wip/cddlib
+
+
+.include "../../devel/gmp/buildlink3.mk"
+.endif	# CDDLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-cddlib
