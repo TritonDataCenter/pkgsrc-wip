0d933088c69e8965818aae120d1f97fc59f40bed
phonohawk <phonohawk>
2010-02-26 10:16:20 +0000

    Initial revision

---
+++ hs-hinstaller/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-hinstaller
+
+.if !defined(HS_HINSTALLER_BUILDLINK3_MK)
+HS_HINSTALLER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hinstaller+=	hs-hinstaller>=2008.2.16
+BUILDLINK_PKGSRCDIR.hs-hinstaller?=	../../wip/hs-hinstaller
+.endif	# HS_HINSTALLER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hinstaller
