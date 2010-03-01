1ee80d9d75cb431afa58dddb121fb10ca9e86a96
helgoman <helgoman>
2010-03-01 20:51:59 +0000

    Initial revision

---
+++ qt4-qscintilla/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	qt4-qscintilla
+
+.if !defined(QT4_QSCINTILLA_BUILDLINK3_MK)
+QT4_QSCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt4-qscintilla+=	qt4-qscintilla>=2.4.2nb1
+BUILDLINK_PKGSRCDIR.qt4-qscintilla?=	../../wip/qt4-qscintilla
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# QT4_QSCINTILLA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt4-qscintilla
