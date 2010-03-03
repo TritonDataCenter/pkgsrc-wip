78b2a781c93af9b895a2715210034bb44c402a71
phonohawk <phonohawk>
2010-03-03 09:51:17 +0000

    Initial revision

---
+++ hs-regions/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-regions
+
+.if !defined(HS_REGIONS_BUILDLINK3_MK)
+HS_REGIONS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-regions+=	hs-regions>=0.4
+BUILDLINK_PKGSRCDIR.hs-regions?=	../../wip/hs-regions
+
+.include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
+.include "../../wip/hs-MonadCatchIO-transformers/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
+.endif	# HS_REGIONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-regions
