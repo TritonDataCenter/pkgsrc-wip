9ef31510c774568102660ac9d66bed6fab558693
phonohawk <phonohawk>
2010-03-03 09:36:48 +0000

    Initial revision

---
+++ hs-base-unicode-symbols/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-base-unicode-symbols
+
+.if !defined(HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK)
+HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-base-unicode-symbols+=	hs-base-unicode-symbols>=0.1.2
+BUILDLINK_PKGSRCDIR.hs-base-unicode-symbols?=	../../wip/hs-base-unicode-symbols
+.endif	# HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-base-unicode-symbols
