9afa3ba614afd99d03300d386f5fea91ae389f1d
tnn2 <tnn2>
2010-03-08 16:54:25 +0000

    Add two patches from martin@ to deal w/ crashes on NetBSD/sparc64. While here, bump snapshot.

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -16,6 +16,6 @@ PATCHDIR=	${.CURDIR}/../../wip/xulrunner/patches
 WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
 
 # snapshot overrides
-DISTNAME=	c15947bf8073
+DISTNAME=	ae59a9d6b719
 MASTER_SITES=	http://hg.mozilla.org/releases/mozilla-1.9.2/archive/
-WRKSRC=		${WRKDIR}/mozilla-1-9-2-c15947bf8073
+WRKSRC=		${WRKDIR}/mozilla-1-9-2-ae59a9d6b719
