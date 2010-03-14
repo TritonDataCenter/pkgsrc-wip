2a4dd4f9a0dfbf9179185a20bffdfe5184a5e3ec
shindenmorr <shindenmorr>
2010-03-14 23:03:55 +0000

    Update to newest snapshot, required to support new GG.

--- libgadu/buildlink3.mk
+++ libgadu/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	libgadu
 .if !defined(LIBGADU_BUILDLINK3_MK)
 LIBGADU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.7.1
-BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.7.1
+BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.9.0
+BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.9.0
 BUILDLINK_PKGSRCDIR.libgadu?=	../../wip/libgadu
 .endif # LIBGADU_BUILDLINK3_MK
 
