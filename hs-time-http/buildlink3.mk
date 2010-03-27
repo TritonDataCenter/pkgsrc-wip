72504ec38535be2584a56f501ad40bd99d8abd95
phonohawk <phonohawk>
2010-03-27 05:17:36 +0000

    Initial revision

---
+++ hs-time-http/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-time-http
+
+.if !defined(HS_TIME_HTTP_BUILDLINK3_MK)
+HS_TIME_HTTP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-time-http+=	hs-time-http>=0.1
+BUILDLINK_PKGSRCDIR.hs-time-http?=	../../wip/hs-time-http
+
+.include "../../wip/hs-parsec/buildlink3.mk"
+.endif	# HS_TIME_HTTP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-time-http
