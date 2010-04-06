1d7bc7214565f25eb34396640017f26fa17fe62c
obache <obache>
2010-04-06 11:50:37 +0000

    Update ibus to 1.3.1.

--- ibus/buildlink3.mk
+++ ibus/buildlink3.mk
@@ -7,6 +7,7 @@ BUILDLINK_TREE+=	ibus
 IBUS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ibus+=	ibus>=1.2.0.20091024
+BUILDLINK_ABI_DEPENDS.ibus?=	ibus>=1.3.1
 BUILDLINK_PKGSRCDIR.ibus?=	../../wip/ibus
 
 .include "../../devel/gettext-lib/buildlink3.mk"
