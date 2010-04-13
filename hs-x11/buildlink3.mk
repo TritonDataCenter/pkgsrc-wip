d084d1b284d0bb241174aa74770c5933f86ae074
emil_s <emil_s>
2010-04-13 17:58:46 +0000

    Update hs-x11 1.4.5 -> 1.5.0.0 Simplifiy makefile (use mk/haskell.mk) and update maintainer email.

--- hs-x11/buildlink3.mk
+++ hs-x11/buildlink3.mk
@@ -6,11 +6,9 @@ BUILDLINK_TREE+=	hs-x11
 HS_X11_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.hs-x11?=	build
-BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.4.5
-BUILDLINK_ABI_DEPENDS.hs-x11?=	hs-x11>=1.4.5
+BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.5.0.0
 BUILDLINK_PKGSRCDIR.hs-x11?=	../../wip/hs-x11
 
-.include "../../lang/ghc/buildlink3.mk"
 .endif # HS_X11_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-x11
