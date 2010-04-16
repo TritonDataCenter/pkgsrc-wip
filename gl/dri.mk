adf5a3e5f13f8b05c2fa11122f0b6054dcc6a9c1
tnn2 <tnn2>
2010-04-16 14:36:10 +0000

    fix libdrm reference

--- gl/dri.mk
+++ gl/dri.mk
@@ -1,12 +1,11 @@
 # $NetBSD$
 
-BUILDLINK_API_DEPENDS.libdrm+= libdrm>=2.4.15
 .  include "../../textproc/expat/buildlink3.mk"
 .  include "../../x11/dri2proto/buildlink3.mk"
 .  include "../../x11/glproto/buildlink3.mk"
 .  include "../../x11/libXdamage/buildlink3.mk"
 .  include "../../x11/libXfixes/buildlink3.mk"
-.  include "../../wip/libdrm/buildlink3.mk"
+.  include "../../x11/libdrm/buildlink3.mk"
 .  include "../../x11/libXxf86vm/buildlink3.mk"
 .  include "../../x11/xf86driproto/buildlink3.mk"
 .  include "../../x11/xf86vidmodeproto/buildlink3.mk"
