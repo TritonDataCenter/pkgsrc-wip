46c7970384dc41dea6f5abc0852854c9eb3f45c1
schaecsn <schaecsn>
2010-04-18 19:22:45 +0000

    Hack to compile pstotext with -O on gcc-4.x.

---
+++ pstotext/hacks.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+
+.if !defined(PSTOTEXT_HACKS_MK)
+PSTOTEXT_HACKS_MK=   defined
+
+.include "../../mk/compiler.mk"
+
+###
+### With debian's security patch patch-ab, pstotext miscompiles under several
+### versions of gcc-4.x when -O2 is used (verified under linux and freebsd).
+### For the time being, pstotext will be optimized on every platform with -O1
+### when a version of gcc-4 is used.
+###
+.if !empty(CC_VERSION:Mgcc-4.*)
+PKG_HACKS+=             optimisation
+BUILD_MAKE_FLAGS+=	CFLAGS=-O
+.endif
+
+.endif  # PSTOTEXT_HACKS_MK
