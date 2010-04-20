a0a05fb911d4362dacb90196cc41ac6f80dc8a90
phonohawk <phonohawk>
2010-04-20 03:39:04 +0000

    Initial revision

---
+++ hs-bindings-DSL/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-bindings-DSL
+
+.if !defined(HS_BINDINGS_DSL_BUILDLINK3_MK)
+HS_BINDINGS_DSL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-bindings-DSL+=	hs-bindings-DSL>=1.0
+BUILDLINK_PKGSRCDIR.hs-bindings-DSL?=	../../wip/hs-bindings-DSL
+.endif	# HS_BINDINGS_DSL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-bindings-DSL
