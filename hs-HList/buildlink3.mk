a235f112a8364bc4c603844017e92c71fafd3254
phonohawk <phonohawk>
2010-04-21 03:29:41 +0000

    Initial revision

---
+++ hs-HList/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-HList
+
+.if !defined(HS_HLIST_BUILDLINK3_MK)
+HS_HLIST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-HList+=	hs-HList>=0.2
+BUILDLINK_PKGSRCDIR.hs-HList?=	../../wip/hs-HList
+.endif	# HS_HLIST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-HList
