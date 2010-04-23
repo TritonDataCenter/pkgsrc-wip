ed096598b885e99c564e5a6536c7fe5b3c13c8d7
phonohawk <phonohawk>
2010-04-23 06:34:21 +0000

    Initial revision

---
+++ hs-tfp/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-tfp
+
+.if !defined(HS_TFP_BUILDLINK3_MK)
+HS_TFP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tfp+=	hs-tfp>=0.2
+BUILDLINK_PKGSRCDIR.hs-tfp?=	../../wip/hs-tfp
+.endif	# HS_TFP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tfp
