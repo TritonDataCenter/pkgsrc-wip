af6b3e7004bfd9bb976d1e546d39a2af42b99054
shindenmorr <shindenmorr>
2010-04-26 15:38:29 +0000

    correct buildlink3.mk file

--- libdshconfig/buildlink3.mk
+++ libdshconfig/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	libdshconfig
 .if !defined(LIBDSHCONFIG_BUILDLINK3_MK)
 LIBDSHCONFIG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdshconfig+=	libdshconfig>=0.20.9
-BUILDLINK_DEPMETHOD.libdshconfig?=	build
+BUILDLINK_API_DEPENDS.libdshconfig+=	libdshconfig>=0.20.13
 BUILDLINK_PKGSRCDIR.libdshconfig?=	../../wip/libdshconfig
-.endif # LIBDSHCONFIG_BUILDLINK3_MK
+.endif	# LIBDSHCONFIG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdshconfig
