c58d4aa6b480c2094e6aaf8d573ad022803f7c26
thomasklausner <thomasklausner>
2010-05-01 23:05:33 +0000

    Initial revision

---
+++ talloc/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	talloc
+
+.if !defined(TALLOC_BUILDLINK3_MK)
+TALLOC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.talloc+=	talloc>=2.0.1
+BUILDLINK_PKGSRCDIR.talloc?=	../../wip/talloc
+.endif	# TALLOC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-talloc
