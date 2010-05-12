9f910941f38ae9a5ff16774ebcef7baa1609bc22
phonohawk <phonohawk>
2010-05-12 07:47:14 +0000

    Upstream update to HDBC-2.2.6

--- hs-HDBC/buildlink3.mk
+++ hs-HDBC/buildlink3.mk
@@ -5,11 +5,12 @@ BUILDLINK_TREE+=	hs-HDBC
 .if !defined(HS_HDBC_BUILDLINK3_MK)
 HS_HDBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HDBC+=	hs-HDBC>=2.1.1
+BUILDLINK_API_DEPENDS.hs-HDBC+=	hs-HDBC>=2.2
 BUILDLINK_PKGSRCDIR.hs-HDBC?=	../../wip/hs-HDBC
 
 .include "../../wip/hs-convertible/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-utf8-string/buildlink3.mk"
 .endif	# HS_HDBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-HDBC
