4ee68bdf28a2b05c5862e2f30b0ebd86f2370895
obache <obache>
2010-05-20 05:14:46 +0000

    Initial revision

---
+++ libdlna/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+#
+
+BUILDLINK_TREE+=	libdlna
+
+.if !defined(LIBDLNA_BUILDLINK3_MK)
+LIBDLNA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libdlna+=	libdlna>=0.2.3
+BUILDLINK_PKGSRCDIR.libdlna?=	../../wip/libdlna
+
+#.include "../../multimedia/ffmpeg/buildlink3.mk"
+.endif	# LIBDLNA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libdlna
