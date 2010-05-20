d2afd51680b24a43cf6ea3f6b1683a72b4323acd
obache <obache>
2010-05-20 05:16:08 +0000

    need to buildlink ffmpeg.

--- libdlna/buildlink3.mk
+++ libdlna/buildlink3.mk
@@ -9,7 +9,7 @@ LIBDLNA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libdlna+=	libdlna>=0.2.3
 BUILDLINK_PKGSRCDIR.libdlna?=	../../wip/libdlna
 
-#.include "../../multimedia/ffmpeg/buildlink3.mk"
+.include "../../multimedia/ffmpeg/buildlink3.mk"
 .endif	# LIBDLNA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdlna
