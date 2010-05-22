0b7d71b1a23b9e4a08f853d932e36bc8d9126233
jihbed <jihbed>
2010-05-22 23:02:15 +0000

    Initial revision

---
+++ mopac7/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libmopac7
+
+.if !defined(LIBMOPAC7_BUILDLINK3_MK)
+LIBMOPAC7_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libmopac7+=	libmopac7>=1.11
+BUILDLINK_PKGSRCDIR.libmopac7?=	../../wip/mopac7
+.endif	# LIBMOPAC7_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libmopac7
