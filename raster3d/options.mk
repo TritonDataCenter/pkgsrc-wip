f8b2a7c8bf015986b59dfa0342f3e91f2e1393e1
obache <obache>
2010-06-01 08:14:50 +0000

    Update to 2.7s and some fixes.

--- raster3d/options.mk
+++ raster3d/options.mk
@@ -1,7 +1,15 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.raster3d
-PKG_SUPPORTED_OPTIONS=	magick
-PKG_SUGGESTED_OPTIONS=	magick
+PKG_SUPPORTED_OPTIONS=	imagemagick
+PKG_SUGGESTED_OPTIONS=	imagemagick
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	magick
+
+.if !empty(PKG_OPTIONS:Mimagemagick)
+DEPENDS+=	ImageMagick>=5.5.4:../../graphics/ImageMagick
+PLIST.magick=	yes
+CFLAGS+=	-DIMAGEPIPE
+.endif
