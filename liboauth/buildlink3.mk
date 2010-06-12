606c5abbef17769d52664d273ea458bae5b5f179
jihbed <jihbed>
2010-06-12 23:15:38 +0000

    Initial revision

---
+++ liboauth/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	liboauth
+
+.if !defined(LIBOAUTH_BUILDLINK3_MK)
+LIBOAUTH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.liboauth+=	liboauth>=0.8.8
+BUILDLINK_PKGSRCDIR.liboauth?=	../../wip/liboauth
+
+
+.include "../../devel/nss/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.endif	# LIBOAUTH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-liboauth
