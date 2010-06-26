c70ba70e0a9cdae547afd77a664ee9d3da7f1a40
ryo-on <ryo-on>
2010-06-26 00:30:36 +0000

    Remove inappropriate PKGNAME packages.

--- libtorrent-rasterbar/buildlink3.mk
+++ libtorrent-rasterbar/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libtorrent-rasterbar
 .if !defined(LIBTORRENT_RASTERBAR_BUILDLINK3_MK)
 LIBTORRENT_RASTERBAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=0.15.0
+BUILDLINK_API_DEPENDS.libtorrent-rasterbar+=	libtorrent-rasterbar>=0.14.10
 BUILDLINK_PKGSRCDIR.libtorrent-rasterbar?=	../../wip/libtorrent-rasterbar
 
 .endif	# LIBTORRENT_RASTERBAR_BUILDLINK3_MK
