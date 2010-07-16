62bcc0731b00c05b1cc4116ac2a9fb3bb16717ac
jihbed <jihbed>
2010-07-16 19:17:10 +0000

    Initial revision

---
+++ py-repositoryhandler/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	py26-repositoryhandler
+
+.if !defined(PY26_REPOSITORYHANDLER_BUILDLINK3_MK)
+PY26_REPOSITORYHANDLER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py26-repositoryhandler+=	py26-repositoryhandler>=0.3
+BUILDLINK_PKGSRCDIR.py26-repositoryhandler?=	../../wip/py-repositoryhandler
+
+.include "../../devel/subversion-base/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.endif	# PY26_REPOSITORYHANDLER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py26-repositoryhandler
