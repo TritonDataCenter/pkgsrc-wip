323a0ff6751b650e7645fdd1bbc68cfdab95efa0
jihbed <jihbed>
2010-08-09 21:36:25 +0000

    Initial revision

---
+++ py-chaintipy/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR =	PKG_OPTIONS.chiantipy
+PKG_SUPPORTED_OPTIONS=	wx qt
+PKG_SUGGESTED_OPTIONS=	wx
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
+
+.if !empty(PKG_OPTIONS:Mqt)
+.  include "../../x11/py-qt4/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mwx)
+.  include "../../x11/py-wxWidgets/buildlink3.mk"
+.endif
