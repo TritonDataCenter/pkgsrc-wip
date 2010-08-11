8b8c00ef8049472b303b0af12fa0bfce84f54512
jihbed <jihbed>
2010-08-11 10:41:28 +0000

    Initial revision

---
+++ gtkglextmm/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	gtkglextmm
+
+.if !defined(GTKGLEXTMM_BUILDLINK3_MK)
+GTKGLEXTMM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtkglextmm+=	gtkglextmm>=1.2.0
+BUILDLINK_PKGSRCDIR.gtkglextmm?=	../../category/gtkglextmm
+
+.include "../../graphics/gtkglext/buildlink3.mk"
+.include "../../x11/gtkmm/buildlink3.mk"
+.endif	# GTKGLEXTMM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtkglextmm
