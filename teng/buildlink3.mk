c8a12b26b004087f8a03e6fe890e84e157e9f074
ondratu <ondratu>
2010-08-30 19:21:32 +0000

    Initial revision

---
+++ teng/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	teng
+
+.if !defined(TENG_BUILDLINK3_MK)
+TENG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.teng+=	teng>=2.1.0
+BUILDLINK_PKGSRCDIR.teng?=	../../wip/teng
+.endif	# TENG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-teng
