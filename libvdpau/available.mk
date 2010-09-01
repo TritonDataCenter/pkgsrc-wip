784d8440f114d0a7a0f8f31ec1d3e38a64661593
cheusov <cheusov>
2010-09-01 20:03:33 +0000

    Mark the package as available only under FreeBSD, Linux and Solaris.

--- libvdpau/available.mk
+++ libvdpau/available.mk
@@ -6,7 +6,8 @@
 # [Open]Solaris.  The following condition is here to NOT list these
 # platforms in multiple places, i.e. in mplayer, xine, xbmc etc.
 # Have a look at buildlink3.mk too.
-.if (${OPSYS} == "Linux" || ${OPSYS} == "FreeBSD" || ${OPSYS} == "SunOS")
+VDPAU_AVAILABLE_ON=	Linux FreeBSD SunOS
+.if !empty(VDPAU_AVAILABLE_ON:M${OPSYS})
 VDPAU_AVAILABLE=	yes
 .else
 VDPAU_AVAILABLE=	no
