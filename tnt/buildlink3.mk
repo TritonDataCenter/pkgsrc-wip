e8d7380e1c1b0b8f873041595698e3573d151a18
jihbed <jihbed>
2010-09-02 11:56:15 +0000

    Initial revision

---
+++ tnt/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	tnt
+
+.if !defined(TNT_BUILDLINK3_MK)
+TNT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.tnt+=	tnt>=3.0.12
+BUILDLINK_PKGSRCDIR.tnt?=	../../wip/tnt
+.endif	# TNT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-tnt
