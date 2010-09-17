1efbf1b264c44e65cf75aa00e9234cf50d1433e9
obache <obache>
2010-09-17 09:09:11 +0000

    catch up again.

---
+++ python27/dist.mk
@@ -0,0 +1,8 @@
+# $NetBSD$
+
+PY_DISTVERSION=	2.7
+DISTNAME=	Python-${PY_DISTVERSION}
+EXTRACT_SUFX=	.tar.bz2
+DISTINFO_FILE=	${.CURDIR}/../../wip/python27/distinfo
+PATCHDIR=	${.CURDIR}/../../wip/python27/patches
+MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
