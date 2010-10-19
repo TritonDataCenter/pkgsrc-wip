b4f5d99c51320d2d847756f48184ba9a60be773d
othyro <othyro>
2010-10-19 21:52:31 +0000

    This will be used shortly.

---
+++ tkpng/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	tkpng
+
+.if !defined(TKPNG_BUILDLINK3_MK)
+TKPNG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.tkpng+=	tkpng>=0.9
+BUILDLINK_PKGSRCDIR.tkpng?=	../../wip/tkpng
+.endif # TKPNG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-tkpng
