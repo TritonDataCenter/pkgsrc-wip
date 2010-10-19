7c863c3badacfaee3fe1226dd2b009b967401f7e
othyro <othyro>
2010-10-19 21:55:16 +0000

    Make sure wip/tk85 gets pulled in.

--- tkpng/buildlink3.mk
+++ tkpng/buildlink3.mk
@@ -7,6 +7,8 @@ TKPNG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tkpng+=	tkpng>=0.9
 BUILDLINK_PKGSRCDIR.tkpng?=	../../wip/tkpng
+
+.include "../../wip/tk85/buildlink3.mk"
 .endif # TKPNG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-tkpng
