3d8eb7be88002675d752e29c5120a22ea81c297d
mjoellnir <mjoellnir>
2010-10-28 20:29:25 +0000

    libpar2 is the library to archivers/par2. It is not included in pkgsrc so far, nor does installing archivers/par2 create it as a side-effect.

---
+++ libpar2/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libpar2
+
+.if !defined(LIBPAR2_BUILDLINK3_MK)
+LIBPAR2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libpar2+=	libpar2>=0.2
+BUILDLINK_PKGSRCDIR.libpar2?=	../../wip/libpar2
+
+BUILDLINK_INCDIRS.libpar2= include/libpar2
+BUILDLINK_LIBDIRS.libpar2= lib/libpar2
+
+.include "../../devel/libsigc++/buildlink3.mk"
+.endif	# LIBPAR2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libpar2
