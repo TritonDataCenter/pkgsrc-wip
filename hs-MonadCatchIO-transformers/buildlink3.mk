ba6f4bd80d3b5e053bf339276d22523245adf422
phonohawk <phonohawk>
2010-11-02 16:50:07 +0000

    Upstream update to MonadCatchIO-transformers-0.2.2.0

--- hs-MonadCatchIO-transformers/buildlink3.mk
+++ hs-MonadCatchIO-transformers/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-MonadCatchIO-transformers
 .if !defined(HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK)
 HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.0.2.0
+BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.2.2.0
 BUILDLINK_PKGSRCDIR.hs-MonadCatchIO-transformers?=	../../wip/hs-MonadCatchIO-transformers
 
 .include "../../wip/hs-transformers/buildlink3.mk"
