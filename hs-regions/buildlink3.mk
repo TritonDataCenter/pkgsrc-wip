3fc1b601723b12ba3b4a3f7538cf1f1b2f016016
phonohawk <phonohawk>
2010-11-02 17:00:10 +0000

    Upstream update to regions-0.7.0.1

--- hs-regions/buildlink3.mk
+++ hs-regions/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-regions
 .if !defined(HS_REGIONS_BUILDLINK3_MK)
 HS_REGIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regions+=	hs-regions>=0.4
+BUILDLINK_API_DEPENDS.hs-regions+=	hs-regions>=0.7.0.1
 BUILDLINK_PKGSRCDIR.hs-regions?=	../../wip/hs-regions
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
