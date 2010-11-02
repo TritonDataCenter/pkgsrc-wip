58cd915717889b119d9fd6c76a4235149b8421a0
silasdb <silasdb>
2010-11-02 13:24:38 +0000

    Initial revision.
    Open CASCADE Technology is a software development platform freely available in
    open source. It includes components for 3D surface and solid modeling,
    visualization, data exchange and rapid application development.

---
+++ opencascade/options.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.opencascade
+PKG_SUPPORTED_OPTIONS=	x11 debug
+PKG_SUGGESTED_OPTIONS=	x11
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		x11
+
+.if !empty(PKG_OPTIONS:Mx11)
+CONFIGURE_ARGS+=	--with-x
+PLIST.x11=		yes
+.include "../../graphics/Mesa/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+INSTALL_UNSTRIPPED=	yes
+.endif
