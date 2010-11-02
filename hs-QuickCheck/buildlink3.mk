8c9662f5e99078e3a2c7c18651e0545ab31f042c
phonohawk <phonohawk>
2010-11-02 16:02:15 +0000

    Upstream update to QuickCheck-2.3.0.2

--- hs-QuickCheck/buildlink3.mk
+++ hs-QuickCheck/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-QuickCheck
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.1.0.2nb1
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.3.0.2
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../wip/hs-QuickCheck
 
 .include "../../wip/hs-mtl/buildlink3.mk"
