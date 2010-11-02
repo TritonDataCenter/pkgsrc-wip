4226a95b6f53aa6496c7367c03ed316dd1969853
phonohawk <phonohawk>
2010-11-02 16:07:11 +0000

    Upstream update to non-negative-0.1

--- hs-non-negative/buildlink3.mk
+++ hs-non-negative/buildlink3.mk
@@ -5,10 +5,11 @@ BUILDLINK_TREE+=	hs-non-negative
 .if !defined(HS_NON_NEGATIVE_BUILDLINK3_MK)
 HS_NON_NEGATIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-non-negative+=	hs-non-negative>=0.0.5
+BUILDLINK_API_DEPENDS.hs-non-negative+=	hs-non-negative>=0.1
 BUILDLINK_PKGSRCDIR.hs-non-negative?=	../../wip/hs-non-negative
 
-.include "../../wip/hs-QuickCheck1/buildlink3.mk"
+.include "../../wip/hs-QuickCheck/buildlink3.mk"
+.include "../../wip/hs-utility-ht/buildlink3.mk"
 .endif	# HS_NON_NEGATIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-non-negative
