1d104e4bd95292a2933972341b893a4b3344338c
phonohawk <phonohawk>
2010-11-02 16:44:04 +0000

    Initial revision

---
+++ hs-explicit-iomodes/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-explicit-iomodes
+
+.if !defined(HS_EXPLICIT_IOMODES_BUILDLINK3_MK)
+HS_EXPLICIT_IOMODES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-explicit-iomodes+=	hs-explicit-iomodes>=0.5
+BUILDLINK_PKGSRCDIR.hs-explicit-iomodes?=	../../wip/hs-explicit-iomodes
+
+.include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
+.include "../../wip/hs-tagged/buildlink3.mk"
+.endif	# HS_EXPLICIT_IOMODES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-explicit-iomodes
---
+++ hs-tagged/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-tagged
+
+.if !defined(HS_TAGGED_BUILDLINK3_MK)
+HS_TAGGED_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.1.1
+BUILDLINK_PKGSRCDIR.hs-tagged?=	../../wip/hs-tagged
+
+.include "../../wip/hs-data-default/buildlink3.mk"
+.endif	# HS_TAGGED_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tagged
