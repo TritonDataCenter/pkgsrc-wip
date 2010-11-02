2a6e918670ae6559ba330488095c2bd8108d4bc1
phonohawk <phonohawk>
2010-11-02 16:28:59 +0000

    Upstream update to base-unicode-symbols-0.2.1.1

--- hs-base-unicode-symbols/buildlink3.mk
+++ hs-base-unicode-symbols/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-base-unicode-symbols
 .if !defined(HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK)
 HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-unicode-symbols+=	hs-base-unicode-symbols>=0.1.2
+BUILDLINK_API_DEPENDS.hs-base-unicode-symbols+=	hs-base-unicode-symbols>=0.2.1.1
 BUILDLINK_PKGSRCDIR.hs-base-unicode-symbols?=	../../wip/hs-base-unicode-symbols
 .endif	# HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK
 
