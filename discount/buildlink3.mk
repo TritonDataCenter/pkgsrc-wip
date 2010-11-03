705acfc5eead180dd572e3492db79003b8d1edfc
zecrazytux <zecrazytux>
2010-11-03 21:54:20 +0000

    Added a buildlink3.mk file to discount (which provides a library, libmarkdown)

---
+++ discount/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	discount
+
+.if !defined(DISCOUNT_BUILDLINK3_MK)
+DISCOUNT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.discount+=	discount>=2.0.1
+BUILDLINK_PKGSRCDIR.discount?=	../../wip/discount
+.endif	# DISCOUNT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-discount
