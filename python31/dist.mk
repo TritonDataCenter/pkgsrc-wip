44ed3ceb67fabf2ed2745a29f7fa659a92e1a86b
obache <obache>
2010-11-15 12:08:16 +0000

    Change styles to same as other python packages.

---
+++ python31/dist.mk
@@ -0,0 +1,8 @@
+# $NetBSD$
+
+PY_DISTVERSION=	3.1.1
+DISTNAME=	Python-${PY_DISTVERSION}
+EXTRACT_SUFX=	.tar.bz2
+DISTINFO_FILE=	${.CURDIR}/../../wip/python31/distinfo
+PATCHDIR=	${.CURDIR}/../../wip/python31/patches
+MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
