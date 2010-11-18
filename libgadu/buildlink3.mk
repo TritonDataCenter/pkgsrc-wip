26d54b5d1137e86e007cad6675ff503c015fd95b
shindenmorr <shindenmorr>
2010-11-18 14:57:06 +0000

    update buildlink3.mk with rest of package

--- libgadu/buildlink3.mk
+++ libgadu/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	libgadu
 .if !defined(LIBGADU_BUILDLINK3_MK)
 LIBGADU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.9.0
-BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.9.0
+BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.9.0rc2
+BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.9.0rc2
 BUILDLINK_PKGSRCDIR.libgadu?=	../../wip/libgadu
 .endif # LIBGADU_BUILDLINK3_MK
 
