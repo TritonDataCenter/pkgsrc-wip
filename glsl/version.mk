1ccfea3ea595c4c30690c92e144b8400d272236a
pallegra <pallegra>
2010-11-22 23:39:34 +0000

    Initial revision

---
+++ glsl/version.mk
@@ -0,0 +1,9 @@
+# $NetBSD$
+# used by wip/gl/Makefile.common
+# used by wip/MesaLib/Makefile
+
+MESA_DISTNAME=		MesaLib-${MESA_VERSION}
+MESA_SITES=		ftp://freedesktop.org/pub/mesa/${MESA_VERSION}/
+MESA_VERSION=		7.9
+MESA_DEMOS_VERSION=	7.8.2
+MESA_PKGVERSION=	${MESA_VERSION}
