def01ef2d6e1bfaf1adc58d65efc68c170f44586
obache <obache>
2010-11-28 13:04:22 +0000

    Update to Python-2.7.1 release.

--- python27/dist.mk
+++ python27/dist.mk
@@ -1,8 +1,8 @@
 # $NetBSD$
 
-PY_DISTVERSION=	2.7.1rc1
+PY_DISTVERSION=	2.7.1
 DISTNAME=	Python-${PY_DISTVERSION}
 EXTRACT_SUFX=	.tar.bz2
 DISTINFO_FILE=	${.CURDIR}/../../wip/python27/distinfo
 PATCHDIR=	${.CURDIR}/../../wip/python27/patches
-MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION:C/rc[0-9]$//}/
+MASTER_SITES=	http://www.python.org/ftp/python/${PY_DISTVERSION}/
