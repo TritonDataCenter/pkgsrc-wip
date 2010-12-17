8ae86d80156687b66fc4b09c0f4394d4f2e11b47
asau <asau>
2010-12-17 03:06:42 +0000

    Use external OpenPA.

--- mpi-ch/buildlink3.mk
+++ mpi-ch/buildlink3.mk
@@ -8,7 +8,7 @@ MPICH2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mpich2+=	mpich2>=1.2.99.6307
 BUILDLINK_PKGSRCDIR.mpich2?=	../../wip/mpi-ch
 
-#.include "../../parallel/openpa/buildlink3.mk"
+.include "../../parallel/openpa/buildlink3.mk"
 .endif	# MPICH2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mpich2
