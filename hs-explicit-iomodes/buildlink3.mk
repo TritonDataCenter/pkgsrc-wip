6423421e9d95994248ccf563b7374a77562c1ab6
phonohawk <phonohawk>
2010-12-30 08:39:10 +0000

    Upstream update to explicit-iomodes-0.6.0.1

--- hs-explicit-iomodes/buildlink3.mk
+++ hs-explicit-iomodes/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-explicit-iomodes
 .if !defined(HS_EXPLICIT_IOMODES_BUILDLINK3_MK)
 HS_EXPLICIT_IOMODES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-explicit-iomodes+=	hs-explicit-iomodes>=0.5
+BUILDLINK_API_DEPENDS.hs-explicit-iomodes+=	hs-explicit-iomodes>=0.6
 BUILDLINK_PKGSRCDIR.hs-explicit-iomodes?=	../../wip/hs-explicit-iomodes
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
