9c1844e59ce4feae5d3da82327a6963596f8ba2f
phonohawk <phonohawk>
2010-12-30 07:55:18 +0000

    Upstream update to network-2.3

--- hs-network/buildlink3.mk
+++ hs-network/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-network
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.2.1.2
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.3
 BUILDLINK_PKGSRCDIR.hs-network?=	../../wip/hs-network
 
 .include "../../wip/hs-parsec/buildlink3.mk"
