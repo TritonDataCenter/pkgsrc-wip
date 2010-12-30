776a6fbaf3e31209d29ed61f2f5f56de1640f073
phonohawk <phonohawk>
2010-12-30 09:30:43 +0000

    Initial revision

---
+++ hs-bindings-EsounD/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-bindings-EsounD
+
+.if !defined(HS_BINDINGS_ESOUND_BUILDLINK3_MK)
+HS_BINDINGS_ESOUND_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-bindings-EsounD+=	hs-bindings-EsounD>=0.1
+BUILDLINK_PKGSRCDIR.hs-bindings-EsounD?=	../../wip/hs-bindings-EsounD
+
+.include "../../audio/esound/buildlink3.mk"
+.include "../../wip/hs-bindings-DSL/buildlink3.mk"
+.include "../../wip/hs-bindings-audiofile/buildlink3.mk"
+.endif	# HS_BINDINGS_ESOUND_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-bindings-EsounD
