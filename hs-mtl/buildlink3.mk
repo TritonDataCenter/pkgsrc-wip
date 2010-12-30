971aa082d7d6bc1b277e820b1ac1573da04ac4f9
phonohawk <phonohawk>
2010-12-30 06:54:20 +0000

    Upstream update to mtl-2.0.1.0

--- hs-mtl/buildlink3.mk
+++ hs-mtl/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-mtl
 .if !defined(HS_MTL_BUILDLINK3_MK)
 HS_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=1.1.0.2nb1
+BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=2.0
 BUILDLINK_PKGSRCDIR.hs-mtl?=	../../wip/hs-mtl
 .endif # HS_MTL_BUILDLINK3_MK
 
