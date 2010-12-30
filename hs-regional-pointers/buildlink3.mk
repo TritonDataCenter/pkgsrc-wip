afbc44df830b830090368ae74c67ce337d61a7e8
phonohawk <phonohawk>
2010-12-30 08:49:24 +0000

    Upstream update to regional-pointers-0.5

--- hs-regional-pointers/buildlink3.mk
+++ hs-regional-pointers/buildlink3.mk
@@ -5,11 +5,11 @@ BUILDLINK_TREE+=	hs-regional-pointers
 .if !defined(HS_REGIONAL_POINTERS_BUILDLINK3_MK)
 HS_REGIONAL_POINTERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regional-pointers+=	hs-regional-pointers>=0.4
+BUILDLINK_API_DEPENDS.hs-regional-pointers+=	hs-regional-pointers>=0.5
 BUILDLINK_PKGSRCDIR.hs-regional-pointers?=	../../wip/hs-regional-pointers
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
-.include "../../wip/hs-MonadCatchIO-transformers/buildlink3.mk"
+.include "../../wip/hs-monad-peel/buildlink3.mk"
 .include "../../wip/hs-regions/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_REGIONAL_POINTERS_BUILDLINK3_MK
