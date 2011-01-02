f0be0b3752de4cef640642090c57c7e6f8294c55
helgoman <helgoman>
2011-01-02 21:47:10 +0000

    update to 0.22.0

--- pylint/buildlink3.mk
+++ pylint/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	pylint
 .if !defined(PYLINT_BUILDLINK3_MK)
 PYLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pylint+=	pylint>=0.21.0nb1
+BUILDLINK_API_DEPENDS.pylint+=	pylint>=0.22.0
 BUILDLINK_PKGSRCDIR.pylint?=	../../wip/pylint
 .endif	# PYLINT_BUILDLINK3_MK
 
