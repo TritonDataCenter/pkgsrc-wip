34582c30b6f1fe836107d6572d2d5ddb2271d5dd
phonohawk <phonohawk>
2011-01-06 15:30:33 +0000

    Initial revision

---
+++ hs-haskell-lexer/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-haskell-lexer
+
+.if !defined(HS_HASKELL_LEXER_BUILDLINK3_MK)
+HS_HASKELL_LEXER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-haskell-lexer+=	hs-haskell-lexer>=1.0
+BUILDLINK_PKGSRCDIR.hs-haskell-lexer?=	../../wip/hs-haskell-lexer
+.endif	# HS_HASKELL_LEXER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-haskell-lexer
