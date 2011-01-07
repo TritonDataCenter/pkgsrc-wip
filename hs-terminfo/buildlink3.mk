ee823f8304723247745979d265b06b609e8509e9
phonohawk <phonohawk>
2011-01-07 09:48:12 +0000

    Upstream update to terminfo-0.3.1.3

--- hs-terminfo/buildlink3.mk
+++ hs-terminfo/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-terminfo
 .if !defined(HS_TERMINFO_BUILDLINK3_MK)
 HS_TERMINFO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-terminfo+=	hs-terminfo>=0.3.1.1
+BUILDLINK_API_DEPENDS.hs-terminfo+=	hs-terminfo>=0.3
 BUILDLINK_PKGSRCDIR.hs-terminfo?=	../../wip/hs-terminfo
 
 .include "../../devel/ncurses/buildlink3.mk"
