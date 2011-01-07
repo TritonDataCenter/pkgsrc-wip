f439b0cb5e212d8e77a8d887132c575defeea2d0
phonohawk <phonohawk>
2011-01-07 10:11:58 +0000

    Upstream update to regex-base-0.93.2

--- hs-regex-base/buildlink3.mk
+++ hs-regex-base/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-regex-base
 .if !defined(HS_REGEX_BASE_BUILDLINK3_MK)
 HS_REGEX_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.93.1nb1
+BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.93
 BUILDLINK_PKGSRCDIR.hs-regex-base?=	../../wip/hs-regex-base
 
 .include "../../wip/hs-mtl/buildlink3.mk"
