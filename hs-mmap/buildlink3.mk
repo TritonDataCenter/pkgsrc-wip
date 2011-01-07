7eb443594658d578feb0eb72b71e38fee1c4dcb0
phonohawk <phonohawk>
2011-01-07 09:31:57 +0000

    Upstream update to mmap-0.5.7

--- hs-mmap/buildlink3.mk
+++ hs-mmap/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-mmap
 .if !defined(HS_MMAP_BUILDLINK3_MK)
 HS_MMAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmap+=	hs-mmap>=0.5.4
+BUILDLINK_API_DEPENDS.hs-mmap+=	hs-mmap>=0.5
 BUILDLINK_PKGSRCDIR.hs-mmap?=	../../wip/hs-mmap
 .endif	# HS_MMAP_BUILDLINK3_MK
 
