5f1fbfe41118b8f25fbc900d7f733a5e6e52dd46
phonohawk <phonohawk>
2011-01-07 10:08:05 +0000

    fix buildlink3.mk

--- hs-HTTP/buildlink3.mk
+++ hs-HTTP/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-HTTP
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.0.8
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.1
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../wip/hs-HTTP
 
 .include "../../wip/hs-mtl/buildlink3.mk"
