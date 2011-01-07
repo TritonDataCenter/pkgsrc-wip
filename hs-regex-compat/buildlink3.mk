242b5fdbb02d2718c98c038c311f051254a668f3
phonohawk <phonohawk>
2011-01-07 10:15:58 +0000

    Upstream update to regex-compat-0.93.1

--- hs-regex-compat/buildlink3.mk
+++ hs-regex-compat/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-regex-compat
 .if !defined(HS_REGEX_COMPAT_BUILDLINK3_MK)
 HS_REGEX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regex-compat+=	hs-regex-compat>=0.92nb1
+BUILDLINK_API_DEPENDS.hs-regex-compat+=	hs-regex-compat>=0.93
 BUILDLINK_PKGSRCDIR.hs-regex-compat?=	../../wip/hs-regex-compat
 
 .include "../../wip/hs-regex-base/buildlink3.mk"
