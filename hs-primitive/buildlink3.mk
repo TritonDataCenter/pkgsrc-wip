21e2bbe89e9ed7e6d0a3678fbf49579fd2738911
phonohawk <phonohawk>
2011-01-08 04:42:00 +0000

    Initial revision

---
+++ hs-primitive/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-primitive
+
+.if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
+HS_PRIMITIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.3
+BUILDLINK_PKGSRCDIR.hs-primitive?=	../../wip/hs-primitive
+.endif	# HS_PRIMITIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-primitive
