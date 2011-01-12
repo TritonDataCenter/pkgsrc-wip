4d3314f0c7505bacc7c8c838c7d1b72a86abb13e
thomasklausner <thomasklausner>
2011-01-12 10:05:46 +0000

    Fix WRKSRC to use PKGNAME_NOREV; put option handling completely into options.mk; remove unnecessary inclusion of bsd.prefs.mk.

--- fop/options.mk
+++ fop/options.mk
@@ -4,3 +4,14 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.fop
 PKG_SUPPORTED_OPTIONS=	fop-docs
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mfop-docs)
+PLIST_SRC+=	${PKGDIR}/PLIST.docs
+.endif
+
+post-extract:
+.if empty(PKG_OPTIONS:Mfop-docs)
+.for subdir in docs javadocs
+	cd ${WRKSRC} && ${RM} -rf ${subdir}
+.endfor
+.endif
