db00c139c2e32ac57b4f8a972372d7a9062243bc
phonohawk <phonohawk>
2011-01-12 02:34:04 +0000

    Upstream update to deepseq-1.1.0.2

--- hs-deepseq/buildlink3.mk
+++ hs-deepseq/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-deepseq
 .if !defined(HS_DEEPSEQ_BUILDLINK3_MK)
 HS_DEEPSEQ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-deepseq+=	hs-deepseq>=1.1.0.0
+BUILDLINK_API_DEPENDS.hs-deepseq+=	hs-deepseq>=1.1
 BUILDLINK_PKGSRCDIR.hs-deepseq?=	../../wip/hs-deepseq
 .endif	# HS_DEEPSEQ_BUILDLINK3_MK
 
