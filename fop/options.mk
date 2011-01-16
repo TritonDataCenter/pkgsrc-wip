058439f10e4b65c40dd9310f25a227d357893765
thomasklausner <thomasklausner>
2011-01-16 08:43:28 +0000

    Use "doc" option instead of "fop-docs".

--- fop/options.mk
+++ fop/options.mk
@@ -1,16 +1,16 @@
 # $NetBSD: options.mk,v 1.5 2007/02/22 19:27:13 wiz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fop
-PKG_SUPPORTED_OPTIONS=	fop-docs
+PKG_SUPPORTED_OPTIONS=	doc
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mfop-docs)
+.if !empty(PKG_OPTIONS:Mdoc)
 PLIST_SRC+=	${PKGDIR}/PLIST.docs
 .endif
 
 post-extract:
-.if empty(PKG_OPTIONS:Mfop-docs)
+.if empty(PKG_OPTIONS:Mdoc)
 .for subdir in docs javadocs
 	cd ${WRKSRC} && ${RM} -rf ${subdir}
 .endfor
