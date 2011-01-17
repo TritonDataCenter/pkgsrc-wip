77162ac52e68ba202d82f4631459db85087c9873
jihbed <jihbed>
2011-01-17 22:00:14 +0000

    Initial revision

---
+++ ann/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	ann
+
+.if !defined(ANN_BUILDLINK3_MK)
+ANN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ann+=	ann>=1.1.2
+BUILDLINK_PKGSRCDIR.ann?=	../../wip/ann
+.endif	# ANN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ann
