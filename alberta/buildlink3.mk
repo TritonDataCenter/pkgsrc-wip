215844eb51c7e2b0de1181c36eebeb2d48c22ccc
jihbed <jihbed>
2011-01-19 14:00:00 +0000

    Initial revision

---
+++ alberta/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	alberta
+
+.if !defined(ALBERTA_BUILDLINK3_MK)
+ALBERTA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.alberta+=	alberta>=2.0.1
+BUILDLINK_PKGSRCDIR.alberta?=	../../wip/alberta
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../math/blas/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libICE/buildlink3.mk"
+.endif	# ALBERTA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-alberta
