b38ebc0b3ddec49d85f683c7e584214d95671287
phonohawk <phonohawk>
2011-02-17 14:53:10 +0000

    Upstream update to hscolour-1.17

--- hs-hscolour/buildlink3.mk
+++ hs-hscolour/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-hscolour
 .if !defined(HS_HSCOLOUR_BUILDLINK3_MK)
 HS_HSCOLOUR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.15
+BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.17
 BUILDLINK_PKGSRCDIR.hs-hscolour?=	../../wip/hs-hscolour
 .endif	# HS_HSCOLOUR_BUILDLINK3_MK
 
