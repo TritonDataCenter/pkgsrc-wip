847decd0f3062ca209bd791305c0a864dd42d6ca
phonohawk <phonohawk>
2011-02-17 15:09:03 +0000

    Upstream update to hlint-1.8.7

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -5,13 +5,13 @@ BUILDLINK_TREE+=	hlint
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.6.17
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
 .include "../../wip/hs-hscolour/buildlink3.mk"
-.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-uniplate/buildlink3.mk"
 .endif	# HLINT_BUILDLINK3_MK
 
