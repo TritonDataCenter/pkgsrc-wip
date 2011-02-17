c1ce9051978107eed2fe04fc493d781d8a08b950
zecrazytux <zecrazytux>
2011-02-17 21:01:41 +0000

    Upgraded to version 2.0.6: Add new extras and a shared library.

--- discount/buildlink3.mk
+++ discount/buildlink3.mk
@@ -2,13 +2,11 @@
 
 BUILDLINK_TREE+=	discount
 
-BUILDLINK_DEPMETHOD.discount?=  build
-
 .if !defined(DISCOUNT_BUILDLINK3_MK)
 DISCOUNT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.discount+=	discount>=2.0.1
-BUILDLINK_PKGSRCDIR.discount?=	../../wip/discount
+BUILDLINK_PKGSRCDIR.discount?=		../../wip/discount
 .endif	# DISCOUNT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-discount
