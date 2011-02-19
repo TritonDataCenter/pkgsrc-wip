a7232145783ceccf055121a121b3d14e6a71737a
gschwarz <gschwarz>
2011-02-19 17:14:58 +0000

    updated libexosip to 3.5.0

--- libexosip/buildlink3.mk
+++ libexosip/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libexosip2
 .if !defined(LIBEXOSIP2_BUILDLINK3_MK)
 LIBEXOSIP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libexosip2+=	libeXosip2>=3.2.0
+BUILDLINK_API_DEPENDS.libexosip2+=	libeXosip2>=3.5.0
 BUILDLINK_PKGSRCDIR.libexosip2?=	../../wip/libexosip
 .endif # LIBEXOSIP2_BUILDLINK3_MK
 
