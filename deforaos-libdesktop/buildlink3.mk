408b790414eff7efc466c7cfc45f9f227c438af3
khorben <khorben>
2011-02-24 13:21:10 +0000

    DeforaOS' libDesktop depends on Gtk+ 2.0

--- deforaos-libdesktop/buildlink3.mk
+++ deforaos-libdesktop/buildlink3.mk
@@ -8,6 +8,7 @@ DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.1
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
 
+.include "../../x11/gtk2/buildlink3.mk"
 .endif	# DEFORAOS_LIBDESKTOP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-libdesktop
