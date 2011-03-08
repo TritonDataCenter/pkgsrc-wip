74c9b30a5127399af2f3afd6ca857a0265b357b0
ddeus <ddeus>
2011-03-08 13:36:33 +0000

    Merged implementation of Common Lisp packages to mk/cl-package.mk. Added possibility to choose the ECL package (f.e. lang/ecl or wip/ecl).

--- cl-babel/options.mk
+++ cl-babel/options.mk
@@ -1,7 +1,7 @@
 # $NetBSD$
 #
 
-PKG_OPTIONS_VAR=                PKG_OPTIONS.${SHORTNAME}
+PKG_OPTIONS_VAR=                PKG_OPTIONS.${LISP_PREFIX}-${SHORTNAME:S/^cl-//}
 PKG_OPTIONS_REQUIRED_GROUPS=    lisp
 PKG_OPTIONS_GROUP.lisp=         ecl
 PKG_SUGGESTED_OPTIONS+=         ecl
@@ -12,5 +12,6 @@ PLIST_VARS+=                    ${PKG_OPTIONS_GROUP.lisp}
 .for opt in ${PKG_OPTIONS_GROUP.lisp}
 .  if !empty(PKG_OPTIONS:M${opt})
 PLIST.${opt}=                   yes
+COMMON_LISP_SYSTEM:=		${opt}
 .  endif
 .endfor
