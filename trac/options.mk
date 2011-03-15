78341bc01fa972b70d64e925b259ad361ccf0952
obache <obache>
2011-03-15 06:05:44 +0000

    Change default driver for pgsql.

--- trac/options.mk
+++ trac/options.mk
@@ -2,8 +2,9 @@
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.trac
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=		sqlite pgsql psycopg2 mysql
+PKG_OPTIONS_GROUP.db=		sqlite pgsql mysql
 PKG_SUGGESTED_OPTIONS=		sqlite
+PKG_LEGACY_OPTIONS=		psycopg2:pgsql
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,9 +12,6 @@ PKG_SUGGESTED_OPTIONS=		sqlite
 DEPENDS+=	${PYPKGPREFIX}-sqlite2>=2.3.2:../../databases/py-sqlite2
 .endif
 .if !empty(PKG_OPTIONS:Mpgsql)
-DEPENDS+=	${PYPKGPREFIX}-PgSQL>=2.4:../../databases/py-PgSQL
-.endif
-.if !empty(PKG_OPTIONS:Mpsycopg2)
 DEPENDS+=	${PYPKGPREFIX}-psycopg2>=2:../../databases/py-psycopg2
 .endif
 .if !empty(PKG_OPTIONS:Mmysql)
