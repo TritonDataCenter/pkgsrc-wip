821b4129badf419bbe91d85f1582a08f39521e31
jihbed <jihbed>
2011-03-25 18:43:42 +0000

    Initial revision

---
+++ libcuba/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libcuba
+
+.if !defined(LIBCUBA_BUILDLINK3_MK)
+LIBCUBA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libcuba+=	libcuba>=2.1
+BUILDLINK_PKGSRCDIR.libcuba?=	../../wip/libcuba
+.endif	# LIBCUBA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libcuba
