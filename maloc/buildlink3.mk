5e6605972feabfaf2c1aaec2b2f02a803518e318
jihbed <jihbed>
2011-03-25 18:11:59 +0000

    Initial revision

---
+++ maloc/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	maloc
+
+.if !defined(MALOC_BUILDLINK3_MK)
+MALOC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.maloc+=	maloc>=1.4
+BUILDLINK_PKGSRCDIR.maloc?=	../../wip/maloc
+
+.endif	# MALOC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-maloc
