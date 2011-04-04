647d6de474547a80dbf1426dcc1efc6dc679fa6b
obache <obache>
2011-04-04 14:06:35 +0000

    require libuuid for Linux and Solaris.

--- zeromq/buildlink3.mk
+++ zeromq/buildlink3.mk
@@ -8,6 +8,13 @@ ZEROMQ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.zeromq+=	zeromq>=2.0.11
 BUILDLINK_PKGSRCDIR.zeromq?=	../../wip/zeromq
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
+.include "../../devel/libuuid/buildlink3.mk"
+.endif
+
 .endif	# ZEROMQ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-zeromq
