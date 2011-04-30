815f9bc2126a4c6592304c5b535e618a401201ae
jihbed <jihbed>
2011-04-30 22:07:07 +0000

    resolv edependency external package with harminv

---
+++ harminv/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	harminv
+
+.if !defined(HARMINV_BUILDLINK3_MK)
+HARMINV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.harminv+=	harminv>=1.3.1
+BUILDLINK_PKGSRCDIR.harminv?=	../../wip/harminv
+
+.endif	# HARMINV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-harminv
