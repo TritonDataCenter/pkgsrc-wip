71d94b4f0f18d38c3aa71f19ead94079efa5ea45
obache <obache>
2011-05-08 02:37:46 +0000

    replace spaces with a tab.

--- quickml/options.mk
+++ quickml/options.mk
@@ -1,6 +1,6 @@
 # $NetBSD$
 
-PKG_OPTIONS_VAR=        PKG_OPTIONS.quickml
+PKG_OPTIONS_VAR=	PKG_OPTIONS.quickml
 PKG_SUPPORTED_OPTIONS+= limit analog
 
 
@@ -8,16 +8,16 @@ PKG_SUPPORTED_OPTIONS+= limit analog
 
 
 .if !empty(PKG_OPTIONS:Mlimit)
-LIMIT_PATCH=  quickml-0.7-limited.patch
-PATCHFILES+=            ${LIMIT_PATCH}
-#SITES.${LIMIT_PATCH}=        http://linux.matchy.net/view.xcg?c=plugin;plugin=attach_download;p=QuickMLLimited;file_name=
-SITES.${LIMIT_PATCH}=        http://matchy.s28.xrea.com/hiki.xcg?c=plugin;plugin=attach_download;p=QuickMLLimited;file_name=
-PATCH_DIST_STRIP.${LIMIT_PATCH}=     -p1
+LIMIT_PATCH=	quickml-0.7-limited.patch
+PATCHFILES+=	${LIMIT_PATCH}
+#SITES.${LIMIT_PATCH}=	http://linux.matchy.net/view.xcg?c=plugin;plugin=attach_download;p=QuickMLLimited;file_name=
+SITES.${LIMIT_PATCH}=	http://matchy.s28.xrea.com/hiki.xcg?c=plugin;plugin=attach_download;p=QuickMLLimited;file_name=
+PATCH_DIST_STRIP.${LIMIT_PATCH}=	-p1
 .endif
 
 
 .if !empty(PKG_OPTIONS:Manalog)
 .include "../../graphics/ImageMagick/buildlink3.mk"
 .include "../../print/ghostscript/buildlink3.mk"
-DEPENDS+=      gnuplot>=3.7:../../graphics/gnuplot
+DEPENDS+=	gnuplot>=3.7:../../graphics/gnuplot
 .endif
