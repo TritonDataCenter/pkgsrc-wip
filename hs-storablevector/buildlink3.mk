a13132eb64c9201a6171bf95af001e6cbde79b01
phonohawk <phonohawk>
2011-05-10 12:43:13 +0000

    Upstream update to storablevector-0.2.7

--- hs-storablevector/buildlink3.mk
+++ hs-storablevector/buildlink3.mk
@@ -10,6 +10,7 @@ BUILDLINK_PKGSRCDIR.hs-storablevector?=	../../wip/hs-storablevector
 
 .include "../../wip/hs-QuickCheck/buildlink3.mk"
 .include "../../wip/hs-non-negative/buildlink3.mk"
+.include "../../wip/hs-syb/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-utility-ht/buildlink3.mk"
 .endif	# HS_STORABLEVECTOR_BUILDLINK3_MK
