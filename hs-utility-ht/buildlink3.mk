e9a79ae8410b97c4a73d55f6831fa675653d3c22
phonohawk <phonohawk>
2011-05-10 12:36:51 +0000

    Upstream update to utility-ht-0.0.7

--- hs-utility-ht/buildlink3.mk
+++ hs-utility-ht/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-utility-ht
 .if !defined(HS_UTILITY_HT_BUILDLINK3_MK)
 HS_UTILITY_HT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.5.1
+BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0
 BUILDLINK_PKGSRCDIR.hs-utility-ht?=	../../wip/hs-utility-ht
 .endif	# HS_UTILITY_HT_BUILDLINK3_MK
 
