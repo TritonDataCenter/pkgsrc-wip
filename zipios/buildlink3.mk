f33dac5097f18bfc9e0941a704a105820efbf97b
ryo-on <ryo-on>
2011-05-12 12:20:42 +0000

    Initial revision

---
+++ zipios/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	zipios
+
+.if !defined(ZIPIOS_BUILDLINK3_MK)
+ZIPIOS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.zipios+=	zipios>=0.1.5.9.2007.04.28
+BUILDLINK_PKGSRCDIR.zipios?=	../../wip/zipios
+.endif	# ZIPIOS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-zipios
