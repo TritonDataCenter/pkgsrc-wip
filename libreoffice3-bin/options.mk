19e982af0bc81bf0815f2bd333465bff888d832f
ryo-on <ryo-on>
2011-05-28 20:06:53 +0000

    * Restore java option. * Add conflict pattern for misc/openoffice3-bin.

---
+++ libreoffice3-bin/options.mk
@@ -0,0 +1,12 @@
+# $NetBSD: options.mk,v 1.3 2010/09/13 22:32:02 bad Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.libreoffice-bin
+PKG_SUPPORTED_OPTIONS=	java
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mjava)
+PKG_JVMS_ACCEPTED=	jdk15 sun-jdk6
+USE_JAVA=		run
+.include "../../mk/java-vm.mk"
+.endif
