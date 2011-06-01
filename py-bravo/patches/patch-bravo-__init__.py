52143921f42a2d29b11c0dda74a24995b8fa5eb6
schnoebe <schnoebe>
2011-06-01 03:51:46 +0000

    Initial revision

---
+++ py-bravo/patches/patch-bravo-__init__.py
@@ -0,0 +1,9 @@
+$NetBSD$
+
+# correct the release version of the software
+
+--- bravo/__init__.py.orig	2011-05-08 16:36:35.000000000 +0000
++++ bravo/__init__.py
+@@ -1 +1 @@
+-version = u"1.4"
++version = u"1.5"
