048142a580b8cb5bd20f49cfe5b16417d38d26d3
makoto <makoto>
2011-06-01 15:57:33 +0000

    Correct case of buildlink name freewnn -> FreeWnn

--- ja-freewnn-lib/buildlink3.mk
+++ ja-freewnn-lib/buildlink3.mk
@@ -1,13 +1,13 @@
 # $NetBSD: buildlink3.mk,v 1.8 2010/07/11 02:53:24 obache Exp $
 
-BUILDLINK_TREE+=	ja-freewnn-lib
+BUILDLINK_TREE+=	ja-FreeWnn-lib
 
 .if !defined(JA_FREEWNN_LIB_BUILDLINK3_MK)
 JA_FREEWNN_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ja-freewnn-lib+=	ja-freewnn-lib>=1.1.1
-BUILDLINK_PKGSRCDIR.ja-freewnn-lib?=	../../wip/ja-freewnn-lib
-BUILDLINK_DEPMETHOD.ja-freewnn-lib?=	build
+BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-freewnn-lib>=1.1.1
+BUILDLINK_PKGSRCDIR.ja-FreeWnn-lib?=	../../wip/ja-freewnn-lib
+BUILDLINK_DEPMETHOD.ja-FreeWnn-lib?=	build
 .endif # JA_FREEWNN_LIB_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-ja-freewnn-lib
+BUILDLINK_TREE+=	-ja-FreeWnn-lib
