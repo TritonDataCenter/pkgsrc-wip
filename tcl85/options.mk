c7eeb504291cd1081cd23ce49043e1acec1e9e8d
asau <asau>
2011-06-07 20:52:49 +0000

    Pull "bsd.prefs.mk", remove unneeded spaces.

--- tcl85/options.mk
+++ tcl85/options.mk
@@ -4,14 +4,15 @@ PKG_OPTIONS_VAR=	PKG_OPTIONS.tcl
 PKG_SUPPORTED_OPTIONS=	threads debug
 PKG_SUGGESTED_OPTIONS=	threads
 
+.include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
 
-.  if !empty(PKG_OPTIONS:Mdebug)
+.if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-symbols=mem
-.  endif
+.endif
 
-.  if !empty(PKG_OPTIONS:Mthreads)
+.if !empty(PKG_OPTIONS:Mthreads)
 CONFIGURE_ARGS+=	--enable-threads=on
-.  else
+.else
 CONFIGURE_ARGS+=	--enable-threads=off
-.  endif
+.endif
