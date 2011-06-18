f464d244626f9cc894527cadfdc20f4dc800d3b1
obache <obache>
2011-06-18 11:29:24 +0000

    Change PKGNAME to same as pkgsrc/inputmethod one.

--- ja-freewnn-lib/buildlink3.mk
+++ ja-freewnn-lib/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	ja-FreeWnn-lib
 .if !defined(JA_FREEWNN_LIB_BUILDLINK3_MK)
 JA_FREEWNN_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-freewnn-lib>=1.11alpha22
+BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-FreeWnn-lib>=1.11alpha22
 BUILDLINK_PKGSRCDIR.ja-FreeWnn-lib?=	../../wip/ja-freewnn-lib
 .endif # JA_FREEWNN_LIB_BUILDLINK3_MK
 
