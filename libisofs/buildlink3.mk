0fd0cab5b802cff293b7f67e9592b99584d6253f
ryo-on <ryo-on>
2011-06-20 20:05:13 +0000

    Update to 1.1.0
    * API had been changed in 1.0.6.
    
    Changelog:
    libisofs-1.1.0.tar.gz Sat Jun 18 2011
    ===============================================================================
    * Bug fix: Padding as of iso_write_opts_set_tail_blocks() was added only
               after cylinder alignment as of iso_write_opts_set_system_area()
               and thus spoiled this alignment.

--- libisofs/buildlink3.mk
+++ libisofs/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libisofs
 .if !defined(LIBISOFS_BUILDLINK3_MK)
 LIBISOFS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.0.4
+BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.0.6
 BUILDLINK_PKGSRCDIR.libisofs?=	../../wip/libisofs
 
 .include "../../devel/zlib/buildlink3.mk"
