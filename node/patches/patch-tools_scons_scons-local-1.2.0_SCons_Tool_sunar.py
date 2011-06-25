2e1a7c3210b894d76ac3e188101763a311262305
genolopolis <genolopolis>
2011-06-25 12:46:03 +0000

    start integrating packages from Filip Hajny @ joyent to improve solaris build and add other features- take 2

---
+++ node/patches/patch-tools_scons_scons-local-1.2.0_SCons_Tool_sunar.py
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- tools/scons/scons-local-1.2.0/SCons/Tool/sunar.py.orig	2011-04-23 00:06:25.000000000 +0000
++++ tools/scons/scons-local-1.2.0/SCons/Tool/sunar.py
+@@ -42,7 +42,7 @@ def generate(env):
+     """Add Builders and construction variables for ar to an Environment."""
+     SCons.Tool.createStaticLibBuilder(env)
+     
+-    if env.Detect('CC'):
++    if env.Detect('CC') and env['CC'] != 'gcc':
+         env['AR']          = 'CC'
+         env['ARFLAGS']     = SCons.Util.CLVar('-xar')
+         env['ARCOM']       = '$AR $ARFLAGS -o $TARGET $SOURCES'
