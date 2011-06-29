280d995f325f00183e7d6fd3177a15bbc8f7cf47
reinoudz70 <reinoudz70>
2011-06-29 14:20:28 +0000

    Fix buildlink3 references to point to wip

--- libxfce4ui/buildlink3.mk
+++ libxfce4ui/buildlink3.mk
@@ -8,8 +8,8 @@ LIBXFCE4UI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libxfce4ui+=	libxfce4ui>=4.8.0
 BUILDLINK_PKGSRCDIR.libxfce4ui?=	../../wip/libxfce4ui
 
-.include "../../devel/xfconf/buildlink3.mk"
-.include "../../x11/libxfce4util/buildlink3.mk"
+.include "../../wip/xfconf/buildlink3.mk"
+.include "../../wip/libxfce4util/buildlink3.mk"
 .include "../../x11/startup-notification/buildlink3.mk"
 .endif # LIBXFCE4UI_BUILDLINK3_MK
 
