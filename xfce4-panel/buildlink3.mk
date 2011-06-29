a522437a48dff55d29c698a3ddc3898b1efc3a23
reinoudz70 <reinoudz70>
2011-06-29 13:21:43 +0000

    Fix buidlink3 references to point to wip instead of x11

--- xfce4-panel/buildlink3.mk
+++ xfce4-panel/buildlink3.mk
@@ -7,11 +7,11 @@ XFCE4_PANEL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.6.0
 BUILDLINK_ABI_DEPENDS.xfce4-panel?=	xfce4-panel>=4.6.2nb6
-BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../x11/xfce4-panel
+BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../wip/xfce4-panel
 
 .include "../../devel/glib2/buildlink3.mk"
 #.include "../../x11/libxfce4gui/buildlink3.mk"
-.include "../../x11/libxfce4util/buildlink3.mk"
+.include "../../wip/libxfce4util/buildlink3.mk"
 .endif # XFCE4_PANEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xfce4-panel
