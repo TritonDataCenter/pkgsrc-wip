d332ca0f75fd9799311cf271058a178116655676
reinoudz70 <reinoudz70>
2011-06-29 09:45:15 +0000

    Add libxfce4util for xfce 4.8

---
+++ libxfce4util/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.25 2011/04/22 13:42:22 obache Exp $
+
+BUILDLINK_TREE+=	libxfce4util
+
+.if !defined(LIBXFCE4UTIL_BUILDLINK3_MK)
+LIBXFCE4UTIL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.6.0
+BUILDLINK_ABI_DEPENDS.libxfce4util?=	libxfce4util>=4.6.1nb4
+BUILDLINK_PKGSRCDIR.libxfce4util?=	../../x11/libxfce4util
+
+.include "../../devel/glib2/buildlink3.mk"
+.endif # LIBXFCE4UTIL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libxfce4util
