0c4ca43a7669e86beb3e0c1682417768a440daed
reinoudz70 <reinoudz70>
2011-06-30 13:48:00 +0000

    Fix buildlink3 version requirements to the new version for xfce 4.8

--- xfce4-panel/buildlink3.mk
+++ xfce4-panel/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	xfce4-panel
 .if !defined(XFCE4_PANEL_BUILDLINK3_MK)
 XFCE4_PANEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.6.0
-BUILDLINK_ABI_DEPENDS.xfce4-panel?=	xfce4-panel>=4.6.2nb6
+BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.8.5
+BUILDLINK_ABI_DEPENDS.xfce4-panel?=	xfce4-panel>=4.8.5
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../wip/xfce4-panel
 
 .include "../../devel/glib2/buildlink3.mk"
