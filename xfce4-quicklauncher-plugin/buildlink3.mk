ba1c3ab2352dad0a6ce0ef1b991e78a7fef1fe5c
reinoudz70 <reinoudz70>
2011-06-30 14:29:26 +0000

    Fix reference to point to the wip package; it would pull in an old version

--- xfce4-quicklauncher-plugin/buildlink3.mk
+++ xfce4-quicklauncher-plugin/buildlink3.mk
@@ -7,7 +7,7 @@ XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xfce4-quicklauncher-plugin+=	xfce4-quicklauncher-plugin>=1.9.4
 BUILDLINK_ABI_DEPENDS.xfce4-quicklauncher-plugin?=	xfce4-quicklauncher-plugin>=1.9.4nb7
-BUILDLINK_PKGSRCDIR.xfce4-quicklauncher-plugin?=	../../sysutils/xfce4-quicklauncher-plugin
+BUILDLINK_PKGSRCDIR.xfce4-quicklauncher-plugin?=	../../wip/xfce4-quicklauncher-plugin
 
 .include "../../wip/xfce4-panel/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
