5507d9fe59c7856aaced42cd505031a3923cf7c8
reinoudz70 <reinoudz70>
2011-07-02 20:11:49 +0000

    Add the xfce4ui library just in case

--- xfce4-panel/buildlink3.mk
+++ xfce4-panel/buildlink3.mk
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.xfce4-panel?=	xfce4-panel>=4.8.5
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../wip/xfce4-panel
 
 .include "../../devel/glib2/buildlink3.mk"
-#.include "../../x11/libxfce4gui/buildlink3.mk"
+.include "../../wip/libxfce4ui/buildlink3.mk"
 .include "../../wip/libxfce4util/buildlink3.mk"
 .endif # XFCE4_PANEL_BUILDLINK3_MK
 
