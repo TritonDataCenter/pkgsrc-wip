969ea51b6cb4ff1dca33d084b2d07bcfc67c5d73
tnn2 <tnn2>
2011-07-07 14:22:13 +0000

    check in some firefox-5.0 work-in-progress (doesn't build yet)

--- xulrunner/mozilla-common.mk
+++ xulrunner/mozilla-common.mk
@@ -1,6 +1,6 @@
 # $NetBSD: mozilla-common.mk,v 1.7 2009/11/28 17:46:01 tnn Exp $
 #
-# common Makefile fragment for mozilla packages based on gecko 1.9.3
+# common Makefile fragment for mozilla packages based on gecko 2.0.
 # 
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
@@ -28,6 +28,7 @@ CONFIGURE_ARGS+=	--disable-system-cairo
 CONFIGURE_ARGS+=	--disable-system-pixman
 CONFIGURE_ARGS+=	--with-system-jpeg
 CONFIGURE_ARGS+=	--with-system-zlib --with-system-bz2
+CONFIGURE_ARGS+=	--with-system-libevent
 CONFIGURE_ARGS+=	--enable-system-sqlite
 CONFIGURE_ARGS+=	--disable-crashreporter
 CONFIGURE_ARGS+=	--disable-libnotify
@@ -83,9 +84,10 @@ PREFER.bzip2?=	pkgsrc
 .include "../../audio/alsa-lib/buildlink3.mk"
 .endif
 .include "../../archivers/bzip2/buildlink3.mk"
-BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.7.1
+BUILDLINK_API_DEPENDS.sqlite3+=	sqlite3>=3.7.4
 CONFIGURE_ENV+=	ac_cv_sqlite_secure_delete=yes	# c.f. patches/patch-al
 .include "../../databases/sqlite3/buildlink3.mk"
+.include "../../devel/libevent/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk"
