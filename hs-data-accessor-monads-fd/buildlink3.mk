84bc84c992c8041ef6b142dd3b8ad377d9ebde7a
phonohawk <phonohawk>
2011-07-08 08:33:34 +0000

    Initial revision

---
+++ hs-data-accessor-monads-fd/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-data-accessor-monads-fd
+
+.if !defined(HS_DATA_ACCESSOR_MONADS_FD_BUILDLINK3_MK)
+HS_DATA_ACCESSOR_MONADS_FD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-data-accessor-monads-fd+=	hs-data-accessor-monads-fd>=0.2
+BUILDLINK_PKGSRCDIR.hs-data-accessor-monads-fd?=	../../wip/hs-data-accessor-monads-fd
+
+.include "../../wip/hs-data-accessor/buildlink3.mk"
+.include "../../wip/hs-monads-fd/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
+.endif	# HS_DATA_ACCESSOR_MONADS_FD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-data-accessor-monads-fd
