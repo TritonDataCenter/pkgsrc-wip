23fd95b84160469ef65c247289a1cca637a2bd95
phonohawk <phonohawk>
2011-07-08 09:58:31 +0000

    Initial revision

---
+++ hs-io-storage/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-io-storage
+
+.if !defined(HS_IO_STORAGE_BUILDLINK3_MK)
+HS_IO_STORAGE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-io-storage+=	hs-io-storage>=0.3
+BUILDLINK_PKGSRCDIR.hs-io-storage?=	../../wip/hs-io-storage
+.endif	# HS_IO_STORAGE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-io-storage
---
+++ hs-xdg-basedir/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-xdg-basedir
+
+.if !defined(HS_XDG_BASEDIR_BUILDLINK3_MK)
+HS_XDG_BASEDIR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-xdg-basedir+=	hs-xdg-basedir>=0.2
+BUILDLINK_PKGSRCDIR.hs-xdg-basedir?=	../../wip/hs-xdg-basedir
+.endif	# HS_XDG_BASEDIR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-xdg-basedir
