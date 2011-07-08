d74c2aee7cfd750b34d579aa5ce5560e9cd1a7b1
phonohawk <phonohawk>
2011-07-08 09:42:59 +0000

    Initial revision

---
+++ hs-executable-path/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-executable-path
+
+.if !defined(HS_EXECUTABLE_PATH_BUILDLINK3_MK)
+HS_EXECUTABLE_PATH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-executable-path+=	hs-executable-path>=0.0
+BUILDLINK_PKGSRCDIR.hs-executable-path?=	../../wip/hs-executable-path
+.endif	# HS_EXECUTABLE_PATH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-executable-path
