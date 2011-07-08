18b2be936a084131d5d98134365cfdcda87df531
phonohawk <phonohawk>
2011-07-08 09:13:08 +0000

    Initial revision

---
+++ hs-Diff/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-Diff
+
+.if !defined(HS_DIFF_BUILDLINK3_MK)
+HS_DIFF_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.1
+BUILDLINK_PKGSRCDIR.hs-Diff?=	../../wip/hs-Diff
+.endif	# HS_DIFF_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-Diff
