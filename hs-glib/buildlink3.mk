eb4d1e6fab07f2567641514f51203ddbd274a753
phonohawk <phonohawk>
2011-07-12 09:58:49 +0000

    buildlink2.mk: missing dependency on devel/glib2

--- hs-glib/buildlink3.mk
+++ hs-glib/buildlink3.mk
@@ -8,6 +8,7 @@ HS_GLIB_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-glib+=	hs-glib>=0.12
 BUILDLINK_PKGSRCDIR.hs-glib?=	../../wip/hs-glib
 
+.include "../../devel/glib2/buildlink3.mk"
 .endif	# HS_GLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-glib
