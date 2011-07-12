b1c4e004508557034dd5267f2365e78682c42621
phonohawk <phonohawk>
2011-07-12 04:06:28 +0000

    Initial revision

---
+++ hs-ghc-mtl/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-ghc-mtl
+
+.if !defined(HS_GHC_MTL_BUILDLINK3_MK)
+HS_GHC_MTL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.0
+BUILDLINK_PKGSRCDIR.hs-ghc-mtl?=	../../wip/hs-ghc-mtl
+
+.include "../../wip/hs-MonadCatchIO-mtl/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.endif	# HS_GHC_MTL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-ghc-mtl
