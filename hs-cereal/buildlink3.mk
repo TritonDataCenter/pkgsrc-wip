a6943dc0caf429f1ad35377e806a062ee379da28
phonohawk <phonohawk>
2011-07-12 07:13:22 +0000

    Initial revision

---
+++ hs-cereal/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-cereal
+
+.if !defined(HS_CEREAL_BUILDLINK3_MK)
+HS_CEREAL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-cereal+=	hs-cereal>=0.3
+BUILDLINK_PKGSRCDIR.hs-cereal?=	../../wip/hs-cereal
+.endif	# HS_CEREAL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-cereal
