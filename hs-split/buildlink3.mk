032b4cc43b4ca5ada7a332261472c090b9578381
phonohawk <phonohawk>
2011-07-12 08:15:51 +0000

    Initial revision

---
+++ hs-split/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-split
+
+.if !defined(HS_SPLIT_BUILDLINK3_MK)
+HS_SPLIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.1
+BUILDLINK_PKGSRCDIR.hs-split?=	../../wip/hs-split
+.endif	# HS_SPLIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-split
