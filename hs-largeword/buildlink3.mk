0f24b5eb3bdcafe640cdb519db3edb8b1cf0fccd
phonohawk <phonohawk>
2011-07-12 07:21:00 +0000

    Initial revision

---
+++ hs-largeword/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-largeword
+
+.if !defined(HS_LARGEWORD_BUILDLINK3_MK)
+HS_LARGEWORD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-largeword+=	hs-largeword>=1.0
+BUILDLINK_PKGSRCDIR.hs-largeword?=	../../wip/hs-largeword
+.endif	# HS_LARGEWORD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-largeword
