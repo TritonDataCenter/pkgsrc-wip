f65d9b81aa5e69cb4edf48159c0f39aee02ca4bb
phonohawk <phonohawk>
2011-07-12 04:15:47 +0000

    Initial revision

---
+++ hs-hint/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-hint
+
+.if !defined(HS_HINT_BUILDLINK3_MK)
+HS_HINT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.3
+BUILDLINK_PKGSRCDIR.hs-hint?=	../../wip/hs-hint
+
+.include "../../wip/hs-MonadCatchIO-mtl/buildlink3.mk"
+.include "../../wip/hs-ghc-mtl/buildlink3.mk"
+.include "../../wip/hs-ghc-paths/buildlink3.mk"
+.include "../../wip/hs-haskell-src/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-utf8-string/buildlink3.mk"
+.endif	# HS_HINT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hint
