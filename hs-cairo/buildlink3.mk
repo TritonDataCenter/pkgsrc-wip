33d4fa5d63e5f9b496473de95676cb6d7cad5172
phonohawk <phonohawk>
2011-07-12 09:18:11 +0000

    Initial revision

---
+++ hs-cairo/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-cairo
+
+.if !defined(HS_CAIRO_BUILDLINK3_MK)
+HS_CAIRO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-cairo+=	hs-cairo>=0.12
+BUILDLINK_PKGSRCDIR.hs-cairo?=	../../wip/hs-cairo
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.endif	# HS_CAIRO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-cairo
