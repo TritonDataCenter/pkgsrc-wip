5c28f76450b2439fda5621839bdf176889fb85f9
phonohawk <phonohawk>
2011-07-12 10:00:07 +0000

    buildlink3.mk: missing dependency on graphics/cairo

--- hs-cairo/buildlink3.mk
+++ hs-cairo/buildlink3.mk
@@ -8,6 +8,7 @@ HS_CAIRO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-cairo+=	hs-cairo>=0.12
 BUILDLINK_PKGSRCDIR.hs-cairo?=	../../wip/hs-cairo
 
+.include "../../graphics/cairo/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
 .endif	# HS_CAIRO_BUILDLINK3_MK
 
