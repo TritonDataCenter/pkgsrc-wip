181c2b1db3b9bf81f6e5bcc059ca78732b4f44be
phonohawk <phonohawk>
2011-07-12 15:19:24 +0000

    Initial revision

---
+++ hs-cautious-file/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-cautious-file
+
+.if !defined(HS_CAUTIOUS_FILE_BUILDLINK3_MK)
+HS_CAUTIOUS_FILE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-cautious-file+=	hs-cautious-file>=1.0
+BUILDLINK_PKGSRCDIR.hs-cautious-file?=	../../wip/hs-cautious-file
+.endif	# HS_CAUTIOUS_FILE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-cautious-file
