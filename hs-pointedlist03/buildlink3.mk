a3eb8a27e63cc1e96d4627aeba43ba79918a717a
phonohawk <phonohawk>
2011-07-12 16:18:23 +0000

    Initial revision

---
+++ hs-pointedlist03/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-pointedlist03
+
+.if !defined(HS_POINTEDLIST03_BUILDLINK3_MK)
+HS_POINTEDLIST03_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-pointedlist03+=	hs-pointedlist03>=0.3
+BUILDLINK_PKGSRCDIR.hs-pointedlist03?=	../../wip/hs-pointedlist03
+
+.include "../../wip/hs-binary/buildlink3.mk"
+.include "../../wip/hs-data-accessor/buildlink3.mk"
+.endif	# HS_POINTEDLIST03_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-pointedlist03
