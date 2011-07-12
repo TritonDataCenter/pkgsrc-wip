1afd5453a6a3b77e0e6eaeff3eda9cb714ea03f2
phonohawk <phonohawk>
2011-07-12 08:22:49 +0000

    Initial revision

---
+++ hs-unix-compat/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-unix-compat
+
+.if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
+HS_UNIX_COMPAT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.2
+BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../wip/hs-unix-compat
+.endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-unix-compat
