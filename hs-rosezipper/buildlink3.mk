ef1b17efe5cb80b36c7b7ec47d6f3c6663c7e59a
phonohawk <phonohawk>
2011-07-12 08:10:28 +0000

    Initial revision

---
+++ hs-rosezipper/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-rosezipper
+
+.if !defined(HS_ROSEZIPPER_BUILDLINK3_MK)
+HS_ROSEZIPPER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-rosezipper+=	hs-rosezipper>=0.2
+BUILDLINK_PKGSRCDIR.hs-rosezipper?=	../../wip/hs-rosezipper
+.endif	# HS_ROSEZIPPER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-rosezipper
