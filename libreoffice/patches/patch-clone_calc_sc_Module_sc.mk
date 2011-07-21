a0d9b5c42cb8a173343bbe68c36a53af62ac4fb8
ftigeot <ftigeot>
2011-07-21 05:16:36 +0000

    Update to a newer snapshot

--- libreoffice/patches/patch-clone_calc_sc_Module_sc.mk
+++ libreoffice/patches/patch-clone_calc_sc_Module_sc.mk
@@ -2,12 +2,13 @@ $NetBSD$
 
 --- clone/calc/sc/Module_sc.mk.orig	2011-05-11 12:36:55 +0000
 +++ clone/calc/sc/Module_sc.mk
-@@ -33,9 +33,4 @@ $(eval $(call gb_Module_add_targets,sc,\
+@@ -33,10 +33,4 @@ $(eval $(call gb_Module_add_targets,sc,\
  	Package_xml \
  ))
  
 -$(eval $(call gb_Module_add_check_targets,sc,\
 -	CppunitTest_sc_ucalc \
+-	CppunitTest_sc_test_filters \
 -))
 -
 -
