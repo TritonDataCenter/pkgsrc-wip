b8bf2e54b195e8fd72860072504a4137dfadbac4
phonohawk <phonohawk>
2011-07-27 05:52:11 +0000

    Initial revision

---
+++ hs-hxt-unicode/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-hxt-unicode
+
+.if !defined(HS_HXT_UNICODE_BUILDLINK3_MK)
+HS_HXT_UNICODE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hxt-unicode+=	hs-hxt-unicode>=9.0
+BUILDLINK_PKGSRCDIR.hs-hxt-unicode?=	../../wip/hs-hxt-unicode
+
+.include "../../wip/hs-hxt-charproperties/buildlink3.mk"
+.endif	# HS_HXT_UNICODE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hxt-unicode
