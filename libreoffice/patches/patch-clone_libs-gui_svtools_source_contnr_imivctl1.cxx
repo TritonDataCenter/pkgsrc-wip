b953191712617c0db8bc368d38528ce7985417d5
asau <asau>
2011-07-27 16:46:25 +0000

    Fix function signature.

---
+++ libreoffice/patches/patch-clone_libs-gui_svtools_source_contnr_imivctl1.cxx
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- clone/libs-gui/svtools/source/contnr/imivctl1.cxx.orig	2011-07-15 14:53:56.000000000 +0000
++++ clone/libs-gui/svtools/source/contnr/imivctl1.cxx
+@@ -292,7 +292,7 @@ void SvxIconChoiceCtrl_Impl::FontModifie
+     ShowCursor( sal_True );
+ }
+ 
+-void SvxIconChoiceCtrl_Impl::InsertEntry( SvxIconChoiceCtrlEntry* pEntry, size_t nPos,
++void SvxIconChoiceCtrl_Impl::InsertEntry( SvxIconChoiceCtrlEntry* pEntry, sal_uLong nPos,
+     const Point* pPos )
+ {
+     StopEditTimer();
