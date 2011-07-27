2b098e961c6ab1253f2f595b4fbb889f554cb7e7
phonohawk <phonohawk>
2011-07-27 05:23:07 +0000

    Initial revision

---
+++ hs-hxt-charproperties/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-hxt-charproperties
+
+.if !defined(HS_HXT_CHARPROPERTIES_BUILDLINK3_MK)
+HS_HXT_CHARPROPERTIES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.1
+BUILDLINK_PKGSRCDIR.hs-hxt-charproperties?=	../../wip/hs-hxt-charproperties
+.endif	# HS_HXT_CHARPROPERTIES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hxt-charproperties
