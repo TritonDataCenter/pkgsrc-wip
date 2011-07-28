7a7ffce536849dde00ba4ab2112ef0a61d9e349b
hfath <hfath>
2011-07-28 16:08:01 +0000

    Patch away a few more build obstacles; the package compiles now.

---
+++ netatalk22/patches/patch-etc_cnid_dbd_comm.c
@@ -0,0 +1,20 @@
+$NetBSD$
+
+--- etc/cnid_dbd/comm.c.orig	2011-07-22 04:30:42.000000000 +0000
++++ etc/cnid_dbd/comm.c
+@@ -9,6 +9,7 @@
+ #include "config.h"
+ #endif
+ 
++#if !defined(__FreeBSD__) && !defined(__NetBSD__)
+ #ifndef _XOPEN_SOURCE
+ # define _XOPEN_SOURCE 600
+ #endif
+@@ -18,6 +19,7 @@
+ #ifndef _GNU_SOURCE
+ # define _GNU_SOURCE
+ #endif
++#endif
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
