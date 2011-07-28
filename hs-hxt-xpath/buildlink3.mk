d3aa98e12a530bc587bf2a941ab63162db787223
phonohawk <phonohawk>
2011-07-28 13:14:13 +0000

    Upstream update to hxt-xpath-9.1.1

--- hs-hxt-xpath/buildlink3.mk
+++ hs-hxt-xpath/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-hxt-xpath
 .if !defined(HS_HXT_XPATH_BUILDLINK3_MK)
 HS_HXT_XPATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt-xpath+=	hs-hxt-xpath>=8.5.0
+BUILDLINK_API_DEPENDS.hs-hxt-xpath+=	hs-hxt-xpath>=9.1
 BUILDLINK_PKGSRCDIR.hs-hxt-xpath?=	../../wip/hs-hxt-xpath
 
 .include "../../wip/hs-hxt/buildlink3.mk"
