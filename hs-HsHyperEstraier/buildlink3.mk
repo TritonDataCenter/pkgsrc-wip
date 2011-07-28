1832425f97124eac56d278083ed81093a958797a
phonohawk <phonohawk>
2011-07-28 10:20:19 +0000

    Upstream update to HsHyperEstraier-0.4

--- hs-HsHyperEstraier/buildlink3.mk
+++ hs-HsHyperEstraier/buildlink3.mk
@@ -5,11 +5,13 @@ BUILDLINK_TREE+=	hs-HsHyperEstraier
 .if !defined(HS_HSHYPERESTRAIER_BUILDLINK3_MK)
 HS_HSHYPERESTRAIER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HsHyperEstraier+=	hs-HsHyperEstraier>=0.3.2
+BUILDLINK_API_DEPENDS.hs-HsHyperEstraier+=	hs-HsHyperEstraier>=0.4
 BUILDLINK_PKGSRCDIR.hs-HsHyperEstraier?=	../../wip/hs-HsHyperEstraier
 
 .include "../../textproc/hyperestraier/buildlink3.mk"
+.include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
 .include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_HSHYPERESTRAIER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-HsHyperEstraier
