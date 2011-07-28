65130402de07355db72dd414659112b41244246f
phonohawk <phonohawk>
2011-07-28 12:55:38 +0000

    Upstream update to hslogger-1.1.4

--- hs-hslogger/buildlink3.mk
+++ hs-hslogger/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-hslogger
 .if !defined(HS_HSLOGGER_BUILDLINK3_MK)
 HS_HSLOGGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslogger+=	hs-hslogger>=1.0.7
+BUILDLINK_API_DEPENDS.hs-hslogger+=	hs-hslogger>=1.1.4
 BUILDLINK_PKGSRCDIR.hs-hslogger?=	../../wip/hs-hslogger
 
 .include "../../wip/hs-mtl/buildlink3.mk"
