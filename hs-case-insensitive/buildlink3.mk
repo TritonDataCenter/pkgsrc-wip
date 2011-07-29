0a27c00510fcecefd293644b241d099f1d7ce784
phonohawk <phonohawk>
2011-07-29 16:25:46 +0000

    Initial revision

---
+++ hs-case-insensitive/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-case-insensitive
+
+.if !defined(HS_CASE_INSENSITIVE_BUILDLINK3_MK)
+HS_CASE_INSENSITIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=0.3
+BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../wip/hs-case-insensitive
+
+.include "../../wip/hs-hashable/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.endif	# HS_CASE_INSENSITIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-case-insensitive
