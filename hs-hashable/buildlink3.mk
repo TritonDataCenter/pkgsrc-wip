45377c879d87c910230a20f7e1a2b7eb713eb807
phonohawk <phonohawk>
2011-07-29 16:19:15 +0000

    Initial revision

---
+++ hs-hashable/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-hashable
+
+.if !defined(HS_HASHABLE_BUILDLINK3_MK)
+HS_HASHABLE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.1
+BUILDLINK_PKGSRCDIR.hs-hashable?=	../../wip/hs-hashable
+
+.include "../../wip/hs-text/buildlink3.mk"
+.endif	# HS_HASHABLE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hashable
