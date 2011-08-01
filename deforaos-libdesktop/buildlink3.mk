d18c14a69ac851f05fcde6823f20ca7de777a4c6
khorben <khorben>
2011-08-01 12:36:20 +0000

    Packaged DeforaOS libDesktop 0.0.5

--- deforaos-libdesktop/buildlink3.mk
+++ deforaos-libdesktop/buildlink3.mk
@@ -5,9 +5,10 @@ BUILDLINK_TREE+=	deforaos-libdesktop
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.1
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.5
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
 
+.include "../../wip/deforaos-libsystem/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif	# DEFORAOS_LIBDESKTOP_BUILDLINK3_MK
 
