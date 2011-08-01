c259b7cfe0b15ddb187d84bb1b91130cf647f2f8
khorben <khorben>
2011-08-01 12:50:21 +0000

    Allow packages to depend on wip/deforaos-keyboard

---
+++ deforaos-keyboard/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	deforaos-keyboard
+
+.if !defined(DEFORAOS_KEYBOARD_BUILDLINK3_MK)
+DEFORAOS_KEYBOARD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-keyboard+=	deforaos-keyboard>=0.0.2
+BUILDLINK_PKGSRCDIR.deforaos-keyboard?=	../../wip/deforaos-keyboard
+
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# DEFORAOS_KEYBOARD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-keyboard
