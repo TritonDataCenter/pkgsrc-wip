8c1041c62814eecedf2bee3371ab2f321cefd861
pallegra <pallegra>
2011-08-01 13:03:36 +0000

    Update i3 to 4.0. http://i3wm.org/downloads/RELEASE-NOTES-4.0.txt

--- i3-devel/patches/patch-common.mk
+++ i3-devel/patches/patch-common.mk
@@ -1,25 +1,8 @@
 $NetBSD$
 
---- common.mk.orig	2011-07-15 17:22:09.000000000 +0000
+--- common.mk.orig	2011-07-31 21:53:54.000000000 +0000
 +++ common.mk
-@@ -1,12 +1,12 @@
- UNAME=$(shell uname)
--DEBUG=1
-+DEBUG?=1
- COVERAGE=0
- INSTALL=install
--PREFIX=/usr
-+PREFIX?=/usr
- ifeq ($(PREFIX),/usr)
--SYSCONFDIR=/etc
-+SYSCONFDIR?=/etc
- else
--SYSCONFDIR=$(PREFIX)/etc
-+SYSCONFDIR?=$(PREFIX)/etc
- endif
- TERM_EMU=xterm
- # The escaping is absurd, but we need to escape for shell, sed, make, define
-@@ -64,8 +64,8 @@ LIBS += $(call ldflags_for_lib, xcb-rand
+@@ -68,8 +68,8 @@ LIBS += $(call ldflags_for_lib, xcb-rand
  LIBS += $(call ldflags_for_lib, xcb, xcb)
  LIBS += $(call ldflags_for_lib, xcursor, Xcursor)
  LIBS += $(call ldflags_for_lib, x11, X11)
