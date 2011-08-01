21ba5a423682610c19847c4e06d4a478daf64c73
pallegra <pallegra>
2011-08-01 15:48:54 +0000

    Whoooops, previous commit was buggy...

--- i3status/patches/patch-src_print__cpu__temperature.c
+++ i3status/patches/patch-src_print__cpu__temperature.c
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- src/print_cpu_temperature.c.orig	2011-08-01 13:43:55.000000000 +0000
+--- src/print_cpu_temperature.c.orig	2011-07-21 21:11:48.000000000 +0000
 +++ src/print_cpu_temperature.c
 @@ -25,10 +25,12 @@ void print_cpu_temperature_info(int zone
          const char *walk;
