31a14013d70be93ba9ab38b7d6b78b5e7099cb69
jihbed <jihbed>
2011-08-05 18:18:42 +0000

    Initial revision

---
+++ gnome-librest/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	gnome-librest
+
+.if !defined(GNOME_LIBREST_BUILDLINK3_MK)
+GNOME_LIBREST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gnome-librest+=	gnome-librest>=0.7.10
+BUILDLINK_PKGSRCDIR.gnome-librest?=	../../wip/gnome-librest
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif	# GNOME_LIBREST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gnome-librest
