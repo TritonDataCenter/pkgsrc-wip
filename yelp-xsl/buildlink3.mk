7478fa02d8c1f0487824f984bf8f1ffb81a53e83
obache <obache>
2011-08-05 14:13:57 +0000

    No need to buildlink with libxml2 and libxslt.

--- yelp-xsl/buildlink3.mk
+++ yelp-xsl/buildlink3.mk
@@ -8,8 +8,6 @@ YELP_XSL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.yelp-xsl+=	yelp-xsl>=2.31.1
 BUILDLINK_PKGSRCDIR.yelp-xsl?=	../../wip/yelp-xsl
 
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../textproc/libxslt/buildlink3.mk"
 .endif	# YELP_XSL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-yelp-xsl
