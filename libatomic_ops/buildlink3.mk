e6e4c7affd2e14ed041a75bd454aeee6ed7939a8
ryo-on <ryo-on>
2011-08-07 01:40:57 +0000

    Update to 20110807
    Merge from pkgsrc/devel/libatomic_ops and update.

--- libatomic_ops/buildlink3.mk
+++ libatomic_ops/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:24:16 joerg Exp $
 
 BUILDLINK_TREE+=	libatomic_ops
 
 .if !defined(LIBATOMIC_OPS_BUILDLINK3_MK)
 LIBATOMIC_OPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=1.2
+BUILDLINK_API_DEPENDS.libatomic_ops+=	libatomic_ops>=20110807
 BUILDLINK_PKGSRCDIR.libatomic_ops?=	../../wip/libatomic_ops
 .endif # LIBATOMIC_OPS_BUILDLINK3_MK
 
