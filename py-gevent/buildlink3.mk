58a9197ca01a5f09fd960dca8b64c787211d809b
asau <asau>
2011-08-12 12:58:59 +0000

    Don't hardcode python version, follow other packages.

--- py-gevent/buildlink3.mk
+++ py-gevent/buildlink3.mk
@@ -1,15 +1,17 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	py26-gevent
+BUILDLINK_TREE+=	py-gevent
 
 .if !defined(PY26_GEVENT_BUILDLINK3_MK)
 PY26_GEVENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py26-gevent+=	py26-gevent>=0.13.0
-BUILDLINK_PKGSRCDIR.py26-gevent?=	../../wip/py-gevent
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_API_DEPENDS.py-gevent+=	${PYPKGPREFIX}-gevent>=0.13.0
+BUILDLINK_PKGSRCDIR.py-gevent?=		../../wip/py-gevent
 
 .include "../../devel/py-cython/buildlink3.mk"
 .include "../../devel/libevent/buildlink3.mk"
 .endif	# PY26_GEVENT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-py26-gevent
+BUILDLINK_TREE+=	-py-gevent
