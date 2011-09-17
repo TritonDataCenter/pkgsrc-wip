37b9f97397478d6aed142f46d769068ad12eefc4
bubuchka <bubuchka>
2011-09-17 21:33:29 +0000

    R-bigmemory is the base package for other libraries from the bigmemory bundle, particularly R-biganalytics and R-bigtabulate.

---
+++ R-bigmemory/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	Rbigmemory
+
+.if !defined(RBIGMEMORY_BUILDLINK3_MK)
+RBIGMEMORY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.Rbigmemory+=	R-bigmemory>=4.2.11
+BUILDLINK_ABI_DEPENDS.Rbigmemory+=	R-bigmemory>=4.2.11
+BUILDLINK_PKGSRCDIR.Rbigmemory?=	../../wip/R-bigmemory
+.endif # RBIGMEMORY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-Rbigmemory
