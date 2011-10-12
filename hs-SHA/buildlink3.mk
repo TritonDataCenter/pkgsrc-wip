9a11fb140fdb7550f305e058958051a35257a9bf
phonohawk <phonohawk>
2011-10-12 19:02:55 +0000

    Upstream update to SHA-1.5.0.0

--- hs-SHA/buildlink3.mk
+++ hs-SHA/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-SHA
 .if !defined(HS_SHA_BUILDLINK3_MK)
 HS_SHA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-SHA+=	hs-SHA>=1.4.0
+BUILDLINK_API_DEPENDS.hs-SHA+=	hs-SHA>=1.5.0
 BUILDLINK_PKGSRCDIR.hs-SHA?=	../../wip/hs-SHA
 
 .include "../../wip/hs-binary/buildlink3.mk"
