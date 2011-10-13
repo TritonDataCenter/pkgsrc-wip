ffc437cabf17bcbd0fd832e6eae521f8fc9bcd51
ryo-on <ryo-on>
2011-10-13 11:31:48 +0000

    Update to 1.2.29.
    bug fix
    Added new features.
    * storing password is encrypted.
    * SyncML support is improved.
    * and so on.

--- taskcoach/patches/patch-setup.py
+++ taskcoach/patches/patch-setup.py
@@ -2,14 +2,14 @@ $NetBSD$
 
 * Support NetBSD
 
---- setup.py.orig	2011-04-30 15:01:33.000000000 +0000
+--- setup.py.orig	2011-09-18 19:48:34.000000000 +0000
 +++ setup.py
-@@ -76,7 +76,7 @@ for language in languages:
+@@ -64,7 +64,7 @@ for language in languages:
      setupOptions['classifiers'].append('Natural Language :: %s'%language)
  
  system = platform.system()
 -if system == 'Linux':
-+if (system == 'Linux') or (system == 'NetBSD'):
-     setupOptions['package_data'] = {'taskcoachlib': ['bin.in/linux/_pysyncml.so']}
++if system == 'Linux' or (system == 'NetBSD') or (system == 'DragonFly'):
      # Add data files for Debian-based systems:
      current_dist = [dist.lower() for dist in platform.dist()]
+     if 'debian' in current_dist or 'ubuntu' in current_dist:
