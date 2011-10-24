c17e67b1cbce6817ce99d599e6094f072bfb6132
ftigeot <ftigeot>
2011-10-24 14:17:45 +0000

    Update to snapshot 2011-10-17

--- libreoffice/patches/patch-sc_Module_sc.mk
+++ libreoffice/patches/patch-sc_Module_sc.mk
@@ -1,14 +1,16 @@
 $NetBSD$
 
---- sc/Module_sc.mk.orig	2011-08-30 15:00:33.000000000 +0000
+--- sc/Module_sc.mk.orig	2011-10-17 16:49:48.000000000 +0000
 +++ sc/Module_sc.mk
-@@ -34,10 +34,4 @@ $(eval $(call gb_Module_add_targets,sc,\
+@@ -34,12 +34,4 @@ $(eval $(call gb_Module_add_targets,sc,\
  	Package_xml \
  ))
  
 -$(eval $(call gb_Module_add_check_targets,sc,\
+-	CppunitTest_sc_filters_test \
 -	CppunitTest_sc_ucalc \
--	CppunitTest_sc_test_filters \
+-	RdbTarget_sc_filters_test \
+-	RdbTarget_sc_ucalc \
 -))
 -
 -
