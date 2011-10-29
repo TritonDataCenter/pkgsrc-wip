39de44c522ae4cd719a96dd84b043169ad0bbb2c
othyro <othyro>
2011-10-29 01:41:26 +0000

    Make sure ccrtp >=2.0.0

--- ccrtp/buildlink3.mk
+++ ccrtp/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	ccrtp
 .if !defined(CCRTP_BUILDLINK3_MK)
 CCRTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=1.5.0
+BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=2.0.0
 BUILDLINK_PKGSRCDIR.ccrtp?=	../../wip/ccrtp
 
 .include "../../wip/commoncpp2/buildlink3.mk"
