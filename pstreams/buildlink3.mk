76982c050598a67a528f34acd45a9f1e6bf4e6ab
ryo-on <ryo-on>
2011-10-29 10:35:55 +0000

    Fix buildlink3.mk file. Drop MAINTAINERship.

--- pstreams/buildlink3.mk
+++ pstreams/buildlink3.mk
@@ -1,11 +1,11 @@
 # $NetBSD$
 
-BUILDLINK_DEPMETHOD.pstreams?=	build
-
 BUILDLINK_TREE+=	pstreams
 
 .if !defined(PSTREAMS_BUILDLINK3_MK)
 PSTREAMS_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.pstreams?=	build
 
 BUILDLINK_API_DEPENDS.pstreams+=	pstreams>=0.7.0
 BUILDLINK_PKGSRCDIR.pstreams?=	../../wip/pstreams
