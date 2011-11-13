d5dbe2b646833201c91d167fbad5828923046988
ryo-on <ryo-on>
2011-11-13 11:03:41 +0000

    Initial revision

---
+++ libalac/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libalac
+
+.if !defined(LIBALAC_BUILDLINK3_MK)
+LIBALAC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libalac+=	libalac>=3
+BUILDLINK_PKGSRCDIR.libalac?=	../../wip/libalac
+.endif	# LIBALAC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libalac
