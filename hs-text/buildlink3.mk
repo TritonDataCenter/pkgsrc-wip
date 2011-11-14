024a0f63608fad7c7760ebc9968720612beafe03
phonohawk <phonohawk>
2011-11-14 09:39:47 +0000

    Upstream update to text-0.11.1.9

--- hs-text/buildlink3.mk
+++ hs-text/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-text
 .if !defined(HS_TEXT_BUILDLINK3_MK)
 HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=0.11
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-text?=	../../wip/hs-text
 
 .include "../../wip/hs-deepseq/buildlink3.mk"
