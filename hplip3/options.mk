515c83fd5c886456473a70b926993a20fd78439c
schnoebe <schnoebe>
2011-11-17 22:09:36 +0000

    Altered the way the PLIST is created, hopefully to something easier to maintain.
    Update to 3.11.10:
    
    ChangeLog:
    
    Added Support for the Following New Printers:
    
      - HP DeskJet 3070 B611 series
      - HP Envy 110 e-All-in-One
      - HP LaserJet Enterprise 500 Color M551
      - HP OfficeJet Pro 8100 N811
      - HP Officejet Pro 8600
      - HP PhotoSmart 5510 e-All-in-One
      - HP PhotoSmart 6510 e-All-in-one
      - HP PhotoSmart 7510 e-All-in-One
    
    Launchpad bugs fixed:
    
      788761 - "Installed Cartridges" option should have more user-friendly
    	   choice names
      793480 - Deskjet 890C Grayscale Truncation, Improper Scaling
      787580 - HP PhotoSmart Pro B8800 series: Borderless not available on
    	   page sizes larger than Legal
      757899 - setup.py crashed with NameErrorHP Photosmart 6510 e-All-in-one
    	   in detectNetworkDevices(): global name 'pas' is not defined
      809904 - Insecure tmp file handling in hpcupsfax.cpp
      519430 - setup.py crashed with KeyError in detectNetworkDevices()
      838785 - Fix crashes when scanning on Deskjet 2050
      839632 - hpcups crash in ModeJbig::~ModeJbig
      845271 - hp-check does not properly check sane dll.conf for hpaio
      859537 - hplip does not work Oneiric beta 2
      803584 - HP Oficejet 6500 e710a feeder scan - Scans using flatbed not
    	   feeder scanner
      819709 - SANE: Error during device I/O (code=9) when scanning from HP
    	   6500A source document feeder (adf)
      730515 - unable to scan Officejet-6500-E710n-z
      825560 - scan.py crashes with TypeError
    
    Issues fixed:
    
      - Unable to configure the printer on OpenSuSE11.3 64 bit
      - The "Type" of ".cpp" file is "Python Program" when adding a ".cpp"
        file from toolbox to print with HP Officejet 7000 E809a Series via USB
        on Fedora 14 32 bit OS
      - Modified the error message which was displayed in case of missing .asc
        file for manual plug-in install
      - Unable to Configure HP OfficeJet Pro 8500 a910 through hp-setup
      - An error occurs if set "page range" as one reasonable range except
        default then click another tag such as "Basic"
      - Print out is clipping with HP LaserJet  p1505n Device
      - An error occurs if print files when click the activated "Default"
        button of "Page Set" to unactivated
      - The "Default" button is activated when the option is default value in
        print settings tab
      - Unable to perform scan in the terminal by using hp-scan with HP
        OfficeJet 4500g-m device
      - An error is getting displayed in the terminal and the scan job cannot
        be performed, when Scanning using the command "hp-scan"
      - Unable to scan multiple pages from ADF with Color and default
        resolution from XSane with HP OfficeJet 6500 E710
      - Unable to send fax with HP LaserJet Professional M1210 Series devices
        when connected through Network and wireless
      - Fax is supported for HP Officejet Pro 8500 A910 device when connected
        through USB

--- hplip3/options.mk
+++ hplip3/options.mk
@@ -10,10 +10,8 @@ PKG_SUGGESTED_OPTIONS=	fax scan
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		fax scan gui
-
 .if !empty(PKG_OPTIONS:Mfax)
-PLIST.fax=		yes
+PLIST_SRC+=		PLIST.fax
 CONFIGURE_ARGS+=	--enable-fax-build
 CONFIGURE_ARGS+=	--enable-dbus-build
 EGFILES+=		cups/pstotiff.convs cups/pstotiff.types
@@ -27,7 +25,7 @@ CONFIGURE_ARGS+=	--disable-fax-build
 .endif
 
 .if !empty(PKG_OPTIONS:Mscan)
-PLIST.scan=		yes
+PLIST_SRC+=		PLIST.scan
 CONFIGURE_ARGS+=	--enable-scan-build
 MESSAGE_SRC+=		MESSAGE.scan
 MESSAGE_SUBST+=		EGDIR=${EGDIR}
@@ -38,7 +36,7 @@ CONFIGURE_ARGS+=	--disable-scan-build
 .endif
 
 .if !empty(PKG_OPTIONS:Mgui)
-PLIST.gui=		yes
+PLIST_SRC+=		PLIST.gui
 CONFIGURE_ARGS+=	--enable-gui-build
 CONFIGURE_ARGS+=	--enable-qt4
 CONFIGURE_ARGS+=	--enable-policykit
