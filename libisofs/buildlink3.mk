7943cb1219677648c76a198a293902aed89ac27d
ryo-on <ryo-on>
2011-11-22 17:13:03 +0000

    Bump BUILDLINK_API_DEPENDS to  libisofs>=1.1.6

--- libisofs/buildlink3.mk
+++ libisofs/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libisofs
 .if !defined(LIBISOFS_BUILDLINK3_MK)
 LIBISOFS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.1.2
+BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.1.6
 BUILDLINK_PKGSRCDIR.libisofs?=	../../wip/libisofs
 
 .include "../../devel/zlib/buildlink3.mk"
