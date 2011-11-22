baadab0409ec2b9051ef254188697db883ea63d5
ryo-on <ryo-on>
2011-11-22 17:09:03 +0000

    Update to 1.1.8
    Changelog:
    libburn novelties:
    
    * Bug fix: Misinterpreted mode page 2A if block descriptors are present
    
    * Enabled recognition of QEMU DVD-ROM 0.12
    
    * New API call burn_drive_re_assess()
    
    cdrskin novelties:
    
    * Avoiding to intermediately close and open drive device file

--- libburn/buildlink3.mk
+++ libburn/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libburn
 .if !defined(LIBBURN_BUILDLINK3_MK)
 LIBBURN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libburn+=	libburn>=1.1.4
+BUILDLINK_API_DEPENDS.libburn+=	libburn>=1.1.8
 BUILDLINK_PKGSRCDIR.libburn?=	../../wip/libburn
 .endif	# LIBBURN_BUILDLINK3_MK
 
