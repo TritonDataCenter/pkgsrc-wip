c8ace53d074cc332bbb270967f5e60a88dac48cc
phonohawk <phonohawk>
2011-12-01 09:58:22 +0000

    Initial revision

---
+++ hs-blaze-html/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-blaze-html
+
+.if !defined(HS_BLAZE_HTML_BUILDLINK3_MK)
+HS_BLAZE_HTML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.4.2
+BUILDLINK_PKGSRCDIR.hs-blaze-html?=	../../wip/hs-blaze-html
+
+.include "../../wip/hs-blaze-builder/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.endif	# HS_BLAZE_HTML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-blaze-html
