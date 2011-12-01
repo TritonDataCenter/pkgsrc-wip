ad4f5388bb0cea27f6b8ccc906b453b0d73c742d
phonohawk <phonohawk>
2011-12-01 23:05:07 +0000

    Upstream update to tagged-0.2.3.1

--- hs-tagged/buildlink3.mk
+++ hs-tagged/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-tagged
 .if !defined(HS_TAGGED_BUILDLINK3_MK)
 HS_TAGGED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.2
+BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.2.3
 BUILDLINK_PKGSRCDIR.hs-tagged?=	../../wip/hs-tagged
 
 .include "../../wip/hs-data-default/buildlink3.mk"
