492eeccb1a7047d5ee5923aef7ef000098d83ff0
phonohawk <phonohawk>
2011-12-04 13:40:06 +0000

    Initial revision

---
+++ hs-test-framework-quickcheck2/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-test-framework-quickcheck2
+
+.if !defined(HS_TEST_FRAMEWORK_QUICKCHECK2_BUILDLINK3_MK)
+HS_TEST_FRAMEWORK_QUICKCHECK2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-test-framework-quickcheck2+=	hs-test-framework-quickcheck2>=0.2.10
+BUILDLINK_PKGSRCDIR.hs-test-framework-quickcheck2?=	../../wip/hs-test-framework-quickcheck2
+
+.include "../../wip/hs-QuickCheck/buildlink3.mk"
+.include "../../wip/hs-test-framework/buildlink3.mk"
+.endif	# HS_TEST_FRAMEWORK_QUICKCHECK2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-test-framework-quickcheck2
