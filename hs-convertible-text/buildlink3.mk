63bb53f962b42eab54921f8c355002d206aa64fb
phonohawk <phonohawk>
2011-12-05 04:57:58 +0000

    Upstream update to convertible-text-0.4.0

--- hs-convertible-text/buildlink3.mk
+++ hs-convertible-text/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-convertible-text
 .if !defined(HS_CONVERTIBLE_TEXT_BUILDLINK3_MK)
 HS_CONVERTIBLE_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-convertible-text+=	hs-convertible-text>=0.3.0
+BUILDLINK_API_DEPENDS.hs-convertible-text+=	hs-convertible-text>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-convertible-text?=	../../wip/hs-convertible-text
 
 .include "../../wip/hs-attempt/buildlink3.mk"
