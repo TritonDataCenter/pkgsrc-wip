a731d3a499b1ee6804e61be759f10a61136a0d31
ryo-on <ryo-on>
2011-12-11 04:11:20 +0000

    Initial revision

---
+++ qemu/patches/patch-ioport.c
@@ -0,0 +1,31 @@
+$NetBSD$
+
+--- ioport.c.orig	2011-12-01 20:07:34.000000000 +0000
++++ ioport.c
+@@ -182,7 +182,7 @@ static uint32_t ioport_readb_thunk(void 
+     IORange *ioport = opaque;
+     uint64_t data;
+ 
+-    ioport->ops->read(ioport, addr - ioport->base, 1, &data);
++    (*ioport->ops->read)(ioport, addr - ioport->base, 1, &data);
+     return data;
+ }
+ 
+@@ -191,7 +191,7 @@ static uint32_t ioport_readw_thunk(void 
+     IORange *ioport = opaque;
+     uint64_t data;
+ 
+-    ioport->ops->read(ioport, addr - ioport->base, 2, &data);
++    (*ioport->ops->read)(ioport, addr - ioport->base, 2, &data);
+     return data;
+ }
+ 
+@@ -200,7 +200,7 @@ static uint32_t ioport_readl_thunk(void 
+     IORange *ioport = opaque;
+     uint64_t data;
+ 
+-    ioport->ops->read(ioport, addr - ioport->base, 4, &data);
++    (*ioport->ops->read)(ioport, addr - ioport->base, 4, &data);
+     return data;
+ }
+ 
