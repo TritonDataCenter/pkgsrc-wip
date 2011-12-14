7033b82abaea80573fef34dd3a3825e46af8724b
khorben <khorben>
2011-12-14 23:32:02 +0000

    Packaged DeforaOS Locker 0.1.1, adding: - settings application (experimental) with desktop application entry; - new authentication plug-ins (slider, password) - new animation plug-ins (xscreensaver, xterm) - new generic plug-ins (early Openmoko support, systray integration) - keyboard and mouse grabbing while active.

---
+++ deforaos-locker/patches/patch-src_plugins_systray.c
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/plugins/systray.c.orig	2011-10-25 10:56:52.000000000 +0000
++++ src/plugins/systray.c
+@@ -16,7 +16,6 @@
+ 
+ 
+ #include <System.h>
+-#include <Desktop.h>
+ #ifdef DEBUG
+ # include <stdio.h>
+ #endif
