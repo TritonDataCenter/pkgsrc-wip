5b74fb94d54ce203d8715ab2f58c530dc81c5e85
khorben <khorben>
2011-12-14 23:43:03 +0000

    Allow packages to build against DeforaOS Locker

---
+++ deforaos-locker/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	deforaos-locker
+
+.if !defined(DEFORAOS_LOCKER_BUILDLINK3_MK)
+DEFORAOS_LOCKER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-locker+=	deforaos-locker>=0.1.1
+BUILDLINK_PKGSRCDIR.deforaos-locker?=	../../wip/deforaos-locker
+
+.include "../../wip/deforaos-libdesktop/buildlink3.mk"
+.endif	# DEFORAOS_LOCKER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-locker
