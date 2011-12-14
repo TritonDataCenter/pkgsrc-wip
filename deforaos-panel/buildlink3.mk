56bbbd9f388f11b5826595af5b044c09d2f2511f
khorben <khorben>
2011-12-14 23:26:43 +0000

    Allow packages to build against DeforaOS Panel

---
+++ deforaos-panel/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	deforaos-panel
+
+.if !defined(DEFORAOS_PANEL_BUILDLINK3_MK)
+DEFORAOS_PANEL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.2.10
+BUILDLINK_PKGSRCDIR.deforaos-panel?=	../../wip/deforaos-panel
+
+.include "../../wip/deforaos-libsystem/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# DEFORAOS_PANEL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-panel
