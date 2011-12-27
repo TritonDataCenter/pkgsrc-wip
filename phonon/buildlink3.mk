9f3eb8e75adb4b7163a27321536c33d18c04c7e8
absd <absd>
2011-12-27 10:01:07 +0000

    set BUILDLINK_PKGSRCDIR to wip/... rather than multimedia..., plus fix DEPENDS version

--- phonon/buildlink3.mk
+++ phonon/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	phonon
 .if !defined(PHONON_BUILDLINK3_MK)
 PHONON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.phonon+=	phonon>=4.1.83
-BUILDLINK_ABI_DEPENDS.phonon?=	phonon>=4.4.2nb2
-BUILDLINK_PKGSRCDIR.phonon?=	../../multimedia/phonon
+BUILDLINK_API_DEPENDS.phonon+=	phonon>=4.5.0
+BUILDLINK_ABI_DEPENDS.phonon?=	phonon>=4.5.0
+BUILDLINK_PKGSRCDIR.phonon?=	../../wip/phonon
 
 .include "../../x11/qt4-libs/buildlink3.mk"
 .include "../../x11/qt4-qdbus/buildlink3.mk"
