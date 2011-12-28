febff06d5ddebf2ebfee5239a0f2e6e6fda42df5
jihbed <jihbed>
2011-12-28 20:20:32 +0000

    facility lib link

---
+++ bagel/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	bagel
+
+.if !defined(BAGEL_BUILDLINK3_MK)
+BAGEL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.bagel+=	bagel>=2.0.0
+BUILDLINK_PKGSRCDIR.bagel?=	../../wip/bagel
+.endif	# BAGEL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-bagel
