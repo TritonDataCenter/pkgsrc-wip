94892d857271f86d53157c3e1acc3ade08177761
othyro <othyro>
2012-01-03 19:55:58 +0000

    Remove dependency on wip/commoncpp2. wip/ucommon has a compatibility API.

--- ccrtp/buildlink3.mk
+++ ccrtp/buildlink3.mk
@@ -9,7 +9,6 @@ BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=2.0.0
 BUILDLINK_PKGSRCDIR.ccrtp?=	../../wip/ccrtp
 
 .include "../../wip/ucommon/buildlink3.mk"
-.include "../../wip/commoncpp2/buildlink3.mk"
 .endif # CCRTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ccrtp
