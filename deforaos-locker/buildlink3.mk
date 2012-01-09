e09863c91547e0d3fb1b416c4d015b4afeb41416
khorben <khorben>
2012-01-09 16:11:52 +0000

    Updated most of the DeforaOS desktop packages: - portability fixes for Gtk+ 3.0 - revised the respecting plug-in systems (potential instabilities) - bugfixes and improvements

--- deforaos-locker/buildlink3.mk
+++ deforaos-locker/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	deforaos-locker
 .if !defined(DEFORAOS_LOCKER_BUILDLINK3_MK)
 DEFORAOS_LOCKER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-locker+=	deforaos-locker>=0.1.1
+BUILDLINK_API_DEPENDS.deforaos-locker+=	deforaos-locker>=0.1.2
 BUILDLINK_PKGSRCDIR.deforaos-locker?=	../../wip/deforaos-locker
 
 .include "../../wip/deforaos-libdesktop/buildlink3.mk"
--- deforaos-panel/buildlink3.mk
+++ deforaos-panel/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	deforaos-panel
 .if !defined(DEFORAOS_PANEL_BUILDLINK3_MK)
 DEFORAOS_PANEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.2.10
+BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.2.12
 BUILDLINK_PKGSRCDIR.deforaos-panel?=	../../wip/deforaos-panel
 
 .include "../../wip/deforaos-libsystem/buildlink3.mk"
