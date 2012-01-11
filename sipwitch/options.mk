0ec65fce92aa9622d18aa2fc65d37fad5d612f45
othyro <othyro>
2012-01-11 23:30:24 +0000

    Initial revision

---
+++ sipwitch/options.mk
@@ -0,0 +1,11 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.sipwitch
+PKG_SUPPORTED_OPTIONS=	qt
+PKG_SUGGESTED_OPTIONS=	# blank
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mqt)
+.include "../../x11/qt4-tools/buildlink3.mk"
+.endif
