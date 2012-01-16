cd042c07c2b334c88e2494d359c45bcf1f78f9dd
dukzcry <dukzcry>
2012-01-16 00:15:10 +0000

    Initial revision

---
+++ cl-portable-clx/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+#
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.cl-${SHORTNAME:S/^cl-//}
+PKG_OPTIONS_REQUIRED_GROUPS=	lisp
+PKG_OPTIONS_GROUP.lisp=		sbcl
+PKG_SUGGESTED_OPTIONS+=		sbcl
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=			${PKG_OPTIONS_GROUP.lisp}
+.for opt in ${PKG_OPTIONS_GROUP.lisp}
+.  if !empty(PKG_OPTIONS:M${opt})
+PLIST.${opt}=			yes
+COMMON_LISP_SYSTEM:=		${opt}
+.  endif
+.endfor
