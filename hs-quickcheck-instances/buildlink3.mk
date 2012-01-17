a4497d6622a906a737fea4c21e40689d4bc211f1
phonohawk <phonohawk>
2012-01-17 14:21:07 +0000

    Initial revision

---
+++ hs-quickcheck-instances/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-quickcheck-instances
+
+.if !defined(HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK)
+HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-quickcheck-instances+=	hs-quickcheck-instances>=0.1.0
+BUILDLINK_PKGSRCDIR.hs-quickcheck-instances?=	../../wip/hs-quickcheck-instances
+
+.include "../../wip/hs-QuickCheck/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.endif	# HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-quickcheck-instances
