4823dbbdf6c1abb8d503a11231f1fbc1be96f6f4
phonohawk <phonohawk>
2012-02-01 16:28:59 +0000

    Initial revision

---
+++ hs-semigroupoids/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-semigroupoids
+
+.if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
+HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=1.2.6
+BUILDLINK_PKGSRCDIR.hs-semigroupoids?=	../../wip/hs-semigroupoids
+
+.include "../../wip/hs-comonad/buildlink3.mk"
+.include "../../wip/hs-contravariant/buildlink3.mk"
+.include "../../wip/hs-semigroups/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
+.endif	# HS_SEMIGROUPOIDS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-semigroupoids
