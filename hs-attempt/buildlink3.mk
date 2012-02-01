8a97a50bd1931797aadf74667db30b9a804d0d7d
phonohawk <phonohawk>
2012-02-01 16:55:58 +0000

    Upstream update to attempt-0.4.0

--- hs-attempt/buildlink3.mk
+++ hs-attempt/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-attempt
 .if !defined(HS_ATTEMPT_BUILDLINK3_MK)
 HS_ATTEMPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attempt+=	hs-attempt>=0.3.1
+BUILDLINK_API_DEPENDS.hs-attempt+=	hs-attempt>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-attempt?=	../../wip/hs-attempt
 
 .include "../../wip/hs-failure/buildlink3.mk"
