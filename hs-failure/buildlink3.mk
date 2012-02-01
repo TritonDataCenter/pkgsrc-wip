d3d60b35eaf4d21127fce1cc2d8010c778c8f11a
phonohawk <phonohawk>
2012-02-01 15:49:18 +0000

    Upstream update to failure-0.2.0

--- hs-failure/buildlink3.mk
+++ hs-failure/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-failure
 .if !defined(HS_FAILURE_BUILDLINK3_MK)
 HS_FAILURE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-failure+=	hs-failure>=0.1.2
+BUILDLINK_API_DEPENDS.hs-failure+=	hs-failure>=0.2.0
 BUILDLINK_PKGSRCDIR.hs-failure?=	../../wip/hs-failure
 
 .include "../../wip/hs-transformers/buildlink3.mk"
