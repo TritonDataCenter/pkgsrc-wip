8efe99173a2a770766e3b343adbc62b1c3711e55
phonohawk <phonohawk>
2012-02-02 13:11:52 +0000

    Initial revision

---
+++ hs-vector-bytestring/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-vector-bytestring
+
+.if !defined(HS_VECTOR_BYTESTRING_BUILDLINK3_MK)
+HS_VECTOR_BYTESTRING_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-vector-bytestring+=	hs-vector-bytestring>=0.0.0
+BUILDLINK_PKGSRCDIR.hs-vector-bytestring?=	../../wip/hs-vector-bytestring
+
+.include "../../wip/hs-deepseq/buildlink3.mk"
+.include "../../wip/hs-primitive/buildlink3.mk"
+.include "../../wip/hs-vector/buildlink3.mk"
+.endif	# HS_VECTOR_BYTESTRING_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-vector-bytestring
