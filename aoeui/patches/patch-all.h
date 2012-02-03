ff5ba904d018a415d7bc372c20ce1b85d64c27f1
realzhtw <realzhtw>
2012-02-03 17:07:30 +0000

    Updated to 1.6

--- aoeui/patches/patch-all.h
+++ aoeui/patches/patch-all.h
@@ -1,16 +1,15 @@
 $NetBSD$
 
---- all.h.orig	2010-10-14 21:48:53.000000000 +0000
+--- all.h.orig	2011-11-10 18:10:25.000000000 +0000
 +++ all.h
-@@ -23,8 +23,10 @@
+@@ -23,7 +23,9 @@
  #include <sys/stat.h>
  #include <sys/time.h>
  #include <sys/wait.h>
--#ifdef __APPLE__
-+#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__)
- # include <util.h>
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined __APPLE__ || defined BSD
++#if defined __FreeBSD__
 +# include <libutil.h>
++#elif defined __APPLE__ || defined __NetBSD__ || defined __OpenBSD__ || defined __DragonFly__
+ # include <util.h>
  #else
  # include <pty.h>
- #endif
