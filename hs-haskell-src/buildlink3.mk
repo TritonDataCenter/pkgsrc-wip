10e636e35b056ba0985b81b829e0cc57fbca6f29
phonohawk <phonohawk>
2012-02-11 04:54:16 +0000

    Upstream update to haskell-src-1.0.1.5

--- hs-haskell-src/buildlink3.mk
+++ hs-haskell-src/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-haskell-src
 .if !defined(HS_HASKELL_SRC_BUILDLINK3_MK)
 HS_HASKELL_SRC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskell-src+=	hs-haskell-src>=1.0
+BUILDLINK_API_DEPENDS.hs-haskell-src+=	hs-haskell-src>=1.0.1
 BUILDLINK_PKGSRCDIR.hs-haskell-src?=	../../wip/hs-haskell-src
 
 .include "../../wip/hs-syb/buildlink3.mk"
