8aaf9009a32d4ed3cfaae1aab297e013475f086f
phonohawk <phonohawk>
2012-02-11 04:38:13 +0000

    Upstream update to hxt-9.2.0

--- hs-hxt/buildlink3.mk
+++ hs-hxt/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-hxt
 .if !defined(HS_HXT_BUILDLINK3_MK)
 HS_HXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt+=	hs-hxt>=9.1
+BUILDLINK_API_DEPENDS.hs-hxt+=	hs-hxt>=9.2.0
 BUILDLINK_PKGSRCDIR.hs-hxt?=	../../wip/hs-hxt
 
 .include "../../wip/hs-binary/buildlink3.mk"
@@ -13,6 +13,7 @@ BUILDLINK_PKGSRCDIR.hs-hxt?=	../../wip/hs-hxt
 .include "../../wip/hs-hxt-charproperties/buildlink3.mk"
 .include "../../wip/hs-hxt-regex-xmlschema/buildlink3.mk"
 .include "../../wip/hs-hxt-unicode/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-network/buildlink3.mk"
 .include "../../wip/hs-parsec/buildlink3.mk"
 .endif	# HS_HXT_BUILDLINK3_MK
