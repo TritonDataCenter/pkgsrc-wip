b21fe4d0f0ac2f43cabdd72cce580135a04a3b29
obache <obache>
2012-02-19 05:29:00 +0000

    Update mikutter-snapshot to 0.1.0.690.

--- mikutter-snapshot/patches/patch-core_mui_cairo__inner__tl.rb
+++ mikutter-snapshot/patches/patch-core_mui_cairo__inner__tl.rb
@@ -2,7 +2,7 @@ $NetBSD$
 
 * use system bseach library instead of included one.
 
---- core/mui/cairo_inner_tl.rb.orig	2011-09-03 16:58:10.000000000 +0000
+--- core/mui/cairo_inner_tl.rb.orig	2012-02-13 14:45:50.000000000 +0000
 +++ core/mui/cairo_inner_tl.rb
 @@ -1,7 +1,7 @@
  # -*- coding: utf-8 -*-
@@ -11,5 +11,5 @@ $NetBSD$
 -miquire :lib, 'ruby-bsearch-1.5/bsearch'
 +miquire :lib, 'bsearch'
  miquire :lib, 'uithreadonly'
+ require 'gtk2'
  
- class Gtk::TimeLine::InnerTL < Gtk::CRUD
--- mikutter-snapshot/patches/patch-core_utils.rb
+++ mikutter-snapshot/patches/patch-core_utils.rb
@@ -2,9 +2,9 @@ $NetBSD$
 
 * use included escape library instead of system one.
 
---- core/utils.rb.orig	2011-11-11 15:33:04.000000000 +0000
+--- core/utils.rb.orig	2012-02-14 15:07:52.000000000 +0000
 +++ core/utils.rb
-@@ -33,7 +33,7 @@ Dir::chdir(File::dirname(__FILE__))
+@@ -32,7 +32,7 @@ Dir::chdir(File::dirname(__FILE__))
  ['.', 'lib', 'miku'].each{|path|
    $LOAD_PATH.push(File.expand_path(File.join(Dir.pwd, path)))
  }
