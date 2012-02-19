0e7c76dcaf89c9617aefd6d047c9812f5627fdee
guigui2 <guigui2>
2012-02-19 16:45:59 +0000

    Adjust buildlink3 file.

--- xfce4-dev-tools/buildlink3.mk
+++ xfce4-dev-tools/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	xfce4-dev-tools
 .if !defined(XFCE4_DEV_TOOLS_BUILDLINK3_MK)
 XFCE4_DEV_TOOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-dev-tools+=	xfce4-dev-tools>=4.6.0
-BUILDLINK_PKGSRCDIR.xfce4-dev-tools?=	../../devel/xfce4-dev-tools
+BUILDLINK_API_DEPENDS.xfce4-dev-tools+=	xfce4-dev-tools>=4.8.0
+BUILDLINK_PKGSRCDIR.xfce4-dev-tools?=	../../wip/xfce4-dev-tools
 BUILDLINK_DEPMETHOD.xfce4-dev-tools?=	build
 .endif # XFCE4_DEV_TOOLS_BUILDLINK3_MK
 
