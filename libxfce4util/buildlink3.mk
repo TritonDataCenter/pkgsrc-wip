2ef05cd6ab7a6f1754d715bf7fa59afae2fab502
guigui2 <guigui2>
2012-02-19 16:49:33 +0000

    Update wip/libxfce4util to 4.8.2.
    pkgsrc changes:
    - adjust bl3.
    
    Upstream changes:
    
    4.8.2
    =====
    - Change the version string to 4.8.
    - Fixed incorrect assertion in xfce_strjoin (bug #7122).
    - Fix "empty declaration" compiler warnings.
    - Update FSF address and add missing licenses.
    - Fix undef in libxfce4util.h header.
    - Remove the internal slice allocator macros.
    - Use chain to free slice instead of a loop.
    - Translation updates (ko, ru, sk, zh_CN, nl).

--- libxfce4util/buildlink3.mk
+++ libxfce4util/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	libxfce4util
 .if !defined(LIBXFCE4UTIL_BUILDLINK3_MK)
 LIBXFCE4UTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.6.0
-BUILDLINK_ABI_DEPENDS.libxfce4util?=	libxfce4util>=4.6.1nb4
+BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.8.0
+BUILDLINK_ABI_DEPENDS.libxfce4util?=	libxfce4util>=4.8.0
 BUILDLINK_PKGSRCDIR.libxfce4util?=	../../wip/libxfce4util
 
 .include "../../devel/glib2/buildlink3.mk"
