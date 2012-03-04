368df4d30be67a5f51d1cd434323f2b3999b4602
phonohawk <phonohawk>
2012-03-04 06:45:47 +0000

    Initial revision

---
+++ hs-unordered-containers/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-unordered-containers
+
+.if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
+HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.1.4
+BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../wip/hs-unordered-containers
+
+.include "../../wip/hs-hashable/buildlink3.mk"
+.endif	# HS_UNORDERED_CONTAINERS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-unordered-containers
