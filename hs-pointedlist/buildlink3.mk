c1fce83753558057616ff569027ea92432421640
phonohawk <phonohawk>
2012-03-04 07:15:32 +0000

    Upstream update to pointedlist-0.4.0.3

--- hs-pointedlist/buildlink3.mk
+++ hs-pointedlist/buildlink3.mk
@@ -5,10 +5,9 @@ BUILDLINK_TREE+=	hs-pointedlist
 .if !defined(HS_POINTEDLIST_BUILDLINK3_MK)
 HS_POINTEDLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pointedlist+=	hs-pointedlist>=0.4
+BUILDLINK_API_DEPENDS.hs-pointedlist+=	hs-pointedlist>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-pointedlist?=	../../wip/hs-pointedlist
 
-.include "../../wip/hs-binary/buildlink3.mk"
 .include "../../wip/hs-derive/buildlink3.mk"
 .include "../../wip/hs-fclabels/buildlink3.mk"
 .endif	# HS_POINTEDLIST_BUILDLINK3_MK
