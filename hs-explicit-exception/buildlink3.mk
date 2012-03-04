aa982b091414187eac565afcf5e9754ffebb1a42
phonohawk <phonohawk>
2012-03-04 08:40:09 +0000

    Upstream update to explicit-exception-0.1.6

--- hs-explicit-exception/buildlink3.mk
+++ hs-explicit-exception/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-explicit-exception
 .if !defined(HS_EXPLICIT_EXCEPTION_BUILDLINK3_MK)
 HS_EXPLICIT_EXCEPTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-explicit-exception+=	hs-explicit-exception>=0.1.4
+BUILDLINK_API_DEPENDS.hs-explicit-exception+=	hs-explicit-exception>=0.1.6
 BUILDLINK_PKGSRCDIR.hs-explicit-exception?=	../../wip/hs-explicit-exception
 
 .include "../../wip/hs-transformers/buildlink3.mk"
