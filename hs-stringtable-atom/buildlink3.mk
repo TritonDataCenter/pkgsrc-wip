d2d49cbdeaa2a7552264ca58f5e1d8794e449b66
phonohawk <phonohawk>
2012-03-04 07:34:47 +0000

    Initial revision

---
+++ hs-stringtable-atom/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-stringtable-atom
+
+.if !defined(HS_STRINGTABLE_ATOM_BUILDLINK3_MK)
+HS_STRINGTABLE_ATOM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-stringtable-atom+=	hs-stringtable-atom>=0.0.6
+BUILDLINK_PKGSRCDIR.hs-stringtable-atom?=	../../wip/hs-stringtable-atom
+
+.include "../../wip/hs-syb/buildlink3.mk"
+.endif	# HS_STRINGTABLE_ATOM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-stringtable-atom
