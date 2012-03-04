7f17b870cc8b9f815bb254ea8ffcfc4121fdc73c
phonohawk <phonohawk>
2012-03-04 07:47:44 +0000

    Upstream update to executable-path-0.0.3

--- hs-executable-path/buildlink3.mk
+++ hs-executable-path/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-executable-path
 .if !defined(HS_EXECUTABLE_PATH_BUILDLINK3_MK)
 HS_EXECUTABLE_PATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-executable-path+=	hs-executable-path>=0.0
+BUILDLINK_API_DEPENDS.hs-executable-path+=	hs-executable-path>=0.0.3
 BUILDLINK_PKGSRCDIR.hs-executable-path?=	../../wip/hs-executable-path
 .endif	# HS_EXECUTABLE_PATH_BUILDLINK3_MK
 
