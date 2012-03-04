e12f6671e152824c77ef70c67e3d210bcf0daa6b
phonohawk <phonohawk>
2012-03-04 04:37:12 +0000

    Upstream update to ghc-7.4.1

--- ghc/buildlink3.mk
+++ ghc/buildlink3.mk
@@ -5,9 +5,10 @@ BUILDLINK_TREE+=	ghc
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=7.0
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=7.4.1
 BUILDLINK_PKGSRCDIR.ghc?=	../../wip/ghc
 
+.include "../../devel/ncurses/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
 .endif	# GHC_BUILDLINK3_MK
