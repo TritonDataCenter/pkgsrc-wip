d91078a617225743035c45ce18309255a87b1272
phonohawk <phonohawk>
2012-03-04 07:51:42 +0000

    binary is bundled in ghc-7.4.1

--- hs-dyre/buildlink3.mk
+++ hs-dyre/buildlink3.mk
@@ -5,10 +5,9 @@ BUILDLINK_TREE+=	hs-dyre
 .if !defined(HS_DYRE_BUILDLINK3_MK)
 HS_DYRE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dyre+=	hs-dyre>=0.8
+BUILDLINK_API_DEPENDS.hs-dyre+=	hs-dyre>=0.8.6
 BUILDLINK_PKGSRCDIR.hs-dyre?=	../../wip/hs-dyre
 
-.include "../../wip/hs-binary/buildlink3.mk"
 .include "../../wip/hs-executable-path/buildlink3.mk"
 .include "../../wip/hs-ghc-paths/buildlink3.mk"
 .include "../../wip/hs-io-storage/buildlink3.mk"
