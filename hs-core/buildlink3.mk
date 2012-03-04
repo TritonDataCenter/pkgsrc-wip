0e75e6e6b41da36948f6b63b292ed995fd346312
phonohawk <phonohawk>
2012-03-04 08:59:45 +0000

    Initial revision

---
+++ hs-core/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-core
+
+.if !defined(HS_CORE_BUILDLINK3_MK)
+HS_CORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-core+=	hs-core>=0.5
+BUILDLINK_PKGSRCDIR.hs-core?=	../../wip/hs-core
+
+.include "../../wip/hs-parsec/buildlink3.mk"
+.endif	# HS_CORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-core
