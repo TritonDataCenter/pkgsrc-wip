b6f1b193cf63447045acc1cc087eb717cac0a36e
phonohawk <phonohawk>
2012-03-04 07:19:03 +0000

    binary is bundled in ghc-7.4.1

--- hs-SHA/buildlink3.mk
+++ hs-SHA/buildlink3.mk
@@ -8,7 +8,6 @@ HS_SHA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-SHA+=	hs-SHA>=1.5.0
 BUILDLINK_PKGSRCDIR.hs-SHA?=	../../wip/hs-SHA
 
-.include "../../wip/hs-binary/buildlink3.mk"
 .endif	# HS_SHA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-SHA
