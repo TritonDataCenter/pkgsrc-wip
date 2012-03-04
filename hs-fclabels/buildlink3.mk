5860a817286fe1dde979e70eaa31fcb751283a9e
phonohawk <phonohawk>
2012-03-04 07:06:36 +0000

    Upstream update to fclabels-1.0.4

--- hs-fclabels/buildlink3.mk
+++ hs-fclabels/buildlink3.mk
@@ -5,10 +5,11 @@ BUILDLINK_TREE+=	hs-fclabels
 .if !defined(HS_FCLABELS_BUILDLINK3_MK)
 HS_FCLABELS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fclabels+=	hs-fclabels>=0.11
+BUILDLINK_API_DEPENDS.hs-fclabels+=	hs-fclabels>=1.0.4
 BUILDLINK_PKGSRCDIR.hs-fclabels?=	../../wip/hs-fclabels
 
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_FCLABELS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-fclabels
