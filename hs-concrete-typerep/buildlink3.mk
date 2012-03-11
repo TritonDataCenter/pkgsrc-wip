0aaab0745d7f252292927ffef54c8f2812345311
phonohawk <phonohawk>
2012-03-11 05:49:07 +0000

    Initial revision

---
+++ hs-concrete-typerep/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-concrete-typerep
+
+.if !defined(HS_CONCRETE_TYPEREP_BUILDLINK3_MK)
+HS_CONCRETE_TYPEREP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-concrete-typerep+=	hs-concrete-typerep>=0.1.0
+BUILDLINK_PKGSRCDIR.hs-concrete-typerep?=	../../wip/hs-concrete-typerep
+
+.include "../../wip/hs-hashable/buildlink3.mk"
+.endif	# HS_CONCRETE_TYPEREP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-concrete-typerep
