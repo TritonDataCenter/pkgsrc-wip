8abfe6302de51561989a4ce6246dcbae30ae419d
phonohawk <phonohawk>
2012-03-11 05:59:00 +0000

    Upstream update to Diff-0.1.3

--- hs-Diff/buildlink3.mk
+++ hs-Diff/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-Diff
 .if !defined(HS_DIFF_BUILDLINK3_MK)
 HS_DIFF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.1
+BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-Diff?=	../../wip/hs-Diff
 .endif	# HS_DIFF_BUILDLINK3_MK
 
