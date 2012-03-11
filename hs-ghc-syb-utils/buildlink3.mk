0ff55305c73925df9149bdd7d4ee86a18cdce085
phonohawk <phonohawk>
2012-03-11 08:38:37 +0000

    Initial revision

---
+++ hs-ghc-syb-utils/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-ghc-syb-utils
+
+.if !defined(HS_GHC_SYB_UTILS_BUILDLINK3_MK)
+HS_GHC_SYB_UTILS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-ghc-syb-utils+=	hs-ghc-syb-utils>=0.2.1
+BUILDLINK_PKGSRCDIR.hs-ghc-syb-utils?=	../../wip/hs-ghc-syb-utils
+
+.include "../../wip/hs-syb/buildlink3.mk"
+.endif	# HS_GHC_SYB_UTILS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-ghc-syb-utils
