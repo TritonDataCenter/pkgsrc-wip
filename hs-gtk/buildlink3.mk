5e6ed1512bfca7d4cf8fe5466d7800dcf09f5f81
phonohawk <phonohawk>
2012-03-11 08:17:52 +0000

    Upstream update to gtk-0.12.3

--- hs-gtk/buildlink3.mk
+++ hs-gtk/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-gtk
 .if !defined(HS_GTK_BUILDLINK3_MK)
 HS_GTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-gtk+=	hs-gtk>=0.12
+BUILDLINK_API_DEPENDS.hs-gtk+=	hs-gtk>=0.12.3
 BUILDLINK_PKGSRCDIR.hs-gtk?=	../../wip/hs-gtk
 
 .include "../../wip/hs-cairo/buildlink3.mk"
