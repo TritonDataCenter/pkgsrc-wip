455767c24925bc70c5a3b8a894bcad8a6b9ded53
thomasklausner <thomasklausner>
2012-03-16 11:47:22 +0000

    Depend on current version.

--- libexttextcat/buildlink3.mk
+++ libexttextcat/buildlink3.mk
@@ -4,7 +4,7 @@ BUILDLINK_TREE+=	libexttextcat
 
 .if !defined(LIBEXTTEXTCAT_BUILDLINK3_MK)
 LIBEXTTEXTCAT_BUILDLINK3_MK:=
-BUILDLINK_API_DEPENDS.libexttextcat+=	libexttextcat>=2.2
+BUILDLINK_API_DEPENDS.libexttextcat+=	libexttextcat>=3.2
 BUILDLINK_PKGSRCDIR.libexttextcat?=	../../wip/libexttextcat
 .endif
 
