26ecf2556d823387fd83e077e1890a1694245185
outpaddling <outpaddling>
2012-03-22 19:53:47 +0000

    Initial revision

---
+++ armadillo/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	armadillo
+
+.if !defined(ARMADILLO_BUILDLINK3_MK)
+ARMADILLO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=2.2.1
+BUILDLINK_PKGSRCDIR.armadillo?=	../../jb-wip/armadillo
+
+.include "../../math/blas/buildlink3.mk"
+.include "../../math/lapack/buildlink3.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/boost-headers/buildlink3.mk"
+.endif	# ARMADILLO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-armadillo
