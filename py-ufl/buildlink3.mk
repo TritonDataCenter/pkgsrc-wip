df90bf7b34bed7f9f873d1733b8467dcce4fdf4d
outpaddling <outpaddling>
2012-03-27 15:49:51 +0000

    Initial revision

---
+++ py-ufl/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	py26-ufl
+
+.if !defined(PY26_UFL_BUILDLINK3_MK)
+PY26_UFL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py26-ufl+=	py26-ufl>=1.0.0
+BUILDLINK_PKGSRCDIR.py26-ufl?=	../../jb-wip/py-ufl
+.endif	# PY26_UFL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py26-ufl
