8913bdf3ac641c73a2787ca78e5b57ecc167e6a0
outpaddling <outpaddling>
2012-03-27 16:09:28 +0000

    Initial revision

---
+++ swig2/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	swig
+
+.if !defined(SWIG_BUILDLINK3_MK)
+SWIG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.swig+=	swig>=2.0.4
+BUILDLINK_PKGSRCDIR.swig?=	../../jb-wip/swig2
+
+.include "../../devel/pcre/buildlink3.mk"
+.endif	# SWIG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-swig
