1194f65c0bbb8e2792a3d25468813b5c28944de4
outpaddling <outpaddling>
2012-03-27 15:56:16 +0000

    Initial revision

---
+++ py-viper/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	py26-viper
+
+.if !defined(PY26_VIPER_BUILDLINK3_MK)
+PY26_VIPER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py26-viper+=	py26-viper>=1.0.0
+BUILDLINK_PKGSRCDIR.py26-viper?=	../../jb-wip/py-viper
+.endif	# PY26_VIPER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py26-viper
---
+++ ufc/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	ufc
+
+.if !defined(UFC_BUILDLINK3_MK)
+UFC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ufc+=	ufc>=2.0.5
+BUILDLINK_PKGSRCDIR.ufc?=	../../jb-wip/ufc
+.endif	# UFC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ufc
