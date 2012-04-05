2f69a4eb3e05c39302ebdb3e8e9e3bd372555f7f
thomasklausner <thomasklausner>
2012-04-05 13:34:09 +0000

    Remove unnecessary line.

--- cloog/buildlink3.mk
+++ cloog/buildlink3.mk
@@ -6,7 +6,6 @@ BUILDLINK_TREE+=	cloog
 CLOOG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cloog+=	cloog>=0.16.2
-BUILDLINK_ABI_DEPENDS.cloog?=	cloog>=0.16.2
 BUILDLINK_PKGSRCDIR.cloog?=	../../wip/cloog
 
 .endif # CLOOG_BUILDLINK3_MK
