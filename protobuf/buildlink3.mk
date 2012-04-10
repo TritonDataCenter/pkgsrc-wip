531446c54dc508b4ab67c7f83eccb7ba93efe30e
genolopolis <genolopolis>
2012-04-10 02:02:13 +0000

    update to 2.4.1 and add a license

--- protobuf/buildlink3.mk
+++ protobuf/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	protobuf
 .if !defined(PROTOBUF_BUILDLINK3_MK)
 PROTOBUF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=2.3.0
+BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=2.4.0
 BUILDLINK_PKGSRCDIR.protobuf?=	../../wip/protobuf
 .endif	# PROTOBUF_BUILDLINK3_MK
 
