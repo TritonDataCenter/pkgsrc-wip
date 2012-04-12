d295dad5d9677828af522f3ff75e9b04285a38f1
gschwarz <gschwarz>
2012-04-12 18:06:32 +0000

    on Linux (at least some more recent version) exit() is found in <cstdlib>

---
+++ tctool/patches/patch-tctool.cpp
@@ -0,0 +1,14 @@
+$NetBSD$
+
+--- tctool.cpp.orig	2012-04-12 19:51:41.000000000 +0200
++++ tctool.cpp	2012-04-12 19:53:40.000000000 +0200
+@@ -33,6 +33,9 @@
+ #include <string.h>
+ #include <unistd.h>
+ #include <signal.h>
++#ifdef CSTDLIB
++#include <cstdlib>
++#endif
+ 
+ std::string                      options = "I:m:i:r:t:p:c:MUdsSCRh";
+ std::string                      ethDev  = "eth0";
