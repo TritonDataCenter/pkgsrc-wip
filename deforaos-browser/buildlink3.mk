c356915dd415721cb0951d3f4fa309582a1f28ae
khorben <khorben>
2012-04-19 14:27:29 +0000

    Packaged DeforaOS Panel 0.2.13, with: - introduction of a desktop notification tool, panel-notify; - support for screen rotation (through "desktop" from deforaos-browser) - new applet to handle the volume controls (through "mixer" from deforaos-mixer)

---
+++ deforaos-browser/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	deforaos-browser
+
+.if !defined(DEFORAOS_BROWSER_BUILDLINK3_MK)
+DEFORAOS_BROWSER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.deforaos-browser+=	deforaos-browser>=0.4.5
+BUILDLINK_PKGSRCDIR.deforaos-browser?=	../../wip/deforaos-browser
+
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# DEFORAOS_BROWSER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-deforaos-browser
