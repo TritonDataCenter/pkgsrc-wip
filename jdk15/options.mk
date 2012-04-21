5b42c8e0c62b681c5f706150fc31cc1d71b763f9
ftigeot <ftigeot>
2012-04-21 18:19:14 +0000

    Remove broken jce option.

--- jdk15/options.mk
+++ jdk15/options.mk
@@ -1,21 +1,11 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.jdk15
-PKG_SUPPORTED_OPTIONS=		sun-jre-jce inet6
+PKG_SUPPORTED_OPTIONS=		inet6
 
 #.include "../../www/seamonkey/gecko-options.mk"
 .include "../../mk/bsd.options.mk"
 
 .if empty(PKG_OPTIONS:Minet6)
 MAKE_ENV+=		DONT_ENABLE_IPV6=YES
-.endif
-
-###
-### Java(TM) Cryptography Extension (JCE)
-###
-.if !empty(PKG_OPTIONS:Msun-jre-jce)
-DISTFILES+=	jce_policy-1_5_0.zip
-PLIST.jce=	yes
-post-build:
-	cd ${WRKDIR}/jce ; pax -rw -pp . ${JDKIMAGEDIR}/jre/lib/security
 .endif
