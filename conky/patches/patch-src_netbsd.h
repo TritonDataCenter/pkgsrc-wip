1708b7595ed53598b7c3c4874243462f6801b4ae
imilh <imilh>
2012-05-07 08:45:17 +0000

    Merged version 1.9.0

--- conky/patches/patch-src_netbsd.h
+++ conky/patches/patch-src_netbsd.h
@@ -2,7 +2,16 @@ $NetBSD$
 
 --- src/netbsd.h.orig	2010-03-25 17:42:39.000000000 +0000
 +++ src/netbsd.h
-@@ -33,4 +33,6 @@
+@@ -12,6 +12,8 @@
+ #include <kvm.h>
+ #include <nlist.h>
+ 
++#include <pthread.h>
++
+ #include <sys/time.h>
+ #include <sys/sysctl.h>
+ #include <sys/types.h>
+@@ -33,4 +35,6 @@
  int get_entropy_avail(unsigned int *);
  int get_entropy_poolsize(unsigned int *);
  
