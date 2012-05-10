626e6398af506e36727fdc4c9fbe1b463e54f8b2
bubuchka <bubuchka>
2012-05-10 10:31:32 +0000

    Initial revision

---
+++ R-BiocGenerics/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	RBiocGenerics
+
+.if !defined(RBIOCGENERICS_BUILDLINK3_MK)
+RBIOCGENERICS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.RBiocGenerics+=	R-BiocGenerics>=0.2.0
+BUILDLINK_ABI_DEPENDS.RBiocGenerics+=	R-BiocGenerics>=0.2.0
+BUILDLINK_PKGSRCDIR.RBiocGenerics?=	../../wip/R-BiocGenerics
+.endif # RBIOCGENERICS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-RBiocGenerics
