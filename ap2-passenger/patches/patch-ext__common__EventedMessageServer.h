1496122037a5b01f30a4a18cc94a5c37c3ac3c4a
fhajny <fhajny>
2012-05-11 09:14:39 +0000

    Updated Passenger to 3.0.12.
    Changes:
    
    - [Apache] Support Apache 2.4. The event MPM is now also supported.
    - [Nginx] Preferred Nginx version upgraded to 1.0.15.
    - [Nginx] Preferred PCRE version upgraded to 8.30.
    - [Nginx] Fixed compatibility with Nginx < 1.0.10.
    - [Nginx] Nginx is now installed with http_gzip_static_module by default.
    - [Nginx] Fixed a memory disclosure security problem.
    - [Nginx] passenger_show_version_in_header now hides the Phusion Passenger
      version number from the ‘Server:’ header too.
    - Fixed a /proc deprecation warning on Linux kernel >= 3.0.

--- ap2-passenger/patches/patch-ext__common__EventedMessageServer.h
+++ ap2-passenger/patches/patch-ext__common__EventedMessageServer.h
@@ -1,14 +1,14 @@
 $NetBSD$
 
-Typo in macro.
+Explicitly error out if HAS_ALLOCA_H_ not defined.
 --- ext/common/EventedMessageServer.h.orig	2011-11-28 11:51:11.000000000 +0000
 +++ ext/common/EventedMessageServer.h
-@@ -29,7 +29,7 @@
- #include <ev++.h>
- #include <cstdarg>
+@@ -31,6 +31,8 @@
  #include <cstdlib>
--#ifdef HAS_ALLOCA_H_
-+#ifdef HAS_ALLOCA_H
+ #ifdef HAS_ALLOCA_H_
  	#include <alloca.h>
++#else
++	#error "alloca is required."
  #endif
  #include "EventedServer.h"
+ #include "MessageReadersWriters.h"
