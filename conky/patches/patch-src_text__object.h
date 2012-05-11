1091af1cf2c3c9cda0a7d5a60f506a95bc9f94e0
imilh <imilh>
2012-05-11 07:20:25 +0000

    Enabled APM. Implemented ACPI backend using envsys / proplib, should be easier to query vitals.

---
+++ conky/patches/patch-src_text__object.h
@@ -0,0 +1,14 @@
+$NetBSD$
+
+--- src/text_object.h.orig	2012-05-10 21:11:13.000000000 +0000
++++ src/text_object.h
+@@ -311,7 +311,8 @@ enum text_object_type {
+ 	OBJ_pop3_unseen,
+ 	OBJ_pop3_used,
+ #if (defined(__FreeBSD__) || defined(__FreeBSD_kernel__) \
+-		|| defined(__OpenBSD__)) && (defined(i386) || defined(__i386__))
++	|| defined(__OpenBSD__)) && (defined(i386) || defined(__i386__)) \
++	|| defined(__NetBSD__)
+ 	OBJ_apm_adapter,
+ 	OBJ_apm_battery_time,
+ 	OBJ_apm_battery_life,
