311f0864dfc4da0134235c554e947f4e36318572
obache <obache>
2012-05-25 14:19:26 +0000

    AVI2SWF is not included anymore in the latest version.

--- swftools/options.mk
+++ swftools/options.mk
@@ -5,16 +5,9 @@ PKG_SUPPORTED_OPTIONS=	lame
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	avifile
-
 .if !empty(PKG_OPTIONS:Mlame)
 CONFIGURE_ARGS+=	--enable-lame
 .include "../../audio/lame/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-lame
-.endif
-
-.if !empty(PKG_OPTIONS:Mavifile)
-PLIST.avifile=		yes
-.include "../../wip/avifile-devel/buildlink3.mk"
 .endif
