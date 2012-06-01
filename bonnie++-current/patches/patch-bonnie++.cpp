3f7388d43854ca50065ed7618a2176a4f6a1d828
hfath <hfath>
2012-06-01 15:26:59 +0000

    Initial revision

---
+++ bonnie++-current/patches/patch-bonnie++.cpp
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- bonnie++.cpp.orig	2012-06-01 14:51:34.000000000 +0000
++++ bonnie++.cpp
+@@ -49,6 +49,10 @@
+ #include <string.h>
+ #include <signal.h>
+ 
++#ifndef SA_SIGINFO
++#define SA_SIGINFO      0x0040
++#endif
++
+ void usage();
+ 
+ class CGlobalItems
