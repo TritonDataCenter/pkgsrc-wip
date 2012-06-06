c1b5639e51c99fb306e217314ef6b30b82cc0077
ryo-on <ryo-on>
2012-06-06 21:23:41 +0000

    Fix build on netbsd-5.
    * Fix read problem

---
+++ qemu/patches/patch-audio_audio.c
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- /usr/tmp/wip/qemu/work/qemu-1.1.0/audio/audio.c.orig	2012-06-06 21:16:21.000000000 +0000
++++ /usr/tmp/wip/qemu/work/qemu-1.1.0/audio/audio.c
+@@ -1170,7 +1170,7 @@ int AUD_read (SWVoiceIn *sw, void *buf, 
+         return 0;
+     }
+ 
+-    bytes = sw->hw->pcm_ops->read (sw, buf, size);
++    bytes = (*sw->hw->pcm_ops->read) (sw, buf, size);
+     return bytes;
+ }
+ 
