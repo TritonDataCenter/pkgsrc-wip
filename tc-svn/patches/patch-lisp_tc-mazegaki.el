82ad24e245b07e631d056c6819f6af8869f62b4c
makoto <makoto>
2012-07-30 13:50:31 +0000

    Add patches to convert last-command-char to last-command-event

---
+++ tc-svn/patches/patch-lisp_tc-mazegaki.el
@@ -0,0 +1,15 @@
+$NetBSD$
+
+s/last-command-char/last-command-event/
+
+--- lisp/tc-mazegaki.el.orig	2012-07-30 22:24:44.000000000 +0900
++++ lisp/tc-mazegaki.el	2012-07-30 22:29:18.000000000 +0900
+@@ -1583,7 +1583,7 @@
+ 		     (setq comp (append (cdr list) comp))
+ 		     (rplacd list nil)))
+ 		  ((or (= ch ?\t)
+-		       (= ch last-command-char))
++		       (= ch last-command-event))
+ 		   ;; 先頭の候補を選択して終了
+ 		   (insert (substring (car comp)
+ 				      (length yomi-prefix) nil))
