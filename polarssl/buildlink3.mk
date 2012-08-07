eab3fd0345ce15fec19dbaa94b024234f4963ca7
mwilhelmy <mwilhelmy>
2012-08-07 02:06:10 +0000

    Add buildlink3.mk
    Set BUILDLINK_API_DEPENDS.polarssl as polarssl>=0.10.0 because this is the
    first release that was named polarssl rather than XySSL.
    
    Couldn't find anything indicating API breakage in the Changelog inside the
    distribution tarball. Corrections welcome.

---
+++ polarssl/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	polarssl
+
+.if !defined(POLARSSL_BUILDLINK3_MK)
+POLARSSL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.polarssl+=	polarssl>=0.10.0
+BUILDLINK_PKGSRCDIR.gnutls?=	../../wip/polarssl
+
+.endif # POLARSSL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-polarssl
