297d344a0991e8ef200902c55a1e21deaeb69d89
mwilhelmy <mwilhelmy>
2012-08-17 16:03:03 +0000

    Remove unneeded include from patch-screen.c.

--- fish/patches/patch-screen.c
+++ fish/patches/patch-screen.c
@@ -4,14 +4,6 @@ Fix compilation on NetBSD, where init_tabs is not suitable for assignment.
 
 --- screen.c.orig	2009-03-08 14:46:47.000000000 +0000
 +++ screen.c
-@@ -16,6 +16,7 @@ efficient way for transforming that to t
- #include <sys/types.h>
- 
- #ifdef HAVE_SYS_TERMIOS_H
-+#include <termios.h>
- #include <sys/termios.h>
- #endif
- 
 @@ -92,13 +93,14 @@ static int try_sequence( char *seq, wcha
   */
  static int next_tab_stop( int in )
