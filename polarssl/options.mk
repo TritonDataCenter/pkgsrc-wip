c82f8cf62fe3df2a319e07700230785ef844b29a
mwilhelmy <mwilhelmy>
2012-09-10 12:51:33 +0000

    * fix things Thomas Klausner pointed out:   + Use -fPIC, but only for .so   + Replace only-lib option by polarssl-examples * If tests are being built, link them against the static library

--- polarssl/options.mk
+++ polarssl/options.mk
@@ -1,17 +1,17 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.polarssl
-PKG_SUPPORTED_OPTIONS=		only-lib
-PKG_SUGGESTED_OPTIONS=		only-lib
+PKG_SUPPORTED_OPTIONS=		polarssl-examples
+PKG_SUGGESTED_OPTIONS=		# empty
 
 .include "../../mk/bsd.options.mk"
 
 # Build and install only the shared and dynamic libs and include files,
 # not the (mostly demonstrational) programs
 
-.if !empty(PKG_OPTIONS:Monly-lib)
+.if !empty(PKG_OPTIONS:Mpolarssl-examples)
+PLIST_SRC=	${PKGDIR}/PLIST ${PKGDIR}/PLIST.bin
+.else
 MAKE_ENV+=	ONLY_LIB=yes
 INSTALL_ENV+=	ONLY_LIB=yes
-.else
-PLIST_SRC=	${PKGDIR}/PLIST ${PKGDIR}/PLIST.bin
 .endif
