d3a0f98ea8cb98ab49a9208ba45b991b0efe2b84
othyro <othyro>
2012-09-10 22:52:54 +0000

    wip/dbus-sharp-glib will need this.

---
+++ dbus-sharp/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	dbus-sharp
+
+.if !defined(DBUS_SHARP_BUILDLINK3_MK)
+DBUS_SHARP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.dbus-sharp+=	dbus-sharp>=0.7.0
+BUILDLINK_PKGSRCDIR.dbus-sharp?=	../../wip/dbus-sharp
+
+.include "../../lang/mono/buildlink3.mk"
+.endif	# DBUS_SHARP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-dbus-sharp
