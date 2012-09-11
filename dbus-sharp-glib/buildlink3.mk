b4d72e2ba0360be01f489ee0677f6bdda65c55bd
othyro <othyro>
2012-09-11 17:02:34 +0000

    Need to include ../../wip/dbus-sharp.

--- dbus-sharp-glib/buildlink3.mk
+++ dbus-sharp-glib/buildlink3.mk
@@ -8,7 +8,7 @@ DBUS_SHARP_GLIB_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.dbus-sharp-glib+=	dbus-sharp-glib>=0.5.1
 BUILDLINK_PKGSRCDIR.dbus-sharp-glib?=	../../wip/dbus-sharp-glib
 
-#.include "../../wip/dbus-sharp/buildlink3.mk"
+.include "../../wip/dbus-sharp/buildlink3.mk"
 .endif	# DBUS_SHARP_GLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-dbus-sharp-glib
