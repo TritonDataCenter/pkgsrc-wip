d1decdcebaf844f612f397eaeb5c9c4df853bc9d
jihbed <jihbed>
2012-09-17 22:24:25 +0000

    Import mce-1.5.6 as wip/mce.
    This package contains headers defining the DBus method calls provided by the
    Mode Control Entity, and the signals emitted by it.

---
+++ mce/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	mce
+
+.if !defined(MCE_BUILDLINK3_MK)
+MCE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mce+=	mce>=1.5.6
+BUILDLINK_PKGSRCDIR.mce?=	../../wip/mce
+.endif	# MCE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mce
