c2a6fd1be87ed74fee63e6eb87e89941a5c1ad1e
bubuchka <bubuchka>
2012-09-24 01:08:57 +0000

    Whoops, API was changed in between 1.2 -> 1.5.

--- leveldb/buildlink3.mk
+++ leveldb/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	leveldb
 .if !defined(LEVELDB_BUILDLINK3_MK)
 LEVELDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.leveldb+=	leveldb>=1.2
+BUILDLINK_API_DEPENDS.leveldb+=	leveldb>=1.5
 BUILDLINK_PKGSRCDIR.leveldb?=	../../wip/leveldb
 .endif	# LEVELDB_BUILDLINK3_MK
 
