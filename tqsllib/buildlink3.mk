763adde30c93d59679950206976413e5353b69e0
makoto <makoto>
2012-10-02 15:29:55 +0000

    Adding buildlink3.mk for trustedQSL.

---
+++ tqsllib/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	tqsllib
+
+.if !defined(TQSLLIB_BUILDLINK3_MK)
+TQSLLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.tqsllib+=	tqsllib>=2.2
+BUILDLINK_PKGSRCDIR.tqsllib=	../../wip/tqsllib
+.endif # TQSLLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-tqsllib
