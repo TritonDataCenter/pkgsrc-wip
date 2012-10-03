68001493271a45e187a153c501b9b22af414bb96
othyro <othyro>
2012-10-03 15:05:59 +0000

    Import sfml+-1.6 as wip/sfml+.
    SFML is a free multimedia C++ API that provides you low and high level access to
    graphics, input, audio, etc.
    
    This package contains the C++ language bindings of SFML.

---
+++ sfml+/patches/patch-src_SFML_System_Platform.hpp
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Adds NetBSD support.
+
+--- src/SFML/System/Platform.hpp.orig	2010-01-27 14:00:23.000000000 +0000
++++ src/SFML/System/Platform.hpp
+@@ -35,7 +35,7 @@
+ 
+     #include <SFML/System/Win32/Platform.hpp>
+ 
+-#elif defined(SFML_SYSTEM_LINUX) || defined(SFML_SYSTEM_MACOS) || defined(SFML_SYSTEM_FREEBSD)
++#elif defined(SFML_SYSTEM_LINUX) || defined(SFML_SYSTEM_MACOS) || defined(SFML_SYSTEM_FREEBSD) || defined(SFML_SYSTEM_NETBSD)
+ 
+     #include <SFML/System/Unix/Platform.hpp>
+ 
