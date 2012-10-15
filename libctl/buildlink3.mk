7348026b688d4233c72a3e3129b700c3ab015eb0
asau <asau>
2012-10-15 20:06:16 +0000

    Update to libctl 3.2.1. Depend on Guile 1.8.

--- libctl/buildlink3.mk
+++ libctl/buildlink3.mk
@@ -8,9 +8,7 @@ LIBCTL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.1
 BUILDLINK_PKGSRCDIR.libctl?=	../../wip/libctl
 
-# CONFIGURE_ARGS+=	--with-libctl=${PREFIX}
-
-.include "../../lang/guile16/buildlink3.mk"
+.include "../../lang/guile/buildlink3.mk"
 .endif	# LIBCTL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libctl
