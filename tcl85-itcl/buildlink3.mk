c324b86627f570f72480b6f72fe8eb420f8d7aef
othyro <othyro>
2012-10-22 21:34:57 +0000

    Removed wip/tcl-tclOO library linking.

--- tcl85-itcl/buildlink3.mk
+++ tcl85-itcl/buildlink3.mk
@@ -8,7 +8,6 @@ TCL85_ITCL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.tcl85-itcl+=	tcl85-itcl>=3.4.1
 BUILDLINK_PKGSRCDIR.tcl85-itcl?=	../../wip/tcl85-itcl
 
-.include "../../wip/tcl-tclOO/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
 .endif	# TCL85_ITCL_BUILDLINK3_MK
 
