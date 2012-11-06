66bd646b32079b2b09f4a91e4a25d57c2920e142
othyro <othyro>
2012-11-06 14:45:00 +0000

    Added all PKG_BUILD_OPTIONS.

--- twinkle/buildlink3.mk
+++ twinkle/buildlink3.mk
@@ -9,6 +9,25 @@ BUILDLINK_API_DEPENDS.twinkle+=	twinkle>=1.1
 BUILDLINK_ABI_DEPENDS.twinkle?=	twinkle>=1.4.2nb4
 BUILDLINK_PKGSRCDIR.twinkle?=	../../wip/twinkle
 
+pkgbase := twinkle
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS:Milbc)
+.include "../../wip/ilbc-rfc3951/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mkde)
+.include "../../x11/kdelibs3/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mspeex)
+.include "../../audio/speex/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mzrtp)
+.include "../../wip/libzrtpcpp/buildlink3.mk"
+.endif
+
 .include "../../x11/qt3-tools/buildlink3.mk"
 .include "../../x11/qt3-libs/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
