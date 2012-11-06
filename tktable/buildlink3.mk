c3a43c6319e820b61701f88a5c298d595eb51497
othyro <othyro>
2012-11-06 14:59:06 +0000

    Added PKG_BUILD_OPTIONS for pthread option.

--- tktable/buildlink3.mk
+++ tktable/buildlink3.mk
@@ -8,6 +8,15 @@ TKTABLE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.tktable+=	tktable>=2.10
 BUILDLINK_PKGSRCDIR.tktable?=	../../wip/tktable
 
+pkgbase := tktable
+.include "../../mk/pkg-build-options.mk"
+
+
+.if !empty(PKG_BUILD_OPTIONS:Mpthread)
+.include "../../mk/pthread.buildlink3.mk"
+.endif
+
+
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
 .endif # TKTABLE_BUILDLINK3_MK
