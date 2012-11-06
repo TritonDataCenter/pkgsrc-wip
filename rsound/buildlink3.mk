fb2502f2f95f9fde65d2f01cad711a328965179c
othyro <othyro>
2012-11-06 14:50:30 +0000

    Added all PKG_BUILD_OPTIONS.

--- rsound/buildlink3.mk
+++ rsound/buildlink3.mk
@@ -5,6 +5,41 @@ BUILDLINK_TREE+=	rsound
 .if !defined(RSOUND_BUILDLINK3_MK)
 RSOUND_BUILDLINK3_MK:=
 
+pkgbase := rsound
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS:Malsa)
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mjack)
+.include "../../audio/jack/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mlibao)
+.include "../../audio/libao/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Msamplerate)
+.include "../../audio/libsamplerate/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mopenal)
+.include "../../audio/openal/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Moss)
+.include "../../mk/oss.buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mportaudio)
+.include "../../audio/portaudio/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+.endif
+
 BUILDLINK_API_DEPENDS.rsound+=	rsound>=1.1
 BUILDLINK_PKGSRCDIR.rsound?=	../../wip/rsound
 .endif	# RSOUND_BUILDLINK3_MK
