06acb06a71ff3c6d60bfd72258add67092e0d270
slitvinov <slitvinov>
2012-11-16 11:13:16 +0000

    Updated to version: py27-sumatra-0.4.0. Added license, added a patch to all_tests.sh

---
+++ py-sumatra/patches/patch-test_unittests_all__tests.sh
@@ -0,0 +1,12 @@
+$NetBSD$
+
+# hardcoded a python version number
+--- test/unittests/all_tests.sh.orig	2011-10-11 20:35:01.000000000 +0000
++++ test/unittests/all_tests.sh
+@@ -1,5 +1,5 @@
+ #/bin/bash
+-COVERAGE=coverage
++COVERAGE=coverage2.7
+ $COVERAGE -e
+ 
+ for file in test_*.py; do
