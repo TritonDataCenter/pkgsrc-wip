fd639bfc7eef841fd0f4022a90370c285e975049
outpaddling <outpaddling>
2012-11-17 18:31:46 +0000

    Clean up for commit

--- libctl/buildlink3.mk
+++ libctl/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libctl
 .if !defined(LIBCTL_BUILDLINK3_MK)
 LIBCTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.1
+BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.2.1
 BUILDLINK_PKGSRCDIR.libctl?=	../../wip/libctl
 
 .include "../../lang/guile/buildlink3.mk"
