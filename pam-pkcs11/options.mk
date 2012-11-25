c99ab09e0e205a47fd71e75c60e2d5fa98879ea2
lexort <lexort>
2012-11-25 23:08:44 +0000

    General cleanup (but not an update, with no real binary changes intended).
     - Fix COMMENT (had unescaped #)
     - Set LICENSE
     - replace bash in a script
     - drop TODO, as it builds on NetBSD 5 (was about 3, which is EOL)
     - use right path to pcsc-lite, now in pkgsrc
     - add patch comment

--- pam-pkcs11/options.mk
+++ pam-pkcs11/options.mk
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	pcsc-lite ldap
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mpcsc-lite)
-.include "../../wip/pcsc-lite/buildlink3.mk"
+.include "../../security/pcsc-lite/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-pcsclite
 PLIST_SRC+=		PLIST.pcsc-lite
 .endif
@@ -15,4 +15,6 @@ PLIST_SRC+=		PLIST.pcsc-lite
 .include "../../databases/openldap-client/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-ldap
 PLIST_SRC+=		PLIST.ldap
+.else
+CONFIGURE_ARGS+=	--without-ldap
 .endif
