b6cb2497b56909283659f45f19d92a328c826f14
othyro <othyro>
2012-11-26 19:46:21 +0000

    Import supermodel-0.2a as wip/supermodel.
    Supermodel emulates Sega's Model 3 arcade platform, allowing you to play a
    number of ground-breaking arcade classics on your PC. It uses OpenGL and the SDL
    library, and can run on Windows, Linux, Mac OS X, NetBSD, and possibly others.
    The source code is freely available under the terms of the GNU General Public
    License.

---
+++ supermodel/patches/patch-Src_Pkgs_ioapi.h
@@ -0,0 +1,17 @@
+$NetBSD$
+
+Fix implicit declaration of fopen64, ftello64, and fseeko64.
+
+--- Src/Pkgs/ioapi.h.orig	2011-04-23 18:14:02.000000000 +0000
++++ Src/Pkgs/ioapi.h
+@@ -58,6 +58,10 @@
+   #define ftello64 ftell
+   #define fseeko64 fseek
+  #endif
++#else
++ #define fopen64 fopen
++ #define ftello64 ftello
++ #define fseeko64 fseeko
+ #endif
+ #endif
+ 
