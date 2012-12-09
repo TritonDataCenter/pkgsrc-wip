c49ef5bb88d424e0e1de6e4966e656341b96ac1f
othyro <othyro>
2012-12-09 16:56:45 +0000

    string.h provides strcasestr. Removing.

---
+++ west/patches/patch-parse.h
@@ -0,0 +1,14 @@
+$NetBSD$
+
+string.h already defines strcasestr.
+
+--- parse.h.orig	2001-02-07 08:23:20.000000000 +0000
++++ parse.h
+@@ -7,7 +7,6 @@ frame_t **page_getframes( char **pg );
+ link_t **page_getlinks( char **page );
+ void newurl( char *url, char **page );
+ char *leastcp( char *a, char *b );
+-char *strcasestr( char *haystack, char *needle );
+ char *url_fixrelative( char *new, char *ref );
+ int isredirect( char **page );
+ 
