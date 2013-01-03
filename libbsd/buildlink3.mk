0ecd306b5ffa7c8e66461757a4959164aeeca834
mrampke <mrampke>
2013-01-03 21:41:42 +0000

    wip/libbsd/buildlink3.mk: do not construct an empty buildlink3 node
    fix for a bug introduced by trying to silence a pkglint warning. This
    buildlink3.mk is rigged to be a NOOP on platforms where libbsd won't
    build (as per Makefile.common) - the buildlink tree statements must be
    within this .if/.endif block

--- libbsd/buildlink3.mk
+++ libbsd/buildlink3.mk
@@ -1,13 +1,13 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	libbsd
-
-
 # check if available on this platform
 #
 .include "Makefile.common"
+.if defined(USE_LIBBSD)
 
-.if defined(USE_LIBBSD) && !defined(LIBBSD_BUILDLINK3_MK)
+BUILDLINK_TREE+=	libbsd
+
+.if !defined(LIBBSD_BUILDLINK3_MK)
 LIBBSD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.4.2
@@ -15,3 +15,5 @@ BUILDLINK_PKGSRCDIR.libbsd?=	../../wip/libbsd
 .endif	# LIBBSD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libbsd
+
+.endif
