045e2d7dfe1ad1a035d538a4d08c8743a26bcc3d
othyro <othyro>
2013-01-08 03:31:06 +0000

    Fix PKGVERSION.

--- py-pyswisseph/buildlink3.mk
+++ py-pyswisseph/buildlink3.mk
@@ -5,8 +5,11 @@ BUILDLINK_TREE+=	py-pyswisseph
 .if !defined(PY_PYSWISSEPH_BUILDLINK3_MK)
 PY_PYSWISSEPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py-pyswisseph+=	${PYPKGPREFIX}-pyswisseph>=1.77.00-0
+BUILDLINK_API_DEPENDS.py-pyswisseph+=	${PYPKGPREFIX}-pyswisseph>=1.77.00.0
 BUILDLINK_PKGSRCDIR.py-pyswisseph?=	../../wip/py-pyswisseph
+
+.include "../../lang/python/application.mk"
+.include "../../lang/python/extension.mk"
 .endif	# PY_PYSWISSEPH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-pyswisseph
