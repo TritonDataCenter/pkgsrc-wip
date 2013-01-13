62d335f8b1b6c16e5e82ef429767b476216ce9f8
othyro <othyro>
2013-01-13 16:26:41 +0000

    Move xcb-proto to BUILD_DEPENDS; properly require xcb >=1.8; add options framework to handle the doxygen option (builds the docu, but doesn't install it); Bump PKGREVISION.

---
+++ unagi/options.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.unagi
+PKG_SUPPORTED_OPTIONS=	doxygen
+PKG_SUGGESTED_OPTIONS+=	# blank
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdoxygen)
+DEPENDS+=	doxygen-[0-9]*:../../devel/doxygen
+CONFIGURE_ARGS+=	--enable-doxygen-dot --enable-doxygen-man
+CONFIGURE_ARGS+=	--enable-doxygen-rtf --enable-doxygen-xml
+CONFIGURE_ARGS+=	--enable-doxygen-chm --enable-doxygen-pdf
+.else
+CONFIGURE_ARGS+=	--disable-doxygen-doc
+.endif
