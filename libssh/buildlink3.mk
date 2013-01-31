0fc10b305d82126260d91311646fbec188993e59
noud4 <noud4>
2013-01-31 14:59:28 +0000

    import of the real libssh, version 0.5.3 having fixes for, CVE-2012-4559, CVE-2012-4560, CVE-2012-4561 and CVE-2012-4562.

---
+++ libssh/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libssh
+
+.if !defined(LIBSSH_BUILDLINK3_MK)
+LIBSSH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libssh+=	libssh>=0.5.3
+BUILDLINK_ABI_DEPENDS.libssh+=	libssh>=0.5.3
+BUILDLINK_PKGSRCDIR.libssh?=	../../wip/libssh
+
+pkgbase := libssh
+.include "../../mk/pkg-build-options.mk"
+
+PKG_OPTIONS?=		# empty
+.if !empty(PKG_OPTIONS:Mzlib)
+.include "../../devel/zlib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mopenssl)
+.include "../../security/openssl/buildlink3.mk"
+.endif
+.endif # LIBSSH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libssh
