b19e246c9fac55119d4181f2a8b512feda896742
outpaddling <outpaddling>
2013-02-26 14:52:57 +0000

    Import fftw2-openmpi-2.1.5 as wip/fftw2-openmpi.
    This is FFTW, v. 2.1.3.  FFTW is a collection of fast C routines to
    compute the Discrete Fourier Transform in one or more dimensions.

---
+++ fftw2-openmpi/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+# BUILDLINK_DEPMETHOD.fftw2-openmpi?=	build
+
+BUILDLINK_TREE+=	fftw2-openmpi
+
+.if !defined(FFTW2_OPENMPI_BUILDLINK3_MK)
+FFTW2_OPENMPI_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.fftw2-openmpi+=	fftw2-openmpi>=2.1.5
+BUILDLINK_PKGSRCDIR.fftw2-openmpi?=	../../wip/fftw2-openmpi
+
+.include "../../wip/openmpi-prefixed/buildlink3.mk"
+.endif	# FFTW2_OPENMPI_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-fftw2-openmpi
