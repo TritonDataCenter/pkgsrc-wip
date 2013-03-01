387c04df338561210f9513524947411c7f5fc929
szptvlfn <szptvlfn>
2013-03-01 20:47:28 +0000

    Update to 1.18.10

--- happy/buildlink3.mk
+++ happy/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	happy
 .if !defined(HAPPY_BUILDLINK3_MK)
 HAPPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy+=	happy>=1.18.9
+BUILDLINK_API_DEPENDS.happy+=	happy>=1.18.10
 BUILDLINK_PKGSRCDIR.happy?=	../../wip/happy
 
 .endif	# HAPPY_BUILDLINK3_MK
