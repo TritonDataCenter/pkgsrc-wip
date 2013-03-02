430456fe644f6df4406cf0be8114849a9cd0cc1b
phonohawk <phonohawk>
2013-03-02 09:56:07 +0000

    Upstream update to ansi-terminal-0.6

--- hs-ansi-terminal/buildlink3.mk
+++ hs-ansi-terminal/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-ansi-terminal
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.5.5
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=	../../wip/hs-ansi-terminal
 .endif	# HS_ANSI_TERMINAL_BUILDLINK3_MK
 
