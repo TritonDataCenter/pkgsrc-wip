c8fcd8d172e778a10005ef0d124690f3ff8444d9
othyro <othyro>
2013-03-02 15:31:33 +0000

    Import liboggz-1.1.1 as wip/liboggz.
    Oggz comprises liboggz and the tool oggz, which provides commands to inspect,
    edit and validate Ogg files. The oggz-chop tool can also be used to serve time
    ranges of Ogg media over HTTP by any web server that supports CGI.
    
    liboggz is a C library for reading and writing Ogg files and streams. It offers
    various improvements over the reference libogg, including support for seeking,
    validation and timestamp interpretation.

---
+++ liboggz/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=      liboggz
+
+.if !defined(LIBOGGZ_BUILDLINK3_MK)
+LIBOGGZ_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.liboggz+=	liboggz>=1.1.1
+BUILDLINK_PKGSRCDIR.liboggz?=	../../wip/liboggz
+
+.include "../../multimedia/libogg/buildlink3.mk"
+.endif	# LIBOGGZ_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-liboggz
