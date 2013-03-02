3d042478111b47b38b95aa784d8330abf0682812
othyro <othyro>
2013-03-02 23:21:08 +0000

    Move flac, speex, and vorbis to options.mk, but enabled by default. Rename tests option to valgrind.

--- libfishsound/buildlink3.mk
+++ libfishsound/buildlink3.mk
@@ -8,11 +8,23 @@ LIBFISHSOUND_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libfishsound+=	libfishsound>=1.0.0
 BUILDLINK_PKGSRCDIR.libfishsound?=	../../wip/libfishsound
 
+pkgbase := libfishsound
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libfishsound:Mflac)
 .include "../../audio/flac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.libfishsound:Mspeex)
+.include "../../audio/speex/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.libfishsound:Mvorbis)
+.include "../../audio/libvorbis/buildlink3.mk"
+.endif
+
 .include "../../wip/liboggz/buildlink3.mk"
 .include "../../audio/libsndfile/buildlink3.mk"
-.include "../../audio/speex/buildlink3.mk"
-.include "../../audio/libvorbis/buildlink3.mk"
 .endif	# LIBFISHSOUND_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfishsound
