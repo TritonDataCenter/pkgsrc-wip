334e1c20d68a5dc724542a483e7db090d0117237
khorben <khorben>
2013-03-04 20:56:37 +0000

    Packaged DeforaOS Camera 0.0.1, with: - the possibility to take pictures - a new helper tool, "gallery", to list the pictures taken - manual pages - translation into French - support for an embedded mode - support for fullscreen mode

---
+++ deforaos-camera/options.mk
@@ -0,0 +1,11 @@
+# $NetBSD$
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.deforaos-camera
+PKG_SUPPORTED_OPTIONS=	embedded
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Membedded)
+MAKE_FLAGS+=	CPPFLAGS=-DEMBEDDED
+.endif
