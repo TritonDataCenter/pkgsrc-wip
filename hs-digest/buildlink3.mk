adca9ae7b90376386152a7edfb98adddf67f8c42
szptvlfn <szptvlfn>
2013-03-05 22:48:06 +0000

    Update to 0.0.1.2

--- hs-digest/buildlink3.mk
+++ hs-digest/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-digest
 .if !defined(HS_DIGEST_BUILDLINK3_MK)
 HS_DIGEST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.0.5
+BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.1
 BUILDLINK_PKGSRCDIR.hs-digest?=	../../wip/hs-digest
 .endif	# HS_DIGEST_BUILDLINK3_MK
 
