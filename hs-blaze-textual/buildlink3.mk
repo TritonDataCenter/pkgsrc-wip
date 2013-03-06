4ed6fa9bd3017fa5f108db98c68a42c743a798ab
phonohawk <phonohawk>
2013-03-06 15:13:21 +0000

    Upstream update to blaze-textual-0.2.0.8

--- hs-blaze-textual/buildlink3.mk
+++ hs-blaze-textual/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-blaze-textual
 .if !defined(HS_BLAZE_TEXTUAL_BUILDLINK3_MK)
 HS_BLAZE_TEXTUAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2
+BUILDLINK_API_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.0
 BUILDLINK_PKGSRCDIR.hs-blaze-textual?=	../../wip/hs-blaze-textual
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
