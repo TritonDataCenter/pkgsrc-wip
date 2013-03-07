a349ff345ff9efb6ad9e1c69fae64dd49dfa4553
othyro <othyro>
2013-03-07 11:51:40 +0000

    Import antlr3-c-3.4 as wip/antlr3-c.
    ANTLR, ANother Tool for Language Recognition, is a language tool that provides a
    framework for constructing recognizers, interpreters, compilers, and translators
    from grammatical descriptions containing actions in a variety of target
    languages. ANTLR provides excellent support for tree construction, tree walking,
    translation, error recovery, and error reporting.
    
    This package contains the C language bindings for ANTLR.

---
+++ antlr3-c/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libantlr3c
+
+.if !defined(LIBANTLR3C_BUILDLINK3_MK)
+LIBANTLR3C_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libantlr3c+=	libantlr3c>=3.4
+BUILDLINK_PKGSRCDIR.libantlr3c?=	../../wip/antlr3-c
+.endif	# LIBANTLR3C_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libantlr3c
