0ee8d4cf613c9b8bead2716ebd6c4bc97d9fc0cd
szptvlfn <szptvlfn>
2013-03-09 12:52:03 +0000

    one more thing, buildlink3.mk

---
+++ hs-void/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-void
+
+.if !defined(HS_VOID_BUILDLINK3_MK)
+HS_VOID_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.5.12
+BUILDLINK_PKGSRCDIR.hs-void?=	../../wip/hs-void
+
+.endif	# HS_VOID_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-void
