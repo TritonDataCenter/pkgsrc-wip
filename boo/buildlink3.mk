c08d2713609600b7c72fd73887e8cc5717788516
othyro <othyro>
2013-03-10 17:07:48 +0000

    Import boo-0.9.4.9 as wip/boo.
    Boo is a new object oriented statically typed programming language for the
    Common Language Infrastructure with a python inspired syntax and a special focus
    on language and compiler extensibility.

---
+++ boo/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	boo
+
+.if !defined(BOO_BUILDLINK3_MK)
+BOO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.boo+=	boo>=0.9.4.9
+BUILDLINK_PKGSRCDIR.boo?=	../../wip/boo
+
+.include "../../x11/gtksourceview/buildlink3.mk"
+.include "../../databases/shared-mime-info/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
+.endif	# BOO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-boo
