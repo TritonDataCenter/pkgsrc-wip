c5d6544656621b73aaee8f848e9afd153bc19868
szptvlfn <szptvlfn>
2013-04-13 08:37:49 +0000

    add buildlink3.mk, for wip/hs-cgi

---
+++ hs-xhtml/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-xhtml
+
+.if !defined(HS_XHTML_BUILDLINK3_MK)
+HS_XHTML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-xhtml+=	hs-xhtml>=3000.2.1
+BUILDLINK_PKGSRCDIR.hs-xhtml?=	../../wip/hs-xhtml
+.endif	# HS_XHTML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-xhtml
