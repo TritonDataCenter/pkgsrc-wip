9205bf4952f8be7444d27d44029e8f7049558cee
othyro <othyro>
2013-04-15 01:12:53 +0000

    Include buildlink3.mk files for libiconv, gettext-lib, libgpg-error, and libgcrypt after analysis of dynamic object dependencies and installed headers.

--- ccrtp/buildlink3.mk
+++ ccrtp/buildlink3.mk
@@ -8,6 +8,10 @@ CCRTP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=2.0.0
 BUILDLINK_PKGSRCDIR.ccrtp?=	../../wip/ccrtp
 
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../security/libgpg-error/buildlink3.mk"
+.include "../../security/libgcrypt/buildlink3.mk"
 .include "../../wip/ucommon/buildlink3.mk"
 .endif # CCRTP_BUILDLINK3_MK
 
