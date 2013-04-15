50f029d4e97dbda768954d00e44f82c95523e557
othyro <othyro>
2013-04-15 01:09:13 +0000

    Include buildlink3.mk files for pthread, dlopen, and openssl, after analysis on dynamic object dependencies and installed headers.

--- ucommon/buildlink3.mk
+++ ucommon/buildlink3.mk
@@ -8,6 +8,9 @@ UCOMMON_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ucommon+=	ucommon>=5.0.6
 BUILDLINK_PKGSRCDIR.ucommon?=	../../wip/ucommon
 
+.include "../../mk/dlopen.buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 .endif # UCOMMON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ucommon
