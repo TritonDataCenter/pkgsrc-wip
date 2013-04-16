647c8415f6b65eeec143e837ffd8ac78017bff65
szptvlfn <szptvlfn>
2013-04-16 11:21:33 +0000

    Update to 2.1.2.1

--- hs-pureMD5/buildlink3.mk
+++ hs-pureMD5/buildlink3.mk
@@ -5,12 +5,9 @@ BUILDLINK_TREE+=	hs-pureMD5
 .if !defined(HS_PUREMD5_BUILDLINK3_MK)
 HS_PUREMD5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pureMD5+=	hs-pureMD5>=2.1.0
+BUILDLINK_API_DEPENDS.hs-pureMD5+=	hs-pureMD5>=2.1.2
 BUILDLINK_PKGSRCDIR.hs-pureMD5?=	../../wip/hs-pureMD5
 
-.include "../../wip/hs-cereal/buildlink3.mk"
-.include "../../wip/hs-crypto-api/buildlink3.mk"
-.include "../../wip/hs-tagged/buildlink3.mk"
 .endif	# HS_PUREMD5_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-pureMD5
