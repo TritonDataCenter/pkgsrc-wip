7a6f667c0f9be54d6ef4c9e2d5c1f5602e81f373
szptvlfn <szptvlfn>
2013-04-25 22:34:23 +0000

    Update to 0.4.1.1
    changes:
    0.4.1.1
      Bump unix upper bound for GHC 7.7
    0.4.1.0
      Fixed build issues with 64-bit GHC on Windows
    0.4.0.0
      Support directory 1.2
        This also involves a conditional migration to the new "time" package
      fix Windows build with GHC 7.6: import data constructors of FFI types
    0.3.0.2
      Bump `unix` version constraint to allow GHC 7.6.0 RC1's `unix-2.6.0.0`

--- hs-unix-compat/buildlink3.mk
+++ hs-unix-compat/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-unix-compat
 .if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
 HS_UNIX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.3.0
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.4.1
 BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../wip/hs-unix-compat
 .endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
 
