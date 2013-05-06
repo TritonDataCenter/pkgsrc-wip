f1c6004882da2de158731af8039d87355b6dc4ce
szptvlfn <szptvlfn>
2013-05-06 21:29:57 +0000

    A vault is a persistent store for values of arbitrary types. It's like having first-class access to the storage space behind IORefs.
    The data structure is analogous to a bank vault, where you can access
    different bank boxes with different keys; hence the name.
    
    Also provided is a locker type, representing a store for a single element.

---
+++ hs-vault/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-vault
+
+.if !defined(HS_VAULT_BUILDLINK3_MK)
+HS_VAULT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.2.0
+BUILDLINK_PKGSRCDIR.hs-vault?=	../../wip/hs-vault
+.endif	# HS_VAULT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-vault
