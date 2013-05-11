cb1b4bb957230a1fa47491ce03320350fc90f3ee
makoto <makoto>
2013-05-11 05:39:51 +0000

    Sync to pkgsrc/devel/xulrunner 20.0.1 (for future update or debugging).

--- xulrunner/patches/patch-toolkit_toolkit-tiers.mk
+++ xulrunner/patches/patch-toolkit_toolkit-tiers.mk
@@ -1,11 +1,11 @@
-$NetBSD$
+$NetBSD: patch-toolkit_toolkit-tiers.mk,v 1.6 2013/01/10 15:01:31 ryoon Exp $
 
---- toolkit/toolkit-tiers.mk.orig	2011-06-15 21:57:56.000000000 +0000
+--- toolkit/toolkit-tiers.mk.orig	2013-01-04 23:44:46.000000000 +0000
 +++ toolkit/toolkit-tiers.mk
-@@ -90,7 +90,7 @@ ifndef MOZ_NATIVE_BZ2
+@@ -43,7 +43,7 @@ ifdef MOZ_UPDATER
+ ifndef MOZ_NATIVE_BZ2
  tier_platform_dirs += modules/libbz2
  endif
- tier_platform_dirs += modules/libmar
 -tier_platform_dirs += other-licenses/bsdiff
 +#tier_platform_dirs += other-licenses/bsdiff
  endif
