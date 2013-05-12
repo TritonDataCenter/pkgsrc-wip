9cc7b398448f7fe027e3f0569eadc2ef258ccefe
makoto <makoto>
2013-05-12 01:49:16 +0000

    Bump version 20.0.1 to 21.0 commit message for above bumping will be on COMMIT_MSG. Verified only on NetBSD/amd64 6.1_RC2 yet.

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -3,12 +3,13 @@
 # used by devel/xulrunner/Makefile
 # used by www/firefox/Makefile
 
-DISTNAME=	firefox-${FIREFOX_VER}.source
+DISTNAME=	xulrunner-${FIREFOX_VER}.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	20.0
-MOZ_BRANCH_MINOR=	.1
-MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
-		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
+MOZ_BRANCH=	21.0
+MOZ_BRANCH_MINOR=
+#MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/} \
+#		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}/source/}
+MASTER_SITES=	http://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/${FIREFOX_VER}/source/
 EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../wip/xulrunner/distinfo
