5a5bfec7d98b92915999f654f535666f7cb381e8
francksys <francksys>
2013-05-14 13:39:50 +0000

    *** empty log message ***

---
+++ info2man/patches/patch-cs_Pathname.pm
@@ -0,0 +1,14 @@
+$NetBSD$
+
+/* Fix the a get directory bug. Otherwise the path is added two times. */
+--- cs/Pathname.pm.orig	2013-05-14 12:07:36.000000000 +0000
++++ cs/Pathname.pm
+@@ -92,6 +92,8 @@ sub absname($;$)
+ { my($path,$dir)=@_;
+   $dir=pwd() if ! defined $dir;
+ 
++  $dir = "";
++
+   return $path if $path =~ m:^/:;
+   return catpath($dir,$path);
+ }
