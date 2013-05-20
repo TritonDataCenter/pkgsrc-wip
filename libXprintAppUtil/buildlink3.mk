6bf6288368d97af575a12a878e7ea4f477d5f083
othyro <othyro>
2013-05-20 23:34:45 +0000

    Adding buildlink3.mk file for this package. Will be used by xphelloworld.

---
+++ libXprintAppUtil/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libXprintAppUtil
+
+.if !defined(LIBXPRINTAPPUTIL_BUILDLINK3_MK)
+LIBXPRINTAPPUTIL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libXprintAppUtil+=	libXprintAppUtil>=1.0.1
+BUILDLINK_PKGSRCDIR.libXprintAppUtil?=	../../wip/libXprintAppUtil
+
+.include "../../x11/libXau/buildlink3.mk"
+.include "../../x11/libXprintUtil/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.endif	# LIBXPRINTAPPUTIL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libXprintAppUtil
