cb843fa0d580bd660c0146640505abc2bed5d30c
szptvlfn <szptvlfn>
2013-05-21 22:19:51 +0000

    The library provides some basic but useful lazy IO functions. Keep in mind that lazy IO is generally discouraged. Perhaps a coroutine library (e.g. pipes) will better suit your needs.

---
+++ hs-lazy-io/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-lazy-io
+
+.if !defined(HS_LAZY_IO_BUILDLINK3_MK)
+HS_LAZY_IO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-lazy-io+=	hs-lazy-io>=0.1.0
+BUILDLINK_PKGSRCDIR.hs-lazy-io?=	../../wip/hs-lazy-io
+.endif	# HS_LAZY_IO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-lazy-io
