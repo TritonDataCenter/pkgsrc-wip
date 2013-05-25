308c09dc51c029f075afdb3e92a0e91863caaf4a
thomasklausner <thomasklausner>
2013-05-25 13:09:48 +0000

    This does not compile, and is just here for those interested enough in getting it to compile.
    Import xf86-video-intel-2.7.1nb6 as wip/xf86-video-intel-old.
    
    This is the video driver for Intel cards for the modular Xorg server.
    Support hardware includes the following chipsets:
      - i810, i810-dc100, i810e
      - i815
      - i830
      - i845
      - i852
      - i855
      - i915
      - i945
      - i965
    
    An older version of the Intel driver is available in x11/xf86-video-i810.

---
+++ xf86-video-intel-old/patches/patch-src_i915__hwmc.c
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/i915_hwmc.c.orig	2009-04-08 23:59:48.000000000 +0000
++++ src/i915_hwmc.c
+@@ -32,7 +32,6 @@
+ 
+ #include "xf86.h"
+ #include "xf86_OSproc.h"
+-#include "xf86Resources.h"
+ #include "compiler.h"
+ #include "xf86PciInfo.h"
+ #include "xf86Pci.h"
---
+++ xf86-video-intel-old/patches/patch-src_sil164_sil164.c
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/sil164/sil164.c.orig	2009-04-08 23:59:48.000000000 +0000
++++ src/sil164/sil164.c
+@@ -35,7 +35,6 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ 
+ #include "xf86.h"
+ #include "xf86_OSproc.h"
+-#include "xf86Resources.h"
+ #include "compiler.h"
+ #include "miscstruct.h"
+ #include "xf86i2c.h"
