78a2c26d7f09acc70f5b31412394c1ed12fa3f23
makoto <makoto>
2013-05-29 13:59:37 +0000

    Revert to before my commit, sorry. I was testing with wrong environmet.

---
+++ xf86-video-intel-old/patches/patch-src_ivch_ivch.c
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/ivch/ivch.c.orig	2009-04-08 23:59:48.000000000 +0000
++++ src/ivch/ivch.c
+@@ -31,7 +31,6 @@
+ 
+ #include "xf86.h"
+ #include "xf86_OSproc.h"
+-#include "xf86Resources.h"
+ #include "compiler.h"
+ #include "miscstruct.h"
+ #include "xf86i2c.h"
