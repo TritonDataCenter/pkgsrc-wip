e8ad32cae59805807ddaccd81d1a5913abaf1a49
othyro <othyro>
2013-06-08 13:18:02 +0000

    Updated to latest release. Fixes a lot of problesm with standalone vs. bundled libraries. Added static option to package. Reduced dependencies in buildlink3.mk. Link against standalone libcerror.

--- libuna/buildlink3.mk
+++ libuna/buildlink3.mk
@@ -8,15 +8,7 @@ LIBUNA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libuna+=	libuna>=20130103
 BUILDLINK_PKGSRCDIR.libuna?=	../../wip/libuna
 
-pkgbase := libuna
-.include "../../mk/pkg-build-options.mk"
-
-
-.if !empty(PKG_BUILD_OPTIONS.libuna:Mnls)
-.include "../../devel/gettext-lib/buildlink3.mk"
-.endif
-
-#.include "../../wip/libcerror/buildlink3.mk"
+.include "../../wip/libcerror/buildlink3.mk"
 .endif	# LIBUNA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libuna
