b7270fcaa30eacb461e0068a917de6655eb4020c
othyro <othyro>
2013-06-08 13:07:45 +0000

    Add static option by default.

--- libcdata/options.mk
+++ libcdata/options.mk
@@ -2,7 +2,7 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libcdata
 PKG_SUPPORTED_OPTIONS=	nls static wide-curses
-PKG_SUGGESTED_OPTIONS+=	nls wide-curses
+PKG_SUGGESTED_OPTIONS+=	nls static wide-curses
 
 .include "../../mk/bsd.options.mk"
 
