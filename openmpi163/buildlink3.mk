2a1d391ea340b67e5b2c790313300fbb954009b4
outpaddling <outpaddling>
2013-06-21 17:26:49 +0000

    Fix buildlink path to package.

--- openmpi163/buildlink3.mk
+++ openmpi163/buildlink3.mk
@@ -1,13 +1,12 @@
 # $NetBSD: buildlink3.mk,v 1.3 2012/10/24 05:49:26 asau Exp $
 
-BUILDLINK_TREE+=	openmpi
+BUILDLINK_TREE+=	openmpi163
 
 .if !defined(OPENMPI_BUILDLINK3_MK)
 OPENMPI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openmpi+=	openmpi>=1.5.1
-# BUILDLINK_PKGSRCDIR.openmpi?=	../../wip/openmpi
-BUILDLINK_PKGSRCDIR.openmpi?=	../../jb-wip/openmpi163
+BUILDLINK_PKGSRCDIR.openmpi?=	../../wip/openmpi163
 
 MPI_PREFIX?=	$(BUILDLINK_PREFIX.openmpi)
 FIND_PREFIX:=	BUILDLINK_PREFIX.openmpi=openmpi
@@ -18,4 +17,4 @@ PREPEND_PATH+=	$(MPI_PREFIX)/bin
 .include "../../parallel/hwloc/buildlink3.mk"
 .endif # OPENMPI_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-openmpi
+BUILDLINK_TREE+=	-openmpi163
