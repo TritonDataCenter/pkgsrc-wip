a498fe184077aa941b462a436205aa9bce2d06e3
noud4 <noud4>
2013-06-26 15:35:33 +0000

    bump to 2.0.3.7

--- taglib-sharp/buildlink3.mk
+++ taglib-sharp/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	taglib-sharp
 .if !defined(TAGLIB_SHARP_BUILDLINK3_MK)
 TAGLIB_SHARP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.taglib-sharp+=	taglib-sharp>=2.0.3.2
-BUILDLINK_ABI_DEPENDS.taglib-sharp?=	taglib-sharp>=2.0.3.2nb4
+BUILDLINK_API_DEPENDS.taglib-sharp+=	taglib-sharp>=2.0.3.7
+BUILDLINK_ABI_DEPENDS.taglib-sharp?=	taglib-sharp>=2.0.3.7
 BUILDLINK_PKGSRCDIR.taglib-sharp?=	../../wip/taglib-sharp
 
 
