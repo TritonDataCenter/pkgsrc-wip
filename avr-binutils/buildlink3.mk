4baa531b94810fec0b4462c2245e49ca40fdfef5
makoto <makoto>
2013-07-07 04:36:16 +0000

    Fix problem: simulavr will not find -liberty.

--- avr-binutils/buildlink3.mk
+++ avr-binutils/buildlink3.mk
@@ -7,7 +7,7 @@ AVR_BINUTILS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.avr-binutils+=	avr-binutils>=2.22
 BUILDLINK_PKGSRCDIR.avr-binutils?=		../../wip/avr-binutils
-BUILDLINK_FNAME_TRANSFORM.avr-binutils+=	-e "s,${AVR_TARGET}/lib,lib/,g"
+#BUILDLINK_FNAME_TRANSFORM.avr-binutils+=	-e "s,${AVR_TARGET}/lib,lib/,g"
 
 .endif # AVR_BINUTILS_BUILDLINK3_MK
 
