ed4462fd02571b0d1a61f0232826aea3facae614
makoto <makoto>
2013-07-07 07:10:12 +0000

    Bump version 3.21.71 to 3.21.72BK patches/patch-src_navtex_navtex.cxx   -- Removed, no similar code exists anymore. --------------- 2013-07-05  David Freese  <w1hkj@w1hkj.com>         a0ed5be: PSKMAIL/ARQ socket         01675d7: Thor update         858a71f: record_loader fix         223fc9b: Code cleanup         50c1c12: Pskmail Notify RsID         4b9c578: MFSK shut down
    2013-06-27  Robert Stiles  <kk5vd@yahoo.com>
            af5e09a: Cwidth test for zero
    
    2013-06-21  David Freese  <w1hkj@w1hkj.com>
            b6ee3f7: XMLRPC
            389c189: MXE compile
    
    2013-06-19  Stefan Fendt  <smfendt@gmail.com>
            dfc2bd8: RTTY decoder
    
    2013-06-09  Dennis Engdahl  <engdahl@snowcrest.net>
            7d62b22: RigCAT user commands
    
    2013-06-08  David Freese  <w1hkj@w1hkj.com>
            3850862: TX state
            9b26ff6: pskmail events
            e12a373:   * Changed extended rsid iaw doc/rsid_codes.gnumeric
    
    2013-06-01  Remi Chateauneu  <remi.chateauneu@gmail.com>
            dead15a: KML/Synop/RTTY/Navtex
            3bb70e4: Port to MXE / MingW
            34b3baf: QSL VIA
            6cd7017: XML parser
    
    2013-05-31  David Freese  <w1hkj@w1hkj.com>
            2c9c73f: ARQ support changes
            f8e26f3: xmlrpcpp
            237cc8f: PortAudio
            dc7173c: SysV ARQ interface
            1ea46c6: METAR format
            e1004fc: RSID logic
    
    =Version 3.21.72=
            a933da1: RsID Timeout
            ff1d69a: PSKMAIL
            531c40c: Colors/Fonts Dialog
            8be3913: Developer List

--- fldigi/patches/patch-src_spot_notify.cxx
+++ fldigi/patches/patch-src_spot_notify.cxx
@@ -4,11 +4,11 @@ Avoid implicit conversions to bool for streams.
 Require C++11 for Clang and prefer std version over tr1 when in C++11
 mode.
 
---- src/spot/notify.cxx.orig	2013-05-03 14:35:28.000000000 +0000
-+++ src/spot/notify.cxx
-@@ -33,7 +33,10 @@
- #include <cctype>
- #include <cstdlib>
+--- src/spot/notify.cxx.orig	2013-07-06 20:54:45.000000000 +0900
++++ src/spot/notify.cxx	2013-07-07 15:25:39.000000000 +0900
+@@ -35,7 +35,10 @@
+ 
+ #include "timeops.h"
  
 -#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)
 +#if __cplusplus >= 201103L
