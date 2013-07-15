0130d847d63cd3b10f94b943263b1f6314ad9218
othyro <othyro>
2013-07-15 02:22:55 +0000

    Updated to latest release, 1.6.1. avahi is now detected during configure. Requires ucommon>=6.0.0. Fix installation of setuid file. Use libnotify07 instead of libnotify. From ChangeLog:
    from 1.6.0 to 1.6.1
    - improved ssl configure check
    
    from 1.5.0 to 1.6.0
    - usage of new context aware intermediary voip functions
    - fixes for official exosip2 4.0
    
    from 1.4.0 to 1.5.0
    - moved switchview into standalone package
    
    from 1.3.3 to 1.4.0
    - ucommon abi 6 support
    
    from 1.3.2 to 1.3.3
    - systemd related changes; env security model now parsed in system.cpp.
    - systemd related changes; OPTIONS has -b default
    - systemd related changes; pidfile support
    
    from 1.3.1 to 1.3.2
    - more qt automake options for strange qt configs
    - fixed systemd support - soon to add to fedora
    - sipwitch hotspot --public mode option
    
    from 1.3.0 to 1.3.1
    - initial support for exosip2/osip2 4.0
    - more mingw32 build support
    
    from 1.2.5 to 1.3.0
    - contact for autoconfiguring clients; can be set in cfg or computed
    - systemd support lands
    - mingw32 build support
    - osip2 published dev headers brokenly used derived packages config.h
    
    from 1.2.4 to 1.2.5
    - various source build issues with config.h and cleanup
    - fixes for cmake pkgconfig searches
    - fix group and user handling logic
    - fix overlinking for runtime and plugins

--- sipwitch/options.mk
+++ sipwitch/options.mk
@@ -3,10 +3,11 @@
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sipwitch
 PKG_SUPPORTED_OPTIONS=	qt
 PKG_SUGGESTED_OPTIONS=	# blank
+PLIST_VARS=		qt
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt)
 .include "../../x11/qt4-tools/buildlink3.mk"
-PLIST.qt=	yes
+PLIST.qt=		yes
 .endif
