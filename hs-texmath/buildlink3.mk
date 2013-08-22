b0d9884d6a8a4508362cb67897a99a818607ec0b
szptvlfn <szptvlfn>
2013-08-22 22:39:20 +0000

    The texmathml library provides functions to convert LaTeX math formulas to presentation MathML (which can be used in HTML) or OMML (Office Math Markup Language, used in Microsoft Office). It supports basic LaTeX and AMS extensions, and it can parse and apply LaTeX macros.
    Use the test flag to install a standalone executable, texmath, that reads a
    LaTeX formula from stdin and writes MathML to stdout.
    
    Use the cgi flag to install a cgi script, texmath-cgi.

---
+++ hs-texmath/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-texmath
+
+.if !defined(HS_TEXMATH_BUILDLINK3_MK)
+HS_TEXMATH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.6.3
+BUILDLINK_PKGSRCDIR.hs-texmath?=	../../wip/hs-texmath
+
+.endif	# HS_TEXMATH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-texmath
