67adf816832ebbad3fcc49a5b3751643cb709982
fhajny <fhajny>
2013-08-28 13:00:51 +0000

    Remove conflict by installing into a subdir (same as devel/libev). Add bl3.

---
+++ libeio/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libeio
+
+.if !defined(LIBEIO_BUILDLINK3_MK)
+LIBEIO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libeio+=libeio>=4
+BUILDLINK_ABI_DEPENDS.libeio+=libev>=4
+BUILDLINK_PKGSRCDIR.libeio?=	../../wip/libeio
+
+BUILDLINK_LIBDIRS.libeio+=	lib/eio
+BUILDLINK_RPATHDIRS.libeio+=	lib/eio
+BUILDLINK_INCDIRS.libeio+=	include/eio
+
+.endif # LIBEIO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libeio
