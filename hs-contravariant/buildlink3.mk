2e2e758465511adf3d0c963f70087ce7531bb71b
szptvlfn <szptvlfn>
2013-08-31 05:26:28 +0000

    Update to 0.4.4
    CHANGELOG:
    0.4.4, 0.4.3
    * Fixed compatibility with GHC 7.7 and tightened `Safe` Haskell support.
    
    0.4.1
    * Added `Day` convolution under `Data.Functor.Contravariant.Day`.

--- hs-contravariant/buildlink3.mk
+++ hs-contravariant/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-contravariant
 .if !defined(HS_CONTRAVARIANT_BUILDLINK3_MK)
 HS_CONTRAVARIANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=0.4
+BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=0.4.4
 BUILDLINK_PKGSRCDIR.hs-contravariant?=	../../wip/hs-contravariant
 
 .endif	# HS_CONTRAVARIANT_BUILDLINK3_MK
