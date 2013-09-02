53d271d0e50de2f771392893fd356881040566df
szptvlfn <szptvlfn>
2013-09-02 12:07:48 +0000

    Update to 2.6
    changelog:
    QuickCheck (2.6) 2013-03-07
    	* Add convenience Function instances for up to 7-tuples
    	* Make stderr line buffered to reduce console I/O.
    	* Return a flag to say whether the test case was interrupted.

--- hs-QuickCheck/buildlink3.mk
+++ hs-QuickCheck/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-QuickCheck
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.5.1
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.6
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../wip/hs-QuickCheck
 
 .include "../../wip/hs-random/buildlink3.mk"
