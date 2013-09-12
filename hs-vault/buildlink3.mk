049a924440cfdc6497d08fc8603b55994d03e817
szptvlfn <szptvlfn>
2013-09-12 20:11:38 +0000

    Update to 0.3.0.2
    Changelog:
    * 0.3.0.2
     - Fix tarball.
    
    * 0.3.0.1
     - Use CPP to reduce code duplication.
     - GHC 7.4 support
    
    * 0.3.0.0
     - Split modules into Lazy and Strict variants, no default choice.
       Add Hashable instance to Data.Unique.Really for all implementations.
     - Better fix for potential race condition involving 'Unique'.
     - add -Wall flag and fix some warnings it reveals
     - Add strict and lazy variant of the `Vault` data type.

--- hs-vault/buildlink3.mk
+++ hs-vault/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-vault
 .if !defined(HS_VAULT_BUILDLINK3_MK)
 HS_VAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.2.0
+BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.3
 BUILDLINK_PKGSRCDIR.hs-vault?=	../../wip/hs-vault
 .endif	# HS_VAULT_BUILDLINK3_MK
 
