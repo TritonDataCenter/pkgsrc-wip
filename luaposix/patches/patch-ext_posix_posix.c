5fdc11f0118a98c54797d07a601eabb811a3189c
ryo-on <ryo-on>
2013-09-14 09:01:12 +0000

    Fix build on Solaris 10.
    * Add conditional for true and KEY_RESIZE definitions.
    * Add conditional KEY_RESIZE(3)

--- luaposix/patches/patch-ext_posix_posix.c
+++ luaposix/patches/patch-ext_posix_posix.c
@@ -2,7 +2,33 @@ $NetBSD$
 
 --- ext/posix/posix.c.orig	2013-09-09 07:15:14.000000000 +0000
 +++ ext/posix/posix.c
-@@ -4694,8 +4694,12 @@ LUALIB_API int luaopen_posix_c (lua_Stat
+@@ -907,6 +907,7 @@ Create a unique temporary directory.
+ @return path on success, nil otherwise
+ @return error message if failed
+ */
++#if defined(HAVE_MKDTEMP)
+ static int Pmkdtemp(lua_State *L)
+ {
+ 	const char *path = luaL_checkstring(L, 1);
+@@ -925,6 +926,7 @@ static int Pmkdtemp(lua_State *L)
+ 	lalloc(ud, tmppath, path_len, 0);
+ 	return (res == NULL) ? pusherror(L, path) : 1;
+ }
++#endif
+ 
+ static int runexec(lua_State *L, int use_shell)
+ {
+@@ -3807,7 +3809,9 @@ static const luaL_Reg R[] =
+ 	MENTRY( Pmkdir		),
+ 	MENTRY( Pmkfifo		),
+ 	MENTRY( Pmkstemp	),
++#if defined(HAVE_MKDTEMP)
+ 	MENTRY( Pmkdtemp	),
++#endif
+ 	MENTRY( Pmktime		),
+ 	MENTRY( Pnice		),
+ 	MENTRY( Popen		),
+@@ -4694,8 +4698,12 @@ LUALIB_API int luaopen_posix_c (lua_Stat
  	MENTRY( AI_PASSIVE	);
  	MENTRY( AI_CANONNAME	);
  	MENTRY( AI_NUMERICHOST	);
