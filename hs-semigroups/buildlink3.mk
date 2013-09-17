fcd47e44016c44351b2728804efbd4b9140e4f08
szptvlfn <szptvlfn>
2013-09-17 20:29:26 +0000

    Update to 0.11
    CHANGELOG:
    0.11
    * Added the missing instance for `HashSet`.
    
    0.10
    * Added support for `unordered-containers`, `bytestring` and `text`.

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -5,10 +5,13 @@ BUILDLINK_TREE+=	hs-semigroups
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.9.2
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.11
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
+.include "../../wip/hs-hashable/buildlink3.mk"
 .include "../../wip/hs-nats/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.include "../../wip/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_SEMIGROUPS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-semigroups
