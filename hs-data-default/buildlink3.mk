9c2e2bddb92e9de94413d83cd4984ea63308090d
szptvlfn <szptvlfn>
2013-09-18 00:03:29 +0000

    Update to 0.5.3
    changes:
    0.5.3
     - change from "LUKAS MAI" to "THE COPYRIGHT HOLDERS" in LICENSE description
    
    0.5.2
     - split code into separate class/instances packages
    
    0.5.1
     - add GNU Makefile with convenience targets
     - test 7-tuple instance
     - Merge pull request #3 from jaspervdj/master
         Add Default instances for 6- and 7-tuples

--- hs-data-default/buildlink3.mk
+++ hs-data-default/buildlink3.mk
@@ -5,10 +5,14 @@ BUILDLINK_TREE+=	hs-data-default
 .if !defined(HS_DATA_DEFAULT_BUILDLINK3_MK)
 HS_DATA_DEFAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.5.0
+BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.5.3
 BUILDLINK_PKGSRCDIR.hs-data-default?=	../../wip/hs-data-default
 
-.include "../../wip/hs-dlist/buildlink3.mk"
+.include "../../wip/hs-data-default-class/buildlink3.mk"
+.include "../../wip/hs-data-default-instances-base/buildlink3.mk"
+.include "../../wip/hs-data-default-instances-containers/buildlink3.mk"
+.include "../../wip/hs-data-default-instances-dlist/buildlink3.mk"
+.include "../../wip/hs-data-default-instances-old-locale/buildlink3.mk"
 .endif	# HS_DATA_DEFAULT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-data-default
