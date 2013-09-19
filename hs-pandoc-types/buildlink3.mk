f9db5c62a67ce8137138d07b6cd4304afe64fac3
szptvlfn <szptvlfn>
2013-09-19 10:37:33 +0000

    Update to 1.12.1
    changes:
    1.12.1
     - Added ToJSONFilter instances for a -> [a], a -> IO [a].
         These use generics and will be slower, but are needed for
         backwards compatibility.
    1.12
     - Generic: Removed deprecated processWith, processWithM
     - Made setTitle, setAuthors, setDate non-polymorphic.
     - Generalized docTitle, docDate, docAuthor so they work with MetaBlocks.
         This will help with MMD title blocks, where the fields parse as
         MetaBlocks [Plain ils].
     - Walk: Added instance for Walkable a [b].
     - Walkable instances for Pandoc Pandoc and Meta Meta.
     - Switch to left-biased union for mappend of Meta.
     - Monoid instances for Pandoc, Meta.
     - Builder: plain now produces mempty on empty input.
     - Use a derived Show instance for Format.
     - IsString instance for Format.
     - Made Format a newtype.
         It still embeds a string, but comparison is case-insensitive.
     - Changed Text.Pandoc.JSON to use Text.Pandoc.Walk.
     - Added Text.Pandoc.Walk.
     - Added Span and Div generic Inline/Block containers with attributes.
     - Added ToJSONFilter instances to allow use of script arguments.
     - Removed obsolete CPP macros.
     - Added MetaBool to MetaValue.
     - ALlow bytestring 0.10.*.
     - Added JSON serialization/deserialization.
       * pandoc-types now requires GHC generics, hence ghc >= 7.2
       * Text.Pandoc.Definition now derives ToJSON and FromJSON instances
         for pandoc types
       * Text.Pandoc.JSON module exports ToJSONFilter typeclass (a replacement
         for the ToJsonFilter exported by Text.Pandoc, which will soon become
         a deprecated synonym).
     - Definition: Export isNullMeta.
     - Added MetaInlines to MetaValue.
     - Builder: Added deleteMeta method to HasMeta class.
     - Builder: Add HasMeta class (with setMeta).
         setMeta can now be used on either Pandoc or Meta.
     - Use json-like structure for metadata instead of association list.
        - `Text.Pandoc.Definition`:
          + Removed `getMeta` and `getMetas`.
          + Export `MetaValue`, `nullMeta`, `lookupMeta`.
          + `MetaValue` can be pandoc blocks, a raw string,
            or a list or map of other `MetaValue`s.
        - `Text.Pandoc.Builder`:
          + Removed `ToBlocks` type class.
          + Added `ToMetaValue` type class.
          + Removed `setMetas`.  `setMeta` can now be called with a list of values.
     - Made document metadata more flexible.
       * The `Meta` type now takes an association list `[(String, [Block])]`
         instead of three hard-coded parameters for title, authors, date.
       * `Text.Pandoc.Definition` exports new `getMeta` and `getMetas` helper
         functions, plus replacements for the old `docTitle`, `docAuthors`,
         and `docDate`.
       * `Text.Pandoc.Builder` exports new `setMeta` and `setMetas` helper
         functions, as well as a new `ToBlocks` class.

--- hs-pandoc-types/buildlink3.mk
+++ hs-pandoc-types/buildlink3.mk
@@ -5,9 +5,11 @@ BUILDLINK_TREE+=	hs-pandoc-types
 .if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.10
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.1
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../wip/hs-pandoc-types
 
+.include "../../wip/hs-aeson/buildlink3.mk"
+.include "../../wip/hs-syb/buildlink3.mk"
 .endif	# HS_PANDOC_TYPES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-pandoc-types
