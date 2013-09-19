4cb08b6d1f5782d72f3e5f676e7c159e17a0298d
szptvlfn <szptvlfn>
2013-09-19 10:29:19 +0000

    Update to 0.13
    CHANGES:
    0.13
        #616, extend to all HTML5 entities
        Optimise lookupNamedEntity
        Replace escapeXMLChar with escapeXML
        Change all Entity functions to return String, not Int or Char

--- hs-tagsoup/buildlink3.mk
+++ hs-tagsoup/buildlink3.mk
@@ -5,9 +5,10 @@ BUILDLINK_TREE+=	hs-tagsoup
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.12.8
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../wip/hs-tagsoup
 
+.include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_TAGSOUP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tagsoup
