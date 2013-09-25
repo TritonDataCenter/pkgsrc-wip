395acabb9e6d7eaa90bedcc5369e64c4b9977183
ryo-on <ryo-on>
2013-09-25 22:22:20 +0000

    Bump PKGREVISION.
    * Remove unused blocks in libreoffice4/Makefile.
    * Disable doxygen option explicitly.
    * Enable Java option, it is accidentally disabled.

--- libreoffice4/options.mk
+++ libreoffice4/options.mk
@@ -8,18 +8,26 @@ PKG_SUGGESTED_OPTIONS=	java
 
 PLIST_VARS+=	java
 
-.if !empty(PKG_OPTIONS:Mdoc)
+.if !empty(PKG_OPTIONS:Mjava)
 USE_JAVA=		yes
+USE_JAVA2=		yes
 BUILD_DEPENDS+=	apache-ant-[0-9]*:../../devel/apache-ant
 FIND_PREFIX:=		ANTDIR=apache-ant
+BUILD_DEPENDS+=	hsqldb-[0-9]*:../../wip/hsqldb18
+FIND_PREFIX:=		HSQLDB_SYSDIR=hsqldb18
 .include "../../mk/find-prefix.mk"
 CONFIGURE_ARGS+=	--enable-ext-wiki-publisher \
 			--with-java \
 			--with-jdk-home=${PKG_JAVA_HOME} \
-			--with-ant-home=${ANTDIR}/ant
+			--with-ant-home=${ANTDIR} \
+			--enable-scripting-beanshell \
+			--enable-scripting-javascript \
+			--with-system-hsqldb \
+			--with-hsqldb-jar=${HSQLDB_SYSDIR}/lib/java/hsqldb18/hsqldb.jar
 .include "../../mk/java-env.mk"
 .include "../../mk/java-vm.mk"
-PLIST.doc=		yes
+PLIST_SRC+=		${PLIST_SRC_DFLT} PLIST.java
+PLIST.java=		yes
 .else
 CONFIGURE_ARGS+=	--without-java
 .endif
