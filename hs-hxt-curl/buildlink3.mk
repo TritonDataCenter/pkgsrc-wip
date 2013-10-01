b93e529caac85f80cbfa14211b32bd1cdac4c3ce
phonohawk <phonohawk>
2013-10-01 01:47:42 +0000

    Import hs-hxt-curl-9.1.1 as wip/hs-hxt-curl.
    LibCurl interface for HXT

---
+++ hs-hxt-curl/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-hxt-curl
+
+.if !defined(HS_HXT_CURL_BUILDLINK3_MK)
+HS_HXT_CURL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hxt-curl+=	hs-hxt-curl>=9.1.1
+BUILDLINK_PKGSRCDIR.hs-hxt-curl?=	../../wip/hs-hxt-curl
+
+.include "../../wip/hs-curl/buildlink3.mk"
+.include "../../wip/hs-hxt/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
+.endif	# HS_HXT_CURL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hxt-curl
