9c83e0edea9013a50f57ec4cadf7132cc0a00452
genolopolis <genolopolis>
2013-10-03 00:37:54 +0000

    upgrade to 1.0.6 and make work on netbsd

---
+++ gearmand/patches/patch-libtest__lite.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- libtest/lite.h-orig	2013-10-02 18:39:21.000000000 +0000
++++ libtest/lite.h
+@@ -51,7 +51,7 @@
+ # include <string.h>
+ #endif
+ 
+-#if defined(WIN32)
++#if defined(WIN32) || defined(__NetBSD__)
+ # include <malloc.h>
+ #else
+ # include <alloca.h>
