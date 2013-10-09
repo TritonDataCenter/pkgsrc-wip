fe6e8772f24f931ca3b5b725ee95b90ee09c0a04
szptvlfn <szptvlfn>
2013-10-09 11:16:19 +0000

    Update to 2.4.2.0
    changes:
    2.4.2.0
     - Merge branch 'prototype' of https://github.com/tmishima/network
        * changed getPeerCred and add getPeerEid to support FreeBSD
     - Merge branch 'master' of https://github.com/neurocyte/network
        * Define SOCK_NONBLOCK if it is missing
          SOCK_NONBLOCK is not defined by Bionic libc in Android.
     - Merge branch 'master' of https://github.com/TimothyKlim/network
        * add ld options
          ability to set LDFLAGS for homebrew issue on Mac OS X
     - Fix sClose reexported as deprecated
     - Merge branch 'master' of https://github.com/hreinhardt/network
        * configure script should accept --with-gcc flag instead of --with-cc
     - Merge pull request #99 from kazu-yamamoto/stable
        * refactoring accept4.
     - Merge pull request #97 from noqqe/master
         Fix for Issue #96 (OpenBSD build error)
     - Merge pull request #101 from joeyadams/withCStringLen
        * Network.Socket: use withCStringLen instead of withCString in send and sendTo
          This fixes a bug where send and sendTo assume that `length xs` is the byte
          length of the string.  withCString(Len) produces a locale-dependent encoding,
          so this assumption may be untrue if, say, UTF-8 is used.

--- hs-network/buildlink3.mk
+++ hs-network/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-network
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.4.1
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.4.2
 BUILDLINK_PKGSRCDIR.hs-network?=	../../wip/hs-network
 
 .include "../../wip/hs-parsec/buildlink3.mk"
