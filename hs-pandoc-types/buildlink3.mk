49013dfed35b773169d5678cc096962d1e9d8038
szptvlfn <szptvlfn>
2013-10-12 23:51:28 +0000

    Update to 1.12.2.3
    changes:
    1.12.2.3
     - Removed CPP; use objects for all-nullary constructors (for now).
         This gives consistency with old aeson behavior.
     - Require aeson >= 0.6.2.
         Otherwise we can't control how generic json renders.
    1.12.2.2
     - Fixed bug in Walkable Inline Inline / walkM instance for Note.
    1.12.2.1
     - Removed extra #endif. Thanks to Sergei Trofimovich.
    1.12.2
     - Adjusted ToJSON/FromJSON instances for aeson >= 0.6.2.
         aeson 0.6.2 changed its default method of deriving ToJSON/FromJSON
         instances for generics.  Instead of {"Str": "hi"}, the inline
         element Str "hi" was becoming {"tag": "Str", "contents": "hi"}.
         This change causes problems for the python pandocfilters module.
    
         This patch uses CPP macros to ensure that the JSON instances work
         the same regardless of what version of aeson is used.

--- hs-pandoc-types/buildlink3.mk
+++ hs-pandoc-types/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-pandoc-types
 .if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.1
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.2
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../wip/hs-pandoc-types
 
 .include "../../wip/hs-aeson/buildlink3.mk"
