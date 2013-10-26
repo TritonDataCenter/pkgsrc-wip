103f810b839416175b9e4bcef95270b2f45921d1
szptvlfn <szptvlfn>
2013-10-26 22:40:14 +0000

    Update to 0.14.0.5
    changes:
    0.14.0.5
     - Fix minor error in Base85, spaces -> y.
    0.14.0.4
     - Fix compilation with GHC 7.6

--- hs-dataenc/buildlink3.mk
+++ hs-dataenc/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-dataenc
 .if !defined(HS_DATAENC_BUILDLINK3_MK)
 HS_DATAENC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dataenc+=	hs-dataenc>=0.14.0
+BUILDLINK_API_DEPENDS.hs-dataenc+=	hs-dataenc>=0.14.0.5
 BUILDLINK_PKGSRCDIR.hs-dataenc?=	../../wip/hs-dataenc
 .endif	# HS_DATAENC_BUILDLINK3_MK
 
