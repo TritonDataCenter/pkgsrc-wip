10c0f45ab89a5c3d2fd3b18c869c47c240886d69
leot1990 <leot1990>
2013-10-26 20:48:18 +0000

    Ops, forgot to add and commit options.mk in the previous commit! :)

---
+++ mupdf/options.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.mupdf
+PKG_SUPPORTED_OPTIONS=	curl
+
+.include "../../mk/bsd.options.mk"
+
+#
+# curl support
+#
+.if !empty(PKG_OPTIONS.Mcurl)
+.  include "../../mk/pthread.buildlink3.mk"
+.  include "../../www/curl/buildlink3.mk"
+.else
+MAKE_FLAGS+=	NOCURL=yes
+.endif
