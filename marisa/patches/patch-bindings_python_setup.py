ab97b831eabdf5a22e3ea6ac0926a8d25b9b45ea
ishit <ishit>
2013-11-04 00:55:12 +0000

    Add patch comment.

--- marisa/patches/patch-bindings_python_setup.py
+++ marisa/patches/patch-bindings_python_setup.py
@@ -1,5 +1,7 @@
 $NetBSD$
 
+add version for egg file's file name.
+
 --- bindings/python/setup.py.orig	2011-05-14 05:20:52.000000000 +0000
 +++ bindings/python/setup.py
 @@ -5,5 +5,6 @@ marisa_module = Extension("_marisa",
