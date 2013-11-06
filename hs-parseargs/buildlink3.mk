04f6f76470d0db3865446e6c7ca9d392df29796a
szptvlfn <szptvlfn>
2013-11-06 12:06:44 +0000

    Update to 0.1.5.2
    changes from README:
     - The 0.1.5.2 release fixes some missing documentation.
     - The 0.1.5.1 release fixes some warnings and stuff.
     - The 0.1.5 release includes the "soft dash" option, giving the ability to
       allow positional arguments to begin with a dash if possible.
     - The 0.1.4 release includes the ability to mix optional and required
       positional arguments.
     - The various 0.1.3.x point releases include bug fixes and various
       extra-minor enhancements. See the Git log.
     - The 0.1.3 release includes more uniform and usable error handling.

--- hs-parseargs/buildlink3.mk
+++ hs-parseargs/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-parseargs
 .if !defined(HS_PARSEARGS_BUILDLINK3_MK)
 HS_PARSEARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parseargs+=	hs-parseargs>=0.1.2
+BUILDLINK_API_DEPENDS.hs-parseargs+=	hs-parseargs>=0.1.5
 BUILDLINK_PKGSRCDIR.hs-parseargs?=	../../wip/hs-parseargs
 .endif	# HS_PARSEARGS_BUILDLINK3_MK
 
