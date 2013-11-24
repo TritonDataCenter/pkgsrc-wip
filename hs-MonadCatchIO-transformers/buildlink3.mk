2dcdb4fdc81957b2bb0187355064de39103850b3
szptvlfn <szptvlfn>
2013-11-24 12:13:33 +0000

    Update to 0.3.1.0
    changes:
    0.3.1.0
     - Update to base-4.7
     - Add deprecation warnings
     - Add new module Control.Monad.CatchIO.Try

--- hs-MonadCatchIO-transformers/buildlink3.mk
+++ hs-MonadCatchIO-transformers/buildlink3.mk
@@ -5,9 +5,10 @@ BUILDLINK_TREE+=	hs-MonadCatchIO-transformers
 .if !defined(HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK)
 HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.3
+BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.3.1
 BUILDLINK_PKGSRCDIR.hs-MonadCatchIO-transformers?=	../../wip/hs-MonadCatchIO-transformers
 
+.include "../../wip/hs-monads-tf/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK
 
