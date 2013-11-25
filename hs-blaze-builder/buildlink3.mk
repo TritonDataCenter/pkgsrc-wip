bb8cc669b63755c8aa4e3fc40283fe09a36b443c
szptvlfn <szptvlfn>
2013-11-25 10:59:14 +0000

    Update to 0.3.3.0
    CHANGES:
    * 0.3.3.0
      - exposed the 'Buffer' constructor to enable keeping around a pool of
        buffers.
    * 0.3.2.0
      - added 'writeToByteString' to construct a strict bytestring in a single
        step. We can actually view 'Write's as strict-bytestring builders.

--- hs-blaze-builder/buildlink3.mk
+++ hs-blaze-builder/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-blaze-builder
 .if !defined(HS_BLAZE_BUILDER_BUILDLINK3_MK)
 HS_BLAZE_BUILDER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.1
+BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.3
 BUILDLINK_PKGSRCDIR.hs-blaze-builder?=	../../wip/hs-blaze-builder
 
 .include "../../wip/hs-text/buildlink3.mk"
