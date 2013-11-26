ac010864f3748163343dcdf68309683717133b2d
szptvlfn <szptvlfn>
2013-11-26 11:42:01 +0000

    Update to 0.12
    CHANGELOG:
    0.12
    * Added an instance for `Const r`.
    * Added `some1`

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-semigroups
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.11
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.12
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
 .include "../../wip/hs-hashable/buildlink3.mk"
