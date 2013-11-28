c2cc21f72883632129e6f3fb4e7afe31e54f2c7f
szptvlfn <szptvlfn>
2013-11-28 13:19:34 +0000

    Update to 1.8.54
    Changelog:
    1.8.54
        Fix a bug when suggesting const

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hlint
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.53
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.54
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
