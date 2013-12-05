dd80bb6bee29ec26e4935a4c01937e97d09f4634
phonohawk <phonohawk>
2013-12-05 02:50:08 +0000

    Upstream update to haskeline-0.7.1.1

--- hs-haskeline/buildlink3.mk
+++ hs-haskeline/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-haskeline
 .if !defined(HS_HASKELINE_BUILDLINK3_MK)
 HS_HASKELINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.7
+BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.7.1
 BUILDLINK_PKGSRCDIR.hs-haskeline?=	../../wip/hs-haskeline
 
 .include "../../wip/hs-terminfo/buildlink3.mk"
