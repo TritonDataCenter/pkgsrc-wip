fbc07bc17baf6fe4d341b2a5e320b33f2f0b85b5
szptvlfn <szptvlfn>
2013-12-05 23:05:27 +0000

    Update to 0.10.6
    CHANGES:
    0.10.6
        #625, more documentation about args/argPos
        #626, ensure initial lists don't get reversed (fix after #610)
        ( #610 => http://code.google.com/p/ndmitchell/issues/detail?id=610 )
        ( #625 => http://code.google.com/p/ndmitchell/issues/detail?id=625 )
        ( #626 => http://code.google.com/p/ndmitchell/issues/detail?id=626 )

--- hs-cmdargs/buildlink3.mk
+++ hs-cmdargs/buildlink3.mk
@@ -5,9 +5,10 @@ BUILDLINK_TREE+=	hs-cmdargs
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.5
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.6
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../wip/hs-cmdargs
 
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_CMDARGS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-cmdargs
