82541b863c1b821b0bbf2db7a53b28c109d4e026
szptvlfn <szptvlfn>
2013-12-08 19:05:51 +0000

    Update to 0.8.3
    changes:
    0.8.3
     - remove upper boundary on text, may text simply never break anything

--- hs-http-types/buildlink3.mk
+++ hs-http-types/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-http-types
 .if !defined(HS_HTTP_TYPES_BUILDLINK3_MK)
 HS_HTTP_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-types+=	hs-http-types>=0.8.2
+BUILDLINK_API_DEPENDS.hs-http-types+=	hs-http-types>=0.8.3
 BUILDLINK_PKGSRCDIR.hs-http-types?=	../../wip/hs-http-types
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
