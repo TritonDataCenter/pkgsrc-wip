96bc21f412f01b47991439bffcfc1f8031ccdbfc
szptvlfn <szptvlfn>
2013-12-09 22:14:43 +0000

    Update to 4000.2.10
    changes:
    4000.2.10
     - Merge pull request #48 from nominolo/master
       Fix severe performance issue affecting "cabal update"
       * Turn O(n^2) reverse into O(n)
         This patch improves `cabal update` performance by 10x(!) on my system.
         Due to this issue, `cabal update` was using an extreme amount of CPU,
         while it should be I/O-bound.  The reverse was taking 79s, now it
         takes 0.01s.
     ( #48 => https://github.com/haskell/HTTP/pull/48 )

--- hs-HTTP/buildlink3.mk
+++ hs-HTTP/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-HTTP
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.9
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.10
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../wip/hs-HTTP
 
 .include "../../wip/hs-mtl/buildlink3.mk"
