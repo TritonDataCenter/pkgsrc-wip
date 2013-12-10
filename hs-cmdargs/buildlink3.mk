6977fc2194952e13da75b6958ab1a936a5ba00b5
szptvlfn <szptvlfn>
2013-12-10 13:37:42 +0000

    Update to 0.10.7
    CHANGES:
    0.10.7
        #1, fix timestamps in .tar.gz dist file
     ( #1 => https://github.com/ndmitchell/cmdargs/issues/1 )

--- hs-cmdargs/buildlink3.mk
+++ hs-cmdargs/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-cmdargs
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.6
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.7
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../wip/hs-cmdargs
 
 .include "../../wip/hs-transformers/buildlink3.mk"
