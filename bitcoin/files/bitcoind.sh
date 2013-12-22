4ea01e5236fc7e8e9a825c474f70f26f0136e1f3
noud4 <noud4>
2013-12-22 22:25:59 +0000

    Enabled bitcoin rc.d script, data dir and a bitcoin.conf example.

--- bitcoin/files/bitcoind.sh
+++ bitcoin/files/bitcoind.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!@RCD_SCRIPTS_SHELL@
 #
 # $NetBSD$
 #
@@ -13,10 +13,10 @@ fi
 
 name="bitcoind"
 rcvar=$name
-command="/usr/pkg/sbin/bitcoind"
-pidfile="/var/run/bitcoind.pid"
-required_directory="/var/bitcoin"
-command_args="-daemon -pid=${pidfile} -datadir=${required_directory}"
+command="@PREFIX@/sbin/bitcoind"
+pidfile="@VARBASE@/run/bitcoind.pid"
+required_directory="@VARBASE@/bitcoin"
+command_args="-daemon -conf=@PREFIX@/etc/bitcoin.conf -pid=${pidfile} -datadir=${required_directory}"
 
 if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
