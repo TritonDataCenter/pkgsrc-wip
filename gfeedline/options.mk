d66be7fc675fdfd442263c7723b5d620d6b29bd4
noud4 <noud4>
2013-12-22 17:04:23 +0000

    Get the html files rights right at install.

--- gfeedline/options.mk
+++ gfeedline/options.mk
@@ -2,17 +2,16 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gfeedline
 PKG_SUPPORTED_OPTIONS=	proxy spellcheck
-PKG_SUGGESTED_OPTIONS=	# TODO
+PKG_SUGGESTED_OPTIONS=	
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mproxy)
-DEPENDS+=	${PYPKGPREFIX}-libproxy-[0-9]*:../../net/py-libproxy
+#DEPENDS+=	${PYPKGPREFIX}-libproxy-[0-9]*:../../wip/py-libproxy		# TODO
 .endif
 
-#gnome-sushi
+#gnome-sushi									# TODO
 
 .if !empty(PKG_OPTIONS:Mspellcheck)
-# not tested yet
-DEPENDS+=	${PYPKGPREFIX}-gtkspellcheck-[0-9]*:../..//py-gtkspellcheck
+#DEPENDS+=	${PYPKGPREFIX}-gtkspellcheck-[0-9]*:../../wip/py-gtkspellcheck	# TODO
 .endif
