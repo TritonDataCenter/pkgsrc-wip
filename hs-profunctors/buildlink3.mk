b107dd49c5c7648ce076c18d63ecd19f4b75430b
szptvlfn <szptvlfn>
2013-12-27 22:08:28 +0000

    Update to 4.0.2
    CHANGELOG:
    4.0.2
    * Added `assoc` to `Data.Profunctor.Composition` so that we have all 3 associators.

--- hs-profunctors/buildlink3.mk
+++ hs-profunctors/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-profunctors
 .if !defined(HS_PROFUNCTORS_BUILDLINK3_MK)
 HS_PROFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=4.0.1
+BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=4.0.2
 BUILDLINK_PKGSRCDIR.hs-profunctors?=	../../wip/hs-profunctors
 
 .include "../../wip/hs-comonad/buildlink3.mk"
