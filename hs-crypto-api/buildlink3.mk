160318dd7420c3ef77e60cf7746281b520f82725
szptvlfn <szptvlfn>
2014-01-01 08:08:28 +0000

    Update to 0.13
    changes:
    0.13
     - Remove CWC code for now
     - Fix up some comments
     - Expand Exception module with Crypto.Random functions and comments.
     - Derive 'Data' on ReseedInfo
     - Export functions to generate an IV.
     - Derive Data
     - Add an exceptions-based interface
     - First implementation of reference cwc mode
       We still need cwcLazy and to test the current implementation along with
       document that it isn't a performant implementation.
     - Allow a random gen for asym decryption
     - Move all but auth modes over to Crypto.Classes.

--- hs-crypto-api/buildlink3.mk
+++ hs-crypto-api/buildlink3.mk
@@ -5,9 +5,13 @@ BUILDLINK_TREE+=	hs-crypto-api
 .if !defined(HS_CRYPTO_API_BUILDLINK3_MK)
 HS_CRYPTO_API_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.12.2
+BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.13
 BUILDLINK_PKGSRCDIR.hs-crypto-api?=	../../wip/hs-crypto-api
 
+.include "../../wip/hs-cereal/buildlink3.mk"
+.include "../../wip/hs-entropy/buildlink3.mk"
+.include "../../wip/hs-tagged/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_CRYPTO_API_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-crypto-api
