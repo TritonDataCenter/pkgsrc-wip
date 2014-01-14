ffcf21277ee0a27035f362163caa1ae48aeede9f
nros <nros>
2014-01-14 19:27:09 +0000

    Initial commit of qore programming language to pkgsrc-wip. Qore is a modular, multi-threaded, SQL-integrated dynamically typed scripting language with optional hard typing and procedural and object-oriented features, powerful and easy-to-use data types, structures, and operators, a clean and easy-to-lean/read syntax.

---
+++ qore/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.qore
+PKG_SUPPORTED_OPTIONS=	debug doc
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=        --enable-debug
+.else
+CONFIGURE_ARGS+=        --disable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Mdoc)
+.else
+CONFIGURE_ARGS+=        --with-doxygen=no
+.endif
