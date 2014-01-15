0b276bc5ed1456adf36a8ba4ad6cdc2c5e41df52
szptvlfn <szptvlfn>
2014-01-15 23:28:42 +0000

    Update to 0.1.1
    changes:
    0.1.1
     - version bump due to attoparsec 0.11.1

--- hs-http-attoparsec/buildlink3.mk
+++ hs-http-attoparsec/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-http-attoparsec
 .if !defined(HS_HTTP_ATTOPARSEC_BUILDLINK3_MK)
 HS_HTTP_ATTOPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-attoparsec+=	hs-http-attoparsec>=0.1.0
+BUILDLINK_API_DEPENDS.hs-http-attoparsec+=	hs-http-attoparsec>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-http-attoparsec?=	../../wip/hs-http-attoparsec
 
 .include "../../wip/hs-attoparsec/buildlink3.mk"
