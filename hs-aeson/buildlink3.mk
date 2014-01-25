77950504794526e3bf5ea859cf6db6d4b40d1bd4
szptvlfn <szptvlfn>
2014-01-25 14:09:54 +0000

    Update to 0.7.0.0
    changelog:
    0.7.0.0
        * The performance of encoding to and decoding of bytestrings
          have both improved by up to 2x, while also using less
          memory.
        * New dependency: the scientific package lets us parse
          floating point numbers more quickly and accurately.
        * eitherDecode, decodeStrictWith: fixed bugs.
        * Added FromJSON and ToJSON instances for Tree and Scientific.
        * Fixed the ToJSON instances for UTCTime and ZonedTime.

--- hs-aeson/buildlink3.mk
+++ hs-aeson/buildlink3.mk
@@ -5,13 +5,14 @@ BUILDLINK_TREE+=	hs-aeson
 .if !defined(HS_AESON_BUILDLINK3_MK)
 HS_AESON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=0.6.2
+BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=0.7.0
 BUILDLINK_PKGSRCDIR.hs-aeson?=	../../wip/hs-aeson
 
 .include "../../wip/hs-attoparsec/buildlink3.mk"
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
 .include "../../wip/hs-hashable/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-scientific/buildlink3.mk"
 .include "../../wip/hs-syb/buildlink3.mk"
 .include "../../wip/hs-text/buildlink3.mk"
 .include "../../wip/hs-unordered-containers/buildlink3.mk"
