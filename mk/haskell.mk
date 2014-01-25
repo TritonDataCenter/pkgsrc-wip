0eae178a1e1e8a5e74aaa6d028f561aaaa916a6f
obache <obache>
2014-01-25 09:11:34 +0000

    Allow to define multiple sites in MASTER_SITE_HASKELL_HACKAGE.

--- mk/haskell.mk
+++ mk/haskell.mk
@@ -132,7 +132,7 @@ PKGNAME?=	hs-${DISTNAME}
 # Default value of MASTER_SITES.
 _DISTBASE?=	${DISTNAME:C/-[^-]*$//}
 _DISTVERSION?=	${DISTNAME:C/^.*-//}
-MASTER_SITES?=	${MASTER_SITE_HASKELL_HACKAGE}${DISTNAME}/
+MASTER_SITES?=	${MASTER_SITE_HASKELL_HACKAGE:=${DISTNAME}/}
 
 # Default value of HOMEPAGE.
 HOMEPAGE?=	http://hackage.haskell.org/cgi-bin/hackage-scripts/package/${_DISTBASE}
