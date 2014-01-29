3ec773ea94f0c104f647ce707592819af7cd0d98
szptvlfn <szptvlfn>
2014-01-29 22:18:15 +0000

    Import newtype-0.2 as wip/hs-newtype.
    Per Conor McBride, the Newtype typeclass represents the packing and
    unpacking of a newtype, and allows you to operatate under that newtype with
    functions such as ala.

---
+++ hs-newtype/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-newtype
+
+.if !defined(HS_NEWTYPE_BUILDLINK3_MK)
+HS_NEWTYPE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-newtype+=	hs-newtype>=0.2
+BUILDLINK_PKGSRCDIR.hs-newtype?=	../../wip/hs-newtype
+.endif	# HS_NEWTYPE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-newtype
