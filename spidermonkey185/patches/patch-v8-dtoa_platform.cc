34fdbb232cae05d4a1e00cb94e999ce90051cd2d
fhajny <fhajny>
2014-02-03 13:52:52 +0000

    Fix build on recent SunOS platforms; bump PKGREVISION.

---
+++ spidermonkey185/patches/patch-v8-dtoa_platform.cc
@@ -0,0 +1,19 @@
+$NetBSD$
+
+fpclassify is defined on Illumos.
+--- v8-dtoa/platform.cc.orig	2011-03-31 19:08:36.000000000 +0000
++++ v8-dtoa/platform.cc
+@@ -111,6 +111,7 @@ int fpclassify(double x) {
+ #endif  // _MSC_VER
+ 
+ #ifdef SOLARIS
++#ifndef fpclassify
+ 
+ #include <ieeefp.h>
+ 
+@@ -134,4 +135,5 @@ int fpclassify(double x) {
+   }
+ 
+ }
++#endif fpclassify
+ #endif // SOLARIS
