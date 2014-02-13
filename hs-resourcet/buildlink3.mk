6c12d599c5b14aea625311d4ce209f3f958083b3
szptvlfn <szptvlfn>
2014-02-13 13:07:45 +0000

    Update to 0.4.10
    changes:
    0.4.10
     - Rename withResource to with (thanks @Gabriel439)
     - Resource monad

--- hs-resourcet/buildlink3.mk
+++ hs-resourcet/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-resourcet
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=0.4.9
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=0.4.10
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../wip/hs-resourcet
 
 .include "../../wip/hs-lifted-base/buildlink3.mk"
