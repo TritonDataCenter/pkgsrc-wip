414283dea1546ab876ea65c75dfa6c86af7335c8
nros <nros>
2014-02-14 21:03:03 +0000

    Initial import of qore-opengl-module. Opengl module for Qore. This module provides Opengl language bindings for Qore.

---
+++ qore-opengl-module/patches/patch-src_opengl.cc
@@ -0,0 +1,18 @@
+$NetBSD$
+
+* alloca is in stdlib.h on most systems 
+
+--- src/opengl.cc.orig	2009-09-21 11:55:18.000000000 +0000
++++ src/opengl.cc
+@@ -22,7 +22,11 @@
+ 
+ #include "qore-opengl.h"
+ 
++#if defined(__linux__) || defined(__sgi) || defined(__QNX__)
+ #include <alloca.h>
++#else
++#include <stdlib.h>
++#endif
+ 
+ static QoreStringNode *opengl_module_init();
+ static void opengl_module_ns_init(QoreNamespace *rns, QoreNamespace *qns);
