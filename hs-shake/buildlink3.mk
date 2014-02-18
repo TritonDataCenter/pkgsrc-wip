af780b69ddd23b6f92ac68b7e47f49ebcea354a4
szptvlfn <szptvlfn>
2014-02-18 14:29:41 +0000

    Update to 0.11.1
    CHANGES:
    0.11.1
        #94, GHC 7.8 support
        Add a Config module
        #89, support :: as a build rule separator
    ( #94 => https://github.com/ndmitchell/shake/issues/94 )
    ( #89 => https://github.com/ndmitchell/shake/issues/89 )

--- hs-shake/buildlink3.mk
+++ hs-shake/buildlink3.mk
@@ -5,12 +5,11 @@ BUILDLINK_TREE+=	hs-shake
 .if !defined(HS_SHAKE_BUILDLINK3_MK)
 HS_SHAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11
+BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-shake?=	../../wip/hs-shake
 
 .include "../../wip/hs-hashable/buildlink3.mk"
 .include "../../wip/hs-random/buildlink3.mk"
-.include "../../wip/hs-text/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-unordered-containers/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
