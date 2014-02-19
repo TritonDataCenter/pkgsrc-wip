e03b212ff20f4fdee08fc7370a0e14bd362476b6
szptvlfn <szptvlfn>
2014-02-19 09:14:10 +0000

    Update to 0.11.2
    CHANGES:
    0.11.2
        #96, fix a bug in addPath that caused $PATH to be added twice
    ( #96 => https://github.com/ndmitchell/shake/issues/96 )

--- hs-shake/buildlink3.mk
+++ hs-shake/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-shake
 .if !defined(HS_SHAKE_BUILDLINK3_MK)
 HS_SHAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11.1
+BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11.2
 BUILDLINK_PKGSRCDIR.hs-shake?=	../../wip/hs-shake
 
 .include "../../wip/hs-hashable/buildlink3.mk"
