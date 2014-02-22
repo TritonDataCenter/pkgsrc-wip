22cb45a4eaf689205ea0685bfae35b4863ead09f
nros <nros>
2014-02-22 15:01:31 +0000

    Initial import of qore-asn1-module to pkgsrc-wip. ASN.1(Abstract Syntax Notation One) module for Qore provides an API to dynamically create, parse and convert ASN.1 data structures to concrete output formats (like DER).

---
+++ qore-asn1-module/options.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.qore-asn1-module
+PKG_SUPPORTED_OPTIONS=	debug
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=        --enable-debug
+.else
+CONFIGURE_ARGS+=        --disable-debug
+.endif
