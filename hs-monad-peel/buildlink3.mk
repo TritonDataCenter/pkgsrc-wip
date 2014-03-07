1e1de323e34c8c4374f90110f3eee93d2cf325b1
szptvlfn <szptvlfn>
2014-03-07 12:42:22 +0000

    Update to 0.1.1
    changes:
    0.1.1
     - Relax transformers dependency
     - Add liftOp and liftOp_
     - Remove notes about monadic side effects in bracketOnError and onException

--- hs-monad-peel/buildlink3.mk
+++ hs-monad-peel/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-monad-peel
 .if !defined(HS_MONAD_PEEL_BUILDLINK3_MK)
 HS_MONAD_PEEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monad-peel+=	hs-monad-peel>=0.1
+BUILDLINK_API_DEPENDS.hs-monad-peel+=	hs-monad-peel>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-monad-peel?=	../../wip/hs-monad-peel
 
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
