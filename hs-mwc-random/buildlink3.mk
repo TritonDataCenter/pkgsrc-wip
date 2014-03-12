786d8eca78664b6f1b4b61eee321162ec79d3843
szptvlfn <szptvlfn>
2014-03-12 23:14:11 +0000

    Import mwc-random-0.13.1.1 as wip/hs-mwc-random.
    This package contains code for generating high quality random numbers that
    follow either a uniform or normal distribution. The generated numbers are
    suitable for use in statistical applications.
    
    The uniform PRNG uses Marsaglia's MWC256 (also known as MWC8222)
    multiply-with-carry generator, which has a period of 2^8222 and fares well
    in tests of randomness. It is also extremely fast, between 2 and 3 times
    faster than the Mersenne Twister.
    
    Compared to the mersenne-random package, this package has a more convenient
    API, is faster, and supports more statistical distributions.

---
+++ hs-mwc-random/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-mwc-random
+
+.if !defined(HS_MWC_RANDOM_BUILDLINK3_MK)
+HS_MWC_RANDOM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.13.1
+BUILDLINK_PKGSRCDIR.hs-mwc-random?=	../../wip/hs-mwc-random
+
+.include "../../wip/hs-primitive/buildlink3.mk"
+.include "../../wip/hs-vector/buildlink3.mk"
+.endif	# HS_MWC_RANDOM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-mwc-random
