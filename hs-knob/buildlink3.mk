1fa5d5eedf0bdc14e11762f316d7acea3641f55b
szptvlfn <szptvlfn>
2014-03-13 12:10:35 +0000

    Import knob-0.1.1 as wip/hs-knob.
    Create memory-backed Handles, referencing virtual files. This is mostly
    useful for testing Handle-based APIs without having to interact with the
    filesystem.

---
+++ hs-knob/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	hs-knob
+
+.if !defined(HS_KNOB_BUILDLINK3_MK)
+HS_KNOB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-knob+=	hs-knob>=0.1.1
+BUILDLINK_PKGSRCDIR.hs-knob?=	../../wip/hs-knob
+
+.include "../../wip/hs-transformers/buildlink3.mk"
+.endif	# HS_KNOB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-knob
