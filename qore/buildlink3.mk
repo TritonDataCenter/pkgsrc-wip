82b7c9c8ab81ac15453f5ec03b9be6f7e1d996de
nros <nros>
2014-04-03 15:36:49 +0000

    Added dependencies in buildlink for libtool usage.

--- qore/buildlink3.mk
+++ qore/buildlink3.mk
@@ -9,7 +9,9 @@ QORE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.qore+=	qore>=0.8.8
 BUILDLINK_PKGSRCDIR.qore?=	../../wip/qore
 
-
+.include "../../devel/pcre/buildlink3.mk"
+.include "../../devel/libexecinfo/buildlink3.mk"
+.include "../../math/mpfr/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .endif	# QORE_BUILDLINK3_MK
 
