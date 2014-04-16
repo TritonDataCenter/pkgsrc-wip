e9938a8318ded78a6fa7d25c8b4f4e28f9b32d60
thomasklausner <thomasklausner>
2014-04-16 17:19:05 +0000

    Remove incorrect hunk from patch.

--- calibre/patches/patch-setup_extensions.py
+++ calibre/patches/patch-setup_extensions.py
@@ -4,12 +4,6 @@ Correct directory name.
 
 --- setup/extensions.py.orig	2014-04-04 02:33:40.000000000 +0000
 +++ setup/extensions.py
-@@ -1,4 +1,4 @@
--#!/usr/bin/env python
-+#!/usr/pkg/bin/python2.7
- # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
- from __future__ import with_statement
- 
 @@ -567,7 +567,7 @@ class Build(Command):
              self.check_call(qmc + ['qtcurve.pro'])
              self.check_call([make]+([] if iswindows else ['-j%d'%(cpu_count()
