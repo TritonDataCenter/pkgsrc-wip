80bc179b2ca5fe53c5dd8bdda7b492e7743b1607
szptvlfn <szptvlfn>
2014-04-25 13:29:30 +0000

    Update to 2.5.15
    CHANGES:
    2.5.15
        Allow haskell-src-exts-1.15.*
    2.5.14
        #3, support GHC 7.9
    #3 => https://github.com/ndmitchell/derive/pull/3

--- hs-derive/buildlink3.mk
+++ hs-derive/buildlink3.mk
@@ -5,11 +5,12 @@ BUILDLINK_TREE+=	hs-derive
 .if !defined(HS_DERIVE_BUILDLINK3_MK)
 HS_DERIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.5.13
+BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.5.15
 BUILDLINK_PKGSRCDIR.hs-derive?=	../../wip/hs-derive
 
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
 .include "../../wip/hs-syb/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-uniplate/buildlink3.mk"
 .endif	# HS_DERIVE_BUILDLINK3_MK
