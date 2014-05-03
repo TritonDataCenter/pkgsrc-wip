d02850f732a2a0c0858acef238acc8c9c62524b5
thomasklausner <thomasklausner>
2014-05-03 19:01:56 +0000

    Fix paths.

--- libisoburn/buildlink3.mk
+++ libisoburn/buildlink3.mk
@@ -8,8 +8,8 @@ LIBISOBURN_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libisoburn+=	libisoburn>=1.1.8
 BUILDLINK_PKGSRCDIR.libisoburn?=	../../wip/libisoburn
 
-.include "../../wip/libisofs/buildlink3.mk"
-.include "../../wip/libburn/buildlink3.mk"
+.include "../../sysutils/libisofs/buildlink3.mk"
+.include "../../sysutils/libburn/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# LIBISOBURN_BUILDLINK3_MK
 
