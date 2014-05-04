011e4a71f7521328caf8f858453cbd675206ea0b
jihbed <jihbed>
2014-05-04 00:27:26 +0000

    Import py27-lea-1.2 as wip/py-lea.
    Lea is a Python package aiming at working with discrete probability
    distributions in an intuitive way. It allows you to model a broad range of
    random phenomenons, like dice throwing, coin tossing, cards hands, gambling,
    lottery, ? with fair or unfair characteristics More generally, Lea may be used
    for any finite set of discrete values having known probability: numbers,
    boolean variables (true/false), date/times, symbols, ... Each distribution
    is modelled as a plain object, which can be named, displayed, queried or
    processed to produce new distribution objects

---
+++ py-lea/patches/patch-setup.py
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- setup.py.orig	2014-05-04 01:23:19.000000000 +0100
++++ setup.py	2014-05-04 01:24:01.000000000 +0100
+@@ -44,6 +44,4 @@
+                       'alea',
+                       'tlea',
+                       'license' ] ,
+-       data_files = [('.', [ 'COPYING',
+-                             'COPYING.LESSER' ] ) ],
+       )
+\ No newline at end of file
