965b9fea3c6106dc7b9210ff82514a854f83d81e
ryo-on <ryo-on>
2014-05-05 23:57:07 +0000

    Update to 5.0
    * Support OSS backend for NetBSD

--- pulseaudio/buildlink3.mk
+++ pulseaudio/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.19 2013/11/25 12:00:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2014/04/20 02:27:49 ryoon Exp $
 
 BUILDLINK_TREE+=	pulseaudio
 
 .if !defined(PULSEAUDIO_BUILDLINK3_MK)
 PULSEAUDIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pulseaudio+=	pulseaudio>=2.1
-BUILDLINK_ABI_DEPENDS.pulseaudio+=	pulseaudio>=2.1nb13
+BUILDLINK_API_DEPENDS.pulseaudio+=	pulseaudio>=5.0
+BUILDLINK_ABI_DEPENDS.pulseaudio+=	pulseaudio>=5.0
 BUILDLINK_PKGSRCDIR.pulseaudio?=	../../audio/pulseaudio
 
 .include "../../mk/bsd.fast.prefs.mk"
--- pulseaudio/patches/patch-src_pulsecore_svolume__sse.c
+++ pulseaudio/patches/patch-src_pulsecore_svolume__sse.c
@@ -2,14 +2,23 @@ $NetBSD: patch-src_pulsecore_svolume__sse.c,v 1.1 2013/10/24 17:48:53 jperkin Ex
 
 Avoid bad register usage on OSX 32-bit.
 
---- src/pulsecore/svolume_sse.c~	2014-01-19 23:46:10.000000000 +0900
-+++ src/pulsecore/svolume_sse.c	2014-01-19 23:50:48.000000000 +0900
-@@ -255,7 +255,7 @@ static void pa_volume_s16re_sse2(int16_t
- #endif /* defined (__i386__) || defined (__amd64__) */
+--- src/pulsecore/svolume_sse.c.orig	2014-02-22 11:05:30.000000000 +0000
++++ src/pulsecore/svolume_sse.c
+@@ -34,7 +34,7 @@
+ 
+ #include "sample-util.h"
+ 
+-#if (!defined(__FreeBSD__) && defined (__i386__)) || defined (__amd64__)
++#if ((!defined(__FreeBSD__) || defined(__APPLE__)) && defined (__i386__)) || defined (__amd64__)
+ 
+ #define VOLUME_32x16(s,v)                  /* .. |   vh  |   vl  | */                   \
+       " pxor %%xmm4, %%xmm4          \n\t" /* .. |    0  |    0  | */                   \
+@@ -254,7 +254,7 @@ static void pa_volume_s16re_sse2(int16_t
+ #endif /* (!defined(__FreeBSD__) && defined (__i386__)) || defined (__amd64__) */
  
  void pa_volume_func_init_sse(pa_cpu_x86_flag_t flags) {
--#if defined (__i386__) || defined (__amd64__)
-+#if (defined (__i386__) && !defined(__APPLE__)) || defined (__amd64__)
+-#if (!defined(__FreeBSD__) && defined (__i386__)) || defined (__amd64__)
++#if ((!defined(__FreeBSD__) || defined(__APPLE__)) && defined (__i386__)) || defined (__amd64__)
      if (flags & PA_CPU_X86_SSE2) {
          pa_log_info("Initialising SSE2 optimized volume functions.");
  
