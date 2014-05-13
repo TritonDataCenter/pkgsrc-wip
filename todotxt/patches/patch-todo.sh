aa8e60a5d1bef1cc147c197a8555153fd88023d7
ttys <ttys>
2014-05-13 14:01:03 +0000

    bump up todo.sh

--- todotxt/patches/patch-todo.sh
+++ todotxt/patches/patch-todo.sh
@@ -1,8 +1,8 @@
-$NetBSD: patch-todo.sh,v 1.0 2014/05/07 11:41:29 tty` Exp $
+$NetBSD: patch-todo.sh,v 1.0 2014/05/13 14:35:52 tty` Exp $
 
 Change the path for bash.
 
---- todo.sh.orig	2012-03-26 18:56:04.000000000 +0000
+--- todo.sh.orig	2014-05-13 12:31:26.000000000 +0000
 +++ todo.sh
 @@ -1,4 +1,4 @@
 -#! /bin/bash
