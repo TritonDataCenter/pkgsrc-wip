15838a2e6ed3b41a40c7ab6582aa373c14061eae
szptvlfn <szptvlfn>
2014-05-17 20:28:32 +0000

    Update to 0.7.0.1 pkgsrc changes: static -> dynamic ChangeLog: 0.7.0.1 - bump upper bound for `QuickCheck` Version 0.7 (2014-03-17) *St. Patrick's Day* --------------------------------------------
    #### Package changes
    
    * Add `NFData` instance (and `deepseq` dependency)
    * Add `IsString` instance
    * Remove deprecated entities

--- hs-dlist/buildlink3.mk
+++ hs-dlist/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-dlist
 .if !defined(HS_DLIST_BUILDLINK3_MK)
 HS_DLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.6
+BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.7.0
 BUILDLINK_PKGSRCDIR.hs-dlist?=	../../wip/hs-dlist
 .endif	# HS_DLIST_BUILDLINK3_MK
 
