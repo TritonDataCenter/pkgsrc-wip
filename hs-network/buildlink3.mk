b63b001d076ad07b9113ba08afd4c07cecc40169
szptvlfn <szptvlfn>
2014-05-20 21:22:06 +0000

    Update to 2.5.0.0 pkgsrc changes: static -> dynamic Changes from https://github.com/haskell/network 2.5.0.0  - Merge pull request #123 from shockkolate/master    Network.Socket: Added CustomSockOpt data constructor for SocketOption 2.4.2.3  - Bring latest GNU config in.

--- hs-network/buildlink3.mk
+++ hs-network/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-network
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.4.2
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.5.0
 BUILDLINK_PKGSRCDIR.hs-network?=	../../wip/hs-network
 
 .include "../../textproc/hs-parsec/buildlink3.mk"
