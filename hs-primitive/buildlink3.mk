3113dcaa094da5e40ca86f3e3ca1751cb6e1a984
szptvlfn <szptvlfn>
2014-05-22 21:31:26 +0000

    Update to 0.5.3.0 pkgsrc changes: static -> dynamic changelog: ## Changes in version 0.5.3.0
     * Implement `cloneArray` and `cloneMutableArray` primitives
       (with fall-back implementations for GHCs prior to version 7.2.1)
    
    ## Changes in version 0.5.2.1
    
     * Add strict variants of `MutVar` modification functions
       `atomicModifyMutVar'` and `modifyMutVar'`
    
     * Fix compilation on Solaris 10 with GNU C 3.4.3

--- hs-primitive/buildlink3.mk
+++ hs-primitive/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-primitive
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.1
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.3
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../wip/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
