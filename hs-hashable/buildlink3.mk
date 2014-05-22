88d963887abea090c0789ae5946528f4abe8b941
szptvlfn <szptvlfn>
2014-05-22 21:32:36 +0000

    Update to 1.2.2.0
    CHANGES:
    ## Version 1.2.2.0
    
     * Add instances for `Data.ByteString.Short`
    
     * Use a 32-bit default salt on 32-bit archs.

--- hs-hashable/buildlink3.mk
+++ hs-hashable/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-hashable
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.2.1
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.2.2
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../wip/hs-hashable
 
 .include "../../devel/hs-text/buildlink3.mk"
