fe3e21f48089a2e9539111986f0a98cd7a32192e
szptvlfn <szptvlfn>
2014-05-23 21:44:50 +0000

    Update to 0.6.1 pkgsrc changes: static -> dynamic CHANGELOG: 0.6.1  - tested-with: GHC == 7.8.2 ( from exceptions.cabal ) 0.6 --- * Split out `MonadMask` * Added `transformers` 0.4 support
    0.4
    ---
    * Factored out a separate `MonadThrow`.
    
    0.3.3.1
    -------
    * QuickCheck dependency bump

--- hs-exceptions/buildlink3.mk
+++ hs-exceptions/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-exceptions
 .if !defined(HS_EXCEPTIONS_BUILDLINK3_MK)
 HS_EXCEPTIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-exceptions+=	hs-exceptions>=0.3.3
+BUILDLINK_API_DEPENDS.hs-exceptions+=	hs-exceptions>=0.6.1
 BUILDLINK_PKGSRCDIR.hs-exceptions?=	../../wip/hs-exceptions
 
 .include "../../devel/hs-mtl/buildlink3.mk"
