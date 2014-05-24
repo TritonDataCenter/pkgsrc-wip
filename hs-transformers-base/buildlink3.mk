918193049933992e9ed91d071fabf60377e43a3b
szptvlfn <szptvlfn>
2014-05-24 22:00:23 +0000

    Update to 0.4.2 pkgsrc changes: static -> dynamic                 fix mis-targeted bomb Changes from https://github.com/mvv/transformers-base 0.4.2  - Merge pull request #5 from basvandijk/master    Support transformers-0.4.0.0

--- hs-transformers-base/buildlink3.mk
+++ hs-transformers-base/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	hs-transformers-base
 .if !defined(HS_TRANSFORMERS_BASE_BUILDLINK3_MK)
 HS_TRANSFORMERS_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.1
-BUILDLINK_PKGSRCDIR.hs-transformers-base?=	../../devel/hs-transformers-base
+BUILDLINK_API_DEPENDS.hs-transformers-base+=	hs-transformers-base>=0.4.2
+BUILDLINK_PKGSRCDIR.hs-transformers-base?=	../../wip/hs-transformers-base
 
 .include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_TRANSFORMERS_BASE_BUILDLINK3_MK
