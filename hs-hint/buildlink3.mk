b20a0577efc4c499a0e282a0a8427c6e10a752ac
szptvlfn <szptvlfn>
2014-05-24 22:04:37 +0000

    Remove hdevtools, hs-exceptions, imported to pkgsrc/devel.

--- hs-hint/buildlink3.mk
+++ hs-hint/buildlink3.mk
@@ -8,7 +8,7 @@ HS_HINT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-hint?=	../../wip/hs-hint
 
-.include "../../wip/hs-exceptions/buildlink3.mk"
+.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../devel/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-ghc-mtl/buildlink3.mk"
 .include "../../devel/hs-ghc-paths/buildlink3.mk"
--- hs-temporary/buildlink3.mk
+++ hs-temporary/buildlink3.mk
@@ -8,7 +8,7 @@ HS_TEMPORARY_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-temporary+=	hs-temporary>=1.2.0
 BUILDLINK_PKGSRCDIR.hs-temporary?=	../../wip/hs-temporary
 
-.include "../../wip/hs-exceptions/buildlink3.mk"
+.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_TEMPORARY_BUILDLINK3_MK
 
