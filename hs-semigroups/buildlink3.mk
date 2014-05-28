195450be05f9aa248cb461505262b37f2cd512e9
szptvlfn <szptvlfn>
2014-05-28 22:05:59 +0000

    Remove hs-nats, imported to pkgsrc/math. Remove hs-tf-random, imported to pkgsrc/devel.

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.13.0
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
 .include "../../devel/hs-hashable/buildlink3.mk"
-.include "../../wip/hs-nats/buildlink3.mk"
+.include "../../math/hs-nats/buildlink3.mk"
 .include "../../devel/hs-text/buildlink3.mk"
 .include "../../devel/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_SEMIGROUPS_BUILDLINK3_MK
