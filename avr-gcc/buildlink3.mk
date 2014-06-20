fb88bcead881e23c913ace23c5d1f77d68af7d04
makoto <makoto>
2014-06-20 12:37:08 +0000

    Correction (Removing) the harmful line (if ${AVR_TARGET} is empty), as -BUILDLINK_FNAME_TRANSFORM.avr-gcc+=    -e "s,${AVR_TARGET}/lib/,lib/,g" (with comment, getting .builkdlink/lib/ -> .buildlinklib/ )
    Looking for the resolution for packaging emulators/simulavr and
    wip/simulavr-git (former has another problem now).

--- avr-gcc/buildlink3.mk
+++ avr-gcc/buildlink3.mk
@@ -7,7 +7,9 @@ AVR_GCC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.avr-gcc+=		avr-gcc>=4.8.3
 BUILDLINK_PKGSRCDIR.avr-gcc?=		../../wip/avr-gcc
-BUILDLINK_FNAME_TRANSFORM.avr-gcc+=	-e "s,${AVR_TARGET}/lib/,lib/,g"
+# Folling line was harmful, .buildlink/lib/ -> buildlinklib/
+# It did not have first ending slash, probably was OK.
+#BUILDLINK_FNAME_TRANSFORM.avr-gcc+=	-e "s,${AVR_TARGET}/lib/,lib/,g"
 
 .endif # AVR_GCC_BUILDLINK3_MK
 
