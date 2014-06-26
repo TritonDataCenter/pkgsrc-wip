867e8b296646de20fd373f5339c0593ae61704bf
khorben <khorben>
2014-06-26 12:04:35 +0000

    Default to Gtk+ 3.0

--- deforaos-panel/buildlink3.mk
+++ deforaos-panel/buildlink3.mk
@@ -10,7 +10,7 @@ BUILDLINK_API_DEPENDS.deforaos-panel+=	deforaos-panel>=0.2.12
 BUILDLINK_PKGSRCDIR.deforaos-panel?=	../../wip/deforaos-panel
 
 .include "../../wip/deforaos-libsystem/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif	# DEFORAOS_PANEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-panel
