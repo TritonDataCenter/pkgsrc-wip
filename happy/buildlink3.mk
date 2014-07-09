83ee62c14665d9268c4e9a927ed6691208b18ddd
szptvlfn <szptvlfn>
2014-07-09 19:23:25 +0000

    Update to 1.19.4
    CHANGES:
    1.19.4
            * fix for GHC 7.10 (Applicative/Monad, #19, #21)
    1.19.3
            * fix for GHC 7.2 (#16)

--- happy/buildlink3.mk
+++ happy/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	happy
 .if !defined(HAPPY_BUILDLINK3_MK)
 HAPPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy+=	happy>=1.19.2
+BUILDLINK_API_DEPENDS.happy+=	happy>=1.19.4
 BUILDLINK_PKGSRCDIR.happy?=	../../wip/happy
 
 .include "../../devel/hs-mtl/buildlink3.mk"
