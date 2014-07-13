a38aefa9be2b1d712c244abdabf729dd12687b9e
szptvlfn <szptvlfn>
2014-07-13 11:37:15 +0000

    Update to 4000.2.17
    Changes from https://github.com/haskell/HTTP
    4000.2.17
     - Update dependencies for the warp tests after IPv6 changes
     - Fix compilation with network<=2.4
     - Switch to using RFC 2732 literal IPv6 address in tests
       This makes it closer to the IPv4 case and avoids a dependency on whether
       'ip6-localhost' is defined in /etc/hosts or equivalent.
     - Hack to support RFC 2732 addresses in openTCPConnection
     - Add support for RFC2732 literal IPV6 addresses in URLs
     - Switched from obsolete gethostbyname() to getaddrinfo()
       Neat side-effect: IPv6 is now supported as well.
     - Document withSocketsDo call
     - fix openTCPConnection on Windows.
       The problem was that getAddrInfo doesn't initialize the socket library
       implicitly. If that is the first thing that is done with sockets, then
       the program will crash and burn with a misleading error message.
       The initialization is done only once, so the cost of a superfluous call
       is minimal. It is a no-op on other OSes.
    4000.2.16
     - Make all dependencies PVP-compliant
     - Relax mtl restriction in test harness
     - Fix Windows build (broken by dc58d34)
     - Build with both older and newer Warp.
     - Fix warning on non-Windows
     - Add warp-tests flag to cabal file to allow them to be disabled,
       e.g. to simplify the dependencies.
     - Fix runtime problem on GHC 6.10, just in case anyone cares
    4000.2.15
     - Support latest version of case-insensitive
     - Support mtl 2.2
     - Support recent versions of conduit
    4000.2.14
     - Mark -fwarn-as-error flag as "manual" so the cabal solver never chooses it
     - Merge pull request #58 from hvr/ghc-t8883
       Enable FlexibleContexts for Network.Browser
       This is needed for GHC HEAD (and the future GHC 7.10)
       For more details, see
         http://permalink.gmane.org/gmane.comp.lang.haskell.glasgow.user/24612
     - Merge branch 'snoyberg-master'
       Add missing Httpd module for sdist
    4000.2.13
     - Bump network dependency
     - Tighten up PVP compliance for main package.
       Also make sure lower-bounds are actually buildable.
     - Merge pull request #53 from trofi/master
       HTTP.cabal: allow latest wai/warp
       Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>
    4000.2.12
     - Merge pull request #51 from ian-ross/post-issue-14-cleanup
       Re-enable "2 requests" tests; remove conn. pool size limits in tests
     - Document handling of "user:pass@".
     - Add testcases for user:pass@ in the URI
     - Use user:pass@ from URI in Authorization: Basic
       simpleHTTP allows the URI of the format
       "http://user:pass@host.dom/path",
       but user:pass@ part is simply stripped out and ignored. This commit
       introduces a 'normalizer' that takes user:pass@ data from the request's
       URI and adds "Authorization: Basic ..." header to the request. This
       normalizer is called before the normalizeHostURI normalizer, as the
       latter strips the user:pass@ part from the URI.
    4000.2.11
     - Merge branch 'master' of https://github.com/zmthy/HTTP into zmthy-master
     - Merge pull request #49 from ian-ross/issue-14
       Fix #14: connection pool tries to reuse closed connections
     - Merge pull request #50 from juhp/patch-2
       tweak end of 3rd paragraph of description

--- hs-HTTP/buildlink3.mk
+++ hs-HTTP/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-HTTP
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.10
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.2.17
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../wip/hs-HTTP
 
 .include "../../devel/hs-mtl/buildlink3.mk"
