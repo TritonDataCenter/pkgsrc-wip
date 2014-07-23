71d34a95e4b1517c0904ee0cd642f0bd8bf56de0
obache <obache>
2014-07-23 10:07:43 +0000

    No need to depend on git, just use 2.2.1 release tarball.

--- cde/patches/patch-programs_dtmail_libDtMail_Common_Session.C
+++ cde/patches/patch-programs_dtmail_libDtMail_Common_Session.C
@@ -6,7 +6,7 @@ $NetBSD$
     DtMailEnv error;
     iconv_t cd;
     size_t ileft = (size_t) bp_len, oleft = (size_t) bp_len, ret = 0;
--#if defined(_AIX) || defined(sun) || defined(__FreeBSD__)
+-#if defined(_AIX) || defined(sun) || defined(__FreeBSD__) || defined(__NetBSD__)
 +#if defined(_AIX) || defined(sun) || defined(CSRG_BASED)
     const char *ip = (const char *) *bp;
  #else
--- cde/patches/patch-programs_dtprintinfo_libUI_MotifUI_Debug.c
+++ cde/patches/patch-programs_dtprintinfo_libUI_MotifUI_Debug.c
@@ -1,12 +1,12 @@
 $NetBSD$
 
---- programs/dtprintinfo/libUI/MotifUI/Debug.c.orig	2012-09-29 12:06:55.000000000 +0000
+--- programs/dtprintinfo/libUI/MotifUI/Debug.c.orig	2014-03-01 22:48:49.000000000 +0000
 +++ programs/dtprintinfo/libUI/MotifUI/Debug.c
 @@ -105,7 +105,7 @@ SysErrorMsg(
     int n
     )
  {
--#if !defined(linux) && !defined(__FreeBSD__)
+-#if !defined(linux) && !defined(__FreeBSD__) && !defined(__NetBSD__)
 +#if !defined(linux) && !defined(CSRG_BASED)
      extern char *sys_errlist[];
      extern int sys_nerr;
