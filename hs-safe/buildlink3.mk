e1f2a8ac7b767cf96f621e04ffddd078e5d7f529
szptvlfn <szptvlfn>
2014-07-26 21:49:27 +0000

    Update to 0.3.7
    CHANGES:
    0.3.7
        Add Def variants of the Exact functions

--- hs-safe/buildlink3.mk
+++ hs-safe/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-safe
 .if !defined(HS_SAFE_BUILDLINK3_MK)
 HS_SAFE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.6
+BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.7
 BUILDLINK_PKGSRCDIR.hs-safe?=	../../wip/hs-safe
 .endif	# HS_SAFE_BUILDLINK3_MK
 
