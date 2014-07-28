17d6a170a908f976ff57fb4aba86a03a0fcb76cb
obache <obache>
2014-07-28 13:56:46 +0000

    Update cde to 2.2.2.
    add rc.d script for dtlogin.

---
+++ cde/files/dtlogin.sh
@@ -0,0 +1,19 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD$
+#
+# PROVIDE: dtlogin
+# REQUIRE: DAEMON LOGIN
+# KEYWORD: shutdown
+#
+
+. /etc/rc.subr
+
+name="dtlogin"
+rcvar=$name
+command="@PREFIX@/dt/bin/dtlogin"
+command_args="-daemon"
+pidfile="@VARBASE@/dt/Xpid"
+
+load_rc_config $name
+run_rc_command "$1"
