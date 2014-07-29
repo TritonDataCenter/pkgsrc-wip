c53029e0cc7f68e71e223d875d3cd199d56ca2b2
szptvlfn <szptvlfn>
2014-07-29 21:37:24 +0000

    Update to 4.0.4
    CHANGELOG from http://github.com/ekmett/semigroupoids
    4.0.4
    -----
    * `contravariant` 1.0 support.
    
    4.0.3
    ---
    * Added flags to provide unsupported cabal sandbox build modes.
    
    4.0.1
    -----
    * Fixed bitrot in the `Data.Functor.Extend` documentation.
    * Fixed warnings on GHC 7.8.1rc2 caused by importing `Control.Monad.Instances`.

--- hs-semigroupoids/buildlink3.mk
+++ hs-semigroupoids/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-semigroupoids
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=4.0
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=4.0.4
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=	../../wip/hs-semigroupoids
 
 .include "../../wip/hs-comonad/buildlink3.mk"
