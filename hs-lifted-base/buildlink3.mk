67cf311dbd2cc40c1790bed7bd31cd24ea59da14
szptvlfn <szptvlfn>
2014-07-31 21:47:56 +0000

    Update to 0.2.3.0
    Changes from
    0.2.3.0
     - Updates for base-4.7.0 (GHC 7.8)
    0.2.2.2
     - Removed references to base-unicode-symbols
     - Merge branch 'without-unicode-symbol-package' of git://github.com/minamiyama1994/lifted-base
     - Support transformers-0.4.0.0
    0.2.2.1
     - Use Build-type: Simple
     - Fixed build on Mac OS X

--- hs-lifted-base/buildlink3.mk
+++ hs-lifted-base/buildlink3.mk
@@ -5,10 +5,9 @@ BUILDLINK_TREE+=	hs-lifted-base
 .if !defined(HS_LIFTED_BASE_BUILDLINK3_MK)
 HS_LIFTED_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lifted-base+=	hs-lifted-base>=0.2.2
+BUILDLINK_API_DEPENDS.hs-lifted-base+=	hs-lifted-base>=0.2.3
 BUILDLINK_PKGSRCDIR.hs-lifted-base?=	../../wip/hs-lifted-base
 
-.include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
 .include "../../devel/hs-monad-control/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
 .endif	# HS_LIFTED_BASE_BUILDLINK3_MK
