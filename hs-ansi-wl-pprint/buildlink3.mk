e4ef769a64343209009bbc858c8442128db864b3
szptvlfn <szptvlfn>
2014-08-01 12:12:09 +0000

    Remove hs-ansi-terminal, imported to pkgsrc/devel. Remove hs-stringsearch, imported to pkgsrc/textproc.

--- hs-ansi-wl-pprint/buildlink3.mk
+++ hs-ansi-wl-pprint/buildlink3.mk
@@ -8,7 +8,7 @@ HS_ANSI_WL_PPRINT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=0.6.7
 BUILDLINK_PKGSRCDIR.hs-ansi-wl-pprint?=	../../wip/hs-ansi-wl-pprint
 
-.include "../../wip/hs-ansi-terminal/buildlink3.mk"
+.include "../../devel/hs-ansi-terminal/buildlink3.mk"
 .endif	# HS_ANSI_WL_PPRINT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-ansi-wl-pprint
