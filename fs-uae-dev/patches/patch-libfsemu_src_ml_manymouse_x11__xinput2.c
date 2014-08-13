c9255b0c723a97366f5edc6c208d2771de32b020
rhialto <rhialto>
2014-08-13 19:52:09 +0000

    Update to version 2.5.8dev6.

--- fs-uae-dev/patches/patch-libfsemu_src_ml_manymouse_x11__xinput2.c
+++ fs-uae-dev/patches/patch-libfsemu_src_ml_manymouse_x11__xinput2.c
@@ -2,7 +2,7 @@ $NetBSD$
 
 Include proper header file.
 
---- libfsemu/src/ml/manymouse/x11_xinput2.c.orig	2014-05-13 17:57:41.000000000 +0000
+--- libfsemu/src/ml/manymouse/x11_xinput2.c.orig	2014-05-10 20:17:29.000000000 +0000
 +++ libfsemu/src/ml/manymouse/x11_xinput2.c
 @@ -25,6 +25,7 @@
  #include <stdlib.h>
