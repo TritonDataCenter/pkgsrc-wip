6c3926fae39efbda9cf117755c04762038336d74
szptvlfn <szptvlfn>
2014-08-18 21:22:47 +0000

    Remove hs-digest, imported to pkgsrc/security. Remove hs-hscolour, imported to pkgsrc/textproc.

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -11,7 +11,7 @@ BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 .include "../../devel/hs-cmdargs/buildlink3.mk"
 .include "../../devel/hs-cpphs/buildlink3.mk"
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
-.include "../../wip/hs-hscolour/buildlink3.mk"
+.include "../../textproc/hs-hscolour/buildlink3.mk"
 .include "../../devel/hs-transformers/buildlink3.mk"
 .include "../../devel/hs-uniplate/buildlink3.mk"
 .endif	# HLINT_BUILDLINK3_MK
