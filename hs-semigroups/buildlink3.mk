250eaf4b76c15b2a6112a8c94685955d3c101c95
szptvlfn <szptvlfn>
2014-08-21 21:43:10 +0000

    Update to 0.15.2
    CHANGELOG:
    0.15.2
    ------
    * Fixed a Trustworthiness problem for GHC 7.8+
    
    0.15.1
    ------
    * Nathan van Doorn fixed a number of embarassing bugs in the `Enum` instances.
    
    0.15
    ----
    * `instance IsList NonEmpty`
    
    0.14
    ----
    * Allow for manual removal of dependencies to support advanced sandbox users who explicitly want to avoid compiling certain dependencies
      they know they aren't using.
    
      We will fix bugs caused by any combination of these package flags, but the API of the package should be considered the default build
      configuration with all of the package dependency flags enabled.
    
    * Will now build as full-fledged `Safe` Haskell if you configure with -f-hashable.
    
    * Added some missing `Generic`/`Generic`/`Hashable` instances

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	hs-semigroups
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.13.0
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.15.2
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
 .include "../../devel/hs-hashable/buildlink3.mk"
