3ff35f51d602da8d99f2f5ba8fc0b5afd951646e
szptvlfn <szptvlfn>
2014-08-24 09:48:00 +0000

    refactoring

--- xmonad-contrib/options.mk
+++ xmonad-contrib/options.mk
@@ -11,7 +11,13 @@ USE_TOOLS+=	patch
 .if !empty(PKG_OPTIONS:Mxft2)
 .include "../../x11/hs-X11-xft/buildlink3.mk"
 .else
-post-configure:
-	${RUN} cd ${WRKSRC} && \
-		${PATCH} <../../files/no_use_xft
+SUBST_CLASSES+=	1
+SUBST_STAGE.1=	pre-patch
+SUBST_FILES.1=	xmonad-contrib.cabal
+SUBST_SED.1=	-e "s|render text|render text+  default: False|"
+
+SUBST_CLASSES+=	2
+SUBST_STAGE.2=	post-patch
+SUBST_FILES.2=	xmonad-contrib.cabal
+SUBST_FILTER_CMD.2=	${TR} '+' \\n
 .endif
