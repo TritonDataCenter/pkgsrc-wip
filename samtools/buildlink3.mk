2337ac4c3b99e69dcadc25a78d2552529ca24c1b
outpaddling <outpaddling>
2014-09-04 21:46:49 +0000

    Import samtools-0.1.19 as wip/samtools.
    AM (Sequence Alignment/Map) format is a generic format for storing
    large nucleotide sequence alignments
    
    WWW: http://port.home.page/

---
+++ samtools/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	samtools
+
+.if !defined(SAMTOOLS_BUILDLINK3_MK)
+SAMTOOLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.samtools+=	samtools>=0.1.19
+BUILDLINK_PKGSRCDIR.samtools?=	../../jb-wip/samtools
+.endif	# SAMTOOLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-samtools
