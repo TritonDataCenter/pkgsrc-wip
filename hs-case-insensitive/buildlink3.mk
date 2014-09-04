2d7022a767f7ff5a57ae71f389e9195a2fd42e9a
szptvlfn <szptvlfn>
2014-09-04 21:32:55 +0000

    Update to 1.2.0.0
    Changes from https://github.com/basvandijk/case-insensitive
    1.2.0.0
     - Derived a Data instance for CI

--- hs-case-insensitive/buildlink3.mk
+++ hs-case-insensitive/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	hs-case-insensitive
 .if !defined(HS_CASE_INSENSITIVE_BUILDLINK3_MK)
 HS_CASE_INSENSITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.1.0.3
+BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.0.0
 BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../wip/hs-case-insensitive
 
 .include "../../devel/hs-hashable/buildlink3.mk"
