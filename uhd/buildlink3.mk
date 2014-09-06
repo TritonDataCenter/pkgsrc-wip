805631705512828eab8dff92b8cb289d31e6e251
makoto <makoto>
2014-09-06 05:23:28 +0000

    (This is git version but with Release TAGS) Import uhd-3.7.2rc1 as wip/uhd.
    UHD is the free & open-source software driver and API for the
    Universal Software Radio Peripheral (USRP(TM)) SDR platform,
    created and sold by Ettus Research, LLC.
    
    UHD supports all Ettus Research USRP(TM) hardware, including
    all motherboards and daughterboards, and the combinations
    thereof.

---
+++ uhd/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=        uhd
+
+.if !defined(UHD_BUILDLINK3_MK)
+UHD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.uhd+=    uhd>=2.22
+BUILDLINK_PKGSRCDIR.uhd?=              ../../wip/uhd-git
+.endif # UHD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=        -uhd
