32e7826e3a0f00dde8af248b202ce514e2d9ae9f
makoto <makoto>
2014-09-18 03:43:28 +0000

    - PLIST update - Remove self package from CONFLICTS+= - Remove already .if 0 ... .endif clause - Add REPLACE_PYTHON files onto TODO - Correction of versions on buildlink3.mk - Add two lines on Makefile   +BUILD_DEPENDS+=  ${PYPKGPREFIX}-sphinx-[0-9]*:../../textproc/py-sphinx   +DEPENDS+=        ${PYPKGPREFIX}-lxml-[0-9]*:../../textproc/py-lxml/

--- gnuradio-default/buildlink3.mk
+++ gnuradio-default/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	gnuradio-default
 .if !defined(GNURADIO_DEFAULT_BUILDLINK3_MK)
 GNURADIO_DEFAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-default+=	gnuradio-default>=3.0.4
-BUILDLINK_ABI_DEPENDS.gnuradio-default+=	gnuradio-default>=3.3.0nb10
+BUILDLINK_API_DEPENDS.gnuradio-default+=	gnuradio-default>=3.7.5
+BUILDLINK_ABI_DEPENDS.gnuradio-default+=	gnuradio-default>=3.7.5
 BUILDLINK_PKGSRCDIR.gnuradio-default?=	../../wip/gnuradio-default
 BUILDLINK_CPPFLAGS.gnuradio-default+=    -I${BUILDLINK_PREFIX.gnuradio-default}/include/gnuradio
 .endif # GNURADIO_DEFAULT_BUILDLINK3_MK
