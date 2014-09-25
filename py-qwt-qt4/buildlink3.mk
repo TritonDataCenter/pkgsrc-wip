46ad45bad80a21c27cf6daaf086c5118c9850533
makoto <makoto>
2014-09-25 02:59:57 +0000

    Filename was wrong, corrected.

--- py-qwt-qt4/buildlink3.mk
+++ py-qwt-qt4/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=        py-qwt-qt4
 .if !defined(PY_QWT_QT4_BUILDLINK3_MK)
 PY_QWT_QT4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py-qwt-qt4+=   py-qwt-qt4>=5.2.0
-BUILDLINK_ABI_DEPENDS.py-qwt-qt4+=   py-qwt-qt4>=5.2.0
+BUILDLINK_API_DEPENDS.py-qwt-qt4+=   ${PYPKGVERSION}-qwt-qt4>=5.2.0
+BUILDLINK_ABI_DEPENDS.py-qwt-qt4+=   ${PYPKGVERSION}-qwt-qt4>=5.2.0
 BUILDLINK_PKGSRCDIR.py-qwt-qt4?=     ../../wip/py-qwt-qt4
-.endif # PY_QWT_QT4_BUILDLINK3_MKx
+.endif # PY_QWT_QT4_BUILDLINK3_MK
 
 BUILDLINK_TREE+=        -py-qwt-qt4
