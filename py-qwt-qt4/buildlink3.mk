249d0208de1712b5a9b37f24a70358132d7ed9aa
makoto <makoto>
2014-09-25 03:40:16 +0000

    Still missing buildlink3.mk ? Adding it.

---
+++ py-qwt-qt4/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=        py-qwt-qt4
+
+.if !defined(PY_QWT_QT4_BUILDLINK3_MK)
+PY_QWT_QT4_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py-qwt-qt4+=   ${PYPKGVERSION}-qwt-qt4>=5.2.0
+BUILDLINK_ABI_DEPENDS.py-qwt-qt4+=   ${PYPKGVERSION}-qwt-qt4>=5.2.0
+BUILDLINK_PKGSRCDIR.py-qwt-qt4?=     ../../wip/py-qwt-qt4
+.endif # PY_QWT_QT4_BUILDLINK3_MK
+
+BUILDLINK_TREE+=        -py-qwt-qt4
