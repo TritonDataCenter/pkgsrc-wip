4e709446655ace2fabb11f67db5ab873d550c204
thomasklausner <thomasklausner>
2014-09-28 14:20:08 +0000

    Import nss-hg-2014.09.28nb20140928 as wip/nss-hg.
    Network Security Services (NSS) is a set of libraries designed to
    support cross-platform development of security-enabled server
    applications.  Applications built with NSS can support SSL v2 and
    v3, TLS, PKCS #5, PKCS #7, PKCS #11, PKCS #12, S/MIME, X.509 v3
    certificates, and other security standards.
    
    This package contains the hg version.

---
+++ nss-hg/patches/patch-coreconf_OpenBSD.mk
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- coreconf/OpenBSD.mk.orig	2014-09-28 13:54:02.000000000 +0000
++++ coreconf/OpenBSD.mk
+@@ -24,7 +24,7 @@ OS_LIBS			+= -pthread
+ DSO_LDOPTS		+= -pthread
+ endif
+ 
+-DLL_SUFFIX		= so.1.0
++DLL_SUFFIX		= so
+ 
+ OS_CFLAGS		= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -ansi -Wall -Wno-switch -pipe -DOPENBSD
+ 
