bc7d1a62bce4fe3f87cd4f116d5c10e22f2161da
nros <nros>
2014-10-15 17:53:35 +0000

    import of libqtlua for qt4 to pkgsrc-wip as qt4-libqtlua.
    The QtLua library aims to make Qt4 applications scriptable using the
    Lua scripting language. It is an alternative to the QtScript module.
    
    QtLua does not generate or use generated binding code for Qt. Instead it
    provides useful C++ wrapper classes to make C++ and lua objects
    accessible from both lua and C++. It takes advantages of the Qt meta
    object system to expose QObject members to lua scripts.
    
    QtLua is not designed to develop an application in pure Lua but rather
    to extend a Qt C++ application using Lua as scripting language.

---
+++ qt4-libqtlua/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+#
+
+BUILDLINK_TREE+=	qt4-libqtlua
+
+.if !defined(QT4_LIBQTLUA_BUILDLINK3_MK)
+QT4_LIBQTLUA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt4-libqtlua+=	qt4-libqtlua>=2.0
+BUILDLINK_PKGSRCDIR.qt4-libqtlua?=	../../wip/qt4-libqtlua
+
+.include "../../lang/lua51/buildlink3.mk"
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# QT4_LIBQTLUA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt4-libqtlua
