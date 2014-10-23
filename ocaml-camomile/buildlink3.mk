b106aba55f80cb19b70b7a6ca3faa3a78d2397ef
jgoamakf <jgoamakf>
2014-10-23 07:43:40 +0000

    Add buildlink3.mk.

---
+++ ocaml-camomile/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	ocaml-camomile
+
+.if !defined(OCAML_CAMOMILE_BUILDLINK3_MK)
+OCAML_CAMOMILE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-camomile+=	ocaml-camomile>=0.8.4
+BUILDLINK_PKGSRCDIR.ocaml-camomile?=	../../wip/ocaml-camomile
+
+.include "../../lang/ocaml/buildlink3.mk"
+
+.endif	# OCAML_CAMOMILE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-camomile
