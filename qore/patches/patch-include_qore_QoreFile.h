a99165668200ce254f5cf3ae26e54b3176648022
nros <nros>
2014-11-01 15:59:35 +0000

    Don't use LIBSUFFIX on linux. Fixes build on linux. Remove patch-Makefile.in in qore-openldap-module.

--- qore/patches/patch-include_qore_QoreFile.h
+++ qore/patches/patch-include_qore_QoreFile.h
@@ -1,7 +1,7 @@
 $NetBSD$
 * Fixes build on NetBSD 6
 http://sourceforge.net/p/qore/code/6803/
---- include/qore/QoreFile.h.orig	2013-02-22 16:11:01.000000000 +0000
+--- include/qore/QoreFile.h.orig	2013-03-05 09:44:51.000000000 +0000
 +++ include/qore/QoreFile.h
 @@ -34,6 +34,10 @@
  
