9ba9269ecda37a65be02a6705c8f0449064532df
szptvlfn <szptvlfn>
2014-11-06 13:02:51 +0000

    Update to 1.5.1
    CHANGELOG:
    # 1.5.1
    * We no longer export Show (Q a) for GHC >= 7.4. This was causing random hangs when users tried to somehow run declaration splices from the REPL.
    * We no longer depend on tagged for GHC >= 7.8, since `Proxy` is now in `base`.
    
    # 1.5
    * Added a flag to disable `template-haskell` support for GHC stage1 platforms.
    * Added instances of `Reifies` for `GHC.TypeLits`

--- hs-reflection/buildlink3.mk
+++ hs-reflection/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	hs-reflection
 .if !defined(HS_REFLECTION_BUILDLINK3_MK)
 HS_REFLECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=1.4
-BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=1.4
+BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=1.5.1
+BUILDLINK_ABI_DEPENDS.hs-reflection+=	hs-reflection>=1.5.1
 BUILDLINK_PKGSRCDIR.hs-reflection?=	../../wip/hs-reflection
 
 .include "../../devel/hs-tagged/buildlink3.mk"
