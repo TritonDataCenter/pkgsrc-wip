145579d2e21bf025a8eca6ac167a073753e2d683
szptvlfn <szptvlfn>
2014-11-24 14:00:16 +0000

    Update to 0.1.4
    changes (does not provide):
    add TZ string for Version 3

--- hs-timezone-olson/buildlink3.mk
+++ hs-timezone-olson/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	hs-timezone-olson
 .if !defined(HS_TIMEZONE_OLSON_BUILDLINK3_MK)
 HS_TIMEZONE_OLSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.3
-BUILDLINK_ABI_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.3
+BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.4
+BUILDLINK_ABI_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.4
 BUILDLINK_PKGSRCDIR.hs-timezone-olson?=	../../wip/hs-timezone-olson
 
 .include "../../devel/hs-extensible-exceptions/buildlink3.mk"
