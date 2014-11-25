66b8b3ea21434cec0d090e27325acd330130ee1a
jgoamakf <jgoamakf>
2014-11-25 07:51:13 +0000

    Remove unnecessary .include.

--- ocaml-bin_prot/buildlink3.mk
+++ ocaml-bin_prot/buildlink3.mk
@@ -6,10 +6,6 @@ OCAML_BIN_PROT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-bin_prot+=	ocaml-bin_prot>=112.01.00
 BUILDLINK_PKGSRCDIR.ocaml-bin_prot?=	../../wip/ocaml-bin_prot
-
-.include "../../devel/ocaml-type_conv/buildlink3.mk"
-.include "../../lang/camlp4/buildlink3.mk"
-.include "../../lang/ocaml/buildlink3.mk"
 .endif	# OCAML_BIN_PROT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-bin_prot
