b4394c6ea8420087997e5eb9d51421a3592e2f2a
nros <nros>
2014-12-25 15:30:50 +0000

    import of libchamplain-0.12 as libchamplain012 to pkgsrc-wip.
    libchamplain is a Clutter based widget to display rich, eye-candy and
    interactive maps.

---
+++ libchamplain012/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+#
+
+BUILDLINK_TREE+=	libchamplain012
+
+.if !defined(LIBCHAMPLAIN012_BUILDLINK3_MK)
+LIBCHAMPLAIN012_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libchamplain012+=	libchamplain012>=0.12.9
+BUILDLINK_PKGSRCDIR.libchamplain012?=	../../wip/libchamplain012
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../graphics/clutter/buildlink3.mk"
+.include "../../graphics/clutter-gtk/buildlink3.mk"
+.include "../../wip/libmemphis02/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# LIBCHAMPLAIN012_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libchamplain012
