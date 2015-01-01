34c7a49ff1088903a9edf1be3a8f43380030a000
obache <obache>
2015-01-01 08:25:54 +0000

    Import mate-settings-daemon-1.8.1 as wip/mate-settings-daemon.
    mate-settings-daemon is a fork of gnome-settings-daemon

---
+++ mate-settings-daemon/options.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-settings-daemon
+PKG_OPTIONS_OPTIONAL_GROUPS+=	audio
+PKG_OPTIONS_GROUP.audio=	pulseaudio gstreamer
+PKG_SUGGESTED_OPTIONS=	gstreamer
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-pulse
+.else
+CONFIGURE_ARGS+=	--disable-pulse
+.endif
+
+.if !empty(PKG_OPTIONS:Mgstreamer)
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-gstreamer
+.else
+CONFIGURE_ARGS+=	--disable-gstreamer
+.endif
