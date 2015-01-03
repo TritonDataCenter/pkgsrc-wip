a39c644a358c3f101a8eff9c49e46872fc37f9d5
obache <obache>
2015-01-03 07:11:11 +0000

    Bump API

--- system-tools-backends/buildlink3.mk
+++ system-tools-backends/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	system-tools-backends
 .if !defined(SYSTEM_TOOLS_BACKENDS_BUILDLINK3_MK)
 SYSTEM_TOOLS_BACKENDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.system-tools-backends+=	system-tools-backends>=2.8.0
+BUILDLINK_API_DEPENDS.system-tools-backends+=	system-tools-backends>=2.10.0
 BUILDLINK_PKGSRCDIR.system-tools-backends?=	../../wip/system-tools-backends
 
 .include "../../devel/glib2/buildlink3.mk"
