e67e6cff5b2fd161cb9f26fc683e7075c4d7787d
obache <obache>
2015-01-12 12:34:46 +0000

    fixes pkgbase.

--- libdbusmenu-gtk3/buildlink3.mk
+++ libdbusmenu-gtk3/buildlink3.mk
@@ -1,14 +1,14 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	libdbusmenu-gtk
+BUILDLINK_TREE+=	libdbusmenu-gtk3
 
-.if !defined(LIBDBUSMENU_GTK_BUILDLINK3_MK)
-LIBDBUSMENU_GTK_BUILDLINK3_MK:=
+.if !defined(LIBDBUSMENU_GTK3_BUILDLINK3_MK)
+LIBDBUSMENU_GTK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdbusmenu-gtk+=	libdbusmenu-gtk>=12.10.2
-BUILDLINK_PKGSRCDIR.libdbusmenu-gtk?=	../../wip/libdbusmenu-gtk
+BUILDLINK_API_DEPENDS.libdbusmenu-gtk3+=	libdbusmenu-gtk3>=12.10.2
+BUILDLINK_PKGSRCDIR.libdbusmenu-gtk3?=	../../wip/libdbusmenu-gtk3
 
 .include "../../wip/libdbusmenu-glib/buildlink3.mk"
-.endif	# LIBDBUSMENU_GTK_BUILDLINK3_MK
+.endif	# LIBDBUSMENU_GTK3_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libdbusmenu-gtk
+BUILDLINK_TREE+=	-libdbusmenu-gtk3
