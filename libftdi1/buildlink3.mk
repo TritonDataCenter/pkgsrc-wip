dd93582deebc205fff56910819138c184238bcf7
nros <nros>
2015-01-16 18:09:11 +0000

    Update libftdi1 to version 1.2
    Changelog:
    
    New in 1.2 - 2014-11-21
    -----------------------
    * Support for FT230X devices (Uwe Bonnes)
    * ftdi_usb_get_strings(): Don't try to open an already open device (Denis Sirotkin)
    * Support for finding devices bricked by the Windows driver (Forest Crossman)
    * cmake build system: New LibFTDI1ConfigVersion.cmake file (xantares09)
    * Fix a typo in the MPSSE command CLK_BYTES_OR_LOW (Benjamin Vanheuverzwijn)
    * Minor fixes for MSVC++ (Andrei Errapart)
    * Various small code improvements (Florian Preinstorfer, Jochen Sprickerhof, xantares09)
    
    New in 1.1 - 2014-02-05
    -----------------------
    * Fix FT232H eeprom suspend pulldown setting (Davide Michelizza)
    * Fix FT232H eeprom user area size (Davide Michelizza)
    * Improved mingw build (Paul Fertser and Michel Zou)
    * C++ wrapper: Get/set functions for USB timeouts (Jochen Sprickerhof)
    * Partial support for FT230X (Nathael Pajani)
    * New API function: ftdi_eeprom_set_strings() (Nathael Pajani)
    * Prevent possible segfault in ftdi_eeprom_decode() (Nathael Pajani)
    * Save device release number in eeprom (Jarkko Sonninen)
    * Fix "self powered" eeprom flag (Jarkko Sonninen)
    * Improved python wrapper (Michel Zou)
    * Many buildsystem improvements (Michel Zou and Mike Frysinger)
    * See the git history for more changes and fixes

--- libftdi1/buildlink3.mk
+++ libftdi1/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libftdi1
 .if !defined(LIBFTDI1_BUILDLINK3_MK)
 LIBFTDI1_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libftdi1+=	libftdi1>=1.0
+BUILDLINK_API_DEPENDS.libftdi1+=	libftdi1>=1.2
 BUILDLINK_PKGSRCDIR.libftdi1?=	../../wip/libftdi1
 
 .include "../../devel/libusb/buildlink3.mk"
