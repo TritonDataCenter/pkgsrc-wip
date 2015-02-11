9e169ea8b7d9cc6cb4ef608dedbdca172fac0706
nros <nros>
2015-02-11 12:03:53 +0000

    Change C includes to c* C++ style includes. Fix build on gcc 4.7 and above.

--- gtkmathview/patches/patch-src_backend_ps_T1__FontDataBase.cc
+++ gtkmathview/patches/patch-src_backend_ps_T1__FontDataBase.cc
@@ -1,12 +1,14 @@
 $NetBSD$
-* strcmp need string.h
+* strcmp need cstring
 --- src/backend/ps/T1_FontDataBase.cc.orig	2007-08-17 10:02:40.000000000 +0000
 +++ src/backend/ps/T1_FontDataBase.cc
-@@ -21,6 +21,7 @@
+@@ -20,7 +20,8 @@
+ #include <config.h>
  #include <map>
  #include <string>
- #include <stdlib.h>
-+#include <string.h>
+-#include <stdlib.h>
++#include <cstdlib>
++#include <cstring>
  #include <iostream>
  #include <list>
  
