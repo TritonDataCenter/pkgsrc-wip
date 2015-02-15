20f4cd8e2da18e60d13949ce5a7c4b182bacc550
khorben <khorben>
2015-02-15 23:36:04 +0000

    Additional fix for NetBSD

--- pkg/patches/patch-libpkg_fetch.c
+++ pkg/patches/patch-libpkg_fetch.c
@@ -16,3 +16,15 @@ $NetBSD$
  	if ((linelen = getline(&line, &linecap, repo->ssh)) > 0) {
  		if (line[linelen -1 ] == '\n')
  			line[linelen -1 ] = '\0';
+@@ -509,7 +514,11 @@ pkg_fetch_file_to_fd(struct pkg_repo *re
+ 
+ 	u = fetchParseURL(url);
+ 	if (t != NULL)
++#if defined(__NetBSD__)
++		u->last_modified = *t;
++#else
+ 		u->ims_time = *t;
++#endif
+ 
+ 	if (repo != NULL && strcmp(u->scheme, "ssh") == 0) {
+ 		if ((retcode = start_ssh(repo, u, &sz)) != EPKG_OK)
