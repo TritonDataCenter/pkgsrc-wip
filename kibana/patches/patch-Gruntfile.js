c44d713d001f4cb33d2f549caa6a38f99ab38de7
mbowie <mbowie>
2015-02-22 20:10:33 +0000

    Import kibana-4.0.0 as wip/kibana.
    Functional... rc.subr script is hackish to manage the pid. Not sure if
    there's a better way to handle that.
    
    Node.js build env probably doesn't play well with bulk builds etc at this
    point. It appears to install and modify things outside the pkgsrc env.
    
    Kibana is an open source (Apache Licensed), browser based analytics and
    search dashboard for Elasticsearch. Kibana is a snap to setup and start
    using.
    Kibana strives to be easy to get started with, while also being flexible
    and powerful, just like Elasticsearch.

---
+++ kibana/patches/patch-Gruntfile.js
@@ -0,0 +1,14 @@
+$NetBSD$
+Just build a generic target (Without node binaries bundled)
+
+--- Gruntfile.js.orig	2015-02-20 13:44:12.000000000 +0000
++++ Gruntfile.js	2015-02-20 13:44:29.000000000 +0000
+@@ -15,7 +15,7 @@
+     configFile: __dirname + '/src/server/config/kibana.yml',
+ 
+     nodeVersion: '0.10.35',
+-    platforms: ['darwin-x64', 'linux-x64', 'linux-x86', 'windows'],
++    platforms: [],
+ 
+     unitTestDir: __dirname + '/test/unit',
+     testUtilsDir: __dirname + '/test/utils',
