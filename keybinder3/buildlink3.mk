cbd98a5d3fc1641629308f26a16aba8fc702b9fd
krytarowski <krytarowski>
2015-04-06 10:40:32 +0000

    Adapt package for reuse ../../wip/keybinder common parts.
    Work-in-progress

--- keybinder3/buildlink3.mk
+++ keybinder3/buildlink3.mk
@@ -1,15 +1,15 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	keybinder
+BUILDLINK_TREE+=	keybinder3
 
-.if !defined(KEYBINDER_BUILDLINK3_MK)
-KEYBINDER_BUILDLINK3_MK:=
+.if !defined(KEYBINDER3_BUILDLINK3_MK)
+KEYBINDER3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.keybinder+=	keybinder>=0.3.0
-BUILDLINK_PKGSRCDIR.keybinder?=	../../wip/keybinder
+BUILDLINK3_API_DEPENDS.keybinder+=	keybinder>=0.3.0
+BUILDLINK3_PKGSRCDIR.keybinder?=	../../wip/keybinder
 
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.endif	# KEYBINDER_BUILDLINK3_MK
+.endif	# KEYBINDER3_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-keybinder
+BUILDLINK_TREE+=	-keybinder3
