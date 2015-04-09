690133ac35f82ce1a1d6ed13fe3a1173b3e19a42
krytarowski <krytarowski>
2015-04-09 03:24:42 +0000

    Resurrect marco-1.8.2
    MATE Marco is a fork of GNOME Metacity.

---
+++ marco/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD$
+#
+
+BUILDLINK_TREE+=	marco
+
+.if !defined(MARCO_BUILDLINK3_MK)
+MARCO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.marco+=	marco>=1.8.2
+BUILDLINK_PKGSRCDIR.marco?=	../../wip/marco
+
+.include "../../audio/libcanberra/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../sysutils/libgtop/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/startup-notification/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libXfixes/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.endif	# MARCO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-marco
