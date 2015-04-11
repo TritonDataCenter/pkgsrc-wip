4c6540cbf48aa336d09e34006f020e78460951fc
krytarowski <krytarowski>
2015-04-11 18:01:27 +0000

    Cleanup

--- libfm/buildlink3.mk
+++ libfm/buildlink3.mk
@@ -8,7 +8,8 @@ LIBFM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libfm+=	libfm>=1.2.3
 BUILDLINK_PKGSRCDIR.libfm?=	../../wip/libfm
 
-#.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../sysutils/menu-cache/buildlink3.mk"
 .endif	# LIBFM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfm
