05150d0813f18fc3d18b78e313889d41c9ce88bb
makoto <makoto>
2015-04-18 01:12:07 +0000

    Update to sync pkgsrc proper for further update.

--- audacious/buildlink3.mk
+++ audacious/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.11 2011/04/22 13:42:01 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.29 2014/10/09 15:02:42 adam Exp $
 
-BUILDLINK_TREE+=        audacious
+BUILDLINK_TREE+=	audacious
 
 .if !defined(AUDACIOUS_BUILDLINK3_MK)
 AUDACIOUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.audacious+=       audacious>=3.2.2
-BUILDLINK_ABI_DEPENDS.audacious?=       audacious>=3.2.2
-BUILDLINK_PKGSRCDIR.audacious?= ../../wip/audacious
+BUILDLINK_API_DEPENDS.audacious+=	audacious>=3.5.1
+BUILDLINK_ABI_DEPENDS.audacious+=	audacious>=3.5.1
+BUILDLINK_PKGSRCDIR.audacious?=	../../audio/audacious
 
 .include "../../devel/atk/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
@@ -15,15 +15,8 @@ BUILDLINK_PKGSRCDIR.audacious?= ../../wip/audacious
 .include "../../devel/pango/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
-#.include "../../devel/libmcs/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
-.include "../../x11/libSM/buildlink3.mk"
-.include "../../www/neon/buildlink3.mk"
-.include "../../net/libmms/buildlink3.mk"
-.include "../../devel/libmtp/buildlink3.mk"
-.include "../../sysutils/libnotify/buildlink3.mk"
-.include "../../misc/libcdio/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif # AUDACIOUS_BUILDLINK3_MK
 
-BUILDLINK_TREE+=        -audacious
+BUILDLINK_TREE+=	-audacious
