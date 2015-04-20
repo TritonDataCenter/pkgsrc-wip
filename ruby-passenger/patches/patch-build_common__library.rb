66151ad8bc79cdfb1e747786cf03463b8bf7e86c
fhajny <fhajny>
2015-04-20 16:25:04 +0000

    Disable inotify in embedded libev configure. Fixes build on recent SunOS platforms.

--- ruby-passenger/patches/patch-build_common__library.rb
+++ ruby-passenger/patches/patch-build_common__library.rb
@@ -3,6 +3,15 @@ $NetBSD$
 Fix pkgsrc libeio/libev support.
 --- build/common_library.rb.orig	2013-10-26 22:00:00.000000000 +0000
 +++ build/common_library.rb
+@@ -122,7 +122,7 @@ if USE_VENDORED_LIBEV
+       "--disable-shared --enable-static " +
+       # libev's configure script may select a different default compiler than we
+       # do, so we force our compiler choice.
+-      "CC='#{cc}' CXX='#{cxx}' CFLAGS='#{cflags}' orig_CFLAGS=1"
++      "CC='#{cc}' CXX='#{cxx}' CFLAGS='#{cflags}' orig_CFLAGS=1 @LIBEV_EXTRA_ARGS@"
+   end
+ 
+   libev_sources = Dir["ext/libev/{*.c,*.h}"]
 @@ -141,8 +141,8 @@ if USE_VENDORED_LIBEV
  
    task :clean => 'libev:clean'
