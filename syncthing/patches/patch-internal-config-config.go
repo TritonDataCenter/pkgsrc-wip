deb0a73afd9b643bf0827f8e36d10d873f3168d1
tnn2 <tnn2>
2015-04-25 16:59:22 +0000

    Update to syncthing-0.11.0.
    (This breaks API and protocol compatibility with 0.10.x.)
    
    Important Changes and Improvements:
    
    Conflict Handling uses version vectors instead of plain integers
    CPU Usage Improvements
    Performance Improvements When Syncing Many Small Files or Very Large Ones
    REST Interface Changes
    Event Interface Changes
    External Versioning script support
    Various UI Tweaks
    Stopped Folders are Restarted when the become available

--- syncthing/patches/patch-internal-config-config.go
+++ syncthing/patches/patch-internal-config-config.go
@@ -1,13 +1,13 @@
 $NetBSD$
 
---- internal/config/config.go.orig	2015-03-15 15:42:52.000000000 +0000
+--- internal/config/config.go.orig	2015-04-22 00:37:18.000000000 +0000
 +++ internal/config/config.go
-@@ -178,7 +178,7 @@ type OptionsConfiguration struct {
- 	URAccepted              int      `xml:"urAccepted"` // Accepted usage reporting version; 0 for off (undecided), -1 for off (permanently)
- 	URUniqueID              string   `xml:"urUniqueID"` // Unique ID for reporting purposes, regenerated when UR is turned on.
- 	RestartOnWakeup         bool     `xml:"restartOnWakeup" default:"true"`
--	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" default:"12"` // 0 for off
-+	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" default:"0"` // 0 for off
- 	KeepTemporariesH        int      `xml:"keepTemporariesH" default:"24"`     // 0 for off
- 	CacheIgnoredFiles       bool     `xml:"cacheIgnoredFiles" default:"true"`
- 	ProgressUpdateIntervalS int      `xml:"progressUpdateIntervalS" default:"5"`
+@@ -233,7 +233,7 @@ type OptionsConfiguration struct {
+ 	URAccepted              int      `xml:"urAccepted" json:"urAccepted"` // Accepted usage reporting version; 0 for off (undecided), -1 for off (permanently)
+ 	URUniqueID              string   `xml:"urUniqueID" json:"urUniqueId"` // Unique ID for reporting purposes, regenerated when UR is turned on.
+ 	RestartOnWakeup         bool     `xml:"restartOnWakeup" json:"restartOnWakeup" default:"true"`
+-	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"12"` // 0 for off
++	AutoUpgradeIntervalH    int      `xml:"autoUpgradeIntervalH" json:"autoUpgradeIntervalH" default:"0"` // 0 for off
+ 	KeepTemporariesH        int      `xml:"keepTemporariesH" json:"keepTemporariesH" default:"24"`         // 0 for off
+ 	CacheIgnoredFiles       bool     `xml:"cacheIgnoredFiles" json:"cacheIgnoredFiles" default:"true"`
+ 	ProgressUpdateIntervalS int      `xml:"progressUpdateIntervalS" json:"progressUpdateIntervalS" default:"5"`
