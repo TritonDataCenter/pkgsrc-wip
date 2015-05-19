68a71028a6c9f6c3ad947c62cf125c4a58913006
n-t-roff <n-t-roff>
2015-05-19 14:57:15 +0000

    Final version--ready for review

---
+++ dwb33/patches/patch-postscript_printfont_printfont.sh
@@ -0,0 +1,11 @@
+$NetBSD$
+
+Make executeable
+
+--- postscript/printfont/printfont.sh.orig	2015-05-19 16:27:00.000000000 +0200
++++ postscript/printfont/printfont.sh	2015-05-19 16:27:10.000000000 +0200
+@@ -1,3 +1,4 @@
++#!/bin/sh
+ #
+ # Formatted dump of encoded characters in one or more PostScript fonts.
+ # Arguments should be PostScript font names or the word all, which dumps
