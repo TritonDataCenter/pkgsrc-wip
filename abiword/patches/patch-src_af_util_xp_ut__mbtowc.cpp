ec251b0041666aa6637dca7ae17f5ee17230d2ab
nros <nros>
2015-06-16 09:33:22 +0000

    Use correct big endian bom in ut_mbtowc.c.

--- abiword/patches/patch-src_af_util_xp_ut__mbtowc.cpp
+++ abiword/patches/patch-src_af_util_xp_ut__mbtowc.cpp
@@ -9,8 +9,8 @@ $NetBSD$
  	GError* error = NULL;
 -	gchar* out = g_convert_with_iconv(inptr, inlen, (GIConv)cd, &bytes_read, &bytes_written, &error);
 +	gchar* out = NULL;
-+        char bom_le[4] = {-1,-2,0,0};
-+	char bom_be[4] = {-2,-1,0,0};
++        unsigned char bom_le[4] = {0xFF,0xFE,0,0};
++	unsigned char bom_be[4] = {0,0,0xFE,0xFF};
 +	
 +	out = g_convert_with_iconv(inptr, inlen, (GIConv)cd, &bytes_read, &bytes_written, &error);
 +	// if we get a bom with the conversion redo it to get one without a bom
