11664b7a6d755150b3c3fa736774c8902d6c7318
krytarowski <krytarowski>
2015-07-05 13:06:20 +0000

    Include devel/protobuf-c in buildlink3.mk

--- libgadu/buildlink3.mk
+++ libgadu/buildlink3.mk
@@ -8,6 +8,8 @@ LIBGADU_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.9.0rc2
 BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.9.0rc2
 BUILDLINK_PKGSRCDIR.libgadu?=	../../wip/libgadu
+
+.include "../../devel/protobuf-c/buildlink3.mk"
 .endif # LIBGADU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgadu
