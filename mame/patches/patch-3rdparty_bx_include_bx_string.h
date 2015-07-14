192366b3d7a45693c5e1eac3a9e04305aab40a1a
thomasklausner <thomasklausner>
2015-07-14 18:51:45 +0000

    Import mame-0.163 as wip/mame. This is an update for emulators/mame. It builds, but doesn't seem to detect SDL2, and core dumps on startup.
    MAME stands for Multiple Arcade Machine Emulator. When used in
    conjunction with images of the original arcade game's ROM and disk
    data, MAME attempts to reproduce that game as faithfully as possible
    on a more modern general-purpose computer. MAME can currently
    emulate several thousand different classic arcade video games from
    the late 1970s through the modern era.
    
    ROMs are needed to play the games. Some are freely available on the
    MAME homepage.

---
+++ mame/patches/patch-3rdparty_bx_include_bx_string.h
@@ -0,0 +1,14 @@
+$NetBSD$
+
+alloca.h is obsolete.
+
+--- 3rdparty/bx/include/bx/string.h.orig	2015-06-24 09:53:24.000000000 +0000
++++ 3rdparty/bx/include/bx/string.h
+@@ -7,7 +7,6 @@
+ #define BX_STRING_H_HEADER_GUARD
+ 
+ #include "bx.h"
+-#include <alloca.h>
+ #include <ctype.h>  // tolower
+ #include <stdarg.h> // va_list
+ #include <stdio.h>  // vsnprintf, vsnwprintf
